1447728fa581e9b4af47b401dd3ef0324ab3d54d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:52 -0400

    lisp/cae-workspaces.el

--- lisp/cae-workspaces.el
+++ lisp/cae-workspaces.el
@@ -16,9 +16,3 @@
   (after! persp-mode
     (add-hook 'persp-before-switch-functions #'cae-hacks-hydra-pause-h)
     (add-hook 'persp-activated-functions #'cae-hacks-hydra-resume-h)))
-
-;;; Tab bar
-
-(after! hercules
-  (add-hook 'cae-tab-bar-before-switch-hook #'hercules--hide))
-(add-hook 'cae-tab-bar-before-switch-hook #'cae-hacks-hydra-quit-h)
