a4ebb4738ad763120537e90f769fad374b9f2c1b
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:17 -0400

    init.el

--- init.el
+++ init.el
@@ -40,7 +40,7 @@ overriding other keymaps."
               (define-key general-override-mode-map (kbd "C-c '") nil)))
   (add-hook 'doom-after-init-hook #'cae-general-override-mode t))
 
-(defvar cae-init-ui-enabled-p t
+(defvar cae-init-ui-enabled-p nil
   "Whether our UI section of `config.el' is disabled.")
 (defvar cae-init-tools-enabled-p t
   "Whether our tools section of `config.el' is disabled.")
