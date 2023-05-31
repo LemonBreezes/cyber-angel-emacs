1cc31be58dd1126b3e42b7fb58565a3a25b285c5
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:57 -0400

    config.el

--- config.el
+++ config.el
@@ -37,7 +37,6 @@
 (when (and (modulep! :ui modeline)
            (not (modulep! :ui modeline +light)))
   (setq doom-modeline-hud t
-        doom-modeline-icon t
         doom-modeline-support-imenu t))
 
 (after! which-key
