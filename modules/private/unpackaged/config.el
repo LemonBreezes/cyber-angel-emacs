722e2cc901378abc362ff9247f4a2e2784232d08
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:02 -0400

    modules/private/unpackaged/config.el

--- modules/private/unpackaged/config.el
+++ modules/private/unpackaged/config.el
@@ -11,20 +11,20 @@
   (add-hook 'org-mode-hook #'cae-unpackaged-org-fix-blank-lines-before-save))
 
 ;;; Magit
-(when (modulep! :tools magit)
-  (defalias 'magit-status-goto-initial-section-1 'magit-status-goto-initial-section)
-
-  (map! [remap magit-status] #'cae-unpackaged-magit-save-buffer-show-status
-        [remap magit-status-here] #'cae-unpackaged-magit-save-buffer-show-status-here)
-
-  ;; Restore the window configuration after exiting `magit-status', since we are
-  ;; deleting other windows with `cae-unpackaged-magit-save-buffer-show-status'.
-  (after! magit
-    (setq magit-bury-buffer-function #'magit-restore-window-configuration))
-  (advice-add #'cae-unpackaged-magit-status :before-until
-              #'cae-unpackaged-magit-status-disable-when-gac-enabled-a)
-
-  (defun cae-unpackaged-magit-start-smerge-hydra-h ()
-    (when smerge-mode
-      (+vc/smerge-hydra/body)))
-  (add-hook 'magit-diff-visit-file-hook #'cae-unpackaged-magit-start-smerge-hydra-h))
+;;(when (modulep! :tools magit)
+;;  (defalias 'magit-status-goto-initial-section-1 'magit-status-goto-initial-section)
+;;
+;;  (map! [remap magit-status] #'cae-unpackaged-magit-save-buffer-show-status
+;;        [remap magit-status-here] #'cae-unpackaged-magit-save-buffer-show-status-here)
+;;
+;;  ;; Restore the window configuration after exiting `magit-status', since we are
+;;  ;; deleting other windows with `cae-unpackaged-magit-save-buffer-show-status'.
+;;  (after! magit
+;;    (setq magit-bury-buffer-function #'magit-restore-window-configuration))
+;;  (advice-add #'cae-unpackaged-magit-status :before-until
+;;              #'cae-unpackaged-magit-status-disable-when-gac-enabled-a)
+;;
+;;  (defun cae-unpackaged-magit-start-smerge-hydra-h ()
+;;    (when smerge-mode
+;;      (+vc/smerge-hydra/body)))
+;;  (add-hook 'magit-diff-visit-file-hook #'cae-unpackaged-magit-start-smerge-hydra-h))
