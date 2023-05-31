618393c047714957c09146ffb10f30ae43792647
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:13 -0400

    modules/private/misc-applications/autoload/list-processes.el

---
+++ modules/private/misc-applications/autoload/list-processes.el
@@ -0,0 +1,9 @@
+;;; private/misc-applications/autoload/list-processes.el -*- lexical-binding: t; -*-
+
+;;;###autoload (autoload '+list-processes-hydra/body "private/misc-applications/autoload/list-processes" nil t)
+(defhydra +list-processes-hydra (:color pink :hint nil)
+  ("<f6>" nil "Exit" :exit t)
+  ("q" quit-window nil :exit t)
+  ("d" process-menu-delete-process "Delete process")
+  ("S" tabulated-list-sort "Sort")
+  ("g" revert-buffer "Refresh"))
