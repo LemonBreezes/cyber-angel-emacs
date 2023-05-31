9e4e87ddf8af45702a6d2a2ce06cca2ef5426945
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:46 -0400

    lisp/cae-debug.el

--- lisp/cae-debug.el
+++ lisp/cae-debug.el
@@ -14,13 +14,34 @@
 ;; (macroexpand '(+log 'test))
 ;;      => (message "Values: %s" 'test)
 
-(defun +backtrace-a (&rest args)
+(defun cae-debug-backtrace-a (&rest args)
   (+log args)
   (unless args
     (backtrace)))
 
 (defmacro backtrace! (function)
-  `(advice-add ',function :before #'+backtrace-a))
+  `(advice-add ',function :before #'cae-debug-backtrace-a))
 
 (defmacro unbacktrace! (function)
-  `(advice-remove ',function #'+backtrace-a))
+  `(advice-remove ',function #'cae-debug-backtrace-a))
+
+;;; Power debugging
+
+(defun cae-debug-reraise-error (func &rest args)
+  "Call function FUNC with ARGS and re-raise any error which occurs.
+Useful for debugging post-command hooks and filter functions, which
+normally have their errors suppressed."
+  (condition-case err
+      (apply func args)
+    ((debug error) (signal (car err) (cdr err)))))
+
+(defun toggle-debug-on-hidden-errors (func)
+  "Toggle hidden error debugging for function FUNC."
+  (interactive "a")
+  (cond
+   ((advice-member-p #'cae-debug-reraise-error func)
+    (advice-remove func #'cae-debug-reraise-error)
+    (message "Debug on hidden errors disabled for %s" func))
+   (t
+    (advice-add func :around #'cae-debug-reraise-error)
+    (message "Debug on hidden errors enabled for %s" func))))
