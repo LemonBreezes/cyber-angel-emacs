1b3005f537b714651121ad3b504501541a401bda
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:00 -0400

    lisp/cae-repeat.el

--- lisp/cae-repeat.el
+++ lisp/cae-repeat.el
@@ -6,28 +6,13 @@
   :init
   (add-hook 'doom-first-input-hook #'repeat-mode)
   :config
-  (setq repeat-exit-timeout 3)
   (map! :map help-map "C-r" #'describe-repeat-maps)
+  (setq repeat-exit-timeout 1)
 
   ;; Uses special keys from my esoteric keyboard layout
   (define-repeat-map other-window
     ("o" other-window
-     "O" other-window-previous
-     "q" quit-window
-     "p" +popup/other
-     "k" kill-buffer-and-window
-     "<left>" previous-buffer
-     "<right>" next-buffer
-     "u" winner-undo
-     "r" winner-redo
-     "<tab>" +workspace/switch-right
-     "<backtab>" +workspace/switch-left
-     ;; Number row keys
-     "!" delete-other-windows
-     "@" split-window-below
-     ";" split-window-right
-     ")" +workspace/close-window-or-workspace)
-    (:exit "f" find-file))
+     "O" other-window-previous))
 
   (define-repeat-map isearch-repeat
     ("s" isearch-repeat-forward
