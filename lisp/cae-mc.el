205d5836904d6665741cc893c9f74c31fb54a3a0
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:54 -0400

    lisp/cae-mc.el

--- lisp/cae-mc.el
+++ lisp/cae-mc.el
@@ -21,26 +21,26 @@
         :desc "Mark regexp"                     "%"     #'mc/mark-all-in-region-regexp)
   (after! multiple-cursors-core
     (dolist (cmd '(doom/delete-backward-word
-                 doom/forward-to-last-non-comment-or-eol
-                 mark-sexp
-                 eros-eval-last-sexp
-                 eval-last-sexp
-                 cae-eval-last-sexp))
-    (add-to-list 'mc/cmds-to-run-for-all cmd))
-  (dolist (cmd '(+workspace/new +workspace/load +workspace/save
-                 +workspace/cycle +workspace/other +workspace/delete
-                 +workspace/rename +workspace/display +workspace/new-named
-                 +workspace/swap-left +workspace/switch-to
-                 +workspace/swap-right +workspace/switch-left
-                 +workspace/switch-to-0 +workspace/switch-to-1
-                 +workspace/switch-to-2 +workspace/switch-to-3
-                 +workspace/switch-to-4 +workspace/switch-to-5
-                 +workspace/switch-to-6 +workspace/switch-to-7
-                 +workspace/switch-to-8 +workspace/kill-session
-                 +workspace/switch-right +workspace/switch-to-final
-                 +workspace/restore-last-session +workspace/kill-session-and-quit
-                 +workspace/close-window-or-workspace read-only-mode))
-    (add-to-list 'mc/cmds-to-run-once cmd))
+                   doom/forward-to-last-non-comment-or-eol
+                   mark-sexp
+                   eros-eval-last-sexp
+                   eval-last-sexp
+                   cae-eval-last-sexp))
+      (add-to-list 'mc/cmds-to-run-for-all cmd))
+    (dolist (cmd '(+workspace/new +workspace/load +workspace/save
+                   +workspace/cycle +workspace/other +workspace/delete
+                   +workspace/rename +workspace/display +workspace/new-named
+                   +workspace/swap-left +workspace/switch-to
+                   +workspace/swap-right +workspace/switch-left
+                   +workspace/switch-to-0 +workspace/switch-to-1
+                   +workspace/switch-to-2 +workspace/switch-to-3
+                   +workspace/switch-to-4 +workspace/switch-to-5
+                   +workspace/switch-to-6 +workspace/switch-to-7
+                   +workspace/switch-to-8 +workspace/kill-session
+                   +workspace/switch-right +workspace/switch-to-final
+                   +workspace/restore-last-session +workspace/kill-session-and-quit
+                   +workspace/close-window-or-workspace read-only-mode))
+      (add-to-list 'mc/cmds-to-run-once cmd))
     (add-to-list 'mc/unsupported-minor-modes #'cae-completion-mode)
     (define-key mc/keymap (kbd "C-. .")     #'mc/move-to-column)
     (define-key mc/keymap (kbd "C-. =")     #'mc/compare-chars)
