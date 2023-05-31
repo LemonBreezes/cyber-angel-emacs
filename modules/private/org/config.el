d14b86aa1dbbd9ce83902beafce95b83cbd8dda9
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:07 -0400

    modules/private/org/config.el

--- modules/private/org/config.el
+++ modules/private/org/config.el
@@ -8,5 +8,11 @@
 
 (defvar +org-exit-src-code-hook nil
   "Hook run just before exiting a org source block buffer.")
-(advice-add #'org-edit-src-exit :before #'+org-run-exit-src-code-hooks)
+(advice-add #'org-edit-src-exit :before #'cae-org-run-exit-src-code-hooks)
 (add-hook '+org-exit-src-code-hook #'ws-butler-trim-eob-lines)
+
+(advice-add #'org-insert-heading :after #'cae-org-set-created-timestamp)
+(add-hook 'org-capture-mode-hook
+          (cae-defun org-capture--insert-timestamp ()
+            (when (org-at-heading-p)
+              (cae-org-set-created-timestamp))))
