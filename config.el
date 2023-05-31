f2ba51c856947230d40444ac573f16a7bbe6905f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:12 -0400

    config.el

--- config.el
+++ config.el
@@ -659,7 +659,7 @@
         (:after corfu
          :map corfu-map
          "C-M-i" #'corfu-move-to-minibuffer
-         "}" #'corfu-quick-complete)    ; `}' is easy to type on my keyboard
+         "}" #'corfu-quick-complete)    ; `}' is easy to type on my keyboard layout
         :prefix "M-+"
         "c" #'completion-at-point       ; capf
         "t" #'complete-tag              ; etags
