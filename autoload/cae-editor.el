f481f6fa6868fef97965f37f91255757f3334e53
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:04 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -214,7 +214,7 @@ mark the string and call `edit-indirect-region' with it."
   (interactive)
   (org-ai-global-mode +1)
   (defvar org-ai-global-mode-prefix-map
-          (lookup-key org-ai-global-mode-map (kbd "C-c M-a")))
+    (lookup-key org-ai-global-mode-map (kbd "C-c M-a")))
   (setq unread-command-events (list ?\C-c ?\M-a))
   (setq which-key-inhibit t)
   (add-transient-hook! 'pre-command-hook
@@ -223,4 +223,5 @@ mark the string and call `edit-indirect-region' with it."
    which-key-idle-delay nil
    (lambda ()
      (when which-key-inhibit
-       (which-key-show-keymap 'org-ai-global-mode-prefix-map)))))
+       (which-key-show-keymap 'org-ai-global-mode-prefix-map))))
+  (set-transient-map org-ai-global-mode-prefix-map))
