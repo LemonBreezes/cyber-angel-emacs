b78ae536de3f46c0da62dd844f636ffed3b7823a
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:05 -0400

    config.el

--- config.el
+++ config.el
@@ -450,7 +450,6 @@
   (add-hook 'text-mode-hook   #'copilot-mode)
   (add-hook 'prog-mode-hook   #'copilot-mode)
   (add-hook 'conf-mode-hook   #'copilot-mode)
-  (add-hook 'eshell-mode-hook #'copilot-mode)
   :config
   (setq copilot--base-dir
         (expand-file-name ".local/straight/repos/copilot.el/" doom-emacs-dir))
