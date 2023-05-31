6dca39ff12b5fa097dda5b5c5a8ab6e602447212
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:33 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -39,12 +39,6 @@
                                         ;for debugging font-lock issues.
 (package! git-modes)
 
-;; cae-webkit.el
-;;(package! webkit :recipe (:type git :host github :repo "akirakyle/emacs-webkit"
-;;                          :branch "main"
-;;                          :files (:defaults "*.js" "*.css" "*.so")
-;;                          :pre-build ("make")))
-
 ;;; Editor
 (package! pp+)
 (package! avy :recipe (:repo "LemonBreezes/avy" :branch "master"))
