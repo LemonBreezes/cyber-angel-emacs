562e44d76df23aa4f426a88347a7548abf83236b
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:21 -0400

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -1,5 +1,31 @@
 ;;; ~/.doom.d/lisp/cae-corfu.el -*- lexical-binding: t; -*-
 
+(after! orderless
+  ;; So Orderless splits the string into components and then determines the
+  ;; matching style for each component. This is all regexp stuff.
+  (defun orderless-escapable-split-on-space-or-ampersand (s)
+    (mapcar
+     (lambda (piece)
+       (thread-last piece
+                    (replace-regexp-in-string
+                     (concat (string 0) "\\|" (string 1))
+                     (lambda (x)
+                       (pcase x
+                         ("\0" " ")
+                         ("\1" "&")
+                         (_ x))))
+                    (replace-regexp-in-string (string 1) "&")))
+     (split-string (replace-regexp-in-string
+                    "\\\\\\\\\\|\\\\ \\|\\\\&"
+                    (lambda (x)
+                      (pcase x
+                        ("\\ " "\0")
+                        ("\\&" "\1")
+                        (_ x)))
+                    s 'fixedcase 'literal)
+                   "[ &]+" t)))
+  (setq orderless-component-separator #'orderless-escapable-split-on-space-or-ampersand))
+
 (defun cae-corfu-enable-in-minibuffer-h ()
   (unless (or (bound-and-true-p mct--active)
               (bound-and-true-p vertico--input)
@@ -15,6 +41,7 @@
   (setq corfu-preview-current (if (modulep! :private corfu +tng) 'insert nil)
         corfu-auto-delay (if (modulep! :private corfu +tng) 0.0 0.2)
         corfu-on-exact-match nil
+        corfu-separator ?&
         corfu-preselect (if (modulep! :private corfu +tng) 'prompt t)
         tab-always-indent 'complete)
   (after! corfu-quick
