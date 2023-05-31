7ef768e5cdf1ad9e6ecb09c07c7af3772610f7e3
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:48 -0400

    modules/private/unpackaged/config.el

--- modules/private/unpackaged/config.el
+++ modules/private/unpackaged/config.el
@@ -1,5 +1,7 @@
 ;;; private/unpackaged/config.el -*- lexical-binding: t; -*-
 
+;; This is a module created for stuff I copied from Alphapapa's `unpackaged.el' package.
+
 ;;; Org
 (when (modulep! :lang org)
   (map! :map org-mode-map
