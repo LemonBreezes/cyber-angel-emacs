2b15b07d5e694552c56227974e58312276a37473
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:55 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -3,7 +3,8 @@
 ;;;###autoload
 (defun cae-dired-set-layout-h ()
   (when (and (derived-mode-p 'dired-mode)
-             (one-window-p))
+             (one-window-p)
+             (not (memq this-command '(next-buffer previous-buffer))))
     (dirvish-layout-toggle)))
 
 ;;;###autoload
