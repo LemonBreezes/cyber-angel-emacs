b401cd93221c0bf66138dffec63217e6da32fd14
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:10 -0400

    config.el

--- config.el
+++ config.el
@@ -6,6 +6,8 @@
 
 (defvar cae-config-finished-loading nil
   "Whether the configuration has finished loading.")
+(defvar cae-config-electric-pair-mode nil
+  "Whether electric-pair-mode is enabled globally.")
 
 ;;; UI
 
