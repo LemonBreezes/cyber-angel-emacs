1aa07a131666640b50b115b6d2ba10422e71a4ec
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:23 -0400

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -10,12 +10,8 @@
           (delete '(("\\`C-c !\\'") nil . "checkers")
                   which-key-replacement-alist)))
   (setq flycheck-keymap-prefix (kbd "C-c C"))
-  (after! flycheck
-    ;;(define-key flycheck-mode-map flycheck-keymap-prefix nil)
-    ;;(define-key flycheck-mode-map flycheck-keymap-prefix
-    ;;  flycheck-command-map)
-    (map! :leader
-          (:prefix ("C" . "checkers")))))
+  (map! :leader
+        (:prefix ("C" . "checkers"))))
 (when (modulep! :editor snippets)
   (let ((snippet-prefix "S"))
     (dolist (p (cdr (lookup-key doom-leader-map "&")))
