2bd0059be35247efee9d704b35c7704f37e628d6
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:29 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -23,6 +23,7 @@
 (package! hercules :recipe (:host github :repo "Zetagon/hercules"))
 (package! outline-minor-faces)
 (package! pdf-view-pagemark :recipe (:host github :repo "kimim/pdf-view-pagemark"))
+(package! auto-olivetti :recipe (:repo "https://git.sr.ht/~ashton314/auto-olivetti"))
 
 ;; cae-theme.el
 (package! modus-themes)
