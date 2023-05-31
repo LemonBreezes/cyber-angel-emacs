026b1383b9c428c55b37d03c79b37a828eb765d8
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:02 -0400

    lisp/cae-keyboard.el

--- lisp/cae-keyboard.el
+++ lisp/cae-keyboard.el
@@ -21,14 +21,21 @@
         (cl-return-from cae-keyboard-remap
           (nth (mod (1+ (cl-position arg orbit)) (length orbit)) orbit)))))
   (when (json-alist-p arg)
-    (cl-loop for (key . value) in arg
-             collect
-             (cons (cae-keyboard-remap key) value)))
+    (mapcar (lambda (x)
+              (cons (cae-keyboard-remap (car x)) (cdr x)))
+            arg))
   (cl-return-from cae-keyboard-remap
     (cl-mapcar #'cae-keyboard-remap arg)))
 
 (defun cae-keyboard-remap-to-strings (arg)
   (declare (pure t) (side-effect-free t))
+  (when (characterp arg)
+    (cl-return-from cae-keyboard-remap-to-strings
+      (char-to-string arg)))
+  (when (json-alist-p arg)
+    (mapcar (lambda (x)
+              (cons (cae-keyboard-remap-to-strings (car x)) (cdr x)))
+            arg))
   (cl-mapcar #'char-to-string (cae-keyboard-remap arg)))
 
 (defun cae-keyboard-strings (arg)
