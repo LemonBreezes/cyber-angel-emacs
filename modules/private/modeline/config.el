691bf5e6402be32af2be7894548d40443b7664dd
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:31 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -1,7 +1,7 @@
 ;;; private/modeline/config.el -*- lexical-binding: t; -*-
 
 (defun cae-modeline-buffer-name ()
-  (propertize (if doom-real-buffer-p
+  (propertize (if (doom-unreal-buffer-p (current-buffer))
                   (buffer-name)
                 (breadcrumb-project-crumbs))
               'face '(:inherit variable-pitch
