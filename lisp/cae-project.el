dcfb18c59849cc180d6fb6633de67cc31b586cb4
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:06 -0400

    lisp/cae-project.el

--- lisp/cae-project.el
+++ lisp/cae-project.el
@@ -10,14 +10,19 @@
   "Return the bookmark file for PROJECT."
   (expand-file-name "bookmarks" (doom-project-root project)))
 
+(defun cae-project--bookmark-alist-from-file (file)
+  "Return a bookmark alist from FILE."
+  (let ((bookmark-default-file file)
+        (bookmark-alist))
+    (bookmark-load bookmark-default-file)
+    bookmark-alist))
+
 (defun cae-project--bookmark-alist (&optional project)
   "Return the bookmark alist for the current project."
-
-  (let ((file (cae-project--get-bookmark-file (doom-project-name))))
-    (when (file-exists-p file)
-      (with-temp-buffer
-        (insert-file-contents file)
-        (read (current-buffer))))))
+  (let ((file (cae-project--get-bookmark-file project)))
+    (or (gethash file cae-project-bookmark-cache)
+        (puthash file (cae-project--bookmark-alist-from-file file)
+                 cae-project-bookmark-cache))))
 
 (defun cae-project-bookmark-jump ()
   )
