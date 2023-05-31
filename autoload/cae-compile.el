2cb4c640e2086e956491737c55ba96d3e72fd3be
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:13 -0400

    autoload/cae-compile.el

--- autoload/cae-compile.el
+++ autoload/cae-compile.el
@@ -11,7 +11,8 @@
 (defvar cae-compile-files-to-ignore
   `(,(expand-file-name "ui/doom-dashboard/config.el" doom-modules-dir)
     ,(expand-file-name "lang/org/autoload/org.el" doom-modules-dir)
-    ,(expand-file-name "tools/debugger/autoload/debugger.el" doom-modules-dir)))
+    ,(expand-file-name "tools/debugger/autoload/debugger.el" doom-modules-dir)
+    ,(expand-file-name "lang/cc/autoload.el" doom-modules-dir)))
 
 (defvar cae-compile-native-comp-speed 3)
 
