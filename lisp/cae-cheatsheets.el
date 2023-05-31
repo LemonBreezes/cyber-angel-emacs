cb5087e1527fab02c05f01c8c20346fadf710a51
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:52 -0400

    lisp/cae-cheatsheets.el

--- lisp/cae-cheatsheets.el
+++ lisp/cae-cheatsheets.el
@@ -21,14 +21,16 @@
 (defun cae-cheatsheets-workspace-hydra-pause-h (&rest _)
   (when (bound-and-true-p hydra-curr-map)
     (set-persp-parameter 'hydra-pause-ring
-                         (progn (ring-insert hydra-pause-ring hydra-curr-body-fn)
+                         (progn (ring-insert hydra-pause-ring
+                                             hydra-curr-body-fn)
                                 hydra-pause-ring))
     (hydra-keyboard-quit)))
 
 (defun cae-cheatsheets-workspace-hydra-resume-h (&rest _)
   (unless (or (null (persp-parameter 'hydra-pause-ring))
               (zerop (ring-length (persp-parameter 'hydra-pause-ring))))
-    (run-with-timer 0.001 nil (ring-remove (persp-parameter 'hydra-pause-ring) 0))))
+    (run-with-timer 0.001 nil
+                    (ring-remove (persp-parameter 'hydra-pause-ring) 0))))
 
 (when (modulep! :ui workspaces)
   (after! persp-mode
