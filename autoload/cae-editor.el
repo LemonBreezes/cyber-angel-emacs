792fca320e5a1c7f6a7280ecb5d69d897820f333
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:27 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -40,3 +40,16 @@
            (scan-error t))
          (delete-char (or arg 1)))
         ((sp-delete-char arg))))
+
+;;;###autoload
+(defun cae-auto-sudoedit-maybe-h ()
+  (unless (let ((path (or (buffer-file-name) list-buffers-directory)))
+            (string= (file-attribute-user-id
+                      (file-attributes path 'string))
+                     (if (and (featurep 'tramp)
+                              (tramp-tramp-file-p path))
+                         (tramp-get-remote-uid (tramp-dissect-file-name path)
+                                               'string)
+                       (user-login-name))))
+    (require 'auto-sudoedit)
+    (auto-sudoedit)))
