90dec79c185f19d0a0b08f4d04077a2d2888b074
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:57 -0400

    config.el

--- config.el
+++ config.el
@@ -405,7 +405,8 @@
   ;; there are errors.
   (setq compilation-scroll-output t))
 
-;; Fixes a void function error on `find-file-hook' that occurs sporadically for me.
+;; Fixes a void function error on `find-file-hook' that occurs sporadically for
+;; me.
 (autoload 'tramp-set-connection-local-variables-for-buffer "tramp")
 (after! tramp
   (setq tramp-shell-prompt-pattern "\\(?:^\\|\n\\|\x0d\\)[^]#$%>\n]*#?[]#$%>] *\\(\e\\[[0-9;]*[a-zA-Z] *\\)*") ;; default + 
