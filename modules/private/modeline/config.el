9713b65a35e7e8a94e7154a33ea1c4ce9e1e12fd
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:03 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -138,6 +138,6 @@
                          "cae-keyboard-strings")))
     (add-to-list 'parrot-rotate-dict
                  '(:rot ("kbd"
-                         "cae-keyboard-kbd2")))
+                         "cae-keyboard-kbd")))
     (map! "C-!" #'parrot-rotate-next-word-at-point
           "C-M-!" #'parrot-rotate-prev-word-at-point)))
