67c6eda0ffb555261f338b74d97dbef8aa916446
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:07 -0400

    modules/private/org/autoload/org.el

---
+++ modules/private/org/autoload/org.el
@@ -0,0 +1,13 @@
+;;; private/org/autoload/org.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun +org-set-created-timestamp (&rest _args)
+  "Add a creation timestamp to the current Org entry.
+If the current command is run with a prefix argument, prevent
+from running."
+  (unless (or current-prefix-arg
+              (string-prefix-p "README.org" (buffer-name)))
+    (org-set-property "CREATED_TIME"
+                      (org-timestamp-format
+                       (org-timestamp-from-time (current-time) t t)
+                       (org-time-stamp-format t t)))))
