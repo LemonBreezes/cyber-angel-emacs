41d1538ee895befd7aa67285ddcf6ea3b0e4fd7e
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:14 -0400

    modules/private/misc-applications/+ytel.el

--- modules/private/misc-applications/+ytel.el
+++ modules/private/misc-applications/+ytel.el
@@ -9,7 +9,7 @@
   :config
   (setq ytel-invidious-api-url (or (doom-store-get 'ytel-invidious-api-url)
                                    "https://invidious.snopyta.org/"))
-  (define-key ytel-mode-map (kbd "<return>") #'ytel-watch))
+  (define-key ytel-mode-map (kbd "RET") #'ytel-watch))
 
 ;; (define-key ytel-mode-map "y" #'ytel-watch)
 
