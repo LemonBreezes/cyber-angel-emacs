5c55f30ea3cd26cc66ccc262cd2cb886c8bb883b
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:58 -0400

    modules/private/debugger/config.el

--- modules/private/debugger/config.el
+++ modules/private/debugger/config.el
@@ -2,6 +2,12 @@
 
 (when (and (modulep! :tools lsp)
            (modulep! :tools debugger +lsp))
+  (add-hook! 'dap-terminated-hook
+    (ignore-errors
+      (let ((ignore-window-parameters t))
+        (cl-loop for buf being the buffers
+                 when (string-match-p "gdb" (buffer-name buf)) do
+                 (cae-hacks-always-yes-a #'doom-kill-buffer-and-windows buf)))))
   (when (modulep! :lang cc +lsp)
     (add-transient-hook! 'c-mode-common-hook
       (require 'dap-cpptools)
