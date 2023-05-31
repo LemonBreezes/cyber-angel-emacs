3df8e77a6caead71aa5799811e43d965e52dc11a
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:28 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -1,5 +1,9 @@
 ;;; private/modeline/config.el -*- lexical-binding: t; -*-
 
+;; change the first occurrence of mode-line-buffer-identification to '(:eval (breadcrumb-project-crumbs))
+(setq mode-line-format
+      (cl-subst '(:eval (breadcrumb-project-crumbs)) 'mode-line-buffer-identification mode-line-format))
+
 (use-package! minions
   :defer t :init
   (add-hook 'doom-first-buffer-hook #'minions-mode)
