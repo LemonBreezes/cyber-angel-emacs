89a93c5fcd239988de87599f2522ddde5a414756
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:20 -0400

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -148,12 +148,15 @@
 
 ;; Monkey fix `project.el' overriding the `C-x p' keybinding.
 (when (modulep! :ui popup)
-  (run-with-idle-timer
-   3 t
-   (cae-defun cae-fix-popup-other-keybinding ()
-     (define-key ctl-x-map "p" nil)
-     (map! :map ctl-x-map
-           "p" #'+popup/other))))
+  (unless (boundp 'cae-fix-popup-other-keybinding-idle-timer)
+    (defvar cae-fix-popup-other-keybinding-idle-timer nil)
+    (setq cae-fix-popup-other-keybinding-idle-timer
+          (run-with-idle-timer
+           3 t
+           (cae-defun cae-fix-popup-other-keybinding ()
+             (define-key ctl-x-map "p" nil)
+             (map! :map ctl-x-map
+                   "p" #'+popup/other))))))
 
 ;; I'm surprised Doom Emacs doesn't bind a key for copying links.
 (map! :leader
