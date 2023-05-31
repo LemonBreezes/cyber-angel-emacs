0bf52690e2ef487651d65cee4f905432aa2222a3
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:33 -0400

    config.el

--- config.el
+++ config.el
@@ -671,29 +671,27 @@
 
 (use-package! aggressive-indent
   :init
-  (unless (modulep! :editor lispy)
-    (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)) ;See my `lisp' module.
+  (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode) ;See my `lisp' module.
   (add-hook 'c-mode-common-hook #'aggressive-indent-mode)
   :defer t
   :config
-  (when (and (modulep! :tools lsp)
-             (not (modulep! :tools lsp +eglot)))
-    (add-to-list
-     'aggressive-indent-dont-indent-if
-     '(and (bound-and-true-p lsp-mode)
-           (or (and lsp-enable-on-type-formatting
-                    (lsp--capability "documentOnTypeFormattingProvider"))
-               (and lsp-enable-indentation
-                    (lsp--capability "documentRangeFormattingProvider")))))
-    (dolist (command '(lsp-format-buffer
-                       lsp-format-region
-                       lsp-organize-imports
-                       lsp-organize-imports-remove-unused
-                       prog-fill-reindent-defun
-                       indent-pp-sexp
-                       save-buffer
-                       indent-for-tab-command))
-      (add-to-list 'aggressive-indent-protected-commands command))))
+  (add-to-list
+   'aggressive-indent-dont-indent-if
+   '(and (bound-and-true-p lsp-mode)
+         (or (and lsp-enable-on-type-formatting
+                  (lsp--capability "documentOnTypeFormattingProvider"))
+             (and lsp-enable-indentation
+                  (lsp--capability "documentRangeFormattingProvider")))))
+  (dolist (command '(lsp-format-buffer
+                     lsp-format-region
+                     lsp-organize-imports
+                     lsp-organize-imports-remove-unused
+                     prog-fill-reindent-defun
+                     indent-pp-sexp
+                     save-buffer
+                     indent-for-tab-command))
+    (add-to-list 'aggressive-indent-protected-commands command))
+  (add-to-list 'aggressive-indent-dont-indent-if '(bound-and-true-p lispy-mode)))
 
 (use-package! hungry-delete
   :defer t :init
