bf10b2b48de76d582a8ddd658d59402a4b7a5635
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:43 -0400

    modules/private/vc/autoload.el

--- modules/private/vc/autoload.el
+++ modules/private/vc/autoload.el
@@ -1,11 +1,11 @@
 ;;; private/vc/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-magit-insert-diff-upstream ()
+(defun cae-magit-insert-diff-upstream (&optional branch)
   "From
  https://www.reddit.com/r/emacs/comments/112t0uo/comment/j9g2r5n/?utm_source=share&utm_medium=web2x&context=3."
   (magit-insert-section (diff-upstream nil t)
-    (if-let ((upstream-branch (magit-get-upstream-branch (magit-get-current-branch))))
+    (if-let ((upstream-branch (magit-get-upstream-branch (or branch (magit-get-current-branch)))))
         (progn
           (magit-insert-heading (format "Diff with %s:" (substring-no-properties upstream-branch)))
           (magit--insert-diff
