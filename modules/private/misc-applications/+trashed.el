debe89136be89b6616b7e25dd398d1dced04dfcf
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:14 -0400

    modules/private/misc-applications/+trashed.el

--- modules/private/misc-applications/+trashed.el
+++ modules/private/misc-applications/+trashed.el
@@ -10,4 +10,5 @@
   (advice-add #'trashed :around #'+trashed-revert-buffer-a)
   (add-hook 'trashed-mode-hook #'+trashed-hide-cursor-h)
   (map! :map trashed-mode-map
-        "<f6>" #'+trashed-hydra/body))
+        "<f6>" #'+trashed-hydra/body)
+  (add-hook 'trashed-mode-hook #'doom-mark-buffer-as-real-h))
