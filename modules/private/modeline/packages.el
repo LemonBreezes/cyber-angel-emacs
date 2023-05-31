011d2a9fc145a6e606f0b5981c997e732f082a96
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:03 -0400

    modules/private/modeline/packages.el

--- modules/private/modeline/packages.el
+++ modules/private/modeline/packages.el
@@ -4,4 +4,4 @@
 (package! minions :recipe (:host github :repo "LemonBreezes/minions"))
 (when (modulep! +pretty)
   (package! nyan-mode)
-  (package! parrot :recipe (:host github :repo "positron-solutions/parrot")))
+  (package! parrot :recipe (:host github :repo "LemonBreezes/parrot-1")))
