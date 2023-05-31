c322cbe3dd25f4566931dcde875636fa122d6cf7
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:38 -0400

    modules/private/misc-applications/+ednc.el

--- modules/private/misc-applications/+ednc.el
+++ modules/private/misc-applications/+ednc.el
@@ -2,4 +2,15 @@
 
 (use-package! ednc
   :config
-  (ednc-mode +1))
+  (ednc-mode +1)
+  (defun stack-notifications (&optional hide)
+    (mapconcat (lambda (notification)
+                 (let ((app-name (ednc-notification-app-name notification)))
+                   (unless (member app-name hide)
+                     (push app-name hide)
+                     (ednc-format-notification notification))))
+               (ednc-notifications) ""))
+  (add-to-list 'global-mode-string
+               '((:eval (stack-notifications))))
+  (add-hook 'ednc-notification-presentation-functions
+            (lambda (&rest _) (force-mode-line-update t))))
