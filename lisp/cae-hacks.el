cea730add7ea6bf60b008c7f98472966055e7712
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:08 -0400

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -85,7 +85,7 @@
              (unwind-protect (funcall oldfun variables dir-name)
                (advice-remove #'safe-local-variable-p #'always)))
     (funcall oldfun variables dir-name)))
-
+;
 ;; Use the buffer local value of `diff-hl-reference-revision'
 (defadvice! cae-hacks-diff-hl-reference-revision-a (oldfun &rest args)
   :around '(diff-hl-diff-against-reference)
