04fdb24290d1b1150f4b8751325d154a29ea4394
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:32 -0400

    modules/private/misc-applications/+somafm.el

--- modules/private/misc-applications/+somafm.el
+++ modules/private/misc-applications/+somafm.el
@@ -1,15 +1,11 @@
 ;;; private/misc-applications/somafm.el -*- lexical-binding: t; -*-
 
 (use-package! somafm
-  :defer-incrementally t                ; Somafm is _really_ slow to start up so
-                                        ; we lazily load Somafm and refresh the
-                                        ; channels.
   :init
   (map! :leader
         :prefix +misc-applications-prefix
         "@" #'somafm)
   :config
-  (somafm--refresh-channels)
   (map! :map somafm-mode-map
         :n "q" #'bury-buffer
         :n "r" #'somafm--refresh-channels
