c5d60c4649a5e868f927311dbc387b346196effc
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:40 -0400

    autoload/cae-cheatsheets.el

--- autoload/cae-cheatsheets.el
+++ autoload/cae-cheatsheets.el
@@ -1,6 +1,6 @@
-;;; autoload/cae-hydras.el -*- lexical-binding: t; -*-
+;;; autoload/cae-cheatsheets.el -*- lexical-binding: t; -*-
 
-;;;###autoload (autoload 'cae-embark-collect-cheatsheet-hydra/body "autoload/cae-hydras" nil t)
+;;;###autoload (autoload 'cae-embark-collect-cheatsheet-hydra/body "autoload/cae-cheatsheets" nil t)
 (defhydra cae-embark-collect-cheatsheet-hydra (:color pink :foreign-keys run)
   ("a" embark-act "Act" :column "Act")
   ("A" embark-act-all "Act on all" :column "Act")
@@ -17,3 +17,27 @@
   ("M-<left>" tabulated-list-previous-column "Previous column" :column "Navigate")
   ("M-<right>" tabulated-list-next-column "Next column" :column "Navigate")
   ("<f6>" nil "Exit" :exit t :column nil))
+
+;;;###autoload (autoload 'cae-embark-collect-cheatsheet "autoload/cae-cheatsheets" nil t)
+(hercules-def
+ :toggle-funs #'cae-embark-collect-cheatsheet
+ :keymap 'embark-collect-mode-map
+ :package 'embark)
+
+;;;###autoload (autoload 'cae-debugger-cheatsheet "autoload/cae-cheatsheets" nil t)
+(hercules-def
+ :toggle-funs #'cae-debugger-cheatsheet
+ :keymap 'debugger-mode-map
+ :package 'debug)
+
+;;;###autoload (autoload 'cae-edebug-cheatsheet "autoload/cae-cheatsheets" nil t)
+(hercules-def
+ :toggle-funs #'cae-edebug-cheatsheet
+ :keymap 'edebug-mode-map
+ :package 'edebug)
+
+;;;###autoload (autoload 'cae-macrostep-cheatsheet "autoload/cae-cheatsheets" nil t)
+(hercules-def
+ :toggle-funs #'cae-macrostep-cheatsheet
+ :keymap 'macrostep-keymap
+ :package 'macrostep)
