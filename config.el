61578c0ce9e6dc6b9b80a7d15ed10ab78cf4272d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:39 -0400

    config.el

--- config.el
+++ config.el
@@ -556,7 +556,7 @@
 (when (modulep! :completion vertico)
   (after! embark
     (map! :map embark-collect-mode-map
-          "<f6>" #'embark-collect-hydra/body)))
+          "<f6>" #'embark-collect-cheatsheet)))
 
 (use-package! avy
   :defer t
