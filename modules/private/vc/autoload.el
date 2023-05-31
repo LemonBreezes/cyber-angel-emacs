aeb19a0f356e9677e56015870247cf6bdb6826f4
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:43 -0400

    modules/private/vc/autoload.el

--- modules/private/vc/autoload.el
+++ modules/private/vc/autoload.el
@@ -1,20 +1,11 @@
 ;;; private/vc/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-magit-insert-diff-upstream (&optional branch)
-  "Insert diff between current branch and upstream branch. If
- BRANCH is non-nil, use that branch instead of the current
- branch's upstream branch.
-
-From https://www.reddit.com/r/emacs/comments/112t0uo/comment/j9g2r5n/?utm_source=share&utm_medium=web2x&context=3.
-
-Here is an expample use of this function:
-(magit-add-section-hook 'magit-status-sections-hook
-                        'cae-magit-insert-diff-upstream
-                        'magit-insert-stashes)
-With this, the diff with upstream is inserted before the stashes section."
+(defun cae-magit-insert-diff-upstream ()
+  "From
+ https://www.reddit.com/r/emacs/comments/112t0uo/comment/j9g2r5n/?utm_source=share&utm_medium=web2x&context=3."
   (magit-insert-section (diff-upstream nil t)
-    (if-let ((upstream-branch (or branch (magit-get-upstream-branch (magit-get-current-branch)))))
+    (if-let ((upstream-branch (magit-get-upstream-branch (magit-get-current-branch))))
         (progn
           (magit-insert-heading (format "Diff with %s:" (substring-no-properties upstream-branch)))
           (magit--insert-diff
