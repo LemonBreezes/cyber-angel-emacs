2f580424bfb92a07201f111711d8324640bf26da
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:00 -0400

    modules/private/vc/packages.el

--- modules/private/vc/packages.el
+++ modules/private/vc/packages.el
@@ -6,4 +6,5 @@
 (package! git-email :recipe (:repo "git@git.sr.ht:~lemon_breezes/git-email"))
 (package! vc-backup :recipe (:repo "LemonBreezes/vc-backup"))
 
+;; This package should be installed by Doom, but it's not.  I don't know why.
 (package! git-timemachine)
