39930399eda785b18c3126fc8059572b06e9ae12
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:59 -0400

    modules/private/debugger/config.el

--- modules/private/debugger/config.el
+++ modules/private/debugger/config.el
@@ -3,21 +3,22 @@
 (when (and (modulep! :tools lsp)
            (modulep! :tools debugger +lsp))
 
-  ;; Close the windows when the debugger is closed.
-  (add-hook! #'dap-disconnect
-    (ignore-errors
-      (let ((ignore-window-parameters t))
-        (cl-loop for buf being the buffers
-                 when (string-match-p "gdb" (buffer-name buf)) do
-                 (cae-hacks-always-yes-a #'doom-kill-buffer-and-windows buf)))
-      (hydra-keyboard-quit)))
+  (after! dap-mode
+    ;; Close the windows when the debugger is closed.
+    (add-hook! #'dap-disconnect
+      (ignore-errors
+        (let ((ignore-window-parameters t))
+          (cl-loop for buf being the buffers
+                   when (string-match-p "gdb" (buffer-name buf)) do
+                   (cae-hacks-always-yes-a #'doom-kill-buffer-and-windows buf)))
+        (hydra-keyboard-quit)))
 
-  (map! :map dap-mode-map
-        "C-S-d" #'dap-hydra)
+    (map! :map dap-mode-map
+          "C-S-d" #'dap-hydra)
 
-  ;; Doom keeps emitting errors when trying to retrieve previous debug
-  ;; sessions. This is a workaround.
-  (doom-store-clear "+debugger")
+    ;; Doom keeps emitting errors when trying to retrieve previous debug
+    ;; sessions. This is a workaround.
+    (doom-store-clear "+debugger"))
 
   (when (modulep! :lang cc +lsp)
     (add-transient-hook! 'c-mode-common-hook
