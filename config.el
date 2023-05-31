1d81865bd6aeda3fa6e8e2827ea1e500f8a1093f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:01 -0400

    config.el

--- config.el
+++ config.el
@@ -240,8 +240,7 @@
            lsp-enable-snippet nil
            lsp-enable-text-document-color t
            lsp-enable-folding t
-           lsp-semantic-tokens-enable t
-           lsp-eldoc-render-all t)
+           lsp-semantic-tokens-enable t)
     (after! lsp-ui
       (setq lsp-signature-auto-activate t
             lsp-ui-doc-include-signature t
