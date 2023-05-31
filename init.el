11da7b7e955e3d10778a9cbcc438369d0c1a9dd2
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:25 -0400

    init.el

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 (setq native-comp-async-jobs-number (num-processors))
 
 (doom! :completion
-       helm ;not our main completion engine.
+       helm ;Not our main completion engine.
        (vertico +icons)
 
        :ui
