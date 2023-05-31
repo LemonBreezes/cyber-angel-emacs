d0875418af82e7dd95e08dff27e745e4f497fe84
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:29 -0400

    modules/private/modeline/packages.el

--- modules/private/modeline/packages.el
+++ modules/private/modeline/packages.el
@@ -5,5 +5,4 @@
 (when (modulep! +pretty)
   (package! nyan-mode))
 (package! parrot :recipe (:host github :repo "positron-solutions/parrot"))
-(package! vline)
 (package! breadcrumb :recipe (:host github :repo "joaotavora/breadcrumb"))
