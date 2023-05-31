fd0c64a887d011905d898d3935ccf6afd63b3fb0
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:57 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -1,3 +1,32 @@
 ;;; private/modeline/config.el -*- lexical-binding: t; -*-
 
 (minions-mode 1)
+(setq minions-hidden-modes
+      '(abbrev-mode
+        auto-fill-function
+        eldoc-mode
+        flycheck-mode
+        flyspell-mode
+        git-gutter+-mode
+        git-gutter-mode
+        git-timemachine-mode
+        helm-mode
+        helm-gtags-mode
+        helm-projectile-mode
+        highlight-indentation-mode
+        highlight-numbers-mode
+        highlight-parentheses-mode
+        highlight-symbol-mode
+        hs-minor-mode
+        indent-guide-mode
+        linum-mode
+        nyan-mode
+        projectile-mode
+        rainbow-delimiters-mode
+        smartparens-mode
+        undo-tree-mode
+        volatile-highlights-mode
+        whitespace-mode
+        yas-minor-mode
+        which-key-mode
+        vi-tilde-fringe-mode))
