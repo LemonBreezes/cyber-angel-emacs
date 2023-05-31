c38a650badbe8a67a1ac58f556979163a7adb120
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:13 -0400

    modules/private/misc-applications/autoload/timer-list.el

---
+++ modules/private/misc-applications/autoload/timer-list.el
@@ -0,0 +1,9 @@
+;;; private/misc-applications/autoload/timer-list.el -*- lexical-binding: t; -*-
+
+;;;###autoload (autoload '+timer-list-hydra/body "private/misc-applications/autoload/timer-list" nil t)
+(defhydra +fireplace-hydra (:color pink :hint nil)
+  ("<f6>" nil "Exit" :exit t)
+  ("q" quit-window nil :exit t)
+  ("c" timer-list-cancel "Cancel timer")
+  ("S" tabulated-list-sort "Sort")
+  ("g" revert-buffer "Refresh"))
