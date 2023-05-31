9d71ef152b7a975fe44e524a63df7cfdf48cb2c5
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:15 -0400

    modules/private/dired/config.el

--- modules/private/dired/config.el
+++ modules/private/dired/config.el
@@ -93,8 +93,6 @@
         "K" #'dired-kill-subdir
         "I" #'dired-insert-subdir
         "_" #'dired-up-directory
-        "{" #'dired-prev-dirline
-        "}" #'dired-next-dirline
         "[" #'dired-prev-dirline
         "]" #'dired-next-dirline
         "," #'dired-create-empty-file
