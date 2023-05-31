443b2584776f9ac65dfef8ccad0e81ab0a0278f7
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:14 -0400

    modules/private/modeline/autoload.el

--- modules/private/modeline/autoload.el
+++ modules/private/modeline/autoload.el
@@ -1,7 +1,7 @@
 ;;; private/modeline/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(cae-defun cae-modeline-rotate-next-word-at-point ()
+(defun cae-modeline-rotate-next-word-at-point ()
   (interactive)
   (save-excursion
     (skip-syntax-forward "^w" (line-end-position))
