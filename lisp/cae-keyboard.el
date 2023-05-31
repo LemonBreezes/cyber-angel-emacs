7644e621927ce05be7ca222f6fe1e16a62597925
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:50 -0400

    lisp/cae-keyboard.el

--- lisp/cae-keyboard.el
+++ lisp/cae-keyboard.el
@@ -140,7 +140,8 @@
        (and (featurep 'ispell)
             (not (minibufferp))   ;I added this check basically out of paranoia.
             (get-buffer-window ispell-choices-buffer))
-       (string= (file-name-base (symbol-file this-command)) "ace-window"))))
+       (and (symbol-file this-command)
+            (string= (file-name-base (symbol-file this-command)) "ace-window")))))
 
 (dolist (key-from (mapcar #'char-to-string '(?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9 ?0)))
   (cae-make-conditional-key-translation (cae-keyboard-kbd key-from)
