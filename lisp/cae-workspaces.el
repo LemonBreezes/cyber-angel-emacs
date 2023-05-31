289e9c444a9a34305f75373de2e4b77bf51dba1d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:06 -0400

    lisp/cae-workspaces.el

--- lisp/cae-workspaces.el
+++ lisp/cae-workspaces.el
@@ -2,7 +2,7 @@
 
 (defun cae-workspaces-save-bookmarks-h (_))
 
-(defun cae-workspaces-load-bookmarks-h (&rest ))
+(defun cae-workspaces-load-bookmarks-h (_))
 
 (add-hook 'persp-before-deactivate-functions #'cae-workspaces-save-bookmarks-h)
 (add-hook 'persp-before-activate-functions #'cae-workspaces-load-bookmarks-h)
