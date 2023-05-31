aabb59de38521c98b4ff86e72d6f31f94a6ad29f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:51 -0400

    modules/private/lisp/autoload.el

--- modules/private/lisp/autoload.el
+++ modules/private/lisp/autoload.el
@@ -3,3 +3,18 @@
 ;;;###autoload
 (defun cae-lisp-check-parens-before-save-h ()
   (add-hook 'write-file-functions 'check-parens nil t))
+
+;;;###autoload
+(defun cae-lisp-add-safe-local-variables-on-save ()
+  "Add variables in .dir-locals.el as safe values when the file is saved."
+  (when (and (buffer-file-name)
+             (string= (file-name-nondirectory (buffer-file-name)) ".dir-locals.el"))
+    (let ((locals (dir-locals-read-from-file (buffer-file-name))))
+      (dolist (entry locals)
+        (let ((mode (car entry))
+              (variables (cdr entry)))
+          (dolist (variable variables)
+            (let ((var (car variable))
+                  (value (cdr variable)))
+              (put var 'safe-local-variable
+                   (lambda (_x) (equal _x value))))))))))
