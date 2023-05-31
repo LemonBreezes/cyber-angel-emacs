3202daa664f38496492ca641aa9d002bb28a6497
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:23 -0400

    autoload/cae-copilot.el

--- autoload/cae-copilot.el
+++ autoload/cae-copilot.el
@@ -1,5 +1,6 @@
 ;;; autoload/cae-copilot.el -*- lexical-binding: t; -*-
 
+;;;###autoload
 (defun cae-copilot-accept-completion-maybe ()
   "Accept the completion at point if there one or execute the
 command currently typed."
@@ -12,6 +13,7 @@ command currently typed."
          (lookup-key (current-global-map)
                      (this-command-keys))))))
 
+;;;###autoload
 (defun cae-copilot-accept-completion-by-word-maybe ()
   "Accept the completion at point if there one or execute the
 command currently typed."
@@ -24,12 +26,39 @@ command currently typed."
          (lookup-key (current-global-map)
                      (this-command-keys))))))
 
+;;;###autoload
 (defun cae-copilot-accept-completion-by-line-maybe ()
   "Accept the completion at point if there one or execute the
 command currently typed."
   (interactive)
   (if (copilot--overlay-visible)
       (copilot-accept-completion-by-line)
+    (call-interactively
+     (or (lookup-key (current-local-map)
+                     (this-command-keys))
+         (lookup-key (current-global-map)
+                     (this-command-keys))))))
+
+;;;###autoload
+(defun cae-copilot-next-completion-maybe ()
+  "Accept the completion at point if there one or execute the
+command currently typed."
+  (interactive)
+  (if (copilot--overlay-visible)
+      (copilot-next-completion)
+    (call-interactively
+     (or (lookup-key (current-local-map)
+                     (this-command-keys))
+         (lookup-key (current-global-map)
+                     (this-command-keys))))))
+
+;;;###autoload
+(defun cae-copilot-previous-completion-maybe ()
+  "Accept the completion at point if there one or execute the
+command currently typed."
+  (interactive)
+  (if (copilot--overlay-visible)
+      (copilot-previous-completion)
     (call-interactively
      (or (lookup-key (current-local-map)
                      (this-command-keys))
