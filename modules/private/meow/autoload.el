762704ff479992f6930cc645ff9711216329be53
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:21 -0400

    modules/private/meow/autoload.el

--- modules/private/meow/autoload.el
+++ modules/private/meow/autoload.el
@@ -4,3 +4,14 @@
 (defun cae-meow-use-keyboard-layout-a ()
   (setq last-command-event
         (cae-keyboard-remap-reverse last-command-event)))
+
+;;;###autoload
+(defun cae-meow-update-cursor-a ()
+  (when-let ((cursor
+              (symbol-value
+               (intern (concat "meow-cursor-type-"
+                               (symbol-name meow--current-state))))))
+    (when (car-safe cursor)
+      (setq cursor (car cursor)))
+    (setq cursor-type cursor)
+    (etcc--evil-set-cursor)))
