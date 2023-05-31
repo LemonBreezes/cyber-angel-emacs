0bb9404c593419ef2c8effc2fe526b5cd5acde81
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:56 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -25,7 +25,7 @@
             (if fn (funcall fn) (dirvish-kill dv)))
           (funcall oldfun file wildcards)))
     (funcall oldfun file wildcards))
-  (+log "HELLOOOO" major-mode)
+  (+log "HELLOOOO" major-mode (one-window-p t))
   (when (derived-mode-p 'dired-mode)
     (dirvish-dwim)))
 
