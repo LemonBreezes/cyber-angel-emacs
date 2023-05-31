4b9d75a49f5358085fd5c324f7921287bf8832dc
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:18 -0400

    lisp/cae-evil.el

---
+++ lisp/cae-evil.el
@@ -0,0 +1,8 @@
+;;; lisp/cae-evil.el -*- lexical-binding: t; -*-
+
+;; Unbind text scaling functions. We use C-x C-=.
+(map! :n "C--" nil
+      :n "C-+" nil
+      :n "C-=" nil
+      :n "M-C-=" nil
+      :n "M-C--" nil)
