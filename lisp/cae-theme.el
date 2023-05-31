803a61f37d0095b82707fb3a5b65172b4f0c6006
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:19 -0400

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -85,30 +85,7 @@
 (use-package! theme-magic
   :defer t
   :init
-  (theme-magic-export-theme-mode +1)
-  :config
-  (defadvice! theme-magic--auto-extract-16-doom-colors (orig-fn)
-    :around #'theme-magic--auto-extract-16-colors
-    (if (and (featurep 'doom-themes)
-             (string-prefix-p "doom-" (symbol-name (car custom-enabled-themes))))
-        (list
-         (face-attribute 'default :background)
-         (doom-color 'error)
-         (doom-color 'success)
-         (doom-color 'type)
-         (doom-color 'keywords)
-         (doom-color 'constants)
-         (doom-color 'functions)
-         (face-attribute 'default :foreground)
-         (face-attribute 'shadow :foreground)
-         (doom-blend 'base8 'error 0.1)
-         (doom-blend 'base8 'success 0.1)
-         (doom-blend 'base8 'type 0.1)
-         (doom-blend 'base8 'keywords 0.1)
-         (doom-blend 'base8 'constants 0.1)
-         (doom-blend 'base8 'functions 0.1)
-         (face-attribute 'default :foreground))
-      (funcall orig-fn))))
+  (theme-magic-export-theme-mode +1))
 
 (unless (display-graphic-p)
   (setq +ligatures-in-modes nil
