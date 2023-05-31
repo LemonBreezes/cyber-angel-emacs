94083ee8d21e78adabf9dfa40ac64ef0cf2a78d0
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:58 -0400

    config.el

--- config.el
+++ config.el
@@ -888,6 +888,11 @@
 (use-package! string-edit-at-point      ; Used in `cae-edit-indirect-dwim'.
   :defer t)
 
+(after! outline
+  (after! which-key
+    (add-to-list 'which-key-replacement-alist
+                 `(("C-c @" . nil) . (nil . "outline")))))
+
 
 ;;; Autocompletion
 
