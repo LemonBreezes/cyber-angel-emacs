6fbb1370eccfd47447001ff684e2805f0a548b14
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:19 -0400

    modules/private/org/packages.el

--- modules/private/org/packages.el
+++ modules/private/org/packages.el
@@ -5,8 +5,7 @@
 (package! org-inline-pdf)
 (package! language-detection)
 (package! org-appear)
-(when (modulep! +jira)
-  (package! org-jira))
+(package! org-jira)
 (package! worf :recipe
   (:host github :repo "LemonBreezes/worf"))
 (package! org-tidy)
