ee8ca51a957093a5f7e4e9937dd5892f4bbe906e
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:08 -0400

    config.el

--- config.el
+++ config.el
@@ -11,7 +11,7 @@
 (load! "lisp/cae-fixup-leader-key")
 (load! "lisp/cae-multi")                ;Run parallel Emacs instances.
 (load! "lisp/cae-keyboard")             ;Input hacks.
-(load! "lisp/cae-smartparens")
+(load! "lisp/cae-smartparens")          ;Allow Smartparens to be disabled.
 
 (when (and (modulep! :completion helm)
            (modulep! :completion vertico))
