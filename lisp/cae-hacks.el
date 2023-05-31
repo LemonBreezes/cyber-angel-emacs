6327b1cd87ce69c2c7a6f98c9332c11d7e669935
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:33 -0400

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -150,4 +150,5 @@
            gc-cons-percentage cae-hacks-gc-percentage))))
 
 (advice-add #'save-some-buffers :before #'cae-hacks-disable-gc-temporarily)
+(advice-add #'save-buffers-kill-terminal :before #'cae-hacks-disable-gc-temporarily)
 (add-hook 'git-timemachine-mode-hook #'cae-hacks-disable-gc-temporarily -1)
