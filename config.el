9ae9bc5ca923c1e538548e7a7eda2daaa713646c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:02 -0400

    config.el

--- config.el
+++ config.el
@@ -214,7 +214,7 @@
 (define-key (current-global-map) [remap async-shell-command] 'with-editor-async-shell-command)
 (define-key (current-global-map) [remap shell-command] 'with-editor-shell-command)
 (add-hook 'shell-mode-hook  #'with-editor-export-editor)
-(advice-add #'with-editor-export-editor :around #'cae-hacks-shut-up-a)
+(advice-add #'with-editor-export-editor :around #'doom-shut-up-a)
 (add-hook 'eshell-mode-hook #'with-editor-export-editor)
 
 (when (and (modulep! :checkers spell)
