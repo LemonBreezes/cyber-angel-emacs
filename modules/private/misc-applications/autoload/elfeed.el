63b3f2c960ba8c4ac6c072700333b014115b73f3
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:34 -0400

    modules/private/misc-applications/autoload/elfeed.el

--- modules/private/misc-applications/autoload/elfeed.el
+++ modules/private/misc-applications/autoload/elfeed.el
@@ -7,3 +7,10 @@
   (+workspace/delete +rss-workspace-name)
   (when (buffer-live-p elfeed-log-buffer-name)
     (kill-buffer elfeed-log-buffer-name)))
+
+;;;###autoload
+(defun +elfeed-toggle-log-buffer ()
+  (interactive)
+  (if (get-buffer-window elfeed-log-buffer-name)
+      (delete-window elfeed-log-buffer-name)
+    (pop-to-buffer elfeed-log-buffer-name)))
