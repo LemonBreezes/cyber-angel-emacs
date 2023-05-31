072ca45a1eb02df128f7da235aba4719be911c59
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:06 -0400

    lisp/cae-project.el

---
+++ lisp/cae-project.el
@@ -0,0 +1,12 @@
+;;; lisp/cae-project.el -*- lexical-binding: t; -*-
+
+(defvar cae-project-bookmark-dir (concat doom-cache-dir "cae-project-bookmarks/")
+  "Directory to store project bookmarks.")
+
+(defun cae-project-get-bookmark-file (project)
+  "Return the bookmark file for PROJECT."
+  (expand-file-name "bookmarks" (cae-project-get-root project)))
+
+(defun cae-project-bookmark-jump ())
+
+(defun cae-project-bookmark-set ())
