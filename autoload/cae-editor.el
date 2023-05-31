df876eb60fe195685f827241997a25fbb89511c9
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:04 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -223,6 +223,5 @@ mark the string and call `edit-indirect-region' with it."
    which-key-idle-delay nil
    (lambda ()
      (when which-key-inhibit
-       (which-key-show-keymap 'org-ai-global-mode-prefix-map))))
-  ;;(set-transient-map org-ai-global-mode-prefix-map)
-  )
+       (which-key-show-keymap 'org-ai-global-mode-prefix-map)
+       (set-transient-map org-ai-global-mode-prefix-map)))))
