77497cbccbae499f73aed3492f6f2e1f88da2f36
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:04 -0400

    modules/private/lisp/config.el

--- modules/private/lisp/config.el
+++ modules/private/lisp/config.el
@@ -9,15 +9,23 @@
 (use-package! outline-minor-faces
   :hook (emacs-lisp-mode . outline-minor-faces-add-font-lock-keywords))
 
-(add-hook 'emacs-lisp-mode-hook #'+check-parens-before-save-h)
-(add-hook 'lisp-mode-hook #'+check-parens-before-save-h)
-(add-hook 'scheme-mode-hook #'+check-parens-before-save-h)
-(add-hook 'clojure-mode-hook #'+check-parens-before-save-h)
-(add-hook 'racket-mode-hook #'+check-parens-before-save-h)
-(add-hook 'lfe-mode-hook #'+check-parens-before-save-h)
-(add-hook 'hy-mode-hook #'+check-parens-before-save-h)
-(add-hook 'dune-mode-hook #'+check-parens-before-save-h)
-(add-hook 'fennel-mode-hook #'+check-parens-before-save-h)
+(add-hook 'emacs-lisp-mode-hook #'cae-lisp-check-parens-before-save-h)
+(add-hook 'lisp-mode-hook #'cae-lisp-check-parens-before-save-h)
+(add-hook 'scheme-mode-hook #'cae-lisp-check-parens-before-save-h)
+(add-hook 'clojure-mode-hook #'cae-lisp-check-parens-before-save-h)
+(add-hook 'racket-mode-hook #'cae-lisp-check-parens-before-save-h)
+(add-hook 'lfe-mode-hook #'cae-lisp-check-parens-before-save-h)
+(add-hook 'hy-mode-hook #'cae-lisp-check-parens-before-save-h)
+(add-hook 'dune-mode-hook #'cae-lisp-check-parens-before-save-h)
+(add-hook 'fennel-mode-hook #'cae-lisp-check-parens-before-save-h)
+
+(add-hook 'lisp-data-mode-hook
+          (defun +enable-elisp-mode-in-dir-locals-file ()
+            (when (and (not (eq major-mode 'emacs-lisp-mode))
+                       (buffer-file-name)
+                       (string= (file-name-nondirectory (buffer-file-name))
+                                ".dir-locals.el"))
+              (emacs-lisp-mode))))
 
 ;; This tool helps us a lot with regular expressions
 (after! pcre2el
