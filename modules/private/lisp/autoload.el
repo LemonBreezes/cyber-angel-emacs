388721be88f71f20ff7e5ee22461e85f6ac7acc1
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:04 -0400

    modules/private/lisp/autoload.el

--- modules/private/lisp/autoload.el
+++ modules/private/lisp/autoload.el
@@ -1,5 +1,13 @@
 ;;; private/lisp/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun +check-parens-before-save-h ()
+(defun cae-lisp-check-parens-before-save-h ()
   (add-hook 'write-file-functions 'check-parens nil t))
+
+;;;###autoload
+(defun cae-lisp-enable-elisp-mode-in-dir-locals-file-h ()
+  (when (and (not (eq major-mode 'emacs-lisp-mode))
+             (buffer-file-name)
+             (string= (file-name-nondirectory (buffer-file-name))
+                      ".dir-locals.el"))
+    (emacs-lisp-mode)))
