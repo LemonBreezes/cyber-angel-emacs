95e74a49fa5ba47b6e6779db4a9398b8fd5d3f5d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:14 -0400

    modules/private/misc-applications/autoload/ytel.el

--- modules/private/misc-applications/autoload/ytel.el
+++ modules/private/misc-applications/autoload/ytel.el
@@ -11,3 +11,41 @@
 		   (concat "https://www.youtube.com/watch?v=" id))
     "--ytdl-format=bestvideo[height<=?720]+bestaudio/best")
   (message "Starting streaming..."))
+
+(defvar invidious-instances-url
+  "api.invidious.io/instances.json?pretty=1&sort_by=health")
+
+(defun ytel-instances-fetch-json ()
+  "Fetch list of invidious instances as json, sorted by health."
+  (let
+      ((url-request-method "GET")
+       (url-request-extra-headers
+        '(("Accept" . "application/json"))))
+    (with-current-buffer
+        (url-retrieve-synchronously invidious-instances-url)
+      (goto-char (point-min))
+      (re-search-forward "^$")
+      (let* ((json-object-type 'alist)
+             (json-array-type 'list)
+             (json-key-type 'string))
+        (json-read)))))
+
+(defun ytel-instances-alist-from-json ()
+  "Make the json of invidious instances into an alist."
+  (let ((jsonlist (ytel-instances-fetch-json))
+        (inst ()))
+    (while jsonlist
+      (push (concat "https://" (caar jsonlist)) inst)
+      (setq jsonlist (cdr jsonlist)))
+    (nreverse inst)))
+
+;;;###autoload
+(defun ytel-choose-instance ()
+  "Prompt user to choose an invidious instance to use."
+  (interactive)
+  (setq ytel-invidious-api-url
+        (or (condition-case nil
+                (completing-read "Using instance: "
+                                 (subseq (ytel-instances-alist-from-json) 0 11) nil "confirm" "https://") ; "healthiest" 12 instances; no require match
+              (error nil))
+            "invidious.tube"))) ; fallback
