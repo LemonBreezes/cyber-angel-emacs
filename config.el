2171356cf9062aee68439ff17f1ffa1bb8980385
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:26 -0400

    config.el

--- config.el
+++ config.el
@@ -528,8 +528,8 @@
 
   ;; Query buffers for a diff before killing them. This could be improved so
   ;; that it works more like `save-some-buffers'. It's kind of annoying that for
-  ;; both functions, if there is no underlying file, the diff just errors. Why
-  ;; not pop the buffer up instead in that scenario?
+  ;; both functions, if the underlying file does not exist, the diff just
+  ;; errors. Why not pop the buffer up instead in that scenario?
   (defvar cae-diff-window nil
     "Variable to store the diff window created by 'cae-ask-kill-buffer'.")
   (defun cae-ask-kill-buffer (buffer)
