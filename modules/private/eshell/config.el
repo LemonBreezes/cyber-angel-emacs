ef869e8ea8c6cf619c8e3cc7ee8fcd62b8029810
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:56 -0400

    modules/private/eshell/config.el

--- modules/private/eshell/config.el
+++ modules/private/eshell/config.el
@@ -50,20 +50,6 @@
     :config
     (add-hook 'eshell-mode-hook #'eshell-bookmark-setup))
 
-  ;; (use-package! pcmpl-args
-  ;;   :after eshell
-  ;;   :config
-  ;;   (advice-add #'pcmpl-args-pcomplete
-  ;;               :before
-  ;;               #'pcmpl-args--fix-elisp-completion))
-
-  ;; Silence the pcomplete capf, no errors or messages!
-  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
-
-  ;; Ensure that pcomplete does not write to the buffer
-  ;; and behaves as a pure `completion-at-point-function'.
-  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
-
   ;; Colorize ansi escape sequences in exported buffers
   (advice-add #'eshell-output-object-to-target :around #'+eshell-ansi-buffer-output)
 
