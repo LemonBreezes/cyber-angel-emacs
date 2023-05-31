bc7abadb8163a125f5c418dab7c3c3ee77361923
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:04 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -208,3 +208,19 @@ mark the string and call `edit-indirect-region' with it."
   (let ((avy-style 'pre)
         (avy-column-line-overlay nil))
     (apply oldfun args)))
+
+;;;###autoload
+(cae-defun cae-lazy-load-org-ai ()
+  (interactive)
+  (org-ai-global-mode +1)
+  (defvar org-ai-global-mode-prefix-map
+          (lookup-key org-ai-global-mode-map (kbd "C-c M-a")))
+  (setq unread-command-events (list ?\C-c ?\M-a))
+  (setq which-key-inhibit t)
+  (add-transient-hook! 'pre-command-hook
+    (setq which-key-inhibit nil))
+  (run-with-idle-timer
+   which-key-idle-delay nil
+   (lambda ()
+     (when which-key-inhibit
+       (which-key-show-keymap 'org-ai-global-mode-prefix-map)))))
