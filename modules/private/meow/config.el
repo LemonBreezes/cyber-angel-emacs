14b81d23eabc3eaf0591a1810c874729591ff286
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:25 -0400

    modules/private/meow/config.el

--- modules/private/meow/config.el
+++ modules/private/meow/config.el
@@ -180,12 +180,10 @@
   (when (modulep! +leader)
     (map! :map meow-normal-state-keymap
           doom-leader-key (cmd! () (setq unread-command-events (listify-key-sequence (kbd "C-c"))))
-          ;; "DEL" (cmd! () (setq unread-command-events (listify-key-sequence (kbd "C-c"))))
-          "DEL" #'meow-keypad
+          "DEL" (cmd! () (setq unread-command-events (listify-key-sequence (kbd "C-c"))))
           :map meow-motion-state-keymap
           doom-leader-key (cmd! () (setq unread-command-events (listify-key-sequence (kbd "C-c"))))
-          ;; "DEL" (cmd! () (setq unread-command-events (listify-key-sequence (kbd "C-c"))))
-          "DEL" #'meow-keypad
+          "DEL" (cmd! () (setq unread-command-events (listify-key-sequence (kbd "C-c"))))
           :map meow-beacon-state-keymap
           doom-leader-key nil))
   (map! :leader :desc "help" "h" help-map)
