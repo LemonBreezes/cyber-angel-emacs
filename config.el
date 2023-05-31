e4d141b472ac46392228bd60f153d854cdb756b7
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:30 -0400

    config.el

--- config.el
+++ config.el
@@ -319,7 +319,7 @@
       "Check if the current buffer's display type is posframe."
       (frame-parameter (window-frame window) 'parent-frame))
     :config
-    (add-to-list 'perfect-margin-ignore-regexps "^ \\*which-key\\*$")
+    (add-to-list 'perfect-margin-ignore-regexps "^ ")
     (add-to-list 'perfect-margin-ignore-filters #'cae-perfect-margin-ignore-posframe-p))
 
   (use-package! pdf-view-pagemark
