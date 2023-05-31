ff2ef435e9a6b50a6f51945ad2588a8f573a141b
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:53 -0400

    lisp/cae-hydra.el

--- lisp/cae-hydra.el
+++ lisp/cae-hydra.el
@@ -2,3 +2,27 @@
 
 (defvar cae-hydra--tab-bar-last-hydra-alist nil)
 (defvar cae-hydra--last-hydra nil)
+
+(defun cae-hydra-store-last-hydra-h (&rest _)
+  (when (bound-and-true-p hydra-curr-map)
+    (setq cae-hydra--last-hydra hydra-curr-body-fn)
+    (hydra-keyboard-quit))
+  (and (featurep 'persp-mode)
+       (set-persp-parameter 'cae-hydra--last-hydra
+                            cae-hydra--last-hydra))
+  (setf (alist-get (tab-bar--current-tab)
+                   cae-hydra--tab-bar-last-hydra-alist
+                   nil nil #'eq)
+        cae-hydra--last-hydra))
+
+(defun cae-hydra-restore-last-hydra-h (&rest _)
+  (let ((hydra (or (and (featurep 'persp-mode)
+                        (persp-parameter 'cae-hydra--last-hydra))
+                   (alist-get (tab-bar--current-tab)
+                              cae-hydra--tab-bar-last-hydra-alist
+                              nil nil #'eq)
+                   cae-hydra--last-hydra)))
+    (setq cae-hydra--last-hydra nil)
+    (hydra-keyboard-quit)
+    (cae-hydra-store-last-hydra-h)
+    (funcall hydra)))
