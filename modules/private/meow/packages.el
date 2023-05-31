403c4983db9d80db9d5682e50aa150c7c9be3724
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:22 -0400

    modules/private/meow/packages.el

--- modules/private/meow/packages.el
+++ modules/private/meow/packages.el
@@ -3,5 +3,3 @@
 
 (package! meow)
 (package! evil-terminal-cursor-changer :recipe (:host github :repo "7696122/evil-terminal-cursor-changer"))
-(package! embrace :recipe (:type git :host github :repo "cute-jumper/embrace.el"
-                           :fork (:host github :repo "alexluigit/embrace.el")))
