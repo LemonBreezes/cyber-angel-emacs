ebec2a1127377f335d92d41124ce40897b547e6c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:58 -0400

    modules/private/lisp/config.el

--- modules/private/lisp/config.el
+++ modules/private/lisp/config.el
@@ -32,3 +32,6 @@
   (undefine-key! rxt--read-pcre-mode-map
     "C-c i" "C-c s" "C-c x"))
 (add-hook 'emacs-lisp-mode-hook #'rxt-mode)
+
+(use-package! topsy
+  :defer t :init (add-hook 'emacs-lisp-mode-hook #'topsy-mode))
