10e403b451143424788ef575a62344bffc76a4cf
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:13 -0400

    modules/private/misc-applications/+proced.el

--- modules/private/misc-applications/+proced.el
+++ modules/private/misc-applications/+proced.el
@@ -6,4 +6,6 @@
   (map! :leader :prefix +misc-applications-prefix
         "p" #'proced)
   :config
-  (setq proced-enable-color-flag t))
+  (setq proced-enable-color-flag t)
+  (map! :map proced-mode-map
+        "<f6>" #'+proced-hydra/body))
