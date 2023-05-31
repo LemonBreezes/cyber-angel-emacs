ff04d0f5487e95e6f3d5465ddcee7b64085a194f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:05 -0400

    config.el

--- config.el
+++ config.el
@@ -1187,8 +1187,7 @@
 (map! :leader :prefix ("g" . "gpt"))
 
 (use-package! org-ai
-  :defer t :init
-  (map! "C-c M-a" #'cae-lazy-load-org-ai)
+  :defer t :init (map! "C-c M-a" #'cae-lazy-load-org-ai)
   :config
   (org-ai-global-mode +1)
   (add-hook 'org-mode-hook #'org-ai-mode)
