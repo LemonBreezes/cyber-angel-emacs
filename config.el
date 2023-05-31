1eaa8f5817fa663f382826e152a0839b2b793230
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:18 -0400

    config.el

--- config.el
+++ config.el
@@ -517,6 +517,7 @@
   (add-hook 'c-mode-common-hook #'aggressive-indent-mode)
   :defer t)
 
+;; Doom uses a custom delete backward command, `+default--delete-backward-char-a', which I prefer.
 ;; (use-package! hungry-delete
 ;;   :defer t :init
 ;;   (add-hook 'emacs-lisp-mode-hook #'hungry-delete-mode)
