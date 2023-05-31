bedbdd7e5223f7ce7702564a4c476d7aecf5ef5c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:20 -0400

    config.el

--- config.el
+++ config.el
@@ -19,11 +19,12 @@
 
 ;; Set up fonts
 (unless (memq system-type '(cygwin windows-nt ms-dos))
-  (setq doom-font (font-spec :family "Iosevka Comfy" :size 16)
-        doom-variable-pitch-font (font-spec :family "Iosevka Comfy Duo" :size 6)
-        doom-unicode-font (unless (modulep! :ui unicode)
-                            (font-spec :family "LXGW WenKai" :weight 'light
-                                       :size 15))))
+  (let ((offset (if (string-match-p "WSL" operating-system-release) 2 0)))
+    (setq doom-font (font-spec :family "Iosevka Comfy" :size (+ 16 offset))
+          doom-variable-pitch-font (font-spec :family "Iosevka Comfy Duo" :size (+ 6 offset))
+          doom-unicode-font (unless (modulep! :ui unicode)
+                              (font-spec :family "LXGW WenKai" :weight 'light
+                                         :size (+ 15 offset))))))
 
 ;; Show minibuffer recursion depth
 (autoload 'minibuffer-depth-setup "mb-depth")
