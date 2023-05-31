038f6bd4c954222e8344f72fc5e65aa5b9d3b948
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:43 -0400

    modules/private/vc/autoload.el

--- modules/private/vc/autoload.el
+++ modules/private/vc/autoload.el
@@ -6,7 +6,12 @@
  BRANCH is non-nil, use that branch instead of the current
  branch's upstream branch.
 
-From https://www.reddit.com/r/emacs/comments/112t0uo/comment/j9g2r5n/?utm_source=share&utm_medium=web2x&context=3."
+From https://www.reddit.com/r/emacs/comments/112t0uo/comment/j9g2r5n/?utm_source=share&utm_medium=web2x&context=3.
+
+Here is an expample use of this function:
+(magit-add-section-hook 'magit-status-sections-hook
+                        'cae-magit-insert-diff-upstream
+                        'magit-insert-stashes)"
   (magit-insert-section (diff-upstream nil t)
     (if-let ((upstream-branch (or branch (magit-get-upstream-branch (magit-get-current-branch)))))
         (progn
