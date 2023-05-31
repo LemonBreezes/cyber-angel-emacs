c529bb2bf7eba2058a7d7fe54dcf3f412a7062ef
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:25 -0400

    modules/private/debugger/autoload.el

--- modules/private/debugger/autoload.el
+++ modules/private/debugger/autoload.el
@@ -32,29 +32,3 @@
   (setq which-key-inhibit nil)
   (remove-hook 'pre-command-hook
                #'cae-debugger--which-key-inhibit-hook))
-
-(defvar cae-debugger--global-map (make-sparse-keymap))
-
-;;;###autoload
-(defun cae-debugger-lazy-load-gud ()
-  (interactive)
-  (require 'gud)
-  (require 'edebug)
-  (setq unread-command-events (list ?\C-x ?\C-a))
-  (setq which-key-inhibit t
-        which-key-idle-delay 0)
-  (add-hook 'pre-command-hook #'cae-debugger--which-key-inhibit-hook)
-  (run-with-idle-timer
-   which-key-idle-delay nil
-   (lambda ()
-     (when which-key-inhibit
-       (which-key-show-keymap
-        (prog1 'cae-debugger--global-map
-          (setq cae-debugger--global-map
-                (cons 'keymap
-                      (let* ((alist1 (cdr-safe (lookup-key (current-global-map) (kbd "C-x C-a"))))
-                             (alist2 (cdr-safe (lookup-key (current-local-map) (kbd "C-x C-a"))))
-                             (keys (mapcar #'car alist2)))
-                        (dolist (key keys)
-                          (setq alist1 (assq-delete-all key alist1)))
-                        (append alist1 alist2))))))))))
