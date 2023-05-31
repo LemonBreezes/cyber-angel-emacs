d54c04191423c80a929ed1bd1732e7cf87cc3a78
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:15 -0400

    config.el

--- config.el
+++ config.el
@@ -505,16 +505,7 @@
            :map dired-mode-map
            [remap dired-do-async-shell-command] #'dwim-shell-command
            [remap dired-do-shell-command] #'dwim-shell-command
-           [remap dired-smart-shell-command] #'dwim-shell-command))
-    :config
-    (defun my/dwim-shell-command-convert-to-gif ()
-      "Convert all marked videos to optimized gif(s)."
-      (interactive)
-      (dwim-shell-command-on-marked-files
-       "Convert to gif"
-       "ffmpeg -loglevel quiet -stats -y -i <<f>> -pix_fmt rgb24 -r 15 <<fne>>.gif"
-       :utils "ffmpeg")))
-  )
+           [remap dired-smart-shell-command] #'dwim-shell-command))))
 
 
 ;;; Editor
