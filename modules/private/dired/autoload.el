c49b73115f8ffd6a181f21c7c8441a2bd4f1e067
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:58 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -13,3 +13,18 @@
         (delq '+load-dirvish find-directory-functions))
   (appendq! find-directory-functions '(dired-noselect))
   (dired-noselect dir))
+
+;;;###autoload
+(defun cae-dired-find-file (file &optional wildcards)
+  "Like `find-file', but might exit the current Dirvish session."
+  (interactive
+   (find-file-read-args "Find file: "
+                        (confirm-nonexistent-file-or-buffer)))
+  (let ((dir (file-name-directory file)))
+    (unless (file-equal-p dir default-directory)
+      (find-file dir)))
+  (unless (file-directory-p file)
+    ;; Copied from `dirvish-find-entry-a'
+    (let* ((dv (dirvish-curr)) (fn (nth 4 (dv-type dv))))
+      (if fn (funcall fn) (dirvish-kill dv)))
+    (find-file file)))
