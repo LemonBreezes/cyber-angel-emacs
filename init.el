c418c9ff897c8b10270f37730f82e978d74471e9
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:16 -0400

    init.el

--- init.el
+++ init.el
@@ -5,18 +5,9 @@
 (load! "lisp/cae-hacks")
 
 (setq doom-leader-alt-key "C-c"
-      doom-localleader-alt-key "C-c l")
+      doom-localleader-alt-key "C-c l"
+      doom-leader-key "DEL")
 (setq native-comp-async-jobs-number (num-processors))
-
-;; Also show keybinidng descriptions on my second leader key.
-;; (defun +which-key-add-key-based-replacements-a
-;;     (oldfun key-sequence &rest args)
-;;   (when (string-prefix-p "SPC " key-sequence)
-;;     (apply oldfun (concat "DEL " (string-remove-prefix "SPC " key-sequence))
-;;            args))
-;;   (apply oldfun key-sequence args))
-;; (advice-add #'which-key-add-key-based-replacements :around
-;;             #'+which-key-add-key-based-replacements-a)
 
 (doom! :completion
        (vertico +icons +childframe)
