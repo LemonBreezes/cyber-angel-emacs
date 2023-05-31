cf9203e63c0f67a7b259e35d168b89b40d092575
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:40 -0400

    autoload/cae-hydras.el

---
+++ autoload/cae-hydras.el
@@ -0,0 +1,20 @@
+;;; autoload/cae-hydras.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defhydra embark-collect-cheatsheet-hydra (:color pink :foreign-keys run)
+    ("a" embark-act "Act" :column "Act")
+    ("A" embark-act-all "Act on all" :column "Act")
+    ("E" embark-export "Export" :column "Act")
+    ("S" tabulated-list-sort "Sort" :column "Navigate")
+    ("m" embark-collect-mark "Mark" :column "Act")
+    ("s" isearch-forward "Search forward" :column "Navigate")
+    ("{" outline-previous-heading "Previous heading" :column "Navigate")
+    ("}" outline-next-heading "Next heading" :column "Navigate")
+    ("u" embark-collect-unmark "Unmark" :column "Act")
+    ("U" embark-collect-unmark-all "Unmark all" :column "Act")
+    ("t" embark-collect-toggle-marks "Toggle marks" :column "Act")
+    ("M-<left>" tabulated-list-previous-column "Previous column" :column "Navigate")
+    ("M-<right>" tabulated-list-next-column "Next column" :column "Navigate")
+    ("SPC" scroll-up-command "Scroll up" :column "Navigate")
+    ("DEL" scroll-down-command "Scroll down" :column "Navigate")
+    ("<f6>" nil "Exit" :exit t :column nil))
