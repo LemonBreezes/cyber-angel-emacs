027cdbd9dbbca03e0cac681d78423d7f35becb6a
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:03 -0400

    config.el

--- config.el
+++ config.el
@@ -362,7 +362,8 @@
            (?y . avy-action-yank)
            (?Y . avy-action-yank-line)
            (?i . avy-action-ispell)
-           (?z . avy-action-zap-to-char))))
+           (?z . avy-action-zap-to-char)))
+        avy-styles-alist '((avy-isearch . pre)))
   (setq-hook! '(prog-mode-hook text-mode-hook conf-mode-hook)
     avy-indent-line-overlay t))
 
