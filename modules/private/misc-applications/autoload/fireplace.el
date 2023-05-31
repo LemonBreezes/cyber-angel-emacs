63e2c2fe2328438c8ed9138fc04f04a6339a1985
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:12 -0400

    modules/private/misc-applications/autoload/fireplace.el

--- modules/private/misc-applications/autoload/fireplace.el
+++ modules/private/misc-applications/autoload/fireplace.el
@@ -3,6 +3,7 @@
 ;;;###autoload (autoload '+fireplace-hydra/body "private/misc-applications/autoload/fireplace" nil t)
 (defhydra +fireplace-hydra (:color pink :hint nil)
   ("<f6>" nil "Exit" :exit t)
+  ("q" fireplace-off nil :exit t)
   ("C-*" fireplace-toggle-smoke "Toggle smoke")
   ("C-+" fireplace-up "Fireplace up")
   ("C--" fireplace-down "Fireplace down")
