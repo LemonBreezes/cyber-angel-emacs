5604b7b7bdfb445c7163a03819c3500a50bdcf29
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:25 -0400

    modules/private/modeline/autoload.el

--- modules/private/modeline/autoload.el
+++ modules/private/modeline/autoload.el
@@ -8,3 +8,12 @@
       (goto-char beg))
     (skip-syntax-forward "^w" (line-end-position))
     (call-interactively #'parrot-rotate-next-word-at-point)))
+
+;;;###autoload
+(defun cae-modeline-rotate-prev-word-at-point ()
+  (interactive)
+  (save-excursion
+    (when-let ((beg (car-safe (bounds-of-thing-at-point 'symbol))))
+      (goto-char beg))
+    (skip-syntax-backward "^w" (line-beginning-position))
+    (call-interactively #'parrot-rotate-prev-word-at-point)))
