ac8d67fd44a597c47d2da68b3ef18fb99343ba52
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:57 -0400

    modules/private/modeline/autoload/modeline.el

--- modules/private/modeline/autoload/modeline.el
+++ modules/private/modeline/autoload/modeline.el
@@ -1,7 +1,7 @@
 ;;; private/modeline/autoload/modeline.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-create-file-buffer (filename)
+(defun cae-modeline-create-file-buffer (filename)
   "Create a suitably named buffer for visiting FILENAME, and return it.
 FILENAME (sans directory) is used unchanged if that name is free;
 otherwise a string <2> or <3> or ... is appended to get an unused name.
@@ -9,11 +9,9 @@ otherwise a string <2> or <3> or ... is appended to get an unused name.
 Emacs treats buffers whose names begin with a space as internal buffers.
 To avoid confusion when visiting a file whose name begins with a space,
 this function prepends a \"|\" to the final result if necessary."
-  (let* ((vc-backend (ignore-errors (vc-responsible-backend filename)))
-         (vc-base-path (when vc-backend
-                         (vc-call-backend vc-backend 'root filename)))
-         (name-base (if vc-base-path
-                        (file-relative-name filename vc-base-path)
+  (let* ((project (project-current nil (file-name-directory filename)))
+         (name-base (if project
+                        (file-relative-name filename (project-root project))
                       (file-name-nondirectory filename)))
 	 (name-base (if (string= name-base "")
 	               filename name-base))
@@ -22,3 +20,4 @@ this function prepends a \"|\" to the final result if necessary."
 			             name-base))))
     (uniquify--create-file-buffer-advice buf filename)
     buf))
+(project-current nil (file-name-directory filename))
