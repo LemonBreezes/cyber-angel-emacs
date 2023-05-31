ea48c95eaa20cac9ed10f3cfac8088a277b8f379
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:29 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -1,6 +1,6 @@
 ;;; private/modeline/config.el -*- lexical-binding: t; -*-
 
-(add-hook! 'doom-init-ui-hook
+(add-hook! 'after-change-major-mode-hook
   (cae-defun cae-modeline--set-project-name ()
     (setq-default mode-line-format
                   (cl-subst '(:eval (propertize (breadcrumb-project-crumbs)
