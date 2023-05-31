d36549d73c10ac68a9b67a4a9119ae88d755e6a7
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:55 -0400

    lisp/cae-repeat.el

--- lisp/cae-repeat.el
+++ lisp/cae-repeat.el
@@ -101,7 +101,7 @@
        "7" +workspace/switch-to-6
        "8" +workspace/switch-to-7
        "9" +workspace/switch-to-8
-       "r" +workspace/rename
+       ;;"r" +workspace/rename
        ;;"d" +workspace/display
        )
       (:enter +workspace/display))
