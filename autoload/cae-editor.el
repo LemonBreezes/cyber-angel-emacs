403d720fbf04094f150286dd159ae4f49f58c4d9
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:27 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -20,15 +20,6 @@
           (funcall orig-func buffer-or-name))))))
 
 ;;;###autoload
-(defun dos2unix (buffer)
-  "Automate M-% C-q C-m RET C-q C-j RET"
-  (interactive "*b")
-  (save-excursion
-    (goto-char (point-min))
-    (while (search-forward (string ?\C-m) nil t)
-      (replace-match "" nil t))))
-
-;;;###autoload
 (defun cae-sp-delete-char (&optional arg)
   (interactive "*P")
   (cond ((and delete-active-region
