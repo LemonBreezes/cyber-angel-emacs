2e3d99e19b033d9ae801fee7c9802a8cba4c5640
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:51 -0400

    init.el

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
        (vc-gutter +pretty +diff-hl)
        vi-tilde-fringe
        workspaces
-       ;(window-select +switch-window)
+       ;; (window-select +switch-window)
 
        :editor
        file-templates
