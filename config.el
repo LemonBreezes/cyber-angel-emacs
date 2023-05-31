38aff22fa2ecadaf91c55aa92fb828284b461846
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:50 -0400

    config.el

--- config.el
+++ config.el
@@ -1065,6 +1065,7 @@
     (add-to-list 'mc/cmds-to-run-once #'read-only-mode)
     (add-to-list 'mc/cmds-to-run-for-all #'doom/delete-backward-word)
     (add-to-list 'mc/cmds-to-run-for-all #'doom/forward-to-last-non-comment-or-eol)
+    (add-to-list 'mc/cmds-to-run-for-all #'mark-sexp)
     (add-to-list 'mc/unsupported-minor-modes #'cae-completion-mode)
     (define-key mc/keymap (kbd "C-. .")     #'mc/move-to-column)
     (define-key mc/keymap (kbd "C-. =")     #'mc/compare-chars)
