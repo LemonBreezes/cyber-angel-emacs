69401e01c4cb90231ebb50b4e7d7cdd06af9d62d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:58 -0400

    modules/private/modeline/autoload/minions.el

--- modules/private/modeline/autoload/minions.el
+++ modules/private/modeline/autoload/minions.el
@@ -3,7 +3,6 @@
 ;;;###autoload
 (defun cae-modeline-minions-c-setup ()
   (require 'glasses)
-  (require 'hideif)
   (setq-local minions-available-modes
               (append '((hide-ifdef-mode)
                         (glasses-mode))
