1fa80a04d3d8c3b47125f762687c44f129841169
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:11 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -1,25 +1,6 @@
 ;;; autoload/cae-editor.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-kill-buffer (&optional buffer-or-name)
-  (interactive "bKill buffer: ")
-  (catch 'quit
-    (save-window-excursion
-      (with-current-buffer buffer-or-name
-        (let (done (buf (current-buffer)))
-          (when (and buffer-file-name (buffer-modified-p))
-            (while (not done)
-              (let ((response (read-char-choice
-                               (format "Save file %s? (y, n, d, q) " (buffer-file-name buf))
-                               '(?y ?n ?d ?q))))
-                (setq done (cond
-                            ((eq response ?q) (throw 'quit nil))
-                            ((eq response ?y) (save-buffer) t)
-                            ((eq response ?n) (set-buffer-modified-p nil) t)
-                            ((eq response ?d) (diff-buffer-with-file) nil))))))
-          (kill-buffer buffer-or-name))))))
-
-;;;###autoload
 (defun cae-kill-buffer-a (orig-func &optional buffer-or-name)
   (setq buffer-or-name (or buffer-or-name (current-buffer)))
   (catch 'quit
@@ -37,18 +18,3 @@
                             ((eq response ?n) (set-buffer-modified-p nil) t)
                             ((eq response ?d) (diff-buffer-with-file) nil))))))
           (funcall orig-func buffer-or-name))))))
-
-(defun cae-kill-buffer-fixup-windows (buffer)
-  (let ((windows (get-buffer-window-list buffer)))
-    (cae-kill-buffer buffer)
-    (doom-fixup-windows (cl-remove-if-not #'window-live-p windows))))
-
-;;;###autoload
-(defun cae-kill-this-buffer-in-all-windows (buffer &optional dont-save)
-  (interactive
-   (list (current-buffer) current-prefix-arg))
-  (cl-assert (bufferp buffer) t)
-  (when (and (buffer-modified-p buffer) dont-save)
-    (with-current-buffer buffer
-      (set-buffer-modified-p nil)))
-  (cae-kill-buffer-fixup-windows buffer))
