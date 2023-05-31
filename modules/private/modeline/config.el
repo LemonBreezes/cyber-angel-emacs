fe42d49a8ec629fc998489a15d5d175900faf17b
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:05 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -1,90 +1,80 @@
 ;;; private/modeline/config.el -*- lexical-binding: t; -*-
 
-;; (use-package! minions
-;;   :init
-;;   (add-hook 'doom-first-buffer-hook #'minions-mode)
-;;   :config
-;;   (setq minions-hidden-modes
-;;         '(abbrev-mode
-;;           auto-fill-function
-;;           eldoc-mode
-;;           envrc-mode
-;;           flycheck-mode
-;;           flyspell-mode
-;;           git-gutter+-mode
-;;           git-gutter-mode
-;;           git-timemachine-mode
-;;           helm-mode
-;;           helm-gtags-mode
-;;           helm-projectile-mode
-;;           highlight-indentation-mode
-;;           highlight-numbers-mode
-;;           highlight-parentheses-mode
-;;           highlight-symbol-mode
-;;           hs-minor-mode
-;;           linum-mode
-;;           nyan-mode
-;;           projectile-mode
-;;           rainbow-delimiters-mode
-;;           smartparens-mode
-;;           undo-tree-mode
-;;           volatile-highlights-mode
-;;           yas-minor-mode
-;;           which-key-mode
-;;           vi-tilde-fringe-mode
-;;           page-break-lines-mode
-;;           gcmh-mode
-;;           general-override-mode
-;;           editorconfig-mode
-;;           better-jumper-local-mode
-;;           persp-mode
-;;           ws-butler-mode
-;;           outline-minor-mode
-;;           diff-hl-flydiff-mode
-;;           copilot-mode
-;;           git-auto-commit-mode
-;;           hl-todo-mode
-;;           whitespace-mode
-;;           highlight-quoted-mode
-;;           diff-hl-mode
-;;           lsp-completion-mode
-;;           lsp-diagnostics-mode
-;;           lsp-mode
-;;           lsp-modeline-diagnostics-mode
-;;           lsp-modeline-code-actions-mode
-;;           lsp-modeline-workspace-status-mode
-;;           dtrt-indent-mode
-;;           tree-sitter-mode
-;;           dap-mode
-;;           eat-eshell-mode
-;;           modern-c++-font-lock-mode))
-;;   (setq minions-available-modes
-;;         '(;; (abbrev-mode)
-;;           (auto-fill-mode)
-;;           (auto-revert-mode)
-;;           (auto-revert-tail-mode)
-;;           ;; (flyspell-mode)
-;;           (spell-fu-mode)
-;;           (font-lock-mode)
-;;           (glasses-mode)
-;;           ;; (overwrite-mode)
-;;           (ruler-mode)
-;;           (visual-line-mode)
-;;           (display-fill-column-indicator-mode)))
-;;   (setq minions-prominent-modes '(defining-kbd-macro
-;;                                   projectile-mode
-;;                                   flycheck-mode))
-;;   (after! compile
-;;     (setf (alist-get 'compilation-in-progress
-;;                      (default-value 'minions-mode-line-modes))
-;;           (propertize "[Compiling] "
-;; 	              'help-echo "Compiling; mouse-2: Goto Buffer"
-;;                       'mouse-face 'mode-line-highlight
-;;                       'local-map
-;;                       (make-mode-line-mouse-map
-;;                        'mouse-2
-;; 		       #'compilation-goto-in-progress-buffer))))
-;;   (map! "<f9>" #'minions-minor-modes-menu))
+(use-package! minions
+  :init
+  (add-hook 'doom-first-buffer-hook #'minions-mode)
+  :config
+  (setq minions-hidden-modes
+        '(abbrev-mode
+          auto-fill-function
+          eldoc-mode
+          envrc-mode
+          flycheck-mode
+          flyspell-mode
+          git-gutter+-mode
+          git-gutter-mode
+          git-timemachine-mode
+          helm-mode
+          helm-gtags-mode
+          helm-projectile-mode
+          highlight-indentation-mode
+          highlight-numbers-mode
+          highlight-parentheses-mode
+          highlight-symbol-mode
+          hs-minor-mode
+          linum-mode
+          nyan-mode
+          projectile-mode
+          rainbow-delimiters-mode
+          smartparens-mode
+          undo-tree-mode
+          volatile-highlights-mode
+          yas-minor-mode
+          which-key-mode
+          vi-tilde-fringe-mode
+          page-break-lines-mode
+          gcmh-mode
+          general-override-mode
+          editorconfig-mode
+          better-jumper-local-mode
+          persp-mode
+          ws-butler-mode
+          outline-minor-mode
+          diff-hl-flydiff-mode
+          copilot-mode
+          git-auto-commit-mode
+          hl-todo-mode
+          whitespace-mode
+          highlight-quoted-mode
+          diff-hl-mode
+          lsp-completion-mode
+          lsp-diagnostics-mode
+          lsp-mode
+          lsp-modeline-diagnostics-mode
+          lsp-modeline-code-actions-mode
+          lsp-modeline-workspace-status-mode
+          dtrt-indent-mode
+          tree-sitter-mode
+          dap-mode
+          eat-eshell-mode
+          modern-c++-font-lock-mode))
+  (setq minions-available-modes
+        '(;; (abbrev-mode)
+          (auto-fill-mode)
+          (auto-revert-mode)
+          (auto-revert-tail-mode)
+          ;; (flyspell-mode)
+          (spell-fu-mode)
+          (font-lock-mode)
+          (glasses-mode)
+          ;; (overwrite-mode)
+          (ruler-mode)
+          (visual-line-mode)
+          (display-fill-column-indicator-mode)))
+  (setq minions-prominent-modes '(defining-kbd-macro
+                                  projectile-mode
+                                  flycheck-mode))
+  (map! "<f9>" #'minions-minor-modes-menu))
 
 (add-hook 'doom-first-file-hook #'column-number-mode)
 
