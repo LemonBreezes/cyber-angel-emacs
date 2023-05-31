ae0c3b505bdce5606fbb26bb5fc838ffe4883a89
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:31 -0400

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -32,6 +32,8 @@
               (string-match-p "I-search: " (minibuffer-prompt))
               (string-match-p "Query replace " (minibuffer-prompt))
               (string-match-p "Align regexp" (minibuffer-prompt))
+              (memq (current-local-map)
+                    (list read-passwd-map elfeed-search-filter-map))
               (memq this-command '(evil-ex
                                    evil-ex-search-forward
                                    evil-ex-search-backward))
