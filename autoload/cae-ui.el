e9ba469e1a5dcb2c008212ab84f5bc178b67a20f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:11 -0400

    autoload/cae-ui.el

--- autoload/cae-ui.el
+++ autoload/cae-ui.el
@@ -14,3 +14,16 @@
       (back-to-indentation)
       (font-lock-ensure (point) (point-at-eol))
       (buffer-substring (point) (point-at-eol)))))
+
+;;;###autoload
+(defun rigor/which-key-show-workspace (orig-fun &rest pages-obj)
+    "Show my workspaces in the echo thingy"
+    (let ((out (apply orig-fun pages-obj))
+          (prefix-title (which-key--pages-prefix-title (car pages-obj))))
+      (if (not (or (string-equal prefix-title "workspace")
+                   (string-equal prefix-title "workspaces/windows")))
+          out
+        (cons (car out)
+              (lambda ()
+                (funcall (cdr out))
+                (which-key--echo (concat (current-message) " " (+workspace--tabline))))))))
