9588384159245d444764e0bcec4d57f95099dcdf
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:31 -0400

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -31,7 +31,7 @@
               (bound-and-true-p vertico--input)
               (string-match-p "I-search: " (minibuffer-prompt))
               (string-match-p "Query replace " (minibuffer-prompt))
-              (string-match-p "Align regexp " (minibuffer-prompt))
+              (string-match-p "Align regexp" (minibuffer-prompt))
               (memq this-command '(evil-ex
                                    evil-ex-search-forward
                                    evil-ex-search-backward))
