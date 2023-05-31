330b58803b267d475240a92d7600671367b6468f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:20 -0400

    config.el

--- config.el
+++ config.el
@@ -1040,7 +1040,6 @@
         [remap org-ai-kill-region-at-point] #'cae-ai-org-ai-kill-region-at-point)
   (defvar org-ai-global-mode-prefix-map
     (lookup-key org-ai-global-mode-map (kbd "C-c M-a")))
-  (org-ai-global-mode +1)
   (setq org-ai-default-chat-model "gpt-4")
   (when (modulep! :editor snippets)
     (org-ai-install-yasnippets)))
