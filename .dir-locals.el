d662bc2ba1ad832a0f3207c8c774a412cd08624e
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:29 -0400

    .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,6 +4,7 @@
 ((nil
   . ((eval
       . (progn
+          ;; Prevent an Elisp file from being saved if it contains an error.
           (when (and (derived-mode-p 'emacs-lisp-mode)
                      (buffer-file-name)
                      (not (cl-member (file-name-nondirectory (buffer-file-name))
@@ -13,16 +14,23 @@
                                      :test #'string=))
                      (bound-and-true-p cae-config-finished-loading))
             (add-hook 'write-file-functions 'eval-buffer 1 t))
+
+          ;; Automatically update Eshell aliases.
           (when (and (buffer-file-name) (bound-and-true-p eshell-aliases-file)
                      (file-equal-p (buffer-file-name)
                                    (bound-and-true-p eshell-aliases-file))
                      (fboundp 'eshell-read-aliases-list))
             (add-hook 'after-save-hook 'eshell-read-aliases-list nil t))
+
+          ;; Automatically compile Emacs Lisp files (if enabled).
           (when (bound-and-true-p cae-config-finished-loading)
             (when (and (derived-mode-p 'emacs-lisp-mode)
                        (fboundp 'cae-compile-this-elisp-file)
                        (bound-and-true-p cae-config-compilation-enabled))
               (add-hook 'after-save-hook #'cae-compile-this-elisp-file nil t))
+
+            ;; Automantically commit saved files to Git and push them to the
+            ;; remote.
             (when (and (buffer-file-name)
                        (not (file-in-directory-p (buffer-file-name)
                                                  (concat doom-private-dir "secrets/")))
