fb13a249c162126e289cfa4ea5569cb39acaee19
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:00 -0400

    lisp/cae-repeat.el

--- lisp/cae-repeat.el
+++ lisp/cae-repeat.el
@@ -6,7 +6,7 @@
   :init
   (add-hook 'doom-first-input-hook #'repeat-mode)
   :config
-  (setq r:epeat-exit-timeout 0.5
+  (setq repeat-exit-timeout 0.5
         repeat-exit-key "C-g")
   (map! :map help-map "C-r" #'describe-repeat-maps)
 
