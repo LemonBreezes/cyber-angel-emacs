d135675299a99519c19ee7bbcc04b1682baa42e7
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:46 -0400

    lisp/cae-multi.el

--- lisp/cae-multi.el
+++ lisp/cae-multi.el
@@ -41,7 +41,33 @@
   (setq-hook! 'git-auto-commit-mode-hook
     backup-inhibited t))
 
-(advice-add #'write-abbrev-file :after #'cae-multi-abbrev-push-changes-a)
 (advice-add #'bookmark-set-internal :after #'cae-multi-bookmark-push-changes-a)
 (after! org
   (add-hook 'org-archive-hook #'cae-multi-org-archive-push-changes-h))
+
+;;; Abbrevs
+
+(advice-add #'write-abbrev-file :after #'cae-multi-abbrev-push-changes-a)
+
+(defvar my-abbrev-file-watcher nil
+  "File watcher for the abbrev file.")
+
+(defun my-abbrev-file-watcher-callback (event)
+  "React to changes in the abbrev file by removing the `git-auto-commit-mode' hook."
+  (let ((event-type (nth 1 event))
+        (event-file (nth 2 event)))
+    (when (and (eq event-type 'changed)
+               (file-equal-p event-file abbrev-file-name))
+      (+log "Abbrev file changed, removing git-auto-commit-mode hook"))))
+
+(defun my-setup-abbrev-file-watcher ()
+  "Set up a file watcher for the abbrev file."
+  (when (and abbrev-file-name
+             (file-exists-p abbrev-file-name)
+             (null my-abbrev-file-watcher))
+    (setq my-abbrev-file-watcher
+          (file-notify-add-watch abbrev-file-name
+                                 '(change)
+                                 #'my-abbrev-file-watcher-callback))))
+
+(add-hook 'abbrev-mode-hook #'my-setup-abbrev-file-watcher)
