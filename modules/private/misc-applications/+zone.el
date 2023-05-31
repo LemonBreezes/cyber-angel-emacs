4fd503f207544dcda088527bf85f3e06d4b9e76c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:31 -0400

    modules/private/misc-applications/+zone.el

--- modules/private/misc-applications/+zone.el
+++ modules/private/misc-applications/+zone.el
@@ -7,8 +7,30 @@
         :prefix +misc-applications-eyecandy-prefix
         "z" #'zone-choose)
   :config
-  ;; zone-nyan
-  ;; zone-rainbow
-  ;; zone-pgm-md5
-  ;; zone-pgm-sl
+  ;; remove not interesting programs
+  (setq zone-programs [
+                       zone-nyan
+                       zone-rainbow
+                       zone-pgm-md5
+                       zone-pgm-sl
+                       zone-pgm-jitter
+                       zone-pgm-putz-with-case
+                       zone-pgm-dissolve
+                       ;; zone-pgm-explode
+                       zone-pgm-whack-chars
+                       zone-pgm-rotate
+                       zone-pgm-rotate-LR-lockstep
+                       zone-pgm-rotate-RL-lockstep
+                       zone-pgm-rotate-LR-variable
+                       zone-pgm-rotate-RL-variable
+                       zone-pgm-drip
+
+                       ;; zone-pgm-drip-fretfully
+                       ;; zone-pgm-five-oclock-swan-dive
+                       ;; zone-pgm-martini-swan-dive
+                       zone-pgm-rat-race
+                       zone-pgm-paragraph-spaz])
+  ;; zone-pgm-stress
+  ;; zone-pgm-stress-destress
+  ;; zone-pgm-random-life
   (zone-when-idle (* 5 60)))
