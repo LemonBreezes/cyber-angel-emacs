903b12801d5b3b3d669d7a8950ce65357c6b3495
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:24 -0400

    modules/private/debugger/autoload.el

--- modules/private/debugger/autoload.el
+++ modules/private/debugger/autoload.el
@@ -42,7 +42,7 @@
   (require 'edebug)
   (setq unread-command-events (list ?\C-x ?\C-a))
   (setq which-key-inhibit t)
-  (add-hook 'pre-command-hook #'cae-debugger--which-key-inhibit-hook)
+  ;; (add-hook 'pre-command-hook #'cae-debugger--which-key-inhibit-hook)
   (run-with-idle-timer
    which-key-idle-delay nil
    (lambda ()
