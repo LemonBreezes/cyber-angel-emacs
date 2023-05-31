e5cd85c2d6b62826951b854672dc40ba1f57ca17
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:23 -0400

    modules/private/misc-applications/+elcord.el

--- modules/private/misc-applications/+elcord.el
+++ modules/private/misc-applications/+elcord.el
@@ -13,23 +13,6 @@
         elcord-idle-timer 300
         elcord-idle-message "Going for a walk...")
   (require 'parent-mode)
-  (defadvice! elcord--make-process-for-wsl-a (oldfun)
-    :around #'elcord--make-process
-    (let ((default-directory "~/"))
-      (if (getenv "WSL_DISTRO_NAME")
-          (make-process
-           :name "*elcord-sock*"
-           :command (list
-                     "powershell.exe"
-                     "-NoProfile"
-                     "-ExecutionPolicy" "Bypass"
-                     "-Command" elcord--stdpipe-path "." elcord--discord-ipc-pipe)
-           :connection-type 'pipe
-           :sentinel 'elcord--connection-sentinel
-           :filter 'elcord--connection-filter
-           :noquery t
-           :buffer "*elcord-sock*")
-        (funcall oldfun))))
   (defadvice! +elcord--buffer-boring-p-a (buffer-name)
     :before-until #'elcord--buffer-boring-p
     (or (string-match-p "^\\*\\(doom\\|Messages\\|scratch\\|dashboard\\|elfeed\\|vterm\\|eshell\\|terminal\\|magit\\|help\\|Compile-Log\\|lsp\\|treemacs\\|\\*\\)" buffer-name)
