8e45f66bdd5ff777f7ec32ba9f3dd6810dace221
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:33 -0400

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -148,7 +148,7 @@
      (setq garbage-collection-messages cae-hacks--gc-messages
            cae-hacks--gc-messages nil
            cae-hacks--gcmh-mode nil
-           ;;gc-cons-threshold gcmh-low-cons-threshold
+           gc-cons-threshold gcmh-low-cons-threshold
            gc-cons-percentage cae-hacks-gc-percentage))))
 
 (advice-add #'save-some-buffers :before #'cae-hacks-disable-gc-temporarily)
