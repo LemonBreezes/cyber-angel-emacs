e4998003ac098e032f5f68008cb12a76df2ae821
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:02 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -121,6 +121,8 @@
              parrot-party-on-magit-push t
              parrot-party-on-org-todo-states '("DONE")
              parrot-type 'nyan)
+      (setq parrot-rotate-start-bound-regexp "[\]\[[:space:](){}<>]"
+            parrot-rotate-end-bound-regexp "[\]\[[:space:](){}<>]")
       (advice-remove #'parrot-start-animation #'ignore))
     (add-to-list 'parrot-rotate-dict
                  '(:rot ("add-hook" "remove-hook")))
@@ -131,4 +133,4 @@
     (add-to-list 'parrot-rotate-dict
                  '(:rot ("defadvice!" "undefadvice!")))
     (map! "C-!" #'parrot-rotate-next-word-at-point
-          "C-M-!" #'parrot-rotate-previous-word-at-point)))
+          "C-M-!" #'parrot-rotate-prev-word-at-point)))
