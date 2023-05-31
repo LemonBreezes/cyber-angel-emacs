55f2b1755ed7adcc2dbc2f411863936186c8ebdb
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:16 -0400

    modules/private/meow/autoload.el

--- modules/private/meow/autoload.el
+++ modules/private/meow/autoload.el
@@ -4,3 +4,9 @@
 (defun cae-meow-use-keyboard-layout-a ()
   (setq last-command-event
         (cae-keyboard-remap-reverse last-command-event)))
+
+;;;###autoload
+(defun meow-localleader ()
+  "Enter Doom's localleader keymap."
+  (interactive)
+  (meow--execute-kbd-macro doom-localleader-alt-key))
