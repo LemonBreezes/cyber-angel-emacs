19801600827aec5ae093ecf2b908ea93071ab9f2
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:11 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -19,6 +19,24 @@
                             ((eq response ?d) (diff-buffer-with-file) nil))))))
           (kill-buffer buffer-or-name))))))
 
+;;;###autoload
+(defun cae-kill-buffer-a (orig-func &optional buffer-or-name)
+  (catch 'quit
+    (save-window-excursion
+      (with-current-buffer buffer-or-name
+        (let (done (buf (current-buffer)))
+          (when (and buffer-file-name (buffer-modified-p))
+            (while (not done)
+              (let ((response (read-char-choice
+                               (format "Save file %s? (y, n, d, q) " (buffer-file-name buf))
+                               '(?y ?n ?d ?q))))
+                (setq done (cond
+                            ((eq response ?q) (throw 'quit nil))
+                            ((eq response ?y) (save-buffer) t)
+                            ((eq response ?n) (set-buffer-modified-p nil) t)
+                            ((eq response ?d) (diff-buffer-with-file) nil))))))
+          (apply orig-func (list (current-buffer))))))))
+
 (defun cae-kill-buffer-fixup-windows (buffer)
   (let ((windows (get-buffer-window-list buffer)))
     (cae-kill-buffer buffer)
