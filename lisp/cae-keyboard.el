b8bf3c066abe6fccd20087b6700348660f2903ce
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:10 -0400

    lisp/cae-keyboard.el

--- lisp/cae-keyboard.el
+++ lisp/cae-keyboard.el
@@ -267,18 +267,12 @@
              (,(cae-keyboard-kbd ">") special-lispy-slurp "")
              (,(cae-keyboard-kbd "<") special-lispy-barf "")
              (,(cae-keyboard-kbd ".") special-lispy-repeat "Other")
-             ("h" special-lispy-eval-other-window "Eval")
              ;; The tick and comma keys are special in Lispy because we want
              ;; them to not be rebound.
 
              ;; We have to find two free keys to bind to the tick and comma
              ;; commands.
-             ;;,((let (keys)
-             ;;    (when (eq (length (cl-find ?,
-             ;;                               cae-keyboard-orbits
-             ;;                               :test (lambda (x y) (cl-find x y))))
-             ;;              2)
-             ;;      (pus ))))
+             ,@()
              (,(cae-keyboard-kbd-reverse ",")
               ,(lookup-key cae-keyboard--lispy-mode-map-backup
                            (cae-keyboard-kbd-reverse ","))
@@ -297,6 +291,22 @@
              ("_" special-lispy-underscore "")
              ("'" lispy-tick "")
              ("," nil ""))))
+      (let ((comma-orbit (cl-find ?,
+                                  cae-keyboard-orbits
+                                  :test (lambda (x y) (cl-find x y))))
+            (tick-orbit (cl-find ?'
+                                 cae-keyboard-orbits
+                                 :test (lambda (x y) (cl-find x y))))
+            (semicolon-orbit (cl-find ?\;
+                                      cae-keyboard-orbits
+                                      :test (lambda (x y) (cl-find x y))))
+            keys)
+        (cl-loop for orbit in (list comma-orbit tick-orbit semicolon-orbit)
+                 for key = (cl-find-if (lambda (x)
+                                         (not (cl-find x bindings)))
+                                       orbit)
+                 do (push key keys))
+        (+log "HIII" keys))
       (dolist (binding bindings)
         (define-key lispy-mode-map (car binding) (cadr binding)))
       (eval
