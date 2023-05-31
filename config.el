c6f16ca6065be392c3d8fa0e8afaffd6848473b8
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:58 -0400

    config.el

--- config.el
+++ config.el
@@ -251,8 +251,8 @@
       (setq ccls-initialization-options '(:index (:comments 2)
                                           :completion (:detailedLabel t)
                                           :trackDependency 1
-                                          :threads ,(max 1 (/ (doom-system-cpus) 2))))
-      (set-lsp-priority! 'ccls 2))
+                                          :threads ,(max 1 (/ (doom-system-cpus) 2)))))
+    (add-to-list 'lsp-disabled-clients 'clangd)
     (add-to-list 'lsp-disabled-clients 'mspyls)))
 
 
