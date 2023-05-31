8521c8ec913c70e26d0fa44056fcba51e255bedf
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:07 -0400

    lisp/cae-project.el

--- lisp/cae-project.el
+++ lisp/cae-project.el
@@ -5,7 +5,7 @@
 ;; TODO handle bookmark bmenu
 ;; TODO handle the fringe indicator
 
-(defvar cae-project-bookmark-dir (concat doom-cache-dir "cae-project-bookmarks/")
+(defvar cae-project-bookmark-dir (concat cae-multi-secrets-dir "cae-project-bookmarks/")
   "Directory to store project bookmarks.")
 
 (defvar cae-project-bookmark-cache (make-hash-table :test 'equal)
