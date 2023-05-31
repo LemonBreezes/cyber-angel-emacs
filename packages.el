5bd83d36f8939c92b9664057b9bcd1af9040d602
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:37 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -31,6 +31,7 @@
 (package! w3m)
 (package! ace-link)
 (package! pdftotext :recipe (:host github :repo "tecosaur/pdftotext.el"))
+(package! detached)
 
 ;; These are used for their autoloads.
 (package! crontab-mode)
