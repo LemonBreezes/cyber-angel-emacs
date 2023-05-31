7e4b9492a5a923194b132ee43e838b49d0c0ec7b
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:59 -0400

    modules/private/debugger/autoload.el

---
+++ modules/private/debugger/autoload.el
@@ -0,0 +1,10 @@
+;;; private/debugger/autoload.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun cae-debugger-quit-session-a ()
+  (ignore-errors
+        (let ((ignore-window-parameters t))
+          (cl-loop for buf being the buffers
+                   when (string-match-p "gdb" (buffer-name buf)) do
+                   (cae-hacks-always-yes-a #'doom-kill-buffer-and-windows buf)))
+        (hydra-keyboard-quit)))
