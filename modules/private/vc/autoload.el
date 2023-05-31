ac30c0e18dc4788e68fb8bc2a86ee4d57abdfdf8
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:43 -0400

    modules/private/vc/autoload.el

--- modules/private/vc/autoload.el
+++ modules/private/vc/autoload.el
@@ -3,7 +3,12 @@
 ;;;###autoload
 (defun cae-magit-insert-diff-upstream (&optional branch)
   "From
- https://www.reddit.com/r/emacs/comments/112t0uo/comment/j9g2r5n/?utm_source=share&utm_medium=web2x&context=3."
+ https://www.reddit.com/r/emacs/comments/112t0uo/comment/j9g2r5n/?utm_source=share&utm_medium=web2x&context=3.
+
+Meant to be used like:
+(magit-add-section-hook 'magit-status-sections-hook
+                        'cae-magit-insert-diff-upstream
+                        'magit-insert-stashes)"
   (magit-insert-section (diff-upstream nil t)
     (if-let ((upstream-branch (magit-get-upstream-branch (or branch (magit-get-current-branch)))))
         (progn
