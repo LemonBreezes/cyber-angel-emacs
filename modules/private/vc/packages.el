443e854b32cfbe032a434ae0e2aecbde47f132b0
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:47 -0400

    modules/private/vc/packages.el

--- modules/private/vc/packages.el
+++ modules/private/vc/packages.el
@@ -4,6 +4,7 @@
 (package! git-link)
 (package! gitignore-templates)
 (package! git-email :recipe (:repo "git@git.sr.ht:~lemon_breezes/git-email"))
+(package! git-email :recipe (:local-repo "packages/git-email"))
 (package! vc-backup :recipe (:repo "LemonBreezes/vc-backup"))
 (package! magit-stats)
 (package! embark-vc)
