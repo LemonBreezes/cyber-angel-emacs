e985b5478767e6aaa18e19144582f07e2a470dfd
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:25 -0400

    config.el

--- config.el
+++ config.el
@@ -492,9 +492,8 @@
     (load! "lisp/cae-evil"))
 
   (when (modulep! :editor fold)
-    ;;(add-hook 'doom-first-file-hook #'vimish-fold-global-mode)
-    ;;(setq vimish-fold-indication-mode 'right-fringe)
-    )
+    (add-hook 'doom-first-file-hook #'vimish-fold-global-mode)
+    (setq vimish-fold-indication-mode 'right-fringe))
 
   (autoload 'cae-project-bookmark (concat doom-private-dir
                                           "lisp/cae-project-bookmark"))
