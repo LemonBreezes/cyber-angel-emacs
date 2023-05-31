85e65e974a7a0ff81a5e5aa3db0c0a89b42f6e54
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:05 -0400

    init.el

--- init.el
+++ init.el
@@ -23,86 +23,86 @@
       doom-localleader-key "C-c l")
 (setq native-comp-async-jobs-number (num-processors))
 
-;;(doom! :completion
-;;       (vertico +icons)
-;;
-;;       :ui
-;;       doom-dashboard
-;;       hl-todo
-;;       hydra
-;;       ;; (ligatures +extra +iosevka)
-;;       nav-flash
-;;       (popup +defaults)
-;;       (vc-gutter +pretty +diff-hl)
-;;       vi-tilde-fringe
-;;       workspaces
-;;       ;; (window-select +switch-window)
-;;
-;;       :editor
-;;       file-templates
-;;       format
-;;       snippets
-;;       multiple-cursors
-;;       lispy
-;;
-;;       :emacs
-;;       undo
-;;       ibuffer
-;;       vc
-;;
-;;       :term
-;;       eshell
-;;       ;; (:if (not (memq system-type '(cygwin windows-nt ms-dos))) vterm)
-;;
-;;       :checkers
-;;       (syntax +childframe)
-;;       spell
-;;       grammar
-;;
-;;       :tools
-;;       (debugger +lsp)
-;;       direnv
-;;       editorconfig
-;;       (eval +overlay)
-;;       (lookup +dictionary +docsets +offline)
-;;       (magit +forge)
-;;       (lsp +peek)
-;;       tree-sitter
-;;
-;;       :lang
-;;       (emacs-lisp +tree-sitter)
-;;       (org +roam2 +tree-sitter)
-;;       (cc +lsp +tree-sitter)
-;;       (web +lsp +tree-sitter)
-;;       (sh +lsp +tree-sitter)
-;;       (nix +lsp +tree-sitter)
-;;
-;;       :os
-;;       (:if IS-MAC macos)
-;;       (:if (not (memq system-type '(cygwin windows-nt ms-dos))) tty +osc)
-;;
-;;       :config
-;;       (default +bindings +smartparens)
-;;
-;;       :email
-;;       ;; (mu4e +org)
-;;
-;;       :app
-;;       (rss +org)
-;;
-;;       :private
-;;       (corfu +indexed)
-;;       debugger
-;;       (dirvish +icons +dirvish)
-;;       eshell
-;;       dired
-;;       lisp
-;;       misc-applications
-;;       modeline
-;;       ;; (meow +leader)
-;;       unpackaged
-;;       vc
-;;       )
+(doom! :completion
+       (vertico +icons)
+
+       :ui
+       doom-dashboard
+       hl-todo
+       hydra
+       ;; (ligatures +extra +iosevka)
+       nav-flash
+       (popup +defaults)
+       (vc-gutter +pretty +diff-hl)
+       vi-tilde-fringe
+       workspaces
+       ;; (window-select +switch-window)
+
+       :editor
+       file-templates
+       format
+       snippets
+       multiple-cursors
+       lispy
+
+       :emacs
+       undo
+       ibuffer
+       vc
+
+       :term
+       eshell
+       ;; (:if (not (memq system-type '(cygwin windows-nt ms-dos))) vterm)
+
+       :checkers
+       (syntax +childframe)
+       spell
+       grammar
+
+       :tools
+       (debugger +lsp)
+       direnv
+       editorconfig
+       (eval +overlay)
+       (lookup +dictionary +docsets +offline)
+       (magit +forge)
+       (lsp +peek)
+       tree-sitter
+
+       :lang
+       (emacs-lisp +tree-sitter)
+       (org +roam2 +tree-sitter)
+       (cc +lsp +tree-sitter)
+       (web +lsp +tree-sitter)
+       (sh +lsp +tree-sitter)
+       (nix +lsp +tree-sitter)
+
+       :os
+       (:if IS-MAC macos)
+       (:if (not (memq system-type '(cygwin windows-nt ms-dos))) tty +osc)
+
+       :config
+       ;(default +bindings +smartparens)
+
+       :email
+       ;; (mu4e +org)
+
+       :app
+       (rss +org)
+
+       :private
+       (corfu +indexed)
+       debugger
+       (dirvish +icons +dirvish)
+       eshell
+       dired
+       lisp
+       misc-applications
+       modeline
+       ;; (meow +leader)
+       unpackaged
+       vc
+       )
 
 ;; Local Variables:
 ;; eval: (when (featurep 'lispy) (lispy-mode -1))
