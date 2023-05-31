f64f9febe13ac5e20916110a1c1cb1388145f291
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:18 -0400

    config.el

--- config.el
+++ config.el
@@ -513,9 +513,7 @@
 (use-package! aggressive-indent
   :init
   (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode) ;See my `lisp' module.
-  (unless (and (modulep! :tools lsp)
-               (not (modulep! :tools lsp +eglot)))
-    (add-hook 'c-mode-common-hook #'aggressive-indent-mode))
+  (add-hook 'c-mode-common-hook #'aggressive-indent-mode)
   :defer t)
 
 (use-package! hungry-delete
