b187fa25c3a6507bf20c5794d724e89996c23e0b
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:15 -0400

    init.el

--- init.el
+++ init.el
@@ -118,7 +118,7 @@ overriding other keymaps."
            (rss +org)
 
            :private
-           (corfu +indexed)
+           (corfu +numbers)
            debugger
            (dirvish +icons +dirvish)
            eshell
