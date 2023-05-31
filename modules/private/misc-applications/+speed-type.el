b80d2fca8a6f0b2d1f2603da27a7da2f4e9acc4c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:48 -0400

    modules/private/misc-applications/+speed-type.el

--- modules/private/misc-applications/+speed-type.el
+++ modules/private/misc-applications/+speed-type.el
@@ -7,6 +7,7 @@
         :prefix +misc-applications-games-prefix
         "T" #'speed-type-text)
   :config
+  (add-hook 'speed-type-mode-hook #'visual-line-mode)
   (map! :map speed-type--completed-keymap
         "q" #'kill-this-buffer
         "r" #'speed-type--replay
