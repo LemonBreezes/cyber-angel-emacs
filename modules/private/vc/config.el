6a3fe99f23dd80d5d255b34a8eabcea7a38dea19
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:25 -0400

    modules/private/vc/config.el

--- modules/private/vc/config.el
+++ modules/private/vc/config.el
@@ -56,7 +56,6 @@
                                             (advice-add #'git-link--new :override #'identity)
                                             (call-interactively #'git-link-homepage)
                                           (advice-remove #'git-link--new #'identity))
-                               :cache t
                                :face font-lock-string-face
                                :bind "g")))
         (cl-pushnew info-handler (nthcdr (1+ pos) file-info-handlers)
