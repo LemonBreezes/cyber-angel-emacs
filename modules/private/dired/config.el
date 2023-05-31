a65ae0514784ae381da9de21300108cd65d522c8
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:10 -0400

    modules/private/dired/config.el

--- modules/private/dired/config.el
+++ modules/private/dired/config.el
@@ -108,7 +108,7 @@
         [remap dirvish-emerge-menu] #'cae-dired-dirvish-emerge-menu
         "v" #'dirvish-vc-menu
         "h" #'dirvish-history-jump
-        "N" #'dirvish-narrow            ;Redundant with `M-n'.
+        ;; "N" #'dirvish-narrow            ;Redundant with `M-n'.
         "j" #'+default/search-buffer
         [remap previous-buffer] #'cae-dired-previous-buffer
         [remap next-buffer] #'cae-dired-next-buffer
