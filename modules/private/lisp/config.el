38c0b931cece1945287dea8c18185ee7946c2a5a
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:34 -0400

    modules/private/lisp/config.el

--- modules/private/lisp/config.el
+++ modules/private/lisp/config.el
@@ -138,12 +138,12 @@
         (define-key lispy-mode-map (kbd "<f6>") #'cae-lispy-cheat-sheet/body)))
     (map! :map lispy-mode-map-lispy
           "M-m" nil                  ; formerly `lispy-mark-symbol'. I prefer to
-                                        ; keep the default `M-m' and `C-a'
-                                        ; keybindings.
-          "C-a" nil                   ; formerly `lispy-move-beginning-of-line'.
-          "C-e" nil                   ; formerly `lispy-move-end-of-line'.
-                                        ; Doom's default `C-e' is better.
-          "M-j" nil                     ; formerly `lisp-split'.
+                                     ; keep the default `M-m' and `C-a'
+                                     ; keybindings.
+          "C-a" nil                  ; formerly `lispy-move-beginning-of-line'.
+          "C-e" nil                  ; formerly `lispy-move-end-of-line'.
+                                     ; Doom's default `C-e' is better.
+          "M-j" nil                  ; formerly `lisp-split'.
           "M-r" #'lispy-raise-sexp
           "M-R" #'lispy-raise-some
           "M-S" #'lispy-split)
