b203bc778b2c4b6a469feb33542c35ac12ee6bc8
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:12 -0400

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -48,7 +48,8 @@
            modus-themes-bold-constructs nil
            modus-themes-variable-pitch-ui t
            modus-themes-mixed-fonts t
-           modus-themes-prompts '(italic semibold))))
+           modus-themes-prompts '(italic semibold)
+           modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))))
 (after! ef-themes
   (setq! ef-themes-variable-pitch-ui t
           ef-themes-mixed-fonts t
