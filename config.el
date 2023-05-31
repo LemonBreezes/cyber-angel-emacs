e18eb58c012cd21581e5d29cadc385e6cf083c19
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:27 -0400

    config.el

--- config.el
+++ config.el
@@ -291,8 +291,8 @@
 (define-key (current-global-map)
   [remap shell-command] 'with-editor-shell-command)
 (add-hook 'shell-mode-hook  #'with-editor-export-editor)
-(advice-add #'with-editor-export-editor :around #'cae-hacks-shut-up-a)
 (add-hook 'eshell-mode-hook #'with-editor-export-editor)
+(advice-add #'with-editor-export-editor :around #'cae-hacks-shut-up-a)
 
 (when (and (modulep! :checkers spell)
            (not (modulep! :checkers spell +flyspell)))
