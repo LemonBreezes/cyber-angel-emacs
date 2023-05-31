609fc42a703c95ef18992da0afd0e59c3fe6613a
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:13 -0400

    config.el

--- config.el
+++ config.el
@@ -190,6 +190,8 @@
       :side 'right :select nil :quit t :ttl nil)
     (set-popup-rule! "^\\*tldr\\*$" :size #'cae-popup-resize-help-buffer
       :side 'right :ttl t :select t :quit t :ttl 0) ; which slot/vslot?
+    (set-popup-rule! "^\\*Shortdoc .*" :size #'cae-popup-resize-help-buffer
+      :side 'right :ttl t :select t :quit t :ttl 0) ; which slot/vslot?
     (after! embark
       (set-popup-rule! (regexp-quote embark--verbose-indicator-buffer)
         :size #'+popup-shrink-to-fit :side 'bottom :ttl t))
