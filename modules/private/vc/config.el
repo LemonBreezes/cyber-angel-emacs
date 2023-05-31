c6b988667e355a8028395c1324b65664d915cf89
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:19 -0400

    modules/private/vc/config.el

--- modules/private/vc/config.el
+++ modules/private/vc/config.el
@@ -88,10 +88,9 @@
 
 (use-package! gitignore-templates
   :defer t :init
-  (let ((vc-prefix (if (modulep! :editor evil) "g" "v")))
-    (map! :leader
-          :prefix vc-prefix
-          "I" #'gitignore-templates-insert)))
+  (map! :leader
+        :prefix "i"
+        "I" #'gitignore-templates-insert))
 
 (use-package! git-email
   :defer t :init
