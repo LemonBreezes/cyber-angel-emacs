2b22a83a8a6d013b61ca8ae658c6d48e402185fc
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:06 -0400

    modules/private/dired/config.el

--- modules/private/dired/config.el
+++ modules/private/dired/config.el
@@ -116,7 +116,6 @@
   (map! [remap previous-buffer] #'cae-dired-previous-buffer
         [remap next-buffer] #'cae-dired-next-buffer))
 
-
 ;; TODO Patch these for the behavior I want. Turn them into `:before' advices
 ;; and use hooks if necessary.
 ;; Exit the current Dirvish session when running interactive commands like
