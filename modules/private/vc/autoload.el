79d9cee9d602a12bf04ba8867f937ca9916f9de3
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:12 -0400

    modules/private/vc/autoload.el

--- modules/private/vc/autoload.el
+++ modules/private/vc/autoload.el
@@ -21,7 +21,7 @@ Meant to be used like:
   (apply-partially #'cae-magit-insert-diff-upstream "master"))
 
 ;;;###autoload (autoload 'cae-magit-forge-post-hydra/body "private/vc/autoload" nil t)
-(defhydra cae-magit-forge-post-hydra (:color blue)
+(defhydra cae-magit-forge-post-hydra (:color pink)
   ("<f6>" nil "Exit" :exit t)
   ("C-c C-c" forge-post-submit "Submit")
   ("C-c C-e" forge-post-dispatch  "Dispatch")
