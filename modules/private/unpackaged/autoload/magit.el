4602d65f6aa08cff554d15c7e7539d92c2e8932f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:55 -0400

    modules/private/unpackaged/autoload/magit.el

---
+++ modules/private/unpackaged/autoload/magit.el
@@ -0,0 +1,39 @@
+;;; private/unpackaged/autoload/magit.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun cae-unpackaged-magit-status ()
+  "Open a `magit-status' buffer and close the other window so only Magit is visible.
+If a file was visited in the buffer that was active when this
+command was called, go to its unstaged changes section."
+  (interactive)
+  (let* ((buffer-file-path (when buffer-file-name
+                             (file-relative-name buffer-file-name
+                                                 (locate-dominating-file buffer-file-name ".git"))))
+         (section-ident `((file . ,buffer-file-path) (unstaged) (status))))
+    (call-interactively #'magit-status)
+    (delete-other-windows)
+    (when buffer-file-path
+      (goto-char (point-min))
+      (cl-loop until (when (equal section-ident (magit-section-ident (magit-current-section)))
+                       (magit-section-show (magit-current-section))
+                       (recenter)
+                       t)
+               do (condition-case nil
+                      (magit-section-forward)
+                    (error (cl-return (magit-status-goto-initial-section-1))))))))
+
+;;;###autoload
+(defun cae-unpackaged-magit-save-buffer-show-status ()
+  "Save buffer and show its changes in `magit-status'."
+  (interactive)
+  (save-buffer)
+  (cae-unpackaged-magit-status))
+
+;; Do not jump to changes if they have been automatically commited.
+(defun cae-unpackaged-magit-status-disable-when-gac-enabled-a ()
+  (and (eq this-command 'unpackaged/magit-save-buffer-show-status)
+       (or (bound-and-true-p git-auto-commit-mode)
+           (not (doom-project-root))
+           (not (magit-git-repo-p (doom-project-root)))
+           (not (buffer-file-name))
+           (not (magit-file-tracked-p (buffer-file-name))))))
