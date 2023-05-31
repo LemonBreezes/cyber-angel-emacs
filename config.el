3cd2a270e9a55019406dfbc4e1c8a486ba6e9eda
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:12 -0400

    config.el

--- config.el
+++ config.el
@@ -408,8 +408,7 @@
         electric-pair-inhibit-predicate #'electric-pair-conservative-inhibit
         electric-pair-open-newline-between-pairs t)
   (electric-pair-mode +1)
-  (map! [remap newline] nil)
-  (electric-indent-mode +1))
+  (map! [remap newline] nil))
 
 ;; Hide commands in M-x which do not work in the current mode. Vertico commands
 ;; are hidden in normal buffers.
