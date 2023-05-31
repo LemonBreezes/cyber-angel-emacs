9d86234f6c956caabf0c5b8e67fe7b251a3674f6
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:20 -0400

    config.el

--- config.el
+++ config.el
@@ -773,7 +773,8 @@
 
   (use-package! titlecase
     :defer t :init
-    (map! "M-U" #'cae-titlecase-word-dwim)
+    (map! "M-U" #'cae-titlecase-word-dwim
+          "C-x M-u" #'titlecase-dwim)
     (after! embark
       (define-key embark-region-map "T" #'titlecase-region)
       (define-key embark-heading-map "T" #'titlecase-line)
