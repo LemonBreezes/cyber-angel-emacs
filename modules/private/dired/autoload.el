078d11404fcbaf36cd25fb153bb5684b439293da
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:06 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -34,35 +34,35 @@
 ;;  (when (and (derived-mode-p 'dired-mode)
 ;;             (window-dedicated-p))
 ;;    (dirvish-quit)))
-
-(defun cae-dired-switch-buffer--handle-dirvish (fn)
-  (when (and (derived-mode-p 'dired-mode)
-             (window-dedicated-p))
-    (dirvish-layout-toggle))
-  (call-interactively fn)
-  (when (and (derived-mode-p 'dired-mode)
-             (one-window-p)
-             (not (window-dedicated-p)))
-    (ignore-error user-error
-      (dirvish-layout-switch dirvish-default-layout))))
-
-;;;###autoload
-(defun cae-dired-previous-buffer ()
-  (interactive)
-  (cae-dired-switch-buffer--handle-dirvish #'previous-buffer))
-
-;;;###autoload
-(defun cae-dired-next-buffer ()
-  (interactive)
-  (cae-dired-switch-buffer--handle-dirvish #'next-buffer))
-
-;;;###autoload
-(defun cae-dired-jump ()
-  (interactive)
-  (call-interactively #'dired-jump)
-  (when (one-window-p)
-    (ignore-error user-error
-      (dirvish-layout-switch dirvish-default-layout))))
+;;
+;;(defun cae-dired-switch-buffer--handle-dirvish (fn)
+;;  (when (and (derived-mode-p 'dired-mode)
+;;             (window-dedicated-p))
+;;    (dirvish-layout-toggle))
+;;  (call-interactively fn)
+;;  (when (and (derived-mode-p 'dired-mode)
+;;             (one-window-p)
+;;             (not (window-dedicated-p)))
+;;    (ignore-error user-error
+;;      (dirvish-layout-switch dirvish-default-layout))))
+;;
+;;;;;###autoload
+;;(defun cae-dired-previous-buffer ()
+;;  (interactive)
+;;  (cae-dired-switch-buffer--handle-dirvish #'previous-buffer))
+;;
+;;;;;###autoload
+;;(defun cae-dired-next-buffer ()
+;;  (interactive)
+;;  (cae-dired-switch-buffer--handle-dirvish #'next-buffer))
+;;
+;;;;;###autoload
+;;(defun cae-dired-jump ()
+;;  (interactive)
+;;  (call-interactively #'dired-jump)
+;;  (when (one-window-p)
+;;    (ignore-error user-error
+;;      (dirvish-layout-switch dirvish-default-layout))))
 
 ;;;###autoload
 (defun cae-dired-maximize-buffer ()
