bddc104417fb0a9e6859c08411ccb5f9b8290217
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:48 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -178,7 +178,7 @@
 ;; This block might seem crazy, but it's how I've gotten parrot mode to work
 ;; and be silent on startup.
 (use-package! parrot
-  :defer-incrementally t
+  :defer-incrementally t                ;
   :after (:any magit org)
   :after-call gac-push
   :config
