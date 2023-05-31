12c093af65637cc733524ab77d19e82aa7e9a37c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:27 -0400

    lisp/cae-restore-point.el

--- lisp/cae-restore-point.el
+++ lisp/cae-restore-point.el
@@ -44,7 +44,8 @@
                   View-scroll-half-page-forward
                   View-scroll-half-page-backward
                   chatgpt-shell-mark-at-point-dwim
-                  c-mark-function))
+                  c-mark-function
+                  mark-whole-buffer))
       (add-to-list 'rp/restore-point-commands fn))
     (advice-add #'rp/restore-point-position :after #'deactivate-mark)
     ;; Restore point in the minibuffer.
