b67e5c9e4df4e550b9c123ab87e30dd231d058d0
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:54 -0400

    autoload/cae-compile.el

--- autoload/cae-compile.el
+++ autoload/cae-compile.el
@@ -56,3 +56,17 @@
                      (string= (file-name-nondirectory s) ".local")
                      (string= (file-name-nondirectory s) ".git")
                      (string= (file-name-nondirectory s) "shared-local"))))))))
+
+;;;###autoload
+(defun +straight-rebuild-package-maybe ()
+  (when (and (stringp (buffer-file-name))
+             (stringp (file-name-directory (buffer-file-name)))
+             (string-prefix-p (expand-file-name ".local/straight/" doom-emacs-dir)
+                              (file-name-directory (buffer-file-name))))
+    (require 'straight)
+    (when-let (package (straight--determine-repo buffer-file-name))
+      (mapc #'delete-file (directory-files (file-name-directory
+                                            (buffer-file-name))
+                                           nil
+                                           "flycheck_.*"))
+      (straight-rebuild-package package))))
