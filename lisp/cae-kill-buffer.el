4a4cdaeb68cefeec3baa7597de66cafe6d151d02
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:11 -0400

    .

--- lisp/cae-kill-buffer.el
+++
@@ -1,8 +0,0 @@
-;;; lisp/cae-kill-buffer.el -*- lexical-binding: t; -*-
-
-;; My kill buffer improvements adapted from this stackoverflow answer:
-;; https://emacs.stackexchange.com/questions/3245/kill-buffer-prompt-with-option-to-diff-the-changes
-
-
-
-(advice-add #'doom-kill-buffer-fixup-windows :override #'cae-kill-buffer-fixup-windows)
