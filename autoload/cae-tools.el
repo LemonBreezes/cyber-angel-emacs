fac663e6c6a1d0c2c44f5ce9b6535feb30f7bec7
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:31 -0400

    autoload/cae-tools.el

---
+++ autoload/cae-tools.el
@@ -0,0 +1,23 @@
+;;; autoload/cae-tools.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun ping-status ()
+  (interactive)
+  (let ((buffer (generate-new-buffer "*internet*")))
+    (make-process
+     :name "internet"
+     :connection-type 'pipe
+     :buffer buffer
+     :command (list "ping" "-q" "-c" "1" "8.8.8.8")
+     :sentinel `(lambda (p e)
+                  (with-current-buffer ',buffer
+                    (goto-char (point-min))
+                    (if (or (search-forward "unreachable" nil t)
+                            (search-forward "errors" nil t))
+                        (progn (unless (string-match "ping-status" (format "%s" timer-list))
+                                 (run-with-timer 5 5 'ping-status)
+                                 (alert "no internet")))
+                      (when (string-match "ping-status" (format "%s" timer-list))
+                        (cancel-function-timers 'ping-status))
+                      (alert "internet working"))
+                    (kill-buffer))))))
