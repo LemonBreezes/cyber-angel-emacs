814c980f28fbf71d72ae4712e8e62eff016e1ff1
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:48 -0400

    config.el

--- config.el
+++ config.el
@@ -508,6 +508,7 @@
 (setq kill-buffer-query-functions
       (remq 'process-kill-buffer-query-function
             kill-buffer-query-functions))
+;;
 
 ;; Bind `tab-bar' commands consistently with the built-in keybindings.
 (defadvice! cae-tab-bar-define-keys-a ()
