c5f158100b4562e4f7573d4add626781405585f3
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:40 -0400

    config.el

--- config.el
+++ config.el
@@ -489,6 +489,9 @@
       "C-S-i" #'doom/dumb-dedent
       [remap previous-buffer] #'cae-previous-buffer
       [remap next-buffer] #'cae-next-buffer
+      (:map minibuffer-local-map
+       "C-;" nil
+       "C-s" nil)
       (:when (modulep! :completion vertico)
        [remap apropos] nil
                                         ;`consult-apropos' is obsolete.
