250c3a01d8f71103efc6f977eaced7683868f4bf
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:06 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -36,6 +36,35 @@
 ;;    (dirvish-quit))
 ;;  (apply oldfun args))
 
+
+;;;###autoload
+(defun cae-dired-find-file-a (file &optional wildcards)
+  "Like `find-file', but might exit the current Dirvish session."
+  (if (derived-mode-p 'dired-mode)
+      (progn
+        (when-let ((dir (file-name-directory file)))
+          (unless (file-equal-p dir default-directory)
+            (advice-remove #'find-file #'cae-dired-find-file-a)
+            (unwind-protect
+                (find-file oldfun dir)
+              (advice-add #'find-file :before #'cae-dired-find-file-a))))
+        (unless (file-directory-p file)
+          ;; Copied from `dirvish-find-entry-a'
+          (let* ((dv (dirvish-curr)) (fn (nth 4 (dv-type dv))))
+            (if fn (funcall fn) (dirvish-kill dv)))))))
+
+(defun cae-dired-find-file-h ()
+  (when (and (derived-mode-p 'dired-mode)
+             (one-window-p))
+    (ignore-error user-error
+      (dirvish-layout-switch dirvish-default-layout))))
+
+(defun cae-dired-find-file-other-window-a (&rest _)
+  (when (and (derived-mode-p 'dired-mode)
+             (window-dedicated-p))
+    (dirvish-quit)))
+
+
 (defun cae-dired-switch-buffer--handle-dirvish (fn)
   (when (and (derived-mode-p 'dired-mode)
              (window-dedicated-p))
