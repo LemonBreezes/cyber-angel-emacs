a1ca7bf7a9e612b4869a741a3027207b6a8078f4
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:49 -0400

    modules/private/misc-applications/autoload/somafm.el

--- modules/private/misc-applications/autoload/somafm.el
+++ modules/private/misc-applications/autoload/somafm.el
@@ -1,7 +1,7 @@
 ;;; private/misc-applications/autoload/somafm.el -*- lexical-binding: t; -*-
 
-;;;###autoload
-(defun somafm-mode-line-indicator ()
-  "Return a formatted string of the current song."
-  (when somafm-current-song
-    (list (somafm--format-current-song somafm-current-song) " ")))
+(defun +somafm ()
+  (interactive)
+  (if (display-graphic-p)
+      (call-interactively #'somafm)
+    (call-interactively #'somafm-by-completion)))
