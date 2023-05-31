7131c9dff33a76107dc02adc074f182b0a859611
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:56 -0400

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -2,30 +2,24 @@
 
 ;; This function is like `doom-shut-up-a' but does not write messages to the
 ;; messages buffer.
-(defun my-doom-shut-up-a (oldfun &rest args)
+(defun cae-hacks-doom-shut-up-a (oldfun &rest args)
   (advice-add #'message :override #'ignore)
   (unwind-protect (quiet! (apply oldfun args))
     (advice-remove #'message #'ignore)))
 
 ;; Prevent the minibuffer from "glitching" the workspace switch.
-(defadvice! +workspace/switch-to-ignore-minibuffer-a (&rest _)
+(defadvice! cae-hacks-workspace-ignore-minibuffer-a (&rest _)
   :before-until #'+workspace/switch-to
   (minibuffer-window-active-p (selected-window)))
 
 ;; Prevent hydras from remaining active when switching workspaces.
 (after! (:all persp-mode hydra)
-  (defun +hydra-quit-a (&rest _)
+  (defun cae-hacks-hydar-quit-h (&rest _)
     (hydra-keyboard-quit))
-  (add-hook 'persp-before-switch-functions #'+hydra-quit-a))
-
-;; Prevent hydras from glitching Switch Window out
-(after! hydra
-  (defadvice! +switch-window--list-a (&rest _)
-    :before #'switch-window--list
-    (hydra-keyboard-quit)))
+  (add-hook 'persp-before-switch-functions #'cae-hacks-hydar-quit-h))
 
 ;; Make `advice-remove' ignore the keyword argument
-(defadvice! advice-remove--ignore-keyword-args (args)
+(defadvice! cae-hacks-advice-remove-ignore-keyword-args-a (args)
   :filter-args #'advice-remove
   (if (keywordp (nth 1 args))
       (list (nth 0 args) (nth 2 args))
@@ -33,7 +27,8 @@
 
 ;; Use --no-sandbox when running Chromium, Discord, etc. as the root user.
 (when (eq (user-uid) 0)
-  (defun +call-process-shell-command-a (args)
+  (defadvice! cae-hacks-call-process-shell-command-a (args)
+    :filter-args #'call-process-shell-command
     (when (cl-member (file-name-base (car (split-string (car args) " ")))
                      '("chromium-bin-browser"
                        "chromium-bin"
@@ -45,9 +40,7 @@
       (setf (car args)
             (concat (string-trim-right (car args))
                     " --no-sandbox")))
-    args)
-  (advice-add #'call-process-shell-command :filter-args
-              #'+call-process-shell-command-a))
+    args))
 
 ;; Call `pp-eval-last-sexp' when `eros-eval-last-sexp' is called with a negative
 ;; prefix argument
