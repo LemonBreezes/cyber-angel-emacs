44704d847dd0453689e94e52a82da301533d818f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:31 -0400

    config.el

--- config.el
+++ config.el
@@ -459,11 +459,6 @@
        :n "o" #'ace-link-man))
 (define-key resize-window-repeat-map "_" #'shrink-window)
 
-;; https://github.com/doomemacs/doomemacs/issues/6940
-;; C-x p is used by project-prefix-map instead of +popup/other #6940
-(after! project
-  (map! [remap project-prefix-map] #'+popup/other))
-
 (map! :leader
       :desc "Copy link" "sy" #'link-hint-copy-link)
 
