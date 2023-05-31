a22aae1d6d61e44ff0f249be2356bf515b0eec43
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:54 -0400

    config.el

--- config.el
+++ config.el
@@ -1065,7 +1065,6 @@
         :desc "Mark previous sexps"             "C-M-b" #'mc/mark-previous-sexps
         :desc "Mark regexp"                     "%"     #'mc/mark-all-in-region-regexp)
   (after! multiple-cursors-core
-    (add-to-list 'mc/cmds-to-run-once #'read-only-mode)
     (dolist (cmd '(doom/delete-backward-word
                    doom/forward-to-last-non-comment-or-eol
                    mark-sexp
@@ -1073,6 +1072,21 @@
                    eval-last-sexp
                    cae-eval-last-sexp))
       (add-to-list 'mc/cmds-to-run-for-all cmd))
+    (dolist (cmd '(+workspace/new +workspace/load +workspace/save
+                   +workspace/cycle +workspace/other +workspace/delete
+                   +workspace/rename +workspace/display +workspace/new-named
+                   +workspace/swap-left +workspace/switch-to
+                   +workspace/swap-right +workspace/switch-left
+                   +workspace/switch-to-0 +workspace/switch-to-1
+                   +workspace/switch-to-2 +workspace/switch-to-3
+                   +workspace/switch-to-4 +workspace/switch-to-5
+                   +workspace/switch-to-6 +workspace/switch-to-7
+                   +workspace/switch-to-8 +workspace/kill-session
+                   +workspace/switch-right +workspace/switch-to-final
+                   +workspace/restore-last-session +workspace/kill-session-and-quit
+                   +workspace/close-window-or-workspace
+                   read-only-mode)))
+    (add-hook 'persp-before-switch-functions #'mc/freeze-fake-cursors)
     (add-to-list 'mc/unsupported-minor-modes #'cae-completion-mode)
     (define-key mc/keymap (kbd "C-. .")     #'mc/move-to-column)
     (define-key mc/keymap (kbd "C-. =")     #'mc/compare-chars)
