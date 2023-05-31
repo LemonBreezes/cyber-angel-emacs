7f2e1f8e926d98d7a80907c05cf3ec8d160c5788
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:51 -0400

    modules/private/lisp/autoload.el

--- modules/private/lisp/autoload.el
+++ modules/private/lisp/autoload.el
@@ -7,20 +7,21 @@
 ;;;###autoload
 (defun cae-lisp-newline-and-indent ()
   (interactive)
-  (if (and (minibufferp)
-           (string= (minibuffer-prompt) "Eval: ")
-           ;; Insert a newline if either the sexp is unbalanced or we are in a
-           ;; commented/empty line.
-           (or (comment-only-p (line-beginning-position) (line-end-position))
-               (condition-case error
-                   (scan-sexps (point-min) (point-max))
-                 (scan-error t))))
-      (progn
-        (insert-char ?\n 1)
-        (indent-according-to-mode))
-    (if (bound-and-true-p lispy-mode)
-        (call-interactively #'lispy-newline-and-indent)
-      (call-interactively #'newline-and-indent))))
+  (cond ((and (minibufferp)
+              (string= (minibuffer-prompt) "Eval: ")
+              ;; Insert a newline if either the sexp is unbalanced or we are in a
+              ;; commented/empty line.
+              (or (comment-only-p (line-beginning-position) (line-end-position))
+                  (condition-case error
+                      (scan-sexps (point-min) (point-max))
+                    (scan-error t))))
+         (progn
+           (insert-char ?\n 1)
+           (indent-according-to-mode)))
+        ((bound-and-true-p lispy-mode)
+         (call-interactively #'lispy-newline-and-indent))
+        (t
+         (call-interactively #'newline-and-indent))))
 
 ;;;###autoload
 (defun cae-mark-dir-locals-as-safe-h ()
