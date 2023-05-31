c0e67320b03e78358eee687ec0dafd0ffe731bb4
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:40 -0400

    config.el

--- config.el
+++ config.el
@@ -553,11 +553,14 @@
 
 (when (modulep! :completion vertico)
   (hercules-def
-   :toggle-funs #'embark-collect-mode
-   :keymap 'embark-collect-mode-map
-   :blacklist-funs '(digit-argument negative-argument)
-   :package 'embark
-   :transient t))
+     :toggle-funs #'cae-embark-collect-cheatsheet
+     :keymap 'embark-collect-mode-map
+     :blacklist-funs '(digit-argument negative-argument)
+     :package 'embark
+     :transient t)
+  (after! embark
+    (map! :map embark-collect-mode-map
+          "<f6>" #'cae-embark-collect-cheatsheet)))
 
 (use-package! avy
   :defer t
