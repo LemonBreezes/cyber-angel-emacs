c55da6fea327dff9b72384e1b7e966ca9f531c0c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:16 -0400

    init.el

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 
 (setq doom-leader-alt-key "C-c"
       doom-localleader-alt-key "C-c l"
+      doom-localleader-key "DEL l"
       doom-leader-key "DEL")
 (setq native-comp-async-jobs-number (num-processors))
 
