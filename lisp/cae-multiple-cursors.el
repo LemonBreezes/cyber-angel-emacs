5e51930b6596d07732e47afa1299d9db274ef785
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:26 -0400

    lisp/cae-multiple-cursors.el

--- lisp/cae-multiple-cursors.el
+++ lisp/cae-multiple-cursors.el
@@ -45,7 +45,7 @@
                    +workspace/restore-last-session +workspace/kill-session-and-quit
                    +workspace/close-woutdow-or-workspace read-only-mode
                    save-buffers-kill-terminal))
-      (add-to-list 'mc/cmds-to-run-once cmd))
+      (add-to-list 'mc/cmds-to-run-once cmd nil #'eq))
     (dolist (mode '(cae-completion-mode symbol-overlay-mode goggles-mode
                     lispy-mode corfu-mode hungry-delete-mode
                     worf-mode isearch-mb-mode))
