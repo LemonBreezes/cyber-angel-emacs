ce10c0de617b565ba1c9183380f6c257dee6818b
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:30 -0400

    config.el

--- config.el
+++ config.el
@@ -320,8 +320,7 @@
       (frame-parameter (window-frame window) 'parent-frame))
     :config
     (add-to-list 'perfect-margin-ignore-regexps "^ \\*which-key\\*$")
-    (add-to-list 'perfect-margin-ignore-filters #'cae-perfect-margin-ignore-posframe-p)
-    (add-to-list 'perfect-margin-ignore-filters #'cae-perfect-margin-ignore-which-key-p))
+    (add-to-list 'perfect-margin-ignore-filters #'cae-perfect-margin-ignore-posframe-p))
 
   (use-package! pdf-view-pagemark
     :when (modulep! :tools pdf)
