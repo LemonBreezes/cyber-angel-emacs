df5cce805732e23e60dc0d4a21e3e12cfca2f532
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:12 -0400

    lisp/cae-multi.el

--- lisp/cae-multi.el
+++ lisp/cae-multi.el
@@ -1,8 +1,7 @@
 ;;; ~/.doom.d/lisp/cae-multi.el -*- lexical-binding: t; -*-
 
 ;; This is code written for the purpose of using this Emacs configuration on
-;; multiple machines. It is not intended to be copied to other Emacs
-;; configurations.
+;; multiple machines.
 
 (defvar cae-multi-local-dir (expand-file-name "shared-local/" doom-user-dir))
 (defvar cae-multi-data-dir (expand-file-name "etc/" cae-multi-local-dir))
