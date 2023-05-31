10aee16f05b78aa085f46ca9a1974485c42511f7
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:57 -0400

    modules/private/modeline/autoload/modeline.el

--- modules/private/modeline/autoload/modeline.el
+++ modules/private/modeline/autoload/modeline.el
@@ -1,30 +1,23 @@
 ;;; private/modeline/autoload/modeline.el -*- lexical-binding: t; -*-
 
-;;;###autoload
-(defun cae-modeline-create-file-buffer-a (orig-fn filepath)
-  (let ((buf (funcall orig-fn filepath)))
-    ;; Error's are very unlikely, this is to ensure even the most remote
-    ;; chance of an error doesn't make the file fail to load.
-    (condition-case err
-      (when buf
-        (let ((vc-backend
-               (ignore-errors (vc-responsible-backend filepath))))
-          (when vc-backend
-            (let ((vc-base-path
-                   (vc-call-backend vc-backend 'root filepath)))
-              (when vc-base-path
-                (let* ((name-base
-                        (concat
-                         "\u200B"       ; U+200B ZERO WIDTH SPACE
-                         (file-relative-name filepath vc-base-path)))
-                       (name-unique name-base)
-                       (name-id 0))
-                  (while (get-buffer name-unique)
-                    (setq name-unique
-                          (concat name-base
-                                  (format " <%d>" name-id)))
-                    (setq name-id (1+ name-id)))
-                  (with-current-buffer buf
-                    (rename-buffer name-unique))))))))
-      (error (message "Error creating vc-backend root name: %s" err)))
+(defun cae-create-file-buffer (filename)
+  "Create a suitably named buffer for visiting FILENAME, and return it.
+FILENAME (sans directory) is used unchanged if that name is free;
+otherwise a string <2> or <3> or ... is appended to get an unused name.
+
+Emacs treats buffers whose names begin with a space as internal buffers.
+To avoid confusion when visiting a file whose name begins with a space,
+this function prepends a \"|\" to the final result if necessary."
+  (let* ((vc-backend (ignore-errors (vc-responsible-backend filename)))
+         (vc-base-path (when vc-backend
+                         (vc-call-backend vc-backend 'root filename)))
+         (name-base (if vc-base-path
+                        (file-relative-name filename vc-base-path)
+                      (file-name-nondirectory filename)))
+	 (name-base (if (string= name-base "")
+	               filename name-base))
+	 (buf (generate-new-buffer (if (string-prefix-p " " name-base)
+			               (concat "|" name-base)
+			             name-base))))
+    (uniquify--create-file-buffer-advice buf filename)
     buf))
