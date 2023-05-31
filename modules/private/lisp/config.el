56225d8bbcf42eae1e0c0603bd58f83f6717f14c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:12 -0400

    modules/private/lisp/config.el

--- modules/private/lisp/config.el
+++ modules/private/lisp/config.el
@@ -24,8 +24,8 @@
 (after! pcre2el
   (map! :prefix "C-c"
         (:prefix ("/" . "pcre2el")
-                 (:prefix ("e" . "elisp"))
-                 (:prefix ("p" . "pcre"))))
+         (:prefix ("e" . "elisp"))
+         (:prefix ("p" . "pcre"))))
   (map! :map rxt--read-pcre-mode-map
         "C-c C-i" #'rxt--toggle-i-mode
         "C-c C-t" #'rxt--toggle-s-mode
