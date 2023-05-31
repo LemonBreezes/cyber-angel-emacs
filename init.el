d7abb3ab48b9e731d09ee27b5f706ecc9218fca1
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:06 -0400

    init.el

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
   "Whether our UI section of `config.el' is disabled.")
 (defvar cae-init-tools-enabled-p t
   "Whether our tools section of `config.el' is disabled.")
-(defvar cae-init-editor-enabled-p nil
+(defvar cae-init-editor-enabled-p t
   "Whether our editor section of `config.el' is disabled.")
 (defvar cae-init-autocompletion-enabled-p t
   "Whether our autocompletion section of `config.el' is disabled.")
