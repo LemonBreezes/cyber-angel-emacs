b52003c780e88a95a502ec5df63d07955f0b3a01
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:17 -0400

    config.el

--- config.el
+++ config.el
@@ -540,6 +540,16 @@
   :init
   ;; Loading `tramp-sh' is slow, so we have this hook load auto-sudoedit if we need
   ;; to use sudo on a file before `tramp-sh' is loaded.
+  (defun cae-auto-sudoedit-maybe-h ()
+    (unless (let ((path (or (buffer-file-name) list-buffers-directory)))
+              (string= (file-attribute-user-id
+                        (file-attributes path 'string))
+                       (if (and (featurep 'tramp)
+                                (tramp-tramp-file-p path))
+                           (tramp-get-remote-uid (tramp-dissect-file-name path)
+                                                 'string)
+                         (user-login-name))))
+      (auto-sudoedit)))
   (add-hook 'find-file-hook #'cae-auto-sudoedit-maybe-h -1)
   :config
   (remove-hook 'find-file-hook #'cae-auto-sudoedit-maybe-h)
