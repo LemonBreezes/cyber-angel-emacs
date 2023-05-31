b7ed7a49f549d1f116c2c2bc07d1f6dfdd905303
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:46 -0400

    lisp/cae-multi.el

--- lisp/cae-multi.el
+++ lisp/cae-multi.el
@@ -50,19 +50,6 @@
 (defvar cae-multi-abbrev--file-mtime nil)
 (defvar cae-multi-abbrev--auto-commit-disabled nil)
 
-(defun cae-multi-abbrev-write-file-a (orig-fun &optional file verbose)
-  (let ((mtime (nth 5 (file-attributes abbrev-file-name))))
-    (if (or (null cae-multi-abbrev--file-mtime)
-            (equal mtime cae-multi-abbrev--file-mtime))
-        (progn (funcall orig-fun file verbose)
-               (setq cae-multi-abbrev--file-mtime mtime)
-               (unless cae-multi-abbrev--auto-commit-disabled
-                 (cae-multi-abbrev-push-changes-a file)))
-      (message "Abbrev file modified since last save. Disabling abbrev file auto-commit.")
-      (funcall orig-fun file verbose)
-      (setq cae-multi-abbrev--file-mtime mtime
-            cae-multi-abbrev--auto-commit-disabled t))))
-
 (advice-add #'write-abbrev-file :around #'cae-multi-abbrev-write-file-a)
 
 (after! abbrev
