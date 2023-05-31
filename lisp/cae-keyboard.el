49d6637ea2fdf538f2c229bf0a4a81a5f209c974
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:03 -0400

    lisp/cae-keyboard.el

--- lisp/cae-keyboard.el
+++ lisp/cae-keyboard.el
@@ -336,10 +336,7 @@
     ";=." "⇒"
     ";!=" "≠"
     "-." "->"
-    "=." "=>"
-    (concat "g_dan" (cae-keyboard-kbd "7")) (cmd! (cae-gpt-insert-dan7-prompt))
-    "g_dan7" (cmd! (cae-gpt-insert-dan7-prompt))
-    "g_maximus" (cmd! (cae-gpt-insert-maximus-prompt))))
+    "=." "=>"))
 
 ;;; Programming language specific stuff
 
