1eb64895d25b8177b2540ec2626cea56ad0c7671
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:45 -0400

    lisp/cae-keyboard.el

--- lisp/cae-keyboard.el
+++ lisp/cae-keyboard.el
@@ -341,6 +341,9 @@
   (add-hook 'web-mode-hook  'smart-semicolon-mode)
   (add-hook 'java-mode-hook 'smart-semicolon-mode))
 
+
+;; This package has not been updated since 2014. If I have to add more advices,
+;; I'd might as well clone this package and maintain it myself.
 (use-package! electric-spacing
   :defer t :init
   (add-hook 'c-mode-common-hook #'electric-spacing-mode)
