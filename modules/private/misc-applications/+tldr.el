f2be00b82cb01f68a6f8c042f859bc62fb2f5d6d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:13 -0400

    modules/private/misc-applications/+tldr.el

--- modules/private/misc-applications/+tldr.el
+++ modules/private/misc-applications/+tldr.el
@@ -10,8 +10,6 @@
   (setq tldr-directory-path
         (expand-file-name "tldr" doom-data-dir))
   (when (modulep! :ui popup)
-    (set-popup-rule! "^\\*tldr\\*$" :size #'cae-popup-resize-help-buffer
-      :side 'right :ttl t :select t :quit t :ttl 0) ; which slot/vslot?
     (advice-add #'tldr :around
                 (cae-defun +tldr-use-popup-buffer-a (fn &rest args)
                   (cl-letf (((symbol-function #'switch-to-buffer-other-window)
