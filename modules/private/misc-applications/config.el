f98278e31027a37bacb077ff2181860eb5e557d8
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:31 -0400

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -4,10 +4,12 @@
 (defvar +misc-applications-prefix "j")
 (defvar +misc-applications-lookup-prefix "j l")
 (defvar +misc-applications-games-prefix "j g")
+(defvar +misc-applications-eyecandy-prefix "j e")
 (defvar doom-picture-dir "~/Pictures/")
 (map! :leader :prefix (+misc-applications-prefix . "misc-applications"))
 (map! :leader :prefix (+misc-applications-lookup-prefix . "lookup"))
 (map! :leader :prefix (+misc-applications-games-prefix . "games"))
+(map! :leader :prefix (+misc-applications-eyecandy-prefix . "eyecandy"))
 
 (map! :leader
       :prefix +misc-applications-prefix
@@ -50,7 +52,10 @@
     "+dunnet"
     "+speed-type"
     "+snake"
-    "+tetris"))
+    "+tetris"
+
+    ;; TODO Eyecandy
+    ))
 
 (dolist (file +misc-applications--lisp-files)
   (load! file))
