ceec2c3c40a8e30c87ab588b985ae8fb822fe620
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:51 -0400

    modules/private/lisp/autoload.el

--- modules/private/lisp/autoload.el
+++ modules/private/lisp/autoload.el
@@ -3,3 +3,14 @@
 ;;;###autoload
 (defun cae-lisp-check-parens-before-save-h ()
   (add-hook 'write-file-functions 'check-parens nil t))
+
+;;;###autoload
+(defun cae-lispy-newline-and-indent-plain ()
+  (interactive)
+  (if (and (minibufferp)
+           (string= (minibuffer-prompt) "Eval: ")
+           (comment-only-p (line-beginning-position) (line-end-position)))
+      (progn
+        (insert-char ?\n 1)
+        (indent-according-to-mode))
+    (call-interactively 'lispy-newline-and-indent)))
