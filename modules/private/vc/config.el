5dd44ec99bfc0b61e6b29a1c85c63d57016150b1
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:20 -0400

    modules/private/vc/config.el

--- modules/private/vc/config.el
+++ modules/private/vc/config.el
@@ -90,7 +90,7 @@
   :defer t :init
   (map! :leader
         :prefix "i"
-        :desc "s.gitignore template" "I" #'gitignore-templates-insert))
+        :desc ".gitignore template" "I" #'gitignore-templates-insert))
 
 (use-package! git-email
   :defer t :init
