a45075a5790a4c2c7aca338b30c4312d95fb2dad
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:06 -0400

    lisp/cae-project.el

--- lisp/cae-project.el
+++ lisp/cae-project.el
@@ -11,15 +11,20 @@
           ".bookmarks/"
           (vc-git--symbolic-ref (buffer-file-name))))
 
-(defun cae-project-bookmark-load ()
+(defun cae-project-bookmark-load-h ()
   (let ((bookmark-default-file (cae-project--bookmark-file))
         (bookmark-alist nil))
     (when (file-exists-p bookmark-default-file)
       (bookmark-load bookmark-default-file)
       (set-persp-parameter 'bookmark-alist bookmark-alist))))
 
-(advice-add #'+workspaces-switch-to-project-h :after #'cae-project-bookmark-load)
+(defun cae-project-bookmark-save-h ()
+  (let ((bookmark-default-file (cae-project--bookmark-file)))
+    (bookmark-save)))
+
+(add-hook 'projectile-after-switch-project-hook #'cae-project-bookmark-load)
 
 (setq bookmark-save-flag 1)
+
 
 ;; TODO make the bookmark file update when the branch changes
