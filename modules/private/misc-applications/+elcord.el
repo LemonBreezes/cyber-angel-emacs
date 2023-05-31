39d966022845c02baf2d9c75521f79243c56d430
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:47 -0400

    modules/private/misc-applications/+elcord.el

--- modules/private/misc-applications/+elcord.el
+++ modules/private/misc-applications/+elcord.el
@@ -5,7 +5,7 @@
   :when (and (display-graphic-p)
              (not (or (memq system-type '(cygwin windows-nt ms-dos))
                       (getenv "SSH_TTY")
-                      (string-suffix-p "-WSL2" operating-system-release))))
+                      (getenv "WSL_DISTRO_NAME"))))
   :config
   (setq elcord-quiet t
         elcord-use-major-mode-as-main-icon t
