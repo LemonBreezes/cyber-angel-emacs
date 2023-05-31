8f470e9f8dc52ef34c5f640504ddb8c90920036d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:27 -0400

    modules/private/corfu/config.el

--- modules/private/corfu/config.el
+++ modules/private/corfu/config.el
@@ -49,8 +49,8 @@ derivative.")
                              (cape-capf-buster #'lsp-completion-at-point)))
               ;; Below is so that context specific completions in cape come first.
               1))
-  (add-to-list 'completion-styles 'partial-completion t #'eq)
-  (add-to-list 'completion-styles 'initials t #'eq)
+  (add-to-list 'completion-styles 'partial-completion t)
+  (add-to-list 'completion-styles 'initials t)
   (setq corfu-cycle t
         corfu-separator (when +corfu-want-multi-component ?\s)
         corfu-preselect t
