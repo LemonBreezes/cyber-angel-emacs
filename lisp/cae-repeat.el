e45b66038ee9120d080fa204312443fad49b9cb1
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:40 -0400

    lisp/cae-repeat.el

--- lisp/cae-repeat.el
+++ lisp/cae-repeat.el
@@ -54,8 +54,8 @@
   (autoload 'embark-verbose-indicator "embark")
   (autoload 'which-key--create-buffer-and-show "which-key"))
 
-;; So that my repeat maps are reloaded when I change them. This is paired with a
-;; hook which evaluates this file before save.
+;; This is so that my repeat maps are reloaded when I change them. This is
+;; paired with a hook which evaluates this file before save.
 (when cae-config-finished-loading
   (ignore-errors (repeat-mode -1))
   (ignore-errors (repeat-mode +1)))
