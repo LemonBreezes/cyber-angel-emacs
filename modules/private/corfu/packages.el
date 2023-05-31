2b4aa70c9a13c15896b303794a191be9c64b3b87
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:18 -0400

    modules/private/corfu/packages.el

--- modules/private/corfu/packages.el
+++ modules/private/corfu/packages.el
@@ -1,7 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/corfu/packages.el
 
-(package! corfu :recipe (:files ("*.el" "extensions/*.el") :branch "swap-icon-index"))
+(package! corfu :recipe (:files ("*.el" "extensions/*.el") :branch "swap-icon-index"
+                         :host github :repo "minad/corfu"))
 (package! cape)
 (package! dabbrev)
 (when (modulep! +icons)
