8b90c0abd95829c020c0850c322fb870ac3db26c
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:58 -0400

    modules/private/debugger/config.el

--- modules/private/debugger/config.el
+++ modules/private/debugger/config.el
@@ -3,7 +3,9 @@
 (when (and (modulep! :tools lsp)
            (modulep! :tools debugger +lsp))
   (after! dap-mode
-    (remove-hook 'dap-stopped-hook #'+dap-running-session-mode))
+    (remove-hook 'dap-stopped-hook #'+dap-running-session-mode)
+    (map! :map dap-mode-map
+          "C-S-d" #'dap-hydra))
   (when (modulep! :lang cc +lsp)
     (add-transient-hook! 'c-mode-common-hook
       (require 'dap-cpptools)
