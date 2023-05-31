5a48e0bcfe296a986f26004e4e57941b2876b3cc
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:27 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -47,4 +47,7 @@
 
 (defun cae-auto-sudoedit-exempt-p ()
   (let ((path (or (buffer-file-name) list-buffers-directory)))
-    (string-prefix-p "/usr/share/emacs/" path)))
+    (string-prefix-p (thread-last lisp-directory
+                                  (file-name-parent-directory)
+                                  (file-name-parent-directory))
+                     path)))
