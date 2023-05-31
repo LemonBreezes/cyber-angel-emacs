af4a89d770bfbd9f9888e270f3b1c09775c9cefa
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:31 -0400

    modules/private/misc-applications/autoload/zone.el

---
+++ modules/private/misc-applications/autoload/zone.el
@@ -0,0 +1,21 @@
+;;; private/misc-applications/autoload/zone.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun zone-pgm-md5 ()
+  "MD5 the buffer, then recursively checksum each hash."
+  (let ((prev-md5 (buffer-substring-no-properties ;; Initialize.
+                   (point-min) (point-max))))
+    ;; Whitespace-fill the window.
+    (zone-fill-out-screen (window-width) (window-height))
+    (random t)
+    (goto-char (point-min))
+    (while (not (input-pending-p))
+      (when (eobp)
+        (goto-char (point-min)))
+      (while (not (eobp))
+        (delete-region (point) (line-end-position))
+        (let ((next-md5 (md5 prev-md5)))
+          (insert next-md5)
+          (setq prev-md5 next-md5))
+        (forward-line 1)
+        (zone-park/sit-for (point-min) 0.1)))))
