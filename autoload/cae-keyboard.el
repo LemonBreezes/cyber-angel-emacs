1879d6785c2b89aa18753a93ed839d3dc8a92643
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:20 -0400

    autoload/cae-keyboard.el

--- autoload/cae-keyboard.el
+++ autoload/cae-keyboard.el
@@ -2,6 +2,7 @@
 
 ;; TODO Rewrite this file so that orbits in `cae-keyboard-orbits' always loop
 ;; around. That way, I can improve my handling of the Lispy keybindings.
+;; (read-char) "<prior>" => (error "Non-character input-event")
 
 (unless (featurep 'json)
   (defun json-alist-p (list)
