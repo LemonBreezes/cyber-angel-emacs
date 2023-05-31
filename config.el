2de2d10a6dd33577b36f12d5a3a70f7bc2456a02
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:36 -0400

    config.el

--- config.el
+++ config.el
@@ -601,7 +601,7 @@
                                    :column
                                    (caddr x))))
           (cae-sp-cheat-sheet/body)))
-      (define-key smartparens-mode-map (kbd "C-M-?") #'cae-sp-cheat-sheet/body))
+      (global-set-key (kbd "C-M-?") #'cae-sp-cheat-sheet/body))
     (when (modulep! :editor multiple-cursors)
       (after! multiple-cursors-core
         (dolist (it sp--mc/cursor-specific-vars)
