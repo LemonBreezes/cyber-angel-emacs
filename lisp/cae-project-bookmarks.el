6a3062f393e4ef7a1a17d2920dc527e56a577d32
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:06 -0400

    lisp/cae-project-bookmarks.el

---
+++ lisp/cae-project-bookmarks.el
@@ -0,0 +1,7 @@
+;;; lisp/cae-project-bookmarks.el -*- lexical-binding: t; -*-
+
+(defun cpb-set-bookmark ())
+
+;; Local Variables:
+;; read-symbol-shorthands: (("cpb-" . "cae-project-bookmarks-"))
+;; End:
