64ca0bee30c8edb3bdca62d30ada8d5d69aec4be
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:46 -0400

    autoload/cae-multi.el

--- autoload/cae-multi.el
+++ autoload/cae-multi.el
@@ -7,11 +7,6 @@
       (gac--after-save (find-file-noselect file)))))
 
 ;;;###autoload
-(defun cae-multi-abbrev-push-changes-a (&optional file _)
-  (unless file
-    (cae-multi-commit-file abbrev-file-name)))
-
-;;;###autoload
 (defun cae-multi-bookmark-push-changes-a (&rest _)
   (gac--after-save bookmark-default-file))
 
@@ -20,3 +15,20 @@
   (gac--after-save (buffer-file-name))
   (dolist (file (org-all-archive-files))
         (gac--after-save file)))
+;;;###autoload
+(defun cae-multi-abbrev-write-file-a (orig-fun &optional file verbose)
+  (let ((mtime (nth 5 (file-attributes abbrev-file-name))))
+    (if (or (null cae-multi-abbrev--file-mtime)
+            (equal mtime cae-multi-abbrev--file-mtime))
+        (progn (funcall orig-fun file verbose)
+               (setq cae-multi-abbrev--file-mtime mtime)
+               (unless file
+                 (if cae-multi-abbrev--auto-commit-disabled
+                     (when (y-or-n-p "Abbrev file modified since last save. Enable auto-commit?")
+                       (setq cae-multi-abbrev--auto-commit-disabled nil)
+                       (cae-multi-commit-file abbrev-file-name))
+                   (cae-multi-commit-file abbrev-file-name))))
+      (message "Abbrev file modified since last save. Disabling abbrev file auto-commit.")
+      (funcall orig-fun file verbose)
+      (setq cae-multi-abbrev--file-mtime mtime
+            cae-multi-abbrev--auto-commit-disabled t))))
