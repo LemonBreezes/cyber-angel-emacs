a4879d8c596396b7a727754f1c5cf4f3d1d8eb0f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:14 -0400

    modules/private/meow/autoload.el

--- modules/private/meow/autoload.el
+++ modules/private/meow/autoload.el
@@ -1,1 +1,6 @@
 ;;; private/meow/autoload.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun cae-meow-use-keyboard-layout-a ()
+  (setq last-command-event
+        (cae-keyboard-remap-char last-command-event)))
