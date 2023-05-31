ac46d15f4d18645c8b1bf4312dd54e0bd38a1063
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:56 -0400

    modules/private/eshell/autoload/commands.el

---
+++ modules/private/eshell/autoload/commands.el
@@ -0,0 +1,130 @@
+;;; private/eshell/autoload/commands.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun +eshell-buffer-contents (buffer)
+  "Return fontified buffer contents for BUFFER."
+  (with-current-buffer buffer
+    (font-lock-ensure (point-min) (point-max))
+    (buffer-string)))
+
+;;;###autoload
+(defun +eshell-file-contents (file)
+  "Return fontified file contents for FILE."
+  (let ((buffer (get-file-buffer file)))
+    (if buffer
+        (+eshell-buffer-contents buffer)
+      (unwind-protect
+          (+eshell-buffer-contents
+           (setq buffer
+                 (let ((inhibit-message t)
+                       (non-essential t)
+                       (enable-dir-local-variables nil)
+                       (enable-local-variables (and enable-local-variables :safe)))
+                   (find-file-noselect file))))
+        (when buffer
+          (kill-buffer buffer))))))
+
+;;;###autoload
+(defun eshell/b (regexp)
+  "Output buffer content of buffer matching REGEXP."
+  (cl-loop for buf in (buffer-list)
+           thereis
+           (and (string-match-p regexp (buffer-name buf))
+                (with-current-buffer buf
+                  (buffer-substring-no-properties (point-min) (point-max))))))
+
+;; By LemonBreezes https://github.com/LemonBreezes/.doom.d/blob/master/modules/private/eshell/autoload.el
+;; Originally from Aweshell https://github.com/manateelazycat/aweshell/blob/master/aweshell.el.
+;;;###autoload
+(defun eshell/hat (&rest files)
+  "Output FILES with highlighting."
+  (dolist (f files)
+    (eshell-print (+eshell-file-contents f))))
+
+;;;###autoload
+(defun eshell/swap (file1 file2)
+  "Swap the contents of FILE1 and FILE2."
+  (let ((temp (make-temp-name temporary-file-directory)))
+    (rename-file file1 temp)
+    (rename-file file2 file1)
+    (rename-file temp file2)))
+
+;;; https://github.com/dakra/dmacs/blob/master/init.org#eshell
+;;;###autoload
+(defun eshell/rcd (&optional directory)
+  "Like regular 'cd' but don't jump out of a tramp directory.
+When on a remote directory with tramp don't jump 'out' of the server.
+So if we're connected with sudo to 'remotehost'
+'$ rcd /etc' would go to '/sudo:remotehost:/etc' instead of just
+'/etc' on localhost."
+  (unless (file-remote-p default-directory)
+    (error "not in a remote location"))
+  (with-parsed-tramp-file-name default-directory nil
+    (eshell/cd
+     (tramp-make-tramp-file-name
+      method user nil host nil (or directory "") hop))))
+
+;; https://github.com/howardabrams/hamacs/blob/main/ha-eshell.org
+;;;###autoload
+(defun eshell/set (&rest args)
+  "Creates a buffer local variables."
+  (dolist (arg-pair (seq-partition args 2))
+    (seq-let (var val) arg-pair
+      (let ((var-sym (make-symbol var)))
+        (set (make-local-variable var-sym) val)))))
+
+;; https://github.com/OrgTangle/ntangle/blob/de3ec97c4650cc7b5875ee2614e4c1df5ccb5388/tests/dmacs/init.org#L2435
+;;;###autoload
+(defun eshell/f (filename &optional dir try-count)
+  "Searches for files matching FILENAME in either DIR or the
+current directory. Just a typical wrapper around the standard
+`find' executable.
+
+Since any wildcards in FILENAME need to be escaped, this wraps
+the shell command.
+
+If not results were found, it calls the `find' executable up to
+two more times, wrapping the FILENAME pattern in wildcat
+matches. This seems to be more helpful to me."
+  (let* ((cmd (concat
+               (executable-find "find")
+               " " (or dir ".")
+               "      -not -path '*/.git*'"
+               " -and -not -path '*node_modules*'"
+               " -and -not -path '*classes*'"
+               " -and "
+               " -type f -and "
+               "-iname '" filename "'"))
+         (results (shell-command-to-string cmd)))
+
+    (if (not (s-blank-str? results))
+        results
+      (cond
+       ((or (null try-count) (= 0 try-count))
+        (eshell/f (concat filename "*") dir 1))
+       ((or (null try-count) (= 1 try-count))
+        (eshell/f (concat "*" filename) dir 2))
+       (t "")))))
+
+;; https://github.com/OrgTangle/ntangle/blob/de3ec97c4650cc7b5875ee2614e4c1df5ccb5388/tests/dmacs/init.org#L2435
+;;;###autoload
+(defun eshell/ef (filename &optional dir)
+  "Searches for the first matching filename and loads it into a
+file to edit."
+  (let* ((files (eshell/f filename dir))
+         (file (car (s-split "\n" files))))
+    (find-file file)))
+
+;;;###autoload
+(defun eshell/gst (&rest args)
+  (magit-status-internal (or (pop args) default-directory))
+  (delete-other-windows))
+
+;; https://www.n16f.net/blog/clearing-the-eshell-buffer/
+;;;###autoload
+(defun +eshell-clear ()
+  (interactive)
+  (let ((input (eshell-get-old-input)))
+    (eshell/clear t)
+    (eshell-emit-prompt)
+    (insert input)))
