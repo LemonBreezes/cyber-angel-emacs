299be56e54999d0f11d5ce5be31bde834c75cec0
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:12 -0400

    modules/private/misc-applications/+helm-linux-disks.el

--- modules/private/misc-applications/+helm-linux-disks.el
+++ modules/private/misc-applications/+helm-linux-disks.el
@@ -4,7 +4,7 @@
   :defer t
   :when (and (eq system-type 'gnu/linux)
              (or (modulep! :private helm)
-                     (modulep! :completion helm)))
+                 (modulep! :completion helm)))
   :init
   (map! :leader
         :prefix +misc-applications-prefix
