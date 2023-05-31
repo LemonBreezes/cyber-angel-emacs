bb839f58579537ceec58c23e5830c9ddc989f017
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:13 -0400

    modules/private/misc-applications/autoload/speed-type.el

--- modules/private/misc-applications/autoload/speed-type.el
+++ modules/private/misc-applications/autoload/speed-type.el
@@ -4,3 +4,8 @@
 (defun +evil-insert-a (&rest _)
   (when (modulep! :editor evil)
     (evil-insert-state)))
+
+;;;###autoload (autoload '+speed-type-hydra/body "private/misc-applications/autoload/speed-type" nil t)
+(defhydra +speed-type-hydra (:color blue :hint nil)
+  ("<f6>" nil "Exit" :exit t)
+  ("C-c C-k" speed-type-complete "Complete" :exit t))
