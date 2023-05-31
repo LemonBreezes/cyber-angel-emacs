1fd7c11ac94ce42f8243d0e35c4fb353a0b8e226
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:30 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -38,10 +38,10 @@
 (package! git-modes)
 
 ;; cae-webkit.el
-;; (package! webkit :recipe (:type git :host github :repo "akirakyle/emacs-webkit"
-;;                           :branch "main"
-;;                           :files (:defaults "*.js" "*.css" "*.so")
-;;                           :pre-build ("make")))
+(package! webkit :recipe (:type git :host github :repo "akirakyle/emacs-webkit"
+                          :branch "main"
+                          :files (:defaults "*.js" "*.css" "*.so")
+                          :pre-build ("make")))
 
 ;;; Editor
 (package! pp+)
