fa08b144a89f4d15741b7184f6ce98d7a30adbb9
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:51 -0400

    .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -17,7 +17,6 @@
                      (bound-and-true-p cae-config-finished-loading))
             (add-hook 'write-file-functions 'eval-buffer 1 t))
 
-
           ;; Automatically update Eshell aliases.
           (when (and (buffer-file-name) (bound-and-true-p eshell-aliases-file)
                      (file-equal-p (buffer-file-name)
