8a1a8c722d93b1d2a6bfe0f56241cee5509e1255
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:12 -0400

    modules/private/misc-applications/+alarm-clock.el

--- modules/private/misc-applications/+alarm-clock.el
+++ modules/private/misc-applications/+alarm-clock.el
@@ -1,4 +1,5 @@
 ;;; private/misc-applications/alarm-clock.el -*- lexical-binding: t; -*-
+
 (use-package! alarm-clock
   :defer t
   :init
@@ -11,7 +12,7 @@
   (map! :leader
         :prefix +misc-applications-prefix
         (:prefix ("a" . "alarms")
-        "k" #'alarm-clock-kill))
+         "k" #'alarm-clock-kill))
   (setq alarm-clock-cache-file
         (expand-file-name "alarm-clock.cache" doom-cache-dir))
   (alarm-clock--turn-autosave-on))
