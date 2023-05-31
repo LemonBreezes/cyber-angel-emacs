d3466457360a7e7f27d546237c7d90ceeee4cab1
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:44 -0400

    modules/private/misc-applications/autoload/leetcode.el

---
+++ modules/private/misc-applications/autoload/leetcode.el
@@ -0,0 +1,17 @@
+;;; private/misc-applications/autoload/leetcode.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun +leetcode ()
+  (interactive)
+  (when (modulep! :ui workspaces)
+    (+workspace-switch "leetcode" t))
+  (leetcode))
+
+;;;###autoload
+(defun +leetcode-quit ()
+  (interactive)
+  (leetcode-quit)
+  (when (and (modulep! :ui workspaces)
+             (+workspace-exists-p "leetcode"))
+    (+workspace-delete "leetcode")
+    (+workspace/other)))
