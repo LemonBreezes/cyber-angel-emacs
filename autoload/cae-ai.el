69783200217774bd391cf9a23224fb7c7c993bc4
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:08 -0400

    autoload/cae-ai.el

--- autoload/cae-ai.el
+++ autoload/cae-ai.el
@@ -4,3 +4,18 @@
   (save-mark-and-excursion
     (mark-whole-buffer)
     (call-interactively #'org-ai-on-region)))
+
+;;;###autoload
+(cae-defun cae-lazy-load-org-ai ()
+  (interactive)
+  (require 'org-ai)
+  (setq unread-command-events
+        (listify-key-sequence (kbd "C-c M-a")))
+  (setq which-key-inhibit t)
+  (add-transient-hook! 'pre-command-hook
+    (setq which-key-inhibit nil))
+  (run-with-idle-timer
+   which-key-idle-delay nil
+   (lambda ()
+     (when which-key-inhibit
+       (which-key-show-keymap 'org-ai-global-mode-prefix-map t)))))
