cabfb492f7be7c11c46524089ec6af78c9c6fa56
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:59 -0400

    modules/private/debugger/config.el

--- modules/private/debugger/config.el
+++ modules/private/debugger/config.el
@@ -9,7 +9,8 @@
       (let ((ignore-window-parameters t))
         (cl-loop for buf being the buffers
                  when (string-match-p "gdb" (buffer-name buf)) do
-                 (cae-hacks-always-yes-a #'doom-kill-buffer-and-windows buf)))))
+                 (cae-hacks-always-yes-a #'doom-kill-buffer-and-windows buf)))
+      (hydra-keyboard-quit)))
 
   ;; Doom keeps emitting errors when trying to retrieve previous debug
   ;; sessions. This is a workaround.
