8861ee75e18cbaa30b5bb50ae03ea3a55841d754
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:55 -0400

    modules/private/lisp/config.el

--- modules/private/lisp/config.el
+++ modules/private/lisp/config.el
@@ -18,3 +18,15 @@
 (add-hook 'hy-mode-hook #'+check-parens-before-save-h)
 (add-hook 'dune-mode-hook #'+check-parens-before-save-h)
 (add-hook 'fennel-mode-hook #'+check-parens-before-save-h)
+
+(after! pcre2el
+  (map! :prefix "C-c"
+        (:prefix ("/" . "pcre2el")
+                 (:prefix ("e" . "elisp"))
+                 (:prefix ("p" . "pcre"))))
+  (map! :map rxt--read-pcre-mode-map
+        "C-c C-i" #'rxt--toggle-i-mode
+        "C-c C-t" #'rxt--toggle-s-mode
+        "C-c C-x" #'rxt--toggle-x-mode)
+  (undefine-key! rxt--read-pcre-mode-map
+    "C-c i" "C-c s" "C-c x"))
