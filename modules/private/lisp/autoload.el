49215ceee485c8ee0dea4f89657f5ca7aa313dab
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:04 -0400

    modules/private/lisp/autoload.el

--- modules/private/lisp/autoload.el
+++ modules/private/lisp/autoload.el
@@ -3,3 +3,6 @@
 ;;;###autoload
 (defun cae-lisp-check-parens-before-save-h ()
   (add-hook 'write-file-functions 'check-parens nil t))
+
+(defun cae-lisp-eval-buffer-before-save-h ()
+  (add-hook 'write-file-functions 'eval-buffer 1 t))
