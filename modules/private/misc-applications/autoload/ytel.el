4a3dbc318dc99c86139608d2ba43989b92bc8b15
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:14 -0400

    modules/private/misc-applications/autoload/ytel.el

--- modules/private/misc-applications/autoload/ytel.el
+++ modules/private/misc-applications/autoload/ytel.el
@@ -7,10 +7,9 @@
   (let* ((video (ytel-get-current-video))
      	 (id    (ytel-video-id video)))
     (start-process "ytel mpv" nil
-                   (if (executable-find "mpv.exe")
-                       "mpv.exe"
-		     "mpv")
+                   "mpv"
 		   (concat "https://www.youtube.com/watch?v=" id))
+    (princ (concat "https://www.youtube.com/watch?v=" id))
     "--ytdl-format=bestvideo[height<=?720]+bestaudio/best")
   (message "Starting streaming..."))
 
