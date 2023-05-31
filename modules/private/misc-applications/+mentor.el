eae7de80551dc8d6819d044f104fe856e82edf3d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:23 -0400

    modules/private/misc-applications/+mentor.el

--- modules/private/misc-applications/+mentor.el
+++ modules/private/misc-applications/+mentor.el
@@ -12,8 +12,8 @@
   (advice-add
    #'mentor :before
    (defun +mentor-a ()
-     (setopt mentor-rtorrent-keep-session t
-             mentor-rtorrent-external-rpc (expand-file-name "~/.rtorrent-rpc.socket"))
+     (setq mentor-rtorrent-keep-session t
+           mentor-rtorrent-external-rpc (expand-file-name "~/.rtorrent-rpc.socket"))
      (mentor-setup-rtorrent)
      ;; I got a void variable error for some reason.
      (defvar mentor-rtorrent-client-version)))
