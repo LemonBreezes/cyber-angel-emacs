8b8806bbd7e6a82224458a69011f056e1addc71d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:55 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -56,6 +56,7 @@
 (package! restore-point
   :recipe (:host github :repo "arthurcgusmao/restore-point"))
 (package! symbol-overlay)
+(package! sentex :recipe (:repo "https://codeberg.org/martianh/sentex.git"))
 
 ;; These are used for their autoloads.
 (package! ialign)
