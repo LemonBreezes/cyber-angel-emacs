7d972258c1ebfe55ba749840b575f6220bdb9437
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:29 -0400

    modules/private/lisp/autoload.el

--- modules/private/lisp/autoload.el
+++ modules/private/lisp/autoload.el
@@ -2,7 +2,7 @@
 
 ;;;###autoload
 (defun cae-lisp-check-parens-before-save-h ()
-  (add-hook 'write-file-functions 'check-parens nil t))
+  (add-hook 'write-file-functions #'check-parens nil t))
 
 ;;;###autoload
 (defun cae-lisp-newline-and-indent ()
