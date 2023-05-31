7bf3f0460de4fddc02b358e0ff7c0678a7f06815
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:02 -0400

    autoload/cae-keyboard.el

--- autoload/cae-keyboard.el
+++ autoload/cae-keyboard.el
@@ -5,3 +5,20 @@
   "Insert the current prefix argument."
   (interactive "P")
   (insert (format "%s" arg)))
+
+;;;###autoload
+(defun cae-keyboard-strings (arg)
+  (declare (pure t) (side-effect-free t))
+  (cae-keyboard-apply-recursively #'char-to-string arg))
+
+;;;###autoload
+(defun cae-keyboard-remap (arg)
+  (declare (pure t) (side-effect-free t))
+  (cae-keyboard-apply-recursively #'cae-keyboard-remap-char arg))
+
+;;;###autoload
+(defun cae-keyboard-remap-to-strings (arg)
+  (declare (pure t) (side-effect-free t))
+  (cae-keyboard-apply-recursively
+   (-compose #'char-to-string #'cae-keyboard-remap-char)
+    arg))
