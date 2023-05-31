2499e637325517613f37b26d6d873525299b4453
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:13 -0400

    modules/private/misc-applications/autoload/tetris.el

---
+++ modules/private/misc-applications/autoload/tetris.el
@@ -0,0 +1,10 @@
+;;; private/misc-applications/autoload/tetris.el -*- lexical-binding: t; -*-
+
+;;;###autoload (autoload '+tetris-hydra/body "private/misc-applications/autoload/tetris" nil t)
+(defhydra +tetris-hydra (:color pink :hint nil)
+  ("<f6>" nil "Exit" :exit t)
+  ("q" tetris-end-game nil :exit t)
+  ("n" tetris-start-game "Start game")
+  ("p" tetris-pause-game "Pause game")
+  ("SPC" tetris-move-bottom "Move bottom")
+  ("<up>" tetris-rotate-prev "Rotate clockwise"))
