28ea87a00c4ac3ef91833568e48346f8ade73a88
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:02 -0400

    config.el

--- config.el
+++ config.el
@@ -1,5 +1,6 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
+(load! "lisp/cae-fixup-leader-key")
 (load! "lisp/cae-multi")                ;Run parallel Emacs instances.
 (load! "lisp/cae-keyboard")             ;Input hacks.
 
@@ -13,40 +14,6 @@
            (modulep! :completion vertico))
   ;; Helm is not our main completion system.
   (remove-hook 'doom-first-input-hook #'helm-mode))
-
-;; Doom should not bind leader key prefixes to keys which are not alphanumeric
-;; because then they can be overwriting other packages' keybindings. As an
-;; example, Org mode has `C-c !' bound to `org-time-stamp-inactive' and `C-c &'
-;; bound to `org-mark-ring-goto'.
-(when (modulep! :checkers syntax)
-  (after! which-key
-      (setq which-key-replacement-alist
-            (delete '(("\\`C-c !\\'") nil . "checkers")
-                    which-key-replacement-alist)))
-  (after! flycheck
-    (define-key flycheck-mode-map flycheck-keymap-prefix nil)
-    (setq flycheck-keymap-prefix (kbd "C-c C"))
-    (define-key flycheck-mode-map flycheck-keymap-prefix
-      flycheck-command-map)
-    (map! :leader
-          (:prefix ("C" . "checkers")))))
-(when (modulep! :editor snippets)
-  (dolist (p (cdr (lookup-key doom-leader-map "&")))
-    (cl-destructuring-bind (key . binding) p
-      (define-key doom-leader-map (kbd (concat "S " (char-to-string key))) binding)))
-  (after! yasnippet (define-key yas-minor-mode-map (kbd "C-c &") nil))
-  (define-key doom-leader-map "&" nil)
-  (after! which-key
-    (setq which-key-replacement-alist
-          (let ((case-fold-search nil))
-            (cl-mapcar (lambda (x)
-                         (when (car-safe (car x))
-                           (setf (car (car x))
-                                 (replace-regexp-in-string "C-c &"
-                                                           "C-c S"
-                                                           (car-safe (car x)))))
-                         x)
-                       which-key-replacement-alist)))))
 
 ;;; UI
 
