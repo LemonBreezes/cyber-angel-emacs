4b933a6e9904ef64469577f433fd43b6111cc445
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:29 -0400

    modules/private/eshell/autoload/eshell.el

--- modules/private/eshell/autoload/eshell.el
+++ modules/private/eshell/autoload/eshell.el
@@ -12,16 +12,3 @@
 (defun cae-eshell-set-up-autocompletion ()
   (add-hook 'completion-at-point-functions
             #'cape-file nil t))
-
-;;;###autoload
-(defun cae-eshell-prompt ()
-  (require 'shrink-path)
-  (concat (let ((pwd (eshell/pwd)))
-            (propertize (if (equal pwd "~")
-                            pwd
-                          (abbreviate-file-name (shrink-path-file pwd)))
-                        'face '+eshell-prompt-pwd))
-          (propertize (+eshell--current-git-branch)
-                      'face '+eshell-prompt-git-branch)
-          (propertize " λ" 'face (if (zerop eshell-last-command-status) 'success 'error))
-          " "))
