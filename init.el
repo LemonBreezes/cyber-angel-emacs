10cbd0ec80141623c8357ccc984757b4bc9b6192
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:44 -0400

    init.el

--- init.el
+++ init.el
@@ -76,17 +76,18 @@
        (rss +org)
 
        :private
-       (corfu +indexed)
-       debugger
-       (dirvish +icons +dirvish)
-       eshell
-       dired
-       lisp
-       misc-applications
-       modeline
+       ;(corfu +indexed)
+       ;debugger
+       ;(dirvish +icons +dirvish)
+       ;eshell
+       ;dired
+       ;lisp
+       ;misc-applications
+       ;modeline
        ;; (meow +leader)
-       unpackaged
-       vc)
+       ;unpackaged
+       ;vc
+       )
 
 ;; Local Variables:
 ;; eval: (when (featurep 'lispy) (lispy-mode -1))
