54b81162715bb5cc51c22776640bd98ad655d190
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:09:59 -0400

    modules/private/debugger/config.el

--- modules/private/debugger/config.el
+++ modules/private/debugger/config.el
@@ -2,12 +2,15 @@
 
 (when (and (modulep! :tools lsp)
            (modulep! :tools debugger +lsp))
-  (add-hook! 'dap-terminated-hook
+
+  ;; Close the windows when the debugger is closed.
+  (add-hook! #'dap-disconnect
     (ignore-errors
       (let ((ignore-window-parameters t))
         (cl-loop for buf being the buffers
                  when (string-match-p "gdb" (buffer-name buf)) do
                  (cae-hacks-always-yes-a #'doom-kill-buffer-and-windows buf)))))
+
   (doom-store-clear "+debugger")
   (when (modulep! :lang cc +lsp)
     (add-transient-hook! 'c-mode-common-hook
