b7a085c4d55b99e6978f8da3d94be1e6dd2b4396
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:27 -0400

    init.el

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 (setq doom-leader-alt-key "C-c"
       doom-localleader-alt-key "C-c y"
       doom-leader-key "SPC"
-      doom-localleader-key "SPC y")
+      doom-localleader-key "SPC m")
 (setq native-comp-async-jobs-number (num-processors))
 
 (doom! :completion
