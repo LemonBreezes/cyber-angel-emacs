549353bb98a2784ab104b21c9abcf910e2d967dd
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:35 -0400

    modules/private/lisp/config.el

--- modules/private/lisp/config.el
+++ modules/private/lisp/config.el
@@ -130,7 +130,7 @@
           (cl-loop for x in bindings
                    unless (string= "" (elt x 2))
                    collect
-                   (list (if (string= (cae-keyboard-kbd (car x)) "'")
+                   (list (if (string= (car x) "'")
                              "=" (car x))
                          (intern (elt x 1))
                          (when (string-match "lispy-\\(?:eval-\\)?\\(.+\\)"
