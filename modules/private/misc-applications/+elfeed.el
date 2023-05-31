49fb86a04c28aace00e72f5f97c81a6ef95e9cdd
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:33 -0400

    modules/private/misc-applications/+elfeed.el

--- modules/private/misc-applications/+elfeed.el
+++ modules/private/misc-applications/+elfeed.el
@@ -15,12 +15,12 @@
           "?" #'describe-mode
           "q" #'+elfeed-quit)
     (defhydra cae-elfeed-hydra ()
-      ;;("e" (elfeed-search-set-filter "@6-months-ago +emacs") "emacs")
-      ;;("y" (elfeed-search-set-filter "@6-months-ago +tube") "youtube")
-      ;;("*" (elfeed-search-set-filter "@6-months-ago +star") "Starred")
+      ("E" (elfeed-search-set-filter "@6-months-ago +emacs") "emacs")
+      ("Y" (elfeed-search-set-filter "@6-months-ago +tube") "youtube")
+      ("*" (elfeed-search-set-filter "@6-months-ago +star") "Starred")
       ("m" elfeed-toggle-star "Mark")
-      ;;("a" (elfeed-search-set-filter "@6-months-ago") "All")
-      ;;("t" (elfeed-search-set-filter "@1-day-ago") "Today")
+      ("A" (elfeed-search-set-filter "@6-months-ago") "All")
+      ("T" (elfeed-search-set-filter "@1-day-ago") "Today")
       ("Q" +elfeed-quit "Quit Elfeed" :color blue)
       ("q" nil "quit" :color blue))
     (map! :map elfeed-search-mode-map
