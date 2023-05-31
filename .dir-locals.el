124415cfa03c4951b049e232080ccb0ffa3dd19f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:08 -0400

    .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,27 +3,5 @@
 
 ((nil
   . ((eval
-      . (progn
-          (when (and (derived-mode-p 'emacs-lisp-mode)
-                     (buffer-file-name)
-                     (not (cl-member (file-name-nondirectory (buffer-file-name))
-                                     '("init.el"
-                                       ".dir-locals.el"
-                                       "packages.el")
-                                     :test #'string=)))
-            (add-hook 'write-file-functions 'eval-buffer 1 t))
-          (eval `(after! diff-hl
-                   (setf (buffer-local-value 'diff-hl-reference-revision
-                                                ,(current-buffer))
-                         "HEAD~10")))
-          (when (bound-and-true-p cae-config-finished-loading)
-            (when (and (derived-mode-p 'emacs-lisp-mode)
-                       (fboundp 'cae-compile-this-elisp-file))
-              (add-hook 'after-save-hook #'cae-compile-this-elisp-file nil t))
-            (when (and (buffer-file-name)
-                       (not (file-in-directory-p (buffer-file-name)
-                                                 (concat doom-private-dir "secrets/")))
-                       (require 'git-auto-commit-mode nil t))
-              (git-auto-commit-mode 1)
-              (setq-local gac-automatically-push-p t)))))))
+      . )))
  (magit-status-mode . ((magit-todos-exclude-globs . (".git/" "shared-local/")))))
