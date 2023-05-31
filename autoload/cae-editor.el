2fba12ffcd6db25e90b81afb0519ba2f2fa200a0
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:55 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -156,8 +156,7 @@ This is the format used on Reddit for code blocks."
 ;;;###autoload
 (defun cae-forward-sentence-function (&optional arg)
   (or arg (setq arg 1))
-  (if (or (autoloadp (symbol-function #'sentex-forward-sentence))
-          (fboundp #'sentex-forward-sentence))
+  (if (fboundp #'sentex-forward-sentence)
       (if (< arg 0)
           (sentex-backward-sentence arg)
         (sentex-forward-sentence arg))
