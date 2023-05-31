2ef006e609a3e958f389c5b51db7090898f82c26
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:44 -0400

    modules/private/misc-applications/+forecast.el

--- modules/private/misc-applications/+forecast.el
+++ modules/private/misc-applications/+forecast.el
@@ -5,7 +5,7 @@
   :init
   (map! :leader
         :prefix +misc-applications-prefix
-        "w" #'+forecast)
+        "C-w" #'+forecast)
   :config
   (setq forecast-api-key "3952024acf85777d62f39869da12f853"
         forecast-units 'us
