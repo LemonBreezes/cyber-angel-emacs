335826409a0eb862ae252b22b7715395ddb36719
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:10:29 -0400

    modules/private/eshell/autoload/prompt.el

--- modules/private/eshell/autoload/prompt.el
+++ modules/private/eshell/autoload/prompt.el
@@ -1,3 +1,13 @@
 ;;; private/eshell/autoload/prompt.el -*- lexical-binding: t; -*-
 
-(defun cae-eshell-prompt ())
+(defun cae-eshell-prompt ()
+  (require 'shrink-path)
+  (concat (let ((pwd (eshell/pwd)))
+            (propertize (if (equal pwd "~")
+                            pwd
+                          (abbreviate-file-name (shrink-path-file pwd)))
+                        'face '+eshell-prompt-pwd))
+          (propertize (+eshell--current-git-branch)
+                      'face '+eshell-prompt-git-branch)
+          (propertize " λ" 'face (if (zerop eshell-last-command-status) 'success 'error))
+          " "))
