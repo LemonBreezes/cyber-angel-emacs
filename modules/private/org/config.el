8b2052e5265c9aaeec02be4f7ceac714417f4676
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:07 -0400

    modules/private/org/config.el

--- modules/private/org/config.el
+++ modules/private/org/config.el
@@ -5,3 +5,8 @@
   :init
   (map! :map org-mode-map
         "C-M-y" #'cae-org-rich-yank))
+
+(defvar +org-exit-src-code-hook nil
+  "Hook run just before exiting a org source block buffer.")
+(advice-add #'org-edit-src-exit :before #'+org-run-exit-src-code-hooks)
+(add-hook '+org-exit-src-code-hook #'ws-butler-trim-eob-lines)
