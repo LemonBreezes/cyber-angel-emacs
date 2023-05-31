6416cd9df6e46eb31daa0f2f2b5d44413990207f
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:28 -0400

    config.el

--- config.el
+++ config.el
@@ -806,9 +806,9 @@
           "r" #'symbol-overlay-rename
           "-" #'negative-argument)
     ;; LSP provides its own symbol highlighting.
-    (add-hook 'lsp-mode-hook
-              (cae-defun cae-disable-symbol-overlay-h ()
-                (symbol-overlay-mode -1)))
+    (add-hook! 'lsp-mode-hook
+      (cae-defun cae-disable-symbol-overlay-h ()
+        (symbol-overlay-mode -1)))
     (define-key symbol-overlay-map (kbd "o") 'cae-avy-symbol-at-point)
     ;; For some reason `symbol-overlay-switch-backward' jumps to the first symbol
     ;; overlay in the buffer. This is probably a bug.
