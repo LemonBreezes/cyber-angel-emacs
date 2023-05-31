b8333299ef35334c8985bbc4163a952c6cbdda9d
LemonBreezes <look@strawberrytea.xyz>
2023-05-31 10:11:13 -0400

    modules/private/misc-applications/+devdocs.el

--- modules/private/misc-applications/+devdocs.el
+++ modules/private/misc-applications/+devdocs.el
@@ -4,14 +4,14 @@
   :defer t
   :init
   (map! :leader
-        :prefix +misc-applications-lookup-prefix
-        :prefix ("d" . "devdocs")
-        :desc "Read from first page" "p" #'devdocs-peruse
-        :desc "Install doc" "i" #'devdocs-install
-        :desc "Delete doc" "d" #'devdocs-delete
-        :desc "Search doc" "l" #'devdocs-lookup
-        :desc "Search devdocs website" "s" #'devdocs-search
-        :desc "Update all docs" "u" #'devdocs-update-all)
+        (:prefix +misc-applications-lookup-prefix
+         (:prefix ("d" . "devdocs")
+          :desc "Read from first page" "p" #'devdocs-peruse
+          :desc "Install doc" "i" #'devdocs-install
+          :desc "Delete doc" "d" #'devdocs-delete
+          :desc "Search doc" "l" #'devdocs-lookup
+          :desc "Search devdocs website" "s" #'devdocs-search
+          :desc "Update all docs" "u" #'devdocs-update-all)))
   :config
   (setq devdocs-data-dir (expand-file-name "devdocs" doom-data-dir))
   (map! :map devdocs-mode-map
