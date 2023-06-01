e8d63916c3cf2aeda0da4845d064e1da2a7fd0b8
LemonBreezes <look@strawberrytea.xyz>
2023-06-01 00:52:18 -0400

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -2,7 +2,7 @@
 
 (defun cae-modeline-buffer-name ()
   (propertize (if (doom-real-buffer-p (current-buffer))
-                  (breadcrumb-project-crumbs)
+                  (or (breadcrumb-project-crumbs) "")
                 (buffer-name))
               'face '(:inherit variable-pitch
                       :weight bold)))
