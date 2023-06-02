147ebbea754698230aad1b5167e128fad9064aa7
LemonBreezes <look@strawberrytea.xyz>
2023-06-01 22:10:18 -0400

    modules/private/modeline/autoload.el

--- modules/private/modeline/autoload.el
+++ modules/private/modeline/autoload.el
@@ -1,7 +1,7 @@
 ;;; private/modeline/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-modeline-rotate-next-word-at-point ()
+(defun cae-modeline-rotate-forward-word-at-point ()
   (interactive)
   (save-excursion
     (when-let ((beg (car-safe (bounds-of-thing-at-point 'symbol))))
@@ -9,17 +9,17 @@
     (skip-syntax-forward "^w" (line-end-position))
     (condition-case (call-interactively #'parrot-rotate-next-word-at-point)
         (error
-         (skip-syntax-backward "^w" (line-end-position))
+         (skip-syntax-backward "^w" (line-beginning-position))
          (call-interactively #'parrot-rotate-next-word-at-point)))))
 
 ;;;###autoload
-(defun cae-modeline-rotate-previous-word-at-point ()
+(defun cae-modeline-rotate-backward-word-at-point ()
   (interactive)
   (save-excursion
     (when-let ((beg (car-safe (bounds-of-thing-at-point 'symbol))))
       (goto-char beg))
-    (skip-syntax-backward "^w" (line-beginning-position))
+    (skip-syntax-forward "^w" (line-end-position))
     (condition-case (call-interactively #'parrot-rotate-prev-word-at-point)
         (error
-         (skip-syntax-backward "^w" (line-end-position))
+         (skip-syntax-backward "^w" (line-beginning-position))
          (call-interactively #'parrot-rotate-prev-word-at-point)))))
