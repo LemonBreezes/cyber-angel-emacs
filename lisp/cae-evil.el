0fd0c9676b5200f742171e735bd5ab1d209a7842
LemonBreezes <look@strawberrytea.xyz>
2023-06-04 18:53:07 -0400

    lisp/cae-evil.el

--- lisp/cae-evil.el
+++ lisp/cae-evil.el
@@ -54,8 +54,8 @@
 
 (after! evil-snipe
   (setq evil-snipe-scope 'visible
-        evil-snipe-tab-increment t
-        evil-snipe-auto-scroll t))
+        evil-snipe-tab-increment nil
+        evil-snipe-auto-scroll nil))
 
 ;;Local Variables:
 ;;eval: (unless (modulep! :editor evil) (remove-hook 'write-file-functions #'eval-buffer t))
