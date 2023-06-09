ccd626e3706990dd43aaa3a243d2eb3611e2e541
LemonBreezes <look@strawberrytea.xyz>
2023-06-09 10:04:40 -0400

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -20,7 +20,9 @@
         corfu-auto-delay (if (modulep! :private corfu +tng) 0.0 0.2)
         corfu-on-exact-match nil
         corfu-preselect (if (modulep! :private corfu +tng) 'prompt t)
-        tab-always-indent 'complete)
+        ;;tab-always-indent 'complete
+        tab-first-completion 'word-or-paren
+        )
   (after! corfu-quick
     (setq corfu-quick1 (cae-keyboard-kbd "asdfgh")
           corfu-quick2 (cae-keyboard-kbd "jkl;")))
