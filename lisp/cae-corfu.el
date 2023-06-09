089ea93d33d81808e101c963d233e6ffb420ceac
LemonBreezes <look@strawberrytea.xyz>
2023-06-09 10:09:55 -0400

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -21,7 +21,7 @@
         corfu-on-exact-match nil
         corfu-preselect (if (modulep! :private corfu +tng) 'prompt t)
         tab-always-indent 'complete
-        tab-first-completion 'word-or-paren-or-punct)
+        tab-first-completion 'eol)
   (after! corfu-quick
     (setq corfu-quick1 (cae-keyboard-kbd "asdfgh")
           corfu-quick2 (cae-keyboard-kbd "jkl;")))
