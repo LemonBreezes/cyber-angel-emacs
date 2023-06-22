c89c2fd9a14537bcada166c9aecbbf7c847277a9
LemonBreezes <look@strawberrytea.xyz>
2023-06-22 14:01:10 -0400

    config.el

--- config.el
+++ config.el
@@ -1088,7 +1088,8 @@
                                               (lambda (x) (string-prefix-p "LD_LIBRARY_PATH=" x))
                                               process-environment)))
                     (apply oldfun args))))
-    (define-key shell-maker-mode-map (kbd "C-d") #'cae-ai-chatgpt-quit-or-delete-char)))
+    ;;(define-key shell-maker-mode-map (kbd "C-d") #'cae-ai-chatgpt-quit-or-delete-char)
+    ))
 
 ;;; Email
 
