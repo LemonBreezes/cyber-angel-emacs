8c965addee440f463f95684a0286e4e58f5d5d18
LemonBreezes <look@strawberrytea.xyz>
2023-07-05 20:44:20 -0400

    config.el

--- config.el
+++ config.el
@@ -322,9 +322,9 @@
     (add-hook 'outline-minor-mode-hook #'outline-minor-faces-mode))
 
   ;; Using this package removes the margin from the left side of the window and
-  ;; causes `dired-next-line' to move forward by two characters instead of one.
-  ;; This package is really cool for laptops with small screens but I will look
-  ;; for an alternative next time that use-case comes up.
+  ;; causes `dired-next-line' to move forward by two lines instead of one. This
+  ;; package is really cool for laptops with small screens but I will look for
+  ;; an alternative next time that use-case comes up.
   ;;(use-package! perfect-margin
   ;;  :defer t :init
   ;;  (add-hook 'doom-first-buffer-hook #'perfect-margin-mode)
