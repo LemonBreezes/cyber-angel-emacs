58ec2e33e80185888aba7a3aee980e4b4c28536e
LemonBreezes <look@strawberrytea.xyz>
2023-07-09 01:07:36 -0400

    modules/private/exwm/+startup.el

---
+++ modules/private/exwm/+startup.el
@@ -0,0 +1,36 @@
+;;; private/exwm/+startup.el -*- lexical-binding: t; -*-
+
+;; The way we integrate `persp-mode' and EXWM is by running our most used
+;; programs on startup and using these programs in a dedicated workspace.
+
+(defvar +startup-prefix "k")
+(defvar +startup-directory (expand-file-name "startup/" doom-user-dir))
+(defvar +startup-config-dir (expand-file-name "misc-files/config-files" doom-user-dir))
+
+(map! :leader :prefix (+startup-prefix . "programs"))
+
+(defun +workspace-switch-to-exwm-buffer-maybe (&rest _)
+  (let ((buf (cl-find-if
+              (lambda (buf)
+                (and (buffer-local-value 'exwm-class-name buf)
+                     (string-match-p (downcase (+workspace-current-name))
+                                     (buffer-local-value 'exwm-class-name buf))))
+              (buffer-list))))
+    (when (and buf (not (+popup-window-p)))
+      (unless (doom-visible-buffer-p buf)
+        (switch-to-buffer buf)))))
+
+;; Run startup progrms
+(defun tmp/exwm-init-hook ()
+  (dolist (file (directory-files +startup-directory t "\\.el$"))
+    (load (file-name-sans-extension file) nil t))
+
+  (advice-add #'+workspace/switch-to :after #'+workspace-switch-to-exwm-buffer-maybe)
+  (remove-hook 'exwm-init-hook #'tmp/exwm-init-hook))
+
+;;(after! exwm
+;;  (if exwm--connection
+;;      (progn (tmp/exwm-init-hook)
+;;             (add-to-list 'persp-filter-save-buffers-functions
+;;                          #'exwm--buffer->id))
+;;    (add-hook 'exwm-init-hook #'tmp/exwm-init-hook 'append)))
