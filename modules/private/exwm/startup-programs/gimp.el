170c479c0325716d6c1ccb06f92b8c84fa45efe3
LemonBreezes <look@strawberrytea.xyz>
2023-07-11 17:21:02 -0400

    modules/private/exwm/startup-programs/gimp.el

--- modules/private/exwm/startup-programs/gimp.el
+++ modules/private/exwm/startup-programs/gimp.el
@@ -1,3 +1,5 @@
 ;;; private/exwm/startup-programs/gimp.el -*- lexical-binding: t; -*-
 
 (defvar startup/gimp-process nil)
+(defvar startup/gimp-executable (executable-find "gimp"))
+(defvar startup/gimp-workspace "GIMP")
