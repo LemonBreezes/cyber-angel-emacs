3c71e5bab53c03a0d9654a0451db49cb3677c729
LemonBreezes <look@strawberrytea.xyz>
2023-07-15 11:28:58 -0400

    modules/private/exwm/packages.el

--- modules/private/exwm/packages.el
+++ modules/private/exwm/packages.el
@@ -5,3 +5,7 @@
   (:host github :repo "ch11ng/exwm"))
 (package! exwm-edit)
 (package! exwm-mff)
+(when (modulep! :editor evil +everywhere)
+  (package! exwm-evil
+    :recipe (:host github :repo "LemonBreezes/exwm-evil"))
+  (package! exwm-firefox-evil))
