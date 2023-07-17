9210df7bdaefd65dbc8252048200467ac42fcde1
LemonBreezes <look@strawberrytea.xyz>
2023-07-17 01:27:34 -0400

    modules/private/exwm/config.el

--- modules/private/exwm/config.el
+++ modules/private/exwm/config.el
@@ -29,13 +29,7 @@ expansion occurs within the parent Emacs session.")
     (unless (or (string-prefix-p "sun-awt-X11-" exwm-instance-name)
                 (string= "gimp" exwm-instance-name))
       (exwm-workspace-rename-buffer exwm-title)))
-  (defun +exwm-rename-buffer-to-class ()
-    "Rename the buffer to its `exwm-class-name'."
-    (unless (or (string-prefix-p "sun-awt-X11-" exwm-instance-name)
-                (string= "gimp" exwm-instance-name)
-                (string= (buffer-name) exwm-class-name))
-      (exwm-workspace-rename-buffer exwm-class-name)))
-  (add-hook 'exwm-update-title-hook #'+exwm-rename-buffer-to-class)
+  (add-hook 'exwm-update-title-hook #'+exwm-rename-buffer-to-title)
 
   (use-package! exwm
     :config
