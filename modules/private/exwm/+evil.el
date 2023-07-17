cdd65a1e1f0e7e66e9f9725683f32539a663e7a7
LemonBreezes <look@strawberrytea.xyz>
2023-07-17 01:15:29 -0400

    modules/private/exwm/+evil.el

--- modules/private/exwm/+evil.el
+++ modules/private/exwm/+evil.el
@@ -1,7 +1,8 @@
 ;;; private/exwm/+evil.el -*- lexical-binding: t; -*-
 
 (after! evil
-  (evil-set-initial-state 'exwm-mode 'normal))
+  (evil-set-initial-state 'exwm-mode 'normal)
+  (evil-esc-mode -1))
 (after! evil-snipe
   (add-to-list 'evil-snipe-disabled-modes 'exwm-mode))
 
