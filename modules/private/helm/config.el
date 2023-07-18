1ccb94112c814a544f9d4f222e969d3591c746b9
LemonBreezes <look@strawberrytea.xyz>
2023-07-18 13:15:57 -0400

    modules/private/helm/config.el

--- modules/private/helm/config.el
+++ modules/private/helm/config.el
@@ -91,45 +91,44 @@ Can be negative.")
   (after! which-key
     (which-key-add-key-based-replacements "C-x c" "helm"))
 
-  (map!
-   (:after helm :map helm-map
-    [remap next-line]     #'helm-next-line
-    [remap previous-line] #'helm-previous-line
-    [left]     #'left-char
-    [right]    #'right-char
-    "C-S-f"    #'helm-previous-page
-    "C-S-n"    #'helm-next-source
-    "C-S-p"    #'helm-previous-source
-    (:when (modulep! :editor evil +everywhere)
-     "C-j"    #'helm-next-line
-     "C-k"    #'helm-previous-line
-     "C-S-j"  #'helm-next-source
-     "C-S-k"  #'helm-previous-source)
-    "C-u"      #'helm-delete-minibuffer-contents
-    "C-s"      #'helm-minibuffer-history
-    ;; Swap TAB and C-z
-    "TAB"      #'helm-execute-persistent-action
-    [tab]      #'helm-execute-persistent-action
-    "C-z"      #'helm-select-action)
-   (:after helm-ag :map helm-ag-map
-    "C--"      #'+helm-do-ag-decrease-context
-    "C-="      #'+helm-do-ag-increase-context
-    [left]     nil
-    [right]    nil)
-   (:after helm-files :map (helm-find-files-map helm-read-file-map)
-    [C-return] #'helm-ff-run-switch-other-window
-    "C-w"      #'helm-find-files-up-one-level
-    (:when (modulep! :editor evil +everywhere)
-     "C-h"    #'helm-find-files-up-one-level
-     "C-l"    #'helm-execute-persistent-action))
-   (:after helm-locate :map helm-generic-files-map
-    [C-return] #'helm-ff-run-switch-other-window)
-   (:after helm-buffers :map helm-buffer-map
-    [C-return] #'helm-buffer-switch-other-window)
-   (:after helm-occur :map helm-occur-map
-    [C-return] #'helm-occur-run-goto-line-ow)
-   (:after helm-grep :map helm-grep-map
-    [C-return] #'helm-grep-run-other-window-action))
+  (map! :map helm-map
+        [remap next-line] #'helm-next-line
+        [remap previous-line] #'helm-previous-line
+        [left] #'left-char
+        [right] #'right-char
+        "C-S-f" #'helm-previous-page
+        "C-S-n" #'helm-next-source
+        "C-S-p" #'helm-previous-source
+        (:when (modulep! :editor evil +everywhere)
+         "C-j" #'helm-next-line
+         "C-k" #'helm-previous-line
+         "C-S-j" #'helm-next-source
+         "C-S-k" #'helm-previous-source)
+        "C-u" #'helm-delete-minibuffer-contents
+        "C-s" #'helm-minibuffer-history
+        ;; Swap TAB and C-z
+        "TAB" #'helm-execute-persistent-action
+        [tab] #'helm-execute-persistent-action
+        "C-z" #'helm-select-action
+        (:after helm-ag :map helm-ag-map
+         "C--" #'+helm-do-ag-decrease-context
+         "C-=" #'+helm-do-ag-increase-context
+         [left] nil
+         [right] nil)
+        (:after helm-files :map (helm-find-files-map helm-read-file-map)
+         [C-return] #'helm-ff-run-switch-other-window
+         "C-w" #'helm-find-files-up-one-level
+         (:when (modulep! :editor evil +everywhere)
+          "C-h" #'helm-find-files-up-one-level
+          "C-l" #'helm-execute-persistent-action))
+        (:after helm-locate :map helm-generic-files-map
+         [C-return] #'helm-ff-run-switch-other-window)
+        (:after helm-buffers :map helm-buffer-map
+         [C-return] #'helm-buffer-switch-other-window)
+        (:after helm-occur :map helm-occur-map
+         [C-return] #'helm-occur-run-goto-line-ow)
+        (:after helm-grep :map helm-grep-map
+         [C-return] #'helm-grep-run-other-window-action))
 
   (set-popup-rule! "^\\*helm" :vslot -100 :size 0.3 :ttl nil)
 
