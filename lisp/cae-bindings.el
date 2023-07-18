a51e3a4e808d21fc3223aa72e371b379b18fe72e
LemonBreezes <look@strawberrytea.xyz>
2023-07-18 18:32:21 -0400

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -202,31 +202,32 @@
 (unless (lookup-key help-map (kbd "SPC"))
   (define-key help-map (kbd "SPC") #'cae-pop-mark))
 
-(map! :prefix "C-z"
-      :e "n" #'avy-goto-line-below
-      :e "p" #'avy-goto-line-above
-      ;;"y" #'avy-copy-region
-      :e "c" #'avy-goto-char
-      ;;"m" #'avy-move-region
-      :e "l" #'avy-goto-line
-      :e "e" #'avy-goto-end-of-line
-      :e "." #'cae-avy-symbol-at-point
-      ;;"k" #'avy-kill-region
-      ;;"w" #'avy-kill-ring-save-region
-      :e "a" #'cae-avy-embark-act-on-region
-      :e "j" #'avy-goto-word-1
-      (:when (modulep! :editor fold)
-       :e "f" #'vimish-fold-avy)
-      :e "o" #'switch-window
-      :e "0" #'switch-window-then-delete
-      :e "1" #'switch-window-then-maximize
-      :e "2" #'switch-window-then-split-horizontally
-      :e "3" #'switch-window-then-split-vertically
-      :e "4" #'switch-window-then-kill-buffer
-      ;;"r" #'avy-resume ; `avy-resume' is too buggy to be useful.
-      :e "SPC" #'avy-goto-char-timer
-      (:map isearch-mode-map
-       :e "j" #'avy-isearch))
+(unless (modulep! :editor evil)
+  (map! :prefix "C-z"
+        "n" #'avy-goto-line-below
+        "p" #'avy-goto-line-above
+        ;;"y" #'avy-copy-region
+        "c" #'avy-goto-char
+        ;;"m" #'avy-move-region
+        "l" #'avy-goto-line
+        "e" #'avy-goto-end-of-line
+        "." #'cae-avy-symbol-at-point
+        ;;"k" #'avy-kill-region
+        ;;"w" #'avy-kill-ring-save-region
+        "a" #'cae-avy-embark-act-on-region
+        "j" #'avy-goto-word-1
+        (:when (modulep! :editor fold)
+         "f" #'vimish-fold-avy)
+        "o" #'switch-window
+        "0" #'switch-window-then-delete
+        "1" #'switch-window-then-maximize
+        "2" #'switch-window-then-split-horizontally
+        "3" #'switch-window-then-split-vertically
+        "4" #'switch-window-then-kill-buffer
+        ;;"r" #'avy-resume ; `avy-resume' is too buggy to be useful.
+        "SPC" #'avy-goto-char-timer
+        (:map isearch-mode-map
+         "j" #'avy-isearch)))
 
 (when (modulep! :completion vertico)
   (after! vertico
