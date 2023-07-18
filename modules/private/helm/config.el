72784f3eb3cff0dc2789f4b0e37382ffaca27f0b
LemonBreezes <look@strawberrytea.xyz>
2023-07-18 12:41:32 -0400

    modules/private/helm/config.el

--- modules/private/helm/config.el
+++ modules/private/helm/config.el
@@ -151,7 +151,10 @@ Can be negative.")
         helm-posframe-height 0.35
         helm-posframe-min-width 160
         helm-posframe-min-height 20
-        helm-posframe-border-width 1))
+        helm-posframe-border-width 1)
+  (defadvice! +helm-ensure-window-not-dedicated-a (&rest _)
+    :before #'helm-show-action-buffer
+    (set-window-dedicated-p (get-buffer-window) nil)))
 
 
 (use-package! helm-flx
