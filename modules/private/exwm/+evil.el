cb6e61dd20f9ac84093b2998dd438ac2224fb1f5
LemonBreezes <look@strawberrytea.xyz>
2023-07-18 20:16:59 -0400

    modules/private/exwm/+evil.el

--- modules/private/exwm/+evil.el
+++ modules/private/exwm/+evil.el
@@ -8,11 +8,10 @@
 ;; Fixes focus being lost from EXWM buffers when switching workspaces or
 ;; buffers. This seems to be a problem specific to Evil. and perhaps Doom. There
 ;; is probably a more elegant solution but it requires more digging.
-;;(add-hook 'doom-switch-buffer-hook #'+exwm-refocus-application)
-;;(add-hook 'doom-switch-window-hook #'+exwm-refocus-application)
-;;(run-with-idle-timer 1 t #'+exwm-refocus-application)
-;;(when (featurep! :ui workspaces)
-;;  (add-hook 'persp-before-switch-functions #'+exwm-refocus-application))
+(add-hook 'doom-switch-buffer-hook #'+exwm-refocus-application)
+(add-hook 'doom-switch-window-hook #'+exwm-refocus-application)
+(when (featurep! :ui workspaces)
+  (add-hook 'persp-before-switch-functions #'+exwm-refocus-application))
 
 
 (use-package exwm-evil
