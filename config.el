97cad492220a3f741196be685dcaeafdd43afe1c
LemonBreezes <look@strawberrytea.xyz>
2023-07-20 14:14:50 -0400

    config.el

--- config.el
+++ config.el
@@ -428,7 +428,7 @@
 
   (after! tramp
     (setq tramp-shell-prompt-pattern
-          "[[\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>].* *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*\"]")
+          "\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>].* *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*")
     (dolist (path '("~/.guix-profile/bin" "~/.guix-profile/sbin"
                     "/run/current-system/profile/bin"
                     "/run/current-system/profile/sbin"))
