2a4f5aa0915d655b68cd3bda2894be6715a1148b
LemonBreezes <look@strawberrytea.xyz>
2023-07-21 12:38:41 -0400

    modules/private/exwm/config.el

--- modules/private/exwm/config.el
+++ modules/private/exwm/config.el
@@ -134,6 +134,15 @@ expansion occurs within the parent Emacs session.")
     ;; Show EXWM buffers in buffer switching prompts.
     (add-hook 'exwm-mode-hook #'doom-mark-buffer-as-real-h)
 
+    ;; Fixes focus being lost from EXWM buffers when switching workspaces or
+    ;; buffers.
+    (add-hook 'doom-switch-buffer-hook #'+exwm-refocus-application)
+    (add-hook 'doom-switch-window-hook #'+exwm-refocus-application)
+    (add-hook 'doom-escape-hook #'+exwm-refocus-application)
+    (when (featurep! :ui workspaces)
+      (add-hook 'persp-before-switch-functions #'+exwm-refocus-application))
+
+
     ;; Set up key translations for using Emacs keybindings in X windows.
     ;;(unless (modulep! :editor evil)
     ;;  (setq exwm-input-simulation-keys
