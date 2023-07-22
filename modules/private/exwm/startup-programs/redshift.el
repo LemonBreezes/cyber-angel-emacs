9dcadc7f3e08f8905f9c8fcae0e64ee8cbd62ead
LemonBreezes <look@strawberrytea.xyz>
2023-07-22 12:26:52 -0400

    modules/private/exwm/startup-programs/redshift.el

--- modules/private/exwm/startup-programs/redshift.el
+++ modules/private/exwm/startup-programs/redshift.el
@@ -17,7 +17,7 @@
                               ":"
                               (number-to-string calendar-longitude))
                  (if (cae-night-mode-p)
-                     `("-P" "-O" "3700" "-b"
+                     `("-P" "-O" "4200" "-b"
                        ;; Set monitor brightness lower in certain applications.
                        ,(pcase (+workspace-current-name)
                           ("Teams" "0.6")
