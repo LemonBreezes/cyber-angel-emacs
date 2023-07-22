76eefd83c206d3a7641c862f94368a44b9d17529
LemonBreezes <look@strawberrytea.xyz>
2023-07-22 12:25:16 -0400

    modules/private/exwm/startup-programs/redshift.el

--- modules/private/exwm/startup-programs/redshift.el
+++ modules/private/exwm/startup-programs/redshift.el
@@ -51,6 +51,6 @@
 (add-hook 'doom-load-theme-hook #'startup/restart-redshift)
 ;;(advice-add #'+workspace-switch :after #'startup/restart-redshift)
 
-;;(if (process-live-p startup/redshift-process)
-;;    (startup/restart-redshift)
-;;  (startup/start-redshift))
+(if (process-live-p startup/redshift-process)
+    (startup/restart-redshift)
+  (startup/start-redshift))
