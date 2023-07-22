0a2d457f3edd1a476cfa80f5128d3a8494c34f42
LemonBreezes <look@strawberrytea.xyz>
2023-07-22 12:19:22 -0400

    modules/private/exwm/startup-programs/redshift.el

--- modules/private/exwm/startup-programs/redshift.el
+++ modules/private/exwm/startup-programs/redshift.el
@@ -16,20 +16,16 @@
                  "-l" (concat (number-to-string calendar-latitude)
                               ":"
                               (number-to-string calendar-longitude))
-                 (pcase (modus-themes--current-theme)
-                   ((or modus-vivendi modus-vivendi-tinted
-                        modus-vivendi-tritanopia modus-vivendi-deuteranopia)
-                    `("-P" "-O" "4200" "-b"
-                      ;; Set monitor brightness lower in certain applications.
-                      ,(pcase (+workspace-current-name)
-                         ("Chromium" "1.0")
-                         ("Teams" "0.6")
-                         ("Discord" "1.0")
-                         (_ "1.0"))
-                      "-g" "1.0"))
-                   ((or modus-operandi modus-operandi-tinted
-                        modus-operandi-tritanopia modus-operandi-deuteranopia)
-                    '("-P" "-O" "6300" "-b" "1.0" "-g" "1.0")))))))
+                 (if (cae-night-mode-p)
+                     `("-P" "-O" "4200" "-b"
+                       ;; Set monitor brightness lower in certain applications.
+                       ,(pcase (+workspace-current-name)
+                          ("Chromium" "1.0")
+                          ("Teams" "0.6")
+                          ("Discord" "1.0")
+                          (_ "1.0"))
+                       "-g" "1.0")
+                   '("-P" "-O" "6300" "-b" "1.0" "-g" "1.0"))))))
 
 (defun startup/kill-redshift (&optional arg)
   (interactive "p")
