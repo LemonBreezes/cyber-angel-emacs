153734411eb6606b1272e280c6f57775dd1fe707
LemonBreezes <look@strawberrytea.xyz>
2023-07-23 18:03:48 -0400

    modules/private/vc/config.el

--- modules/private/vc/config.el
+++ modules/private/vc/config.el
@@ -134,5 +134,4 @@
 
 (after! git-timemachine
   (map! :map git-timemachine-mode-map
-        "<f6>" #'git-timemachine-help
-        [remap git-timemachine-help] #'cae-git-timemachine-help))
+        "<f6>" #'git-timemachine-help))
