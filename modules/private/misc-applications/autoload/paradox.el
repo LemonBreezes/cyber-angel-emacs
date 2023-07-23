a80924ee32d23de68187acfd8da69dd209e061dd
LemonBreezes <look@strawberrytea.xyz>
2023-07-23 18:23:17 -0400

    modules/private/misc-applications/autoload/paradox.el

---
+++ modules/private/misc-applications/autoload/paradox.el
@@ -0,0 +1,8 @@
+;;; private/misc-applications/autoload/paradox.el -*- lexical-binding: t; -*-
+
+(defun cae-paradox-menu-quick-help ()
+  (interactive)
+  (if (memq 'last-command '(cae-paradox-menu-quick-help
+                            paradox-menu-quick-help))
+      (clear-minibuffer-message)
+    (call-interactively #'paradox-menu-quick-help)))
