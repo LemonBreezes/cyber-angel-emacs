457e60fc4e07fa7b26dfabbfa3867e0c7d3e2368
LemonBreezes <look@strawberrytea.xyz>
2023-07-23 14:41:19 -0400

    autoload/cae-bindings.el

--- autoload/cae-bindings.el
+++ autoload/cae-bindings.el
@@ -3,5 +3,6 @@
 ;;;###autoload
 (defun cae-embark-act ()
   (interactive)
+  (require 'embark)
   (let ((embark-cycle-key (key-description (this-command-keys))))
     (call-interactively 'embark-act)))
