7e376a788fce56a4aeca752dcfb7f8178e6c7a34
LemonBreezes <look@strawberrytea.xyz>
2023-07-23 18:03:11 -0400

    modules/private/vc/autoload.el

--- modules/private/vc/autoload.el
+++ modules/private/vc/autoload.el
@@ -43,5 +43,5 @@ Meant to be used like:
 (defun cae-git-timemachine-help ()
   (interactive)
   (if transient--original-buffer
-      (keyboard-quit)
+      (doom/escape)
     (call-interactively  #'git-timemachine-help)))
