9491ed81c59b673888632df1d3d64d4d7c74baab
LemonBreezes <look@strawberrytea.xyz>
2023-07-23 15:26:03 -0400

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -254,7 +254,8 @@ mark the string and call `edit-indirect-region' with it."
           ((eq this-command 'cae-avy-symbol-at-point)
            (er/mark-symbol))
           (t (eri/expand-region 1)))
-    (call-interactively action)))
+    (call-interactively action))
+  (pop-mark))
 
 ;;;###autoload
 (defalias 'cae-avy-action-embark-act
