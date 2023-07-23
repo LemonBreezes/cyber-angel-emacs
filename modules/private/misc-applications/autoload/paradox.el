15c0e6dc6885446b90886b16e799a8bbe47daed3
LemonBreezes <look@strawberrytea.xyz>
2023-07-23 18:23:46 -0400

    modules/private/misc-applications/autoload/paradox.el

--- modules/private/misc-applications/autoload/paradox.el
+++ modules/private/misc-applications/autoload/paradox.el
@@ -4,5 +4,5 @@
   (interactive)
   (if (memq 'last-command '(cae-paradox-menu-quick-help
                             paradox-menu-quick-help))
-      (keyboard-quit)
+      (call-interactively #'keyboard-quit)
     (call-interactively #'paradox-menu-quick-help)))
