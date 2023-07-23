eb5325408dc244e2d05b3ca1c0c13608c9b44e6c
LemonBreezes <look@strawberrytea.xyz>
2023-07-23 18:32:22 -0400

    modules/private/misc-applications/autoload/paradox.el

--- modules/private/misc-applications/autoload/paradox.el
+++ modules/private/misc-applications/autoload/paradox.el
@@ -2,4 +2,12 @@
 
 (defun cae-paradox-menu-quick-help ()
   (interactive)
-  (call-interactively #'paradox-menu-quick-help))
+  (if (and (memq last-command '(cae-paradox-menu-quick-help
+                                paradox-menu-quick-help))
+           ;;(string= (current-message)
+           ;;         (mapconcat 'paradox--prettify-key-descriptor
+           ;;                    paradox--key-descriptors "\n"))
+           )
+      (progn (clear-minibuffer-message)
+             (message ""))
+    (call-interactively #'paradox-menu-quick-help)))
