cd39f886e1193b0a2b756b3d04c4c73e32607b9c
LemonBreezes <look@strawberrytea.xyz>
2023-07-24 12:39:06 -0400

    modules/private/misc-applications/+ednc.el

--- modules/private/misc-applications/+ednc.el
+++ modules/private/misc-applications/+ednc.el
@@ -28,17 +28,17 @@
           (kill-buffer)))))
   (add-hook 'ednc-notification-presentation-functions #'show-notification-in-buffer)
 
-  ;;(defun stack-notifications (&optional hide)
-  ;;  (mapconcat (lambda (notification)
-  ;;               (let ((app-name (ednc-notification-app-name notification)))
-  ;;                 (unless (member app-name hide)
-  ;;                   (push app-name hide)
-  ;;                   (ednc-format-notification notification))))
-  ;;             (ednc-notifications) ""))
-  ;;(add-to-list 'global-mode-string
-  ;;             '((:eval (stack-notifications))))
-  ;;(add-hook 'ednc-notification-presentation-functions
-  ;;          (lambda (&rest _) (force-mode-line-update t)))
+  (defun stack-notifications (&optional hide)
+    (mapconcat (lambda (notification)
+                 (let ((app-name (ednc-notification-app-name notification)))
+                   (unless (member app-name hide)
+                     (push app-name hide)
+                     (ednc-format-notification notification))))
+               (ednc-notifications) ""))
+  (add-to-list 'global-mode-string
+               '((:eval (stack-notifications))))
+  (add-hook 'ednc-notification-presentation-functions
+            (lambda (&rest _) (force-mode-line-update t)))
   (map! :map ednc-view-mode-map
         "n" #'next-line
         "p" #'previous-line))
