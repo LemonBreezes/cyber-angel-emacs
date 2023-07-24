ed43eb49ba16272a51a6672b78a56375d9aa770f
LemonBreezes <look@strawberrytea.xyz>
2023-07-24 12:57:03 -0400

    modules/private/misc-applications/autoload/ednc.el

--- modules/private/misc-applications/autoload/ednc.el
+++ modules/private/misc-applications/autoload/ednc.el
@@ -6,3 +6,21 @@
   (require 'ednc)
   (let ((buf (get-buffer-create ednc-log-name)))
     (pop-to-buffer buf)))
+
+;;;###autoload
+(defun +ednc-show-notification-in-buffer (old new)
+  (let ((name (format "*Notification %d*" (ednc-notification-id (or old new)))))
+    (with-current-buffer (get-buffer-create name)
+      (if new (let ((inhibit-read-only t))
+                (if old (erase-buffer) (ednc-view-mode))
+                (set-buffer-multibyte nil)
+                (insert (ednc-format-notification new t))
+                (display-buffer (current-buffer)))
+        (kill-buffer)))))
+
+;;;###autoload
+(defun +ednc-dismiss-all-notifications ()
+  (interactive)
+  (cl-mapcar (lambda (notification)
+               (ednc-dismiss-notification notification))
+             (ednc-notifications)))
