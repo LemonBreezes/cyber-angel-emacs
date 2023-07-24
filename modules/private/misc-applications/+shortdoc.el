34a12f944c0bfe46413b4c735237c657389cb558
LemonBreezes <look@strawberrytea.xyz>
2023-07-24 11:30:24 -0400

    modules/private/misc-applications/+shortdoc.el

--- modules/private/misc-applications/+shortdoc.el
+++ modules/private/misc-applications/+shortdoc.el
@@ -6,8 +6,5 @@
   :init
   (map! :map +misc-applications-lookup-map
         "s" #'shortdoc)
-  (after! which-key
-    (which-key-add-keymap-based-replacements +misc-applications-lookup-map
-      "s" "shortdoc"))
   :config
   (require 'posimacs-shortdocs))
