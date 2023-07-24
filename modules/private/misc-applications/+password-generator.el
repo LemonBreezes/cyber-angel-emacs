ac9e1aec03ab299d4cc549704b65d261f5dac96f
LemonBreezes <look@strawberrytea.xyz>
2023-07-24 01:22:29 -0400

    modules/private/misc-applications/+password-generator.el

--- modules/private/misc-applications/+password-generator.el
+++ modules/private/misc-applications/+password-generator.el
@@ -4,10 +4,11 @@
   :defer t
   :init
   (map! :leader
-        :prefix +misc-applications-insert-prefix
-        "pc" #'password-generator-custom
-        "ps" #'password-generator-simple
-        "pt" #'password-generator-strong
-        "pn" #'password-generator-numeric
-        "pp" #'password-generator-paranoid
-        "ph" #'password-generator-phonetic))
+        (:prefix +misc-applications-insert-prefix
+         (:prefix ("p" . "password-generator")
+          "c" #'password-generator-custom
+          "s" #'password-generator-simple
+          "t" #'password-generator-strong
+          "n" #'password-generator-numeric
+          "p" #'password-generator-paranoid
+          "h" #'password-generator-phonetic))))
