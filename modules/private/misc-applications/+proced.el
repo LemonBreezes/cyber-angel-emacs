6589e3bfe0195152c8a94cbd0bac579facf11e71
LemonBreezes <look@strawberrytea.xyz>
2023-07-24 12:48:30 -0400

    modules/private/misc-applications/+proced.el

--- modules/private/misc-applications/+proced.el
+++ modules/private/misc-applications/+proced.el
@@ -2,9 +2,6 @@
 
 (use-package! proced
   :defer t
-  :init
-  ;;(map! :leader :prefix +misc-applications-system-prefix
-  ;;      :desc "system processes" "s" #'proced)
   :config
   (setq proced-enable-color-flag t)
   (map! :map proced-mode-map
