4f130db4d148070d0acbeeffcae9721c7cd545be
LemonBreezes <look@strawberrytea.xyz>
2023-07-24 00:27:48 -0400

    modules/private/misc-applications/+helm-rage.el

--- modules/private/misc-applications/+helm-rage.el
+++ modules/private/misc-applications/+helm-rage.el
@@ -1,6 +1,6 @@
 ;;; private/misc-applications/+helm-rage.el -*- lexical-binding: t; -*-
 
-(use-package! helm-linux-disks
+(use-package! helm-rage
   :defer t
   :when (or (modulep! :private helm)
             (modulep! :completion helm))
