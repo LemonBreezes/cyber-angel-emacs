32014208dc2b07524228a037b425b46784d49947
LemonBreezes <look@strawberrytea.xyz>
2023-07-24 11:06:45 -0400

    modules/private/misc-applications/+leetcode.el

--- modules/private/misc-applications/+leetcode.el
+++ modules/private/misc-applications/+leetcode.el
@@ -8,9 +8,11 @@
   :init
   (when (memq system-type '(cygwin windows-nt ms-dos))
     (advice-add #'leetcode--install-my-cookie :override #'ignore))
-  (map! :leader
-        :prefix +misc-applications-standalone-apps-prefix
-        :desc "LeetCode" "l" #'+leetcode)
+  (map! :map +misc-applications-standalone-apps-map
+        "l" #'+leetcode)
+  (after! which-key
+    (which-key-add-keymap-based-replacements +misc-applications-standalone-apps-map
+      "l" "LeetCode"))
   :config
   (map! :map leetcode--problems-mode-map
         "q" #'+leetcode-soft-quit
@@ -19,14 +21,14 @@
         :map leetcode--problem-detail-mode-map
         "o" #'link-hint-open-link)
   (add-hook 'leetcode-solution-mode-hook
-    (lambda ()
-      ;; Flycheck will emit errors because the code does not have any
-      ;; import/include/etc statements. Besides, we can't use Flycheck
-      ;; in an interview anyway.
-      (when (bound-and-true-p flycheck-mode)
-        (flycheck-mode -1))
-      ;; Copilot is basically cheating, so disable it too.
-      (when (bound-and-true-p copilot-mode)
-        (copilot-mode -1))))
+            (lambda ()
+              ;; Flycheck will emit errors because the code does not have any
+              ;; import/include/etc statements. Besides, we can't use Flycheck
+              ;; in an interview anyway.
+              (when (bound-and-true-p flycheck-mode)
+                (flycheck-mode -1))
+              ;; Copilot is basically cheating, so disable it too.
+              (when (bound-and-true-p copilot-mode)
+                (copilot-mode -1))))
   (setq leetcode-save-solutions t)
   (setq leetcode-directory "~/src/leetcode"))
