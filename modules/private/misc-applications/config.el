f63e81a12053206e52a5414677f2bdf7891b9188
LemonBreezes <look@strawberrytea.xyz>
2023-07-24 10:11:08 -0400

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -109,8 +109,7 @@
         (dolist (file +misc-applications-lisp-files)
           (load! file))
         (setq unread-command-events
-              (nconc (listify-key-sequence (kbd (concat doom-leader-key " " +misc-applications-prefix)))
-                     unread-command-events))
+              (listify-key-sequence (kbd (concat doom-leader-key " " +misc-applications-prefix))))
         (setq which-key-inhibit t)
         (add-transient-hook! 'pre-command-hook
           (setq which-key-inhibit nil))
