319b03c1d304921dc54a6101327897297059f5d1
StrawberryTea <look@strawberrytea.xyz>
2023-07-26 09:58:37 -0400

    modules/private/exwm/startup-programs/dunst.el

--- modules/private/exwm/startup-programs/dunst.el
+++ modules/private/exwm/startup-programs/dunst.el
@@ -1,51 +1,50 @@
 ;;; startup/dunst.el -*- lexical-binding: t; -*-
 
-(after! alert
-  (setq alert-default-style 'libnotify))
+(when (executable-find "dunstctl")
+  (after! alert
+    (setq alert-default-style 'libnotify))
 
-(defvar startup/dunst-process nil)
+  (defvar startup/dunst-process nil)
 
-(defun startup/start-dunst ()
-  (setq startup/dunst-process
-        (start-process "dunst" " *startup/dunst*" "dunst" "-config"
-                       (expand-file-name
-                        (if (cae-dark-theme-p)
-                            "dunstrc-dark"
-                          "dunstrc-light")
-                        +startup-config-dir))))
+  (defun startup/start-dunst ()
+    (setq startup/dunst-process
+          (start-process "dunst" " *startup/dunst*" "dunst" "-config"
+                         (expand-file-name
+                          (if (cae-dark-theme-p)
+                              "dunstrc-dark"
+                            "dunstrc-light")
+                          +startup-config-dir))))
 
-(defun startup/kill-dunst ()
-  (interactive)
-  (when (process-live-p startup/dunst-process)
-    (kill-process startup/dunst-process)))
+  (defun startup/kill-dunst ()
+    (interactive)
+    (when (process-live-p startup/dunst-process)
+      (kill-process startup/dunst-process)))
 
-(defun startup/restart-dunst ()
-  (interactive)
-  (startup/kill-dunst)
-  (startup/start-dunst))
+  (defun startup/restart-dunst ()
+    (interactive)
+    (startup/kill-dunst)
+    (startup/start-dunst))
 
-(add-hook 'circadian-after-load-theme-hook #'startup/restart-dunst)
+  (add-hook 'circadian-after-load-theme-hook #'startup/restart-dunst)
 
-(map! :leader
-      :prefix +startup-prefix
-      (:prefix ("n" . "Dunst")
-       :desc "Restart Dunst" "r" #'startup/restart-dunst
-       :desc "Kill Dunst" "x" #'startup/kill-dunst
-       :desc "Close notification" "c"
-       (cmd! (let ((shell-file-name "/bin/sh"))
-               (quiet! (shell-command "dunstctl close" nil nil))))
-       :desc "History pop" "h"
-       (cmd! (let ((shell-file-name "/bin/sh"))
-               (quiet! (shell-command "dunstctl history-pop" nil nil))))
-       :desc "Close all notifications" "C"
-       (cmd! (let ((shell-file-name "/bin/sh"))
-               (quiet! (shell-command "dunstctl close-all" nil nil))))
-       :desc "Show context menu" "."
-       (cmd! (let ((shell-file-name "/bin/sh"))
-               (quiet! (shell-command "dunstctl context" nil nil))))))
+  (map! :leader
+        :prefix +startup-prefix
+        (:prefix ("n" . "Dunst")
+         :desc "Restart Dunst" "r" #'startup/restart-dunst
+         :desc "Kill Dunst" "x" #'startup/kill-dunst
+         :desc "Close notification" "c"
+         (cmd! (let ((shell-file-name "/bin/sh"))
+                 (quiet! (shell-command "dunstctl close" nil nil))))
+         :desc "History pop" "h"
+         (cmd! (let ((shell-file-name "/bin/sh"))
+                 (quiet! (shell-command "dunstctl history-pop" nil nil))))
+         :desc "Close all notifications" "C"
+         (cmd! (let ((shell-file-name "/bin/sh"))
+                 (quiet! (shell-command "dunstctl close-all" nil nil))))
+         :desc "Show context menu" "."
+         (cmd! (let ((shell-file-name "/bin/sh"))
+                 (quiet! (shell-command "dunstctl context" nil nil))))))
 
-;;(unless (executable-find "dunstctl")
-;;  (error "dunstctl is missing from your PATH."))
-;;(if (process-live-p startup/dunst-process)
-;;    (startup/restart-dunst)
-;;  (startup/start-dunst))
+  (if (process-live-p startup/dunst-process)
+      (startup/restart-dunst)
+    (startup/start-dunst)))
