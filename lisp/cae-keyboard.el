53b5febdc39f45d16594d59894ffc8477c058e2d
StrawberryTea <look@strawberrytea.xyz>
2023-07-26 09:30:37 -0400

    lisp/cae-keyboard.el

--- lisp/cae-keyboard.el
+++ lisp/cae-keyboard.el
@@ -1,5 +1,34 @@
 ;;; ~/.doom.d/lisp/cae-keyboard.el -*- lexical-binding: t; -*-
 
+;;; Escape in the terminal
+
+(setq normal-escape-enabled t)            ; Makes sure we enable escape checking at the very beginning
+                                          ; 1. define function for when esc is pressed
+(defun normal-escape ()
+  (interactive)
+  (let ((event (read-event nil nil 0.15)))
+    (if (not event)                      ; No event soon after, so just escape out...
+        (progn
+          (setq normal-escape-enabled t) ; Makes sure we re-enable escape
+                                         ; checking Maybe this should
+                                         ; conditionally do keyboard-escape-quit
+                                         ; when in the minibuffer?
+          (keyboard-quit))               ; Recent event happened right after, so just put it back in effect...
+      (setq unread-command-events (cons event unread-command-events))
+      (setq unread-command-events (cons 27 unread-command-events))
+      (setq normal-escape-enabled nil)   ; Disable intercepting esc for a moment, so it'll go through as normal
+      )))
+                                         ; 2. Check before each command
+(defun normal-escape-pre-command-handler () (interactive)
+  (setq normal-escape-enabled t))
+(add-hook 'pre-command-hook 'normal-escape-pre-command-handler)
+                                         ; 3. Define map
+(setq normal-escape-map
+  '((normal-escape-enabled keymap
+     (27 . normal-escape))))
+                                         ; 4. Add to end of emulation-mode-map-alists
+(add-to-ordered-list 'emulation-mode-map-alists 'normal-escape-map 300)
+
 ;;; Remap keys
 
 (define-key key-translation-map (cae-keyboard-kbd "C-x t" "0") (kbd "C-x t 0"))
