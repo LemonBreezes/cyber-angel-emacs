278a54d94003348d6973a6513510b04dcb08594a
StrawberryTea <look@strawberrytea.xyz>
2023-07-26 21:39:09 -0400

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -65,25 +65,6 @@
 
 ;;; Set theme based on time of day.
 
-(defun cae-dark-theme-p ()
-  (eq (frame-parameter nil 'background-mode) 'dark))
-(defun cae-night-time-p ()
-  (when-let ((now (reverse (cl-subseq (decode-time) 0 3)))
-        (sunset (or (doom-store-get 'circadian-sunset)
-                    (require 'circadian nil t)
-                    (circadian-sunset)))
-        (sunrise (or (doom-store-get 'circadian-sunrise)
-                     (require 'circadian nil t)
-                     (circadian-sunrise))))
-    (doom-store-put 'circadian-sunset sunset)
-    (doom-store-put 'circadian-sunrise sunrise)
-    (or (and (>= (cl-first now) (cl-first sunset)))
-        (and (< (cl-first now) (cl-first sunrise)))
-        (and (= (cl-first now) (cl-first sunset))
-             (>= (cl-second now) (cl-second sunset)))
-        (and (= (cl-first now) (cl-first sunrise))
-             (< (cl-second now) (cl-second sunrise))))))
-
 (use-package! circadian
   :defer t :defer-incrementally t
   :config
