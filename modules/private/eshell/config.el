4d012869673f1cd9b65af7af5f7e8301ed29fce9
StrawberryTea <look@strawberrytea.xyz>
2023-07-28 16:20:13 -0400

    modules/private/eshell/config.el

--- modules/private/eshell/config.el
+++ modules/private/eshell/config.el
@@ -150,8 +150,28 @@
     (setq eshell-history-ring ambrevar/eshell-history-global-ring))
   (add-hook 'eshell-mode-hook 'ambrevar/eshell-hist-use-global-history)
 
+  (defun ambrevar/ring-delete-first-item-duplicates (ring)
+    "Remove duplicates of last command in history.
+Return RING.
+
+This should be faster then `seq-uniq'.  Unlike
+`eshell-hist-ignoredups' or `comint-input-ignoredups', it does
+not allow duplicates ever.
+Surrounding spaces are ignored when comparing."
+    (let ((first (ring-ref ring 0))
+          (index 1))
+      (while (<= index (1- (ring-length ring)))
+        (if (string= (string-trim first)
+                     (string-trim (ring-ref ring index)))
+            ;; REVIEW: We could stop at the first match, it would be faster and it
+            ;; would eliminate duplicates if we started from a fresh history.
+            ;; From an existing history that would not clean up existing
+            ;; duplicates beyond the first one.
+            (ring-remove ring index)
+          (setq index (1+ index))))
+      ring))
+
   (defun ambrevar/eshell-history-remove-duplicates ()
-    (require 'functions) ; For `ambrevar/ring-delete-first-item-duplicates'.
     (ambrevar/ring-delete-first-item-duplicates eshell-history-ring))
   (add-hook 'eshell-pre-command-hook 'ambrevar/eshell-history-remove-duplicates)
 
