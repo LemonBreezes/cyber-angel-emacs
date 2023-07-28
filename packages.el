887c23597a607ecedc537c94efce79cd55108ef1
StrawberryTea <look@strawberrytea.xyz>
2023-07-27 22:31:07 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -2,9 +2,11 @@
 (unpin! t)
 (disable-packages! evil-escape
                    flyspell
-                   ccls
-                   compat
-                   mu4e-alert)
+                   ccls                 ; I use clangd.
+                   compat               ; I use the latest version of Emacs.
+                   mu4e-alert           ; Mu4e comes with `mu4e-modeline-mode'
+                                        ; and I don't need email notifications.
+                   )
 (unless (modulep! :config default +smartparens)
   (disable-packages! smartparens))
 (when (modulep! :checkers syntax +flymake)
