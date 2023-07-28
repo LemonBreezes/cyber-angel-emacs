465a66d8a0b055f1d3bf93502a861b5501c146f3
StrawberryTea <look@strawberrytea.xyz>
2023-07-27 21:11:00 -0400

    config.el

--- config.el
+++ config.el
@@ -1179,7 +1179,8 @@
         smtpmail-stream-type 'starttls
         smtpmail-queue-mail nil
         smtpmail-queue-dir "~/.mail/queue/cur/"
-        smtpmail-servers-requiring-authorization ".*"))
+        smtpmail-servers-requiring-authorization ".*"
+        smtpmail-smtp-user user-mail-address))
 
 (when (modulep! :email notmuch)
   (setq +notmuch-sync-backend 'mbsync
