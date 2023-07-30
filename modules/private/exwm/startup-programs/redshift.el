e381692a33cb5abb063595841f7353a62ac0b0ad
StrawberryTea <look@strawberrytea.xyz>
2023-07-30 00:29:45 -0400

    modules/private/exwm/startup-programs/redshift.el

--- modules/private/exwm/startup-programs/redshift.el
+++ modules/private/exwm/startup-programs/redshift.el
@@ -21,7 +21,7 @@
                        ;; Set monitor brightness lower in certain applications.
                        ,(pcase (+workspace-current-name)
                           ("Teams" "0.6")
-                          ("Discord" "0.9")
+                          ("Discord" "0.8")
                           (_ "1.0"))
                        "-g" "1.0")
                    '("-P" "-O" "6300" "-b" "1.0" "-g" "1.0"))))))
