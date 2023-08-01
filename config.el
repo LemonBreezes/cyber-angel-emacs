28110720884193f320fa813c670040856819d29c
StrawberryTea <look@strawberrytea.xyz>
2023-08-01 02:04:17 -0400

    config.el

--- config.el
+++ config.el
@@ -981,8 +981,8 @@
              try-complete-file-name-partially
              try-complete-file-name
              try-complete-lisp-symbol-partially
-             try-complete-lisp-symbol
-             try-expand-line)))
+             try-complete-lisp-symbol)
+           hippie-expand-verbose nil))
 
 
   (use-package! copilot
