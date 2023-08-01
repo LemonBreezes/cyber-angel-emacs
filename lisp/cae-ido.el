d710532d3e00e8175a4fe36da9965db2701c39d5
StrawberryTea <look@strawberrytea.xyz>
2023-08-01 01:45:42 -0400

    lisp/cae-ido.el

--- lisp/cae-ido.el
+++ lisp/cae-ido.el
@@ -1,5 +1,17 @@
 ;;; lisp/cae-ido.el -*- lexical-binding: t; -*-
 
+(pushnew! ido-ignore-files "\\`.DS_Store$" "Icon\\?$")
+(setq ido-ignore-buffers
+      '("\\` " "^\\*ESS\\*" "^\\*Messages\\*" "^\\*[Hh]elp" "^\\*Buffer"
+        "^\\*.*Completions\\*$" "^\\*Ediff" "^\\*tramp" "^\\*cvs-" "_region_"
+        " output\\*$" "^TAGS$" "^\*Ido")
+      ido-auto-merge-work-directories-length -1
+      ido-confirm-unique-completion t
+      ido-case-fold t
+      ido-create-new-buffer 'always
+      ido-enable-flex-matching t
+      ido-everywhere t)
+
 (map! :map (ido-common-completion-map ido-file-completion-map)
       "C-w"  #'ido-delete-backward-word-updir
       :map (ido-common-completion-map ido-file-dir-completion-map)
