656f4694a68fc4579b1f18a81be88582d57b2d86
StrawberryTea <look@strawberrytea.xyz>
2023-08-01 01:39:23 -0400

    lisp/cae-ido.el

---
+++ lisp/cae-ido.el
@@ -0,0 +1,16 @@
+;;; lisp/cae-ido.el -*- lexical-binding: t; -*-
+
+(map! :map (ido-common-completion-map ido-file-completion-map)
+        "C-w"  #'ido-delete-backward-word-updir
+        :map (ido-common-completion-map ido-file-dir-completion-map)
+        "C-n"  #'ido-next-match
+        "C-p"  #'ido-prev-match
+        [down] #'ido-next-match
+        [up]   #'ido-prev-match
+        :map ido-file-completion-map
+        ;; Go to $HOME with ~
+        "~"    (cmd! (if (looking-back "/" (point-min))
+                             (insert "~/")
+                         (call-interactively #'self-insert-command))))
+
+(ido-vertical-mode +1)
