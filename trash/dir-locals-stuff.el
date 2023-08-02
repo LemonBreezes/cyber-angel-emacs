9ca6a9e7770ac9a0629cd748806ae0076ed4bd62
StrawberryTea <look@strawberrytea.xyz>
2023-08-02 14:02:21 -0400

    trash/dir-locals-stuff.el

---
+++ trash/dir-locals-stuff.el
@@ -0,0 +1,15 @@
+;;; trash/.dir-locals-stuff.el -*- lexical-binding: t; -*-
+
+;; Automatically compile Emacs Lisp files (if enabled).
+(when (bound-and-true-p cae-config-finished-loading)
+  (when (and (derived-mode-p 'emacs-lisp-mode)
+             (fboundp 'cae-compile-this-elisp-file)
+             (bound-and-true-p cae-config-compilation-enabled))
+    (add-hook 'after-save-hook #'cae-compile-this-elisp-file nil t))
+
+
+  ;; Byte compile autoload files on save.
+  (when (and (fboundp 'cae-compile-list-files-to-compile)
+             (member (buffer-file-name)
+                     (cae-compile-list-files-to-compile)))
+    (add-hook 'after-save-hook #'elisp-byte-compile-file nil t)))
