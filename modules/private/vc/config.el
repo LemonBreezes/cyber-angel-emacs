2e60b1ee1c974208914e25ec1b977ce7aaf820e0
StrawberryTea <look@strawberrytea.xyz>
2023-08-03 10:21:49 -0400

    modules/private/vc/config.el

--- modules/private/vc/config.el
+++ modules/private/vc/config.el
@@ -142,7 +142,7 @@
     :custom
     (consult-gh-default-clone-directory "~/src/")
     (consult-gh-show-preview t)
-    (consult-gh-preview-key "C-l")
+    (consult-gh-preview-key 'any)
     (consult-gh-issue-action #'consult-gh--issue-view-action)
     (consult-gh-repo-action #'consult-gh--repo-browse-files-action)
     (consult-gh-file-action #'consult-gh--files-view-action)
