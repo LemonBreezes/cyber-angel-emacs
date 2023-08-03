79d6781e839b401c1de195a141bc1c3fbcc38f64
StrawberryTea <look@strawberrytea.xyz>
2023-08-03 17:16:18 -0400

    config.el

--- config.el
+++ config.el
@@ -463,7 +463,8 @@
 
   (after! tramp
     (setq tramp-shell-prompt-pattern
-          "\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>].* *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*")
+          "\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>].* *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*"
+          tramp-use-scp-direct-remote-copying t)
     (dolist (path '("~/.guix-profile/bin" "~/.guix-profile/sbin"
                     "/run/current-system/profile/bin"
                     "/run/current-system/profile/sbin"))
