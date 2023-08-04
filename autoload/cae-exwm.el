232d724f1d0a55b56235c066dcd427caaf0f64b0
StrawberryTea <look@strawberrytea.xyz>
2023-08-04 14:16:35 -0400

    autoload/cae-exwm.el

--- autoload/cae-exwm.el
+++ autoload/cae-exwm.el
@@ -3,4 +3,7 @@
 ;;;###autoload
 (defun cae-exwm-start-dictation ()
   (interactive)
-  (call-interactively #'exwm-edit--compose))
+  (call-interactively #'exwm-edit--compose)
+  (run-at-time exwm-edit-yank-delay nil
+               (lambda ()
+                 (call-interactively #'whisper-run))))
