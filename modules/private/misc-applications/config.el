852e74f5638f00aa7af29d0e9b4f65fa8f918971
StrawberryTea <look@strawberrytea.xyz>
2023-08-04 20:33:30 -0400

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -112,14 +112,6 @@
           "F" #'elfeed-tube-fetch
           [remap save-buffer] #'elfeed-tube-save)))
 
-(use-package! my-repo-pins
-  :defer t :init
-  (map! :map +misc-applications-standalone-apps-map
-        "j" #'my-repo-pins)
-  :config
-  (make-directory "~/code-root" t)
-  (setq my-repo-pins-code-root "~/code-root"))
-
 
 ;;; External apps
 
