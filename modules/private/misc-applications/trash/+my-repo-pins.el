d3b6698df2817d7a16a8749f69ad23d2d0466869
StrawberryTea <look@strawberrytea.xyz>
2023-08-04 20:33:26 -0400

    modules/private/misc-applications/trash/+my-repo-pins.el

---
+++ modules/private/misc-applications/trash/+my-repo-pins.el
@@ -0,0 +1,10 @@
+;;; private/misc-applications/trash/+my-repo-pins.el -*- lexical-binding: t; -*-
+
+
+(use-package! my-repo-pins
+  :defer t :init
+  (map! :map +misc-applications-standalone-apps-map
+        "j" #'my-repo-pins)
+  :config
+  (make-directory "~/code-root" t)
+  (setq my-repo-pins-code-root "~/code-root"))
