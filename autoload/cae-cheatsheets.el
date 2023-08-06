9c9ff50fc61a0e1749b86163e99d726c2a9d879e
StrawberryTea <look@strawberrytea.xyz>
2023-08-06 14:14:23 -0400

    autoload/cae-cheatsheets.el

--- autoload/cae-cheatsheets.el
+++ autoload/cae-cheatsheets.el
@@ -23,7 +23,7 @@
   ("C-c ;" embark-export "Export")
   ("C-c C-e" +vertico/embark-export-write "Export writable")
   ("C-c C-l" embark-collect "Collect")
-  ("<f6>" nil "Exit" :exit t))
+  ("ESC" nil "Exit" :exit t))
 
 ;;;###autoload (autoload 'cae-debugger-cheatsheet "autoload/cae-cheatsheets" nil t)
 (defun cae-debugger-cheatsheet () (interactive))
