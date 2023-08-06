21c7a536e6ef2a39d6fdaf50cf6a477ee8112d6b
StrawberryTea <look@strawberrytea.xyz>
2023-08-05 20:28:59 -0400

    .

--- modules/private/helm/packages.el
+++ modules/private/helm/packages.el
@@ -4,6 +4,6 @@
 (package! helm)
 
 (when (modulep! +childframe)
-  (package! helm-posframe))
+  (package! helm-posframe :recipe (:host github :repo "LemonBreezes/helm-posframe" :branch "fix-window-dedicated-p-error")))
 (when (modulep! +fuzzy)
   (package! helm-flx))
