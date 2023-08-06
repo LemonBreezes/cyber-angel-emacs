917111290ae6857e47b57386cb6828af91ca1d57
StrawberryTea <look@strawberrytea.xyz>
2023-08-06 14:05:54 -0400

    modules/private/ai/config.el

--- modules/private/ai/config.el
+++ modules/private/ai/config.el
@@ -63,7 +63,6 @@
                                             process-environment)))
                   (apply oldfun args))))
   (define-key chatgpt-shell-mode-map (kbd "C-d") #'cae-ai-chatgpt-quit-or-delete-char)
-  (define-key chatgpt-shell-mode-map (kbd "<f6>") #'cae-chatgpt-shell-cheatsheet-hydra/body)
   (advice-add #'shell-maker-welcome-message :override #'ignore))
 
 (use-package! gpt-commit
