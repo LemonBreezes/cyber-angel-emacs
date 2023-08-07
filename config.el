e69d36d422ef7e4d3f19c6269a5525ced95e502e
StrawberryTea <look@strawberrytea.xyz>
2023-08-06 23:01:43 -0400

    config.el

--- config.el
+++ config.el
@@ -955,7 +955,11 @@
     ([remap narrow-to-page] . cae-narrow-to-page))
 
   (use-package! indent-bars
-    :defer t :hook ((python-mode yaml-mode) . indent-bars-mode)))
+    :defer t :hook ((python-mode yaml-mode) . indent-bars-mode))
+
+  (use-package! devil
+    :defer t :init
+    (global-devil-mode +1)))
 
 
 ;;; Autocompletion
