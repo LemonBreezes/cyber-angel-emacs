7f4d9948fc128cca2b5e0395bea96f7126291427
StrawberryTea <look@strawberrytea.xyz>
2023-08-07 19:12:59 -0400

    lisp/cae-visible-mark.el

--- lisp/cae-visible-mark.el
+++ lisp/cae-visible-mark.el
@@ -1,7 +1,7 @@
 ;;; lisp/cae-visible-mark.el -*- lexical-binding: t; -*-
 
 (defface mmv-face
-  '((t :background "#e0e0e0" :foreground "white"))
+  '((t :background "#e0e0e0" :foreground "black"))
   "Face used for showing the mark's position.")
 
 (defvar-local mmv-mark-overlay nil
