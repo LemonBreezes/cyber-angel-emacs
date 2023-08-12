4d5f84f6b48fc8695c37466bc1d40cc2db9e56d2
StrawberryTea <look@strawberrytea.xyz>
2023-08-12 14:27:24 -0400

    config.el

--- config.el
+++ config.el
@@ -71,6 +71,7 @@
 ;; errors.
 (defalias 'rainbow-delimiters-mode #'ignore)
 
+;; `evil-snipe' gives me errors when I have `native-comp-speed' set to 3.
 (remove-hook 'doom-first-input-hook #'evil-snipe-mode)
 (remove-hook 'doom-first-input-hook #'evil-snipe-override-mode)
 
