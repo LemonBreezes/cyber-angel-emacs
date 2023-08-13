d3af228d6e2a612cdf1d1cb4c60921a5c286098c
StrawberryTea <look@strawberrytea.xyz>
2023-08-13 19:30:15 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -59,3 +59,31 @@
   (interactive)
   (dirvish-emerge-mode +1)
   (call-interactively #'dirvish-emerge-menu))
+
+(defun cae-dired-find-file-a (oldfun &rest args)
+  "Like `find-file', but might exit the current Dirvish session."
+  (if-let* ((file (and (eq major-mode 'dired-mode)
+                       (car args)))
+            (dir (file-name-directory file)))
+      (progn
+        (unless (file-equal-p dir default-directory)
+          (advice-remove #'find-file #'cae-dired-find-file-a)
+          (unwind-protect
+              (find-file dir)
+            (advice-add #'find-file :before #'cae-dired-find-file-a)))
+        (unless (file-directory-p file)
+          ;; Copied from `dirvish-find-entry-a'
+          (let* ((dv (dirvish-curr)) (fn (nth 4 (dv-type dv))))
+            (if fn (funcall fn) (dirvish-kill dv)))))
+    (apply oldfun args)))
+
+;;(defun cae-dired-find-file-fullscreen-h ()
+;;  (when (and (derived-mode-p 'dired-mode)
+;;             (one-window-p))
+;;    (ignore-error user-error
+;;      (dirvish-layout-switch dirvish-default-layout))))
+;;
+;;(defun cae-dired-find-file-other-window-a (&rest _)
+;;  (when (and (derived-mode-p 'dired-mode)
+;;             (window-dedicated-p))
+;;    (dirvish-quit)))
