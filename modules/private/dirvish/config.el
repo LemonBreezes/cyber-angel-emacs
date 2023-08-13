3df62447f54e7c13ea47641c8de1445710c257b7
StrawberryTea <look@strawberrytea.xyz>
2023-08-13 15:14:24 -0400

    modules/private/dirvish/config.el

--- modules/private/dirvish/config.el
+++ modules/private/dirvish/config.el
@@ -112,8 +112,6 @@
   (setq dirvish-cache-dir (concat doom-cache-dir "dirvish/"))
   (when (modulep! :ui popup)
     (set-popup-rule! "^ ?\\*Dirvish.*" :ignore t))
-  (evil-make-overriding-map dired-mode-map 'motion)
-  (evil-make-overriding-map dirvish-mode-map 'motion)
   (map! :map dirvish-mode-map
         :n  "?"   #'dirvish-dispatch
         :n  "q"   #'dirvish-quit
