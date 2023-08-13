28a9edd029a60509c85f47fad6d13cec69786e4e
StrawberryTea <look@strawberrytea.xyz>
2023-08-13 15:14:51 -0400

    modules/private/dirvish/config.el

--- modules/private/dirvish/config.el
+++ modules/private/dirvish/config.el
@@ -114,7 +114,7 @@
     (set-popup-rule! "^ ?\\*Dirvish.*" :ignore t))
   (when (modulep! :editor evil)
     (evil-make-overriding-map dired-mode-map 'motion)
-    (evil-make-overriding-map Info-mode-map 'motion))
+  (evil-make-overriding-map dirvish-mode-map 'motion))
   (map! :map dirvish-mode-map
         :n  "?"   #'dirvish-dispatch
         :n  "q"   #'dirvish-quit
