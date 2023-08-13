bc9e1548c5b3f309a5117a14639235775f9250ec
StrawberryTea <look@strawberrytea.xyz>
2023-08-13 19:45:00 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -60,41 +60,13 @@
   (dirvish-emerge-mode +1)
   (call-interactively #'dirvish-emerge-menu))
 
-(defun cae-dired-find-file-a (oldfun &rest args)
-  "Like `find-file', but might exit the current Dirvish session."
-  (if-let* ((file (and (eq major-mode 'dired-mode)
-                       (car args)))
-            (dir (file-name-directory file)))
-      (progn
-        (unless (file-equal-p dir default-directory)
-          (advice-remove #'find-file #'cae-dired-find-file-a)
-          (unwind-protect
-              (find-file dir)
-            (advice-add #'find-file :around #'cae-dired-find-file-a)))
-        (+log "HELLOO")
-        (unless (file-directory-p file)
-          ;; Copied from `dirvish-find-entry-a'
-          (let* ((dv (dirvish-curr)) (fn (nth 4 (dv-type dv))))
-            (if fn (funcall fn) (dirvish-kill dv)))))
-    (apply oldfun args)))
-
-;;(defun cae-dired-find-file-fullscreen-h ()
-;;  (when (and (derived-mode-p 'dired-mode)
-;;             (one-window-p))
-;;    (ignore-error user-error
-;;      (dirvish-layout-switch dirvish-default-layout))))
-;;
-;;(defun cae-dired-find-file-other-window-a (&rest _)
-;;  (when (and (derived-mode-p 'dired-mode)
-;;             (window-dedicated-p))
-;;    (dirvish-quit)))
-
-
+;;;###autoload
 (defun cae-dired-find-file-a (oldfun file &optional wildcards)
   "Like `find-file', but might exit the current Dirvish session."
   (interactive
    (find-file-read-args "Find file: "
                         (confirm-nonexistent-file-or-buffer)))
+  ;; programatically use the same interactive arguments as `find-file'
   (if (derived-mode-p 'dired-mode)
       (progn
         (when-let ((dir (file-name-directory file)))
@@ -109,11 +81,9 @@
           (if fn (funcall fn) (dirvish-kill dv))))
     (funcall oldfun file wildcards)))
 
+;;;###autoload
 (defun cae-dired-find-file-other-window-a (oldfun &rest args)
   (when (and (derived-mode-p 'dired-mode)
              (window-dedicated-p))
     (dirvish-quit))
   (apply oldfun args))
-
-(advice-add #'find-file :around #'cae-dired-find-file-a)
-(advice-add #'find-file-other-window :around #'cae-dired-find-file-other-window-a)
