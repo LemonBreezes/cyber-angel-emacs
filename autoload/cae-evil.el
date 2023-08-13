d8c84be6d1f6d7a7ff5d8c466f31ebf0390e2eb9
StrawberryTea <look@strawberrytea.xyz>
2023-08-13 17:34:11 -0400

    autoload/cae-evil.el

--- autoload/cae-evil.el
+++ autoload/cae-evil.el
@@ -7,3 +7,18 @@
       (when (and (eq (point) (progn (forward-page 1) (1- (point)))))
         (forward-page count))
     (forward-page count)))
+
+;;;###autoload
+;; show the current evil normal state map for this major mode
+;; use annalist
+(defun cae-evil-show-normal-state-map ()
+  (interactive)
+  (when-let ((buf (get-buffer-create "*evil normal state map*"))
+             (map (symbol-value (intern (concat (symbol-name major-mode) "-map")))))
+    (with-current-buffer buf
+      (erase-buffer)
+      (evil-normal-state)
+      (evil-normalize-keymaps)
+      (pp (evil-get-auxiliary-keymap map 'normal)
+          buf)
+      (pop-to-buffer buf))))
