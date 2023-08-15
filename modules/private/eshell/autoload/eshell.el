6b60f05deb421c1ab028d72e2522f80f1278b18a
StrawberryTea <look@strawberrytea.xyz>
2023-08-15 11:49:55 -0400

    modules/private/eshell/autoload/eshell.el

--- modules/private/eshell/autoload/eshell.el
+++ modules/private/eshell/autoload/eshell.el
@@ -1,15 +1,6 @@
 ;;; private/eshell/autoload/eshell.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-eshell-input-filter (str)
-  "Filter some trivial commands from the input history."
-  (not (or (string-blank-p str)
-           (equal "cd" str)
-           (string-prefix-p "cd " str)
-           (string-prefix-p " " str)
-           (string-match-p "^[a-zA-Z]$" str))))
-
-;;;###autoload
 (defun cae-eshell-set-up-autocompletion ()
   (add-hook 'completion-at-point-functions
             #'cape-file nil t))
