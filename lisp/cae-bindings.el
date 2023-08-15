fb7c1d3532ee5598ce7a0a8de376fc4232012895
StrawberryTea <look@strawberrytea.xyz>
2023-08-15 18:26:01 -0400

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -70,7 +70,7 @@
       "C-x x o" #'ov-clear
       "C-x P" #'pop-to-buffer
       "M-Z" #'zap-up-to-char
-      "M-/" #'fancy-dabbrev-expand
+      "M-/" #'cape-dabbrev
       [C-i] #'doom/dumb-indent
       "C-S-i" #'doom/dumb-dedent
       "<escape>" #'keyboard-quit
