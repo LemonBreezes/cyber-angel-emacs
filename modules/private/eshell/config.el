4a1e4a9c58f91182252b2a3a024508d47579aac9
StrawberryTea <look@strawberrytea.xyz>
2023-08-15 11:50:56 -0400

    modules/private/eshell/config.el

--- modules/private/eshell/config.el
+++ modules/private/eshell/config.el
@@ -77,20 +77,20 @@
 ;;
 
 ;; Colorize ansi escape sequences in exported buffers
-(advice-add #'eshell-output-object-to-target :around #'cae-eshell-ansi-buffer-output)
+;;(advice-add #'eshell-output-object-to-target :around #'cae-eshell-ansi-buffer-output)
 
 ;; Parse buffer redirection >#buf and >#.
 (add-hook 'eshell-parse-argument-hook #'cae-eshell-syntax-buffer-redirect)
 
 ;; Filter trivial commands from history.
-(setq eshell-input-filter
-      (cae-defun cae-eshell-input-filter (str)
-        "Filter some trivial commands from the input history."
-        (not (or (string-blank-p str)
-                 (equal "cd" str)
-                 (string-prefix-p "cd " str)
-                 (string-prefix-p " " str)
-                 (string-match-p "^[a-zA-Z]$" str)))))
+;;(setq eshell-input-filter
+;;      (cae-defun cae-eshell-input-filter (str)
+;;        "Filter some trivial commands from the input history."
+;;        (not (or (string-blank-p str)
+;;                 (equal "cd" str)
+;;                 (string-prefix-p "cd " str)
+;;                 (string-prefix-p " " str)
+;;                 (string-match-p "^[a-zA-Z]$" str)))))
 
 ;;Set the prompt
 (autoload 'epe-theme-lambda "eshell-prompt-extras")
