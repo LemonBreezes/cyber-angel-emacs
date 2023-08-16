4f3785ca34e480cb24401004e7a272a9633dd7b7
StrawberryTea <look@strawberrytea.xyz>
2023-08-15 23:13:03 -0400

    lisp/cae-holy.el

--- lisp/cae-holy.el
+++ lisp/cae-holy.el
@@ -1,14 +1,44 @@
 ;;; lisp/cae-holy.el -*- lexical-binding: t; -*-
 
-(unless (modulep! :editor evil)
-  (setq doom-leader-alt-key "C-c"
-        doom-localleader-alt-key "C-c l"
-        doom-leader-key "C-c"
-        doom-localleader-key "C-c l"))
+(setq doom-leader-alt-key "C-c"
+      doom-localleader-alt-key "C-c l"
+      doom-leader-key "C-c"
+      doom-localleader-key "C-c l")
+
+;; Doom should not bind leader key prefixes to keys which are not alphanumeric
+;; because then they can be overwriting other packages' keybindings. As an
+;; example, Org mode has `C-c !' bound to `org-time-stamp-inactive' and `C-c &'
+;; bound to `org-mark-ring-goto'.
+(when (and (modulep! :checkers syntax)
+           (not (modulep! :checkers syntax +flymake)))
+  (setq flycheck-keymap-prefix (kbd "C-c C"))
+  (map! :leader (:prefix-map ("C" . "checkers"))))
+
+;; I like to add bind `<leader> h' to `help-map' like how Doom Emacs does for
+;; Evil.
+(map! :leader :desc "help" "h" help-map)
+
+;; Doom binds it's folding prefix to `C-c C-f' which is a keybinding used by
+;; many major modes.
+(when (modulep! :editor fold)
+  (defvar doom-fold-map (lookup-key doom-leader-map "\C-f"))
+  (define-key doom-leader-map "\C-f" nil)
+  (map! :leader
+        (:prefix-map ("F" . "fold")
+         "k" #'vimish-fold-delete
+         "K" #'vimish-fold-delete-all
+         "c" #'vimish-fold
+         "t" #'+fold/toggle
+         "C" #'+fold/close-all
+         "o" #'+fold/open
+         "O" #'+fold/open-all)))
+
+(map! [remap doom/backward-to-bol-or-indent] #'beginning-of-line
+      (:when (modulep! :tools lookup)
+       [remap xref-find-definitions] #'cae-lookup-definition-dwim))
 
 
 (use-package! goggles
-  :when (not (modulep! :editor evil))
   :defer t :init
   (add-hook 'prog-mode-hook #'goggles-mode)
   (add-hook 'text-mode-hook #'goggles-mode)
@@ -16,9 +46,7 @@
   :config
   (setq-default goggles-pulse t))
 
-
 (use-package! symbol-overlay
-  :when (not (modulep! :editor evil))
   :defer t :init
   (map! "M-i" #'symbol-overlay-put
         "M-I" #'symbol-overlay-remove-all
@@ -48,8 +76,7 @@
 
 (use-package! edit-indirect
   :defer t :init
-  (unless (modulep! :editor evil)
-    (map! :leader "'" #'cae-edit-indirect-dwim))
+  (map! :leader "'" #'cae-edit-indirect-dwim)
   :config
   (add-hook 'edit-indirect-after-creation-hook
             (cae-defun cae-edit-indirect-major-mode-fallback-h ()
@@ -57,3 +84,47 @@
                 (funcall (buffer-local-value
                           'major-mode
                           (overlay-buffer edit-indirect--overlay)))))))
+
+;; (unless (modulep! :editor evil)
+;;   (map! :prefix "C-z"
+;;         "n" #'avy-goto-line-below
+;;         "p" #'avy-goto-line-above
+;;         "t" #'tabgo
+;;         ;;"y" #'avy-copy-region
+;;         "c" #'avy-goto-char
+;;         ;;"m" #'avy-move-region
+;;         "l" #'avy-goto-line
+;;         "e" #'avy-goto-end-of-line
+;;         "." #'cae-avy-symbol-at-point
+;;         ;;"k" #'avy-kill-region
+;;         ;;"w" #'avy-kill-ring-save-region
+;;         "a" #'cae-avy-embark-act-on-region
+;;         "j" #'avy-goto-word-1
+;;         (:when (modulep! :editor fold)
+;;          "f" #'vimish-fold-avy)
+;;         "o" #'switch-window
+;;         "0" #'switch-window-then-delete
+;;         "1" #'switch-window-then-maximize
+;;         "2" #'switch-window-then-split-horizontally
+;;         "3" #'switch-window-then-split-vertically
+;;         "4" #'switch-window-then-kill-buffer
+;;         ;;"r" #'avy-resume ; `avy-resume' is too buggy to be useful.
+;;         "SPC" #'avy-goto-char-timer
+;;         (:map isearch-mode-map
+;;          "j" #'avy-isearch))
+;;   (when (modulep! :completion vertico)
+;;     (after! vertico
+;;       (map! :map vertico-map
+;;             "M-j" #'vertico-quick-jump
+;;             "M-i" #'vertico-quick-exit)))
+;;   (after! embark
+;;     (map! :map embark-collect-mode-map
+;;           "M-j" #'avy-embark-collect-choose
+;;           "M-i" #'avy-embark-collect-act))
+;;   (when (modulep! :private corfu)
+;;     (after! corfu
+;;       (map! :map corfu-map
+;;             "M-j" #'corfu-quick-jump
+;;             "M-i" #'corfu-quick-insert))))
+
+
