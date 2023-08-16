15b4d7b4ace40d0a315e70a1138b8fdb78dc0bb5
StrawberryTea <look@strawberrytea.xyz>
2023-08-16 12:25:43 -0400

    modules/private/vc/config.el

--- modules/private/vc/config.el
+++ modules/private/vc/config.el
@@ -132,7 +132,10 @@
   (map! :leader :desc "Blamer" "tB" #'blamer-mode)
   :config
   (setq blamer-idle-time 0.3
-        blamer-avatar-folder (concat doom-cache-dir "avatars/")))
+        blamer-avatar-folder (concat doom-cache-dir "avatars/"))
+  (setq blamer-author-formatter "  ✎ %s ")
+  (setq blamer-datetime-formatter "[%s]")
+  (setq blamer-commit-formatter " ● %s"))
 
 ;; TODO `igist' and `consult-gh'.
 (use-package! consult-gh
