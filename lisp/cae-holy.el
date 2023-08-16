fccbbb6bae5365aaf724c062e137a33f53a0d755
StrawberryTea <look@strawberrytea.xyz>
2023-08-15 23:08:49 -0400

    lisp/cae-holy.el

--- lisp/cae-holy.el
+++ lisp/cae-holy.el
@@ -1,5 +1,12 @@
 ;;; lisp/cae-holy.el -*- lexical-binding: t; -*-
 
+(unless (modulep! :editor evil)
+  (setq doom-leader-alt-key "C-c"
+        doom-localleader-alt-key "C-c l"
+        doom-leader-key "C-c"
+        doom-localleader-key "C-c l"))
+
+
 (use-package! goggles
   :when (not (modulep! :editor evil))
   :defer t :init
@@ -38,3 +45,15 @@
     (if (eq (car args) -1)
         (nreverse (apply oldfun args))
       (apply oldfun args))))
+
+(use-package! edit-indirect
+  :defer t :init
+  (unless (modulep! :editor evil)
+    (map! :leader "'" #'cae-edit-indirect-dwim))
+  :config
+  (add-hook 'edit-indirect-after-creation-hook
+            (cae-defun cae-edit-indirect-major-mode-fallback-h ()
+              (when (eq major-mode 'fundamental-mode)
+                (funcall (buffer-local-value
+                          'major-mode
+                          (overlay-buffer edit-indirect--overlay)))))))
