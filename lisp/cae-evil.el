e9538c741056627c0848504b22d07b2aa622e395
StrawberryTea <look@strawberrytea.xyz>
2023-08-18 15:42:43 -0400

    lisp/cae-evil.el

--- lisp/cae-evil.el
+++ lisp/cae-evil.el
@@ -9,6 +9,9 @@
   (map! :map evilem-map
         "DEL" (lookup-key evilem-map "\s")))
 
+(unless (cae-display-graphic-p)
+  (map! :m "TAB" #'better-jumper-jump-forward))
+
 ;; Restore Emacs keybindings which Doom overrides. `expand-region-fast-keys'
 ;; and `C-x C-=' make these keybindings redundant.
 (map! "C--" #'negative-argument
