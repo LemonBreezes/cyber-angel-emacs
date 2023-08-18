6002889cb7092f8d04a63653bf295c6597add73e
StrawberryTea <look@strawberrytea.xyz>
2023-08-18 14:18:03 -0400

    modules/private/vc/config.el

--- modules/private/vc/config.el
+++ modules/private/vc/config.el
@@ -1,8 +1,7 @@
 ;;; private/vc/config.el -*- lexical-binding: t; -*-
 
-(when (modulep! :ui vc-gutter)
-  (setq +vc-gutter-in-remote-files t
-        +vc-gutter-in-margin t))
+(setq +vc-gutter-in-remote-files t
+      +vc-gutter-in-margin t)
 
 ;; Got these from Prot's config. Say, how do I even use `add-log' and `vc'?
 (after! add-log
