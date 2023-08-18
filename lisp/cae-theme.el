a34a8b90857ef2b6783051b12f0eb2f4d566b7cf
StrawberryTea <look@strawberrytea.xyz>
2023-08-18 14:13:38 -0400

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -65,8 +65,7 @@
 ;;; Set theme based on time of day.
 
 (use-package! circadian
-  :defer t :defer-incrementally t
-  :config
+  :defer t :defer-incrementally t :config
   (setq circadian-themes
         '((:sunrise . modus-operandi-deuteranopia)
           (:sunset  . modus-vivendi-deuteranopia)))
