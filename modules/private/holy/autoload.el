707225ba6bf9e140f7323ae22329269c8a2d514d
StrawberryTea <look@strawberrytea.xyz>
2023-08-18 16:26:46 -0400

    modules/private/holy/autoload.el

--- modules/private/holy/autoload.el
+++ modules/private/holy/autoload.el
@@ -7,3 +7,39 @@
   (avy-with symbol-overlay-jump-avy
     (avy-process
      (avy--regex-candidates (regexp-quote (thing-at-point 'symbol t))))))
+
+;;;###autoload
+(defun cae-edit-indirect-dwim ()
+  "DWIM version of edit-indirect-region.
+When region is selected, behave like `edit-indirect-region'
+but when no region is selected and the cursor is in a 'string' syntax
+mark the string and call `edit-indirect-region' with it."
+  (interactive)
+  (cond ((region-active-p)
+         (call-interactively #'edit-indirect-region))
+        ((and (derived-mode-p 'org-mode)
+              (ignore-error 'user-error (call-interactively #'org-edit-special))))
+        (t
+         (call-interactively #'string-edit-at-point))))
+
+;;;###autoload
+(defun cae-mark-comment ()
+  "Mark the entire comment around point. Like `er/mark-comment' but
+also marks comment with leading whitespace"
+  (interactive)
+  (when (save-excursion
+          (skip-syntax-forward "\s")
+          (er--point-is-in-comment-p))
+    (let ((p (point)))
+      (skip-syntax-backward "\s")
+      (while (and (or (er--point-is-in-comment-p)
+                      (looking-at "[[:space:]]"))
+                  (not (eobp)))
+        (forward-char 1))
+      (skip-chars-backward "\n\r")
+      (set-mark (point))
+      (goto-char p)
+      (while (or (er--point-is-in-comment-p)
+                 (looking-at "[[:space:]]"))
+        (forward-char -1))
+      (forward-char 1))))
