e6e39b95932ca903bb73f4379101d67dcd42babf
StrawberryTea <look@strawberrytea.xyz>
2023-08-19 12:46:43 -0400

    autoload/cae-popup.el

--- autoload/cae-popup.el
+++ autoload/cae-popup.el
@@ -12,3 +12,17 @@
         (enlarge-window (- (/ (frame-width) 3)
                            (window-width win))
                         t))))
+
+;;;###autoload
+(defun cae-popup-shrink-to-fit (&optional window)
+  "Shrinks WINDOW to fit the buffer contents, if the buffer isn't empty.
+
+Uses `shrink-window-if-larger-than-buffer'."
+  (run-at-time
+   0.0 nil
+   (lambda (window)
+     (unless window
+       (setq window (selected-window)))
+     (unless (= (- (point-max) (point-min)) 0)
+       (shrink-window-if-larger-than-buffer window)))
+   window))
