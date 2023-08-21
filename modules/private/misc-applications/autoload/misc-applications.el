679ad9371b6f4fad8c989a58f56d719a74afbb13
StrawberryTea <look@strawberrytea.xyz>
2023-08-20 23:55:26 -0400

    modules/private/misc-applications/autoload/misc-applications.el

---
+++ modules/private/misc-applications/autoload/misc-applications.el
@@ -0,0 +1,13 @@
+;;; private/misc-applications/autoload/misc-applications.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun +misc-applications-hide-cursor-h ()
+  (setq-local cursor-type nil)
+  (when (featurep 'evil)
+    (setq-local evil-normal-state-cursor '(bar . 0)
+                evil-insert-state-cursor '(bar . 0)
+                evil-visual-state-cursor '(box . 0)
+                evil-motion-state-cursor '(box . 0)
+                evil-replace-state-cursor '(hbar . 0)
+                evil-operator-state-cursor '(hbar . 0)
+                evil-emacs-state-cursor '(hbar . 0))))
