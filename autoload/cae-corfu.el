f474cfe14951b21ed65ae74e5010476443c2600b
StrawberryTea <look@strawberrytea.xyz>
2023-08-22 01:06:38 -0400

    autoload/cae-corfu.el

--- autoload/cae-corfu.el
+++ autoload/cae-corfu.el
@@ -1,29 +1,6 @@
 ;;; autoload/cae-corfu.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-orderless-escapable-split-on-space-or-ampersand (s)
-  (mapcar
-   (lambda (piece)
-     (thread-last piece
-                  (replace-regexp-in-string
-                   (concat (string 0) "\\|" (string 1))
-                   (lambda (x)
-                     (pcase x
-                       ("\0" " ")
-                       ("\1" "&")
-                       (_ x))))
-                  (replace-regexp-in-string (string 1) "&")))
-   (split-string (replace-regexp-in-string
-                  "\\\\\\\\\\|\\\\ \\|\\\\&"
-                  (lambda (x)
-                    (pcase x
-                      ("\\ " "\0")
-                      ("\\&" "\1")
-                      (_ x)))
-                  s 'fixedcase 'literal)
-                 "[ &]+" t)))
-
-;;;###autoload
 (defun cae-corfu-enable-in-minibuffer-h ()
   (unless (or (bound-and-true-p mct--active)
               (bound-and-true-p vertico--input)
