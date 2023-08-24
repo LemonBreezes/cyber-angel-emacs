5552b9fb8b6443d581f53340d3cf4ca0f763f538
StrawberryTea <look@strawberrytea.xyz>
2023-08-24 17:49:02 -0400

    modules/private/eshell/config.el

--- modules/private/eshell/config.el
+++ modules/private/eshell/config.el
@@ -110,7 +110,8 @@
 (add-hook 'eshell-mode-hook #'eshell-elecslash-initialize)
 
 (after! em-hist
-  (setq eshell-history-size (expt 2 16))
+  (setq eshell-history-size (expt 2 16)
+        eshell-hist-ignoredups 'erase)
   (add-to-list 'eshell-expand-input-functions
                #'eshell-expand-history-references))
 
