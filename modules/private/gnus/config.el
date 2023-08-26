5b91d44bb5f300280f041968c8e824268e1d153b
StrawberryTea <look@strawberrytea.xyz>
2023-08-26 19:07:37 -0400

    modules/private/gnus/config.el

--- modules/private/gnus/config.el
+++ modules/private/gnus/config.el
@@ -125,37 +125,3 @@
 (unless (or (modulep! :email mu4e)
             (modulep! :email notmuch))
   (setq mail-user-agent 'gnus-user-agent))
-
-(use-package! bbdb
-  :after (:or gnus message)
-  :defer t :config
-  (require 'bbdb-com)
-  (require 'bbdb-gnus)
-  (bbdb-initialize 'message 'gnus)
-  (bbdb-mua-auto-update-init 'message 'gnus)
-  (setq bbdb-mua-pop-up t)
-  (setq bbdb-allow-duplicates t)
-  (setq bbdb-pop-up-window-size 5)
-  (setq bbdb-ignore-redundant-mails t)
-  (setq bbdb-update-records-p 'create)
-  (setq bbdb-mua-update-interactive-p '(create . query))
-  (setq bbdb-mua-auto-update-p 'create)
-  (add-hook 'mail-setup-hook 'bbdb-mail-aliases)
-  (add-hook 'message-setup-hook 'bbdb-mail-aliases)
-  (add-hook 'bbdb-notice-mail-hook 'bbdb-auto-notes)
-  (setq bbdb-completion-display-record t
-        bbdb-default-area-code nil
-        bbdb-add-aka t
-        bbdb-add-name nil
-        bbdb-add-mails t
-        bbdb-ignore-message-alist '(("Newsgroup" . ".*")))
-  (defalias 'bbdb-y-or-n-p #'(lambda (prompt) t))
-  (setq bbdb-auto-notes-rules
-        '(("Newsgroups" ("[^,]+" newsgroups 0))
-          ("Subject" (".*" last-subj 0 t))
-          ("User-Agent" (".*" mailer 0))
-          ("X-Mailer" (".*" mailer 0))
-          ("Organization" (".*" organization 0))
-          ("X-Newsreader" (".*" mailer 0))
-          ("X-Face" (".+" face 0 'replace))
-          ("Face" (".+" face 0 'replace)))))
