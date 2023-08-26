88c39edcaaa6b9f41900d8e83b720e894fccbd4e
StrawberryTea <look@strawberrytea.xyz>
2023-08-26 11:41:57 -0400

    modules/private/org/config.el

--- modules/private/org/config.el
+++ modules/private/org/config.el
@@ -1,8 +1,7 @@
 ;;; private/org/config.el -*- lexical-binding: t; -*-
 
 (use-package! org-rich-yank
-  :defer t
-  :init
+  :defer t :init
   (map! :map org-mode-map
         "C-M-y" #'cae-org-rich-yank))
 
