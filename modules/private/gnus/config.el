a3d40403e02894b67c50c06ef669c922a3558cde
StrawberryTea <look@strawberrytea.xyz>
2023-08-26 17:41:19 -0400

    modules/private/gnus/config.el

--- modules/private/gnus/config.el
+++ modules/private/gnus/config.el
@@ -122,9 +122,55 @@
   (map! :map gnus-article-mode-map
         "<f6>" #'cae-gnus-article-cheatsheet/body))
 
-(use-package! bbdb
-  :defer t :init
-  (add-hook 'message-setup-hook #'bbdb-mail-aliases)
-  :config
+(use-package bbdb
+  :defer t :config
+  (require 'bbdb-com)
+  (require 'bbdb-anniv)
+  (require 'bbdb-gnus)
   (bbdb-initialize 'message 'gnus)
-  (bbdb-mua-auto-update-init 'message 'gnus))
+  (bbdb-mua-auto-update-init 'message 'gnus)
+  (setq bbdb-mua-pop-up nil)
+  (setq bbdb-allow-duplicates t)
+  (setq bbdb-pop-up-window-size 5)
+  (setq bbdb-ignore-redundant-mails t)
+  (setq bbdb-update-records-p 'create)
+  (setq bbdb-mua-update-interactive-p '(create . query))
+  (setq bbdb-mua-auto-update-p 'create)
+  (add-hook 'mail-setup-hook 'bbdb-mail-aliases)
+  (add-hook 'message-setup-hook 'bbdb-mail-aliases)
+  (add-hook 'bbdb-notice-mail-hook 'bbdb-auto-notes)
+  ;; (add-hook 'list-diary-entries-hook 'bbdb-include-anniversaries)
+  (setq bbdb-always-add-addresses t
+        bbdb-complete-name-allow-cycling t
+        bbdb-completion-display-record t
+        bbdb-default-area-code nil
+        bbdb-dwim-net-address-allow-redundancy t
+        bbdb-electric-p nil
+        bbdb-add-aka nil
+        bbdb-add-name nil
+        bbdb-add-mails t
+        bbdb-new-nets-always-primary 'never
+        bbdb-north-american-phone-numbers-p nil
+        bbdb-offer-save 'auto
+        bbdb-pop-up-target-lines 3
+        bbdb-print-net 'primary
+        bbdb-print-require t
+        bbdb-use-pop-up nil
+        bbdb-user-mail-names gnus-ignored-from-addresses
+        bbdb/gnus-split-crosspost-default nil
+        bbdb/gnus-split-default-group nil
+        bbdb/gnus-split-myaddr-regexp gnus-ignored-from-addresses
+        bbdb/gnus-split-nomatch-function nil
+        bbdb/gnus-summary-known-poster-mark "+"
+        bbdb/gnus-summary-mark-known-posters t
+        bbdb-ignore-message-alist '(("Newsgroup" . ".*")))
+  (defalias 'bbdb-y-or-n-p #'(lambda (prompt) t))
+  (setq bbdb-auto-notes-alist
+        '(("Newsgroups" ("[^,]+" newsgroups 0))
+          ("Subject" (".*" last-subj 0 t))
+          ("User-Agent" (".*" mailer 0))
+          ("X-Mailer" (".*" mailer 0))
+          ("Organization" (".*" organization 0))
+          ("X-Newsreader" (".*" mailer 0))
+          ("X-Face" (".+" face 0 'replace))
+          ("Face" (".+" face 0 'replace)))))
