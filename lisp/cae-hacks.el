d017435c12041c4cf1a8538875428c5c55c53bba
StrawberryTea <look@strawberrytea.xyz>
2023-08-26 11:55:11 -0400

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -80,6 +80,13 @@ It is meant to be used as a `post-gc-hook'."
   :before-until #'+workspace/switch-to
   (minibuffer-window-active-p (selected-window)))
 
+;; Make `advice-remove' ignore the keyword argument
+(defadvice! cae-hacks-advice-remove-ignore-keyword-args-a (args)
+  :filter-args #'advice-remove
+  (if (keywordp (nth 1 args))
+      (list (nth 0 args) (nth 2 args))
+    args))
+
 ;; If `try' is used before the package list is loaded, fetch it.
 (defadvice! cae-hacks-try-package-refresh-contents-maybe (&rest _)
   :before #'try
@@ -140,3 +147,29 @@ It is meant to be used as a `post-gc-hook'."
    '(nil "((nil . (" ("" '(projectile-skel-variable-cons) n)
      resume: ")))")
    str arg))
+
+
+;; For backwards compatibility. `syslog-mode' uses this.
+(defun toggle-read-only (arg)
+  (read-only-mode
+   (cond ((not arg) (not buffer-read-only))
+         ((and (integerp arg) (<= arg 0)) nil)
+         (t t))))
+
+;; For some reason, I had to do this after updating Emacs30 to get
+;; `cape-yasnippet' to work.
+(defalias 'prefix #'string-prefix-p)
+
+;; This is for backwards compatibility with some of my old bookmarks.
+(defalias #'+exwm-firefox-bookmark-handler #'cae-browse-url-generic-bookmark-handler)
+(defalias #'bookmark/jump-to-newest-download #'cae-bookmark-jump-to-newest-download)
+
+;; These are for backwards compatibility.
+(dolist (sym '(cae-keyboard-strings
+               cae-keyboard-remap
+               cae-keyboard-remap-reverse
+               cae-keyboard-remap-to-strings
+               cae-keyboard-kbd
+               cae-keyboard-kbd-reverse
+               cae-keyboard-remap-hydra-hint))
+  (defalias sym #'identity))
