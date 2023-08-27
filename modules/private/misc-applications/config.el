5574bf6fc9749059d7cb1cffcf7f0a0017236b08
StrawberryTea <look@strawberrytea.xyz>
2023-08-26 21:54:28 -0400

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -108,7 +108,8 @@
         :ng "Y" (cmd! () (elfeed-search-set-filter "@6-months-ago +tube"))
         :ng "*" (cmd! () (elfeed-search-set-filter "@6-months-ago +star"))
         :ng "a" (cmd! () (elfeed-search-set-filter "@6-months-ago"))
-        :ng "T" (cmd! () (elfeed-search-set-filter "@1-day-ago")))
+        :ng "T" (cmd! () (elfeed-search-set-filter "@1-day-ago"))
+        :ng "t" #'mark-whole-buffer)
   (map! :map elfeed-search-mode-map
         "<f6>" #'cae-elfeed-hydra/body
         ;; Elfeed maps `h' to `describe-mode', which is not as good.
