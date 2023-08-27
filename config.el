7da041f017f542f3ae336878ce40e08333da864b
StrawberryTea <look@strawberrytea.xyz>
2023-08-27 00:38:22 -0400

    config.el

--- config.el
+++ config.el
@@ -941,8 +941,6 @@
 (after! smtpmail
   (setq smtpmail-smtp-server "smtp.fastmail.com"
         smtpmail-default-smtp-server "smtp.fastmail.com"
-        ;;smtpmail-smtp-service 465
-        ;;smtpmail-stream-type 'tls
         smtpmail-queue-mail nil
         smtpmail-queue-dir "~/.mail/queued-mail/"
         smtpmail-servers-requiring-authorization ".*"
