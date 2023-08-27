e427ce3634ee948715a57b353e02180436b69316
StrawberryTea <look@strawberrytea.xyz>
2023-08-27 16:19:23 -0400

    modules/private/misc-applications/autoload/tetris.el

--- modules/private/misc-applications/autoload/tetris.el
+++ modules/private/misc-applications/autoload/tetris.el
@@ -11,3 +11,43 @@
   ("<left>" tetris-move-left "Move left" :column "Movement")
   ("<right>" tetris-move-right "Move right" :column "Movement")
   ("a" autotetris-mode "Autotetris" :column "Misc"))
+
+;;;###autoload
+(defun +tetris (&optional arg)
+  (interactive "P")
+  (if arg
+      (setq +tetris--old-wconf nil)
+    (if (modulep! :ui workspaces)
+        ;; delete current workspace if empty
+        ;; this is useful when mu4e is in the daemon
+        ;; as otherwise you can accumulate empty workspaces
+        (progn
+          (unless (+workspace-buffer-list)
+            (+workspace-delete (+workspace-current-name)))
+          (+workspace-switch +tetris-workspace-name t))
+      (setq +tetris--old-wconf (current-window-configuration))
+      (delete-other-windows)
+      (switch-to-buffer (doom-fallback-buffer))))
+  (let* ((saves-buf (find-file-noselect (expand-file-name "tetris-scores" shared-game-score-directory)))
+         (highest-score (with-current-buffer saves-buf
+                          (local-set-key (kbd "q") #'+tetris-quit)
+                          (when (featurep 'evil)
+                            (evil-local-set-key 'normal (kbd "q") #'+tetris-quit))
+                          (buffer-substring-no-properties (goto-char (point-min))
+                                                          (pos-eol)))))
+    (call-interactively #'tetris)
+    (setq-local header-line-format highest-score)))
+
+;;;###autoload
+(defun +tetris-quit ()
+  (interactive)
+  (if (modulep! :ui workspaces)
+      (when (+workspace-exists-p +tetris-workspace-name)
+        (+workspace/delete +tetris-workspace-name))
+    (when +tetris--old-wconf
+      (set-window-configuration +tetris--old-wconf)))
+  (kill-buffer "*Tetris*")
+  (when-let ((saves-buf (get-file-buffer (expand-file-name "tetris-scores" shared-game-score-directory))))
+    (with-current-buffer saves-buf
+      (save-buffer)
+      (kill-buffer))))
