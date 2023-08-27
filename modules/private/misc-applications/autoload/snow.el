bf7b4f1c1b4dd74d5f27c7a2195da65c700b3534
StrawberryTea <look@strawberrytea.xyz>
2023-08-27 15:14:47 -0400

    modules/private/misc-applications/autoload/snow.el

---
+++ modules/private/misc-applications/autoload/snow.el
@@ -0,0 +1,26 @@
+;;; private/misc-applications/autoload/snow.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun +snow ()
+  (interactive)
+  (if (modulep! :ui workspaces)
+      ;; delete current workspace if empty
+      ;; this is useful when mu4e is in the daemon
+      ;; as otherwise you can accumulate empty workspaces
+      (progn
+        (unless (+workspace-buffer-list)
+          (+workspace-delete (+workspace-current-name)))
+        (+workspace-switch +snow-workspace-name t))
+    (setq +snow--old-wconf (current-window-configuration))
+    (delete-other-windows)
+    (switch-to-buffer (doom-fallback-buffer)))
+  (call-interactively #'snow))
+
+
+;;;###autoload
+(defun +snow-quit ()
+  (interactive)
+  (if (modulep! :ui workspaces)
+      (+workspace/delete +snow-workspace-name)
+    (set-window-configuration +snow--old-wconf))
+  (kill-buffer snow-buffer-name))
