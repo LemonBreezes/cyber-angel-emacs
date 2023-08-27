7094413cb6c158b5d4c95548aef1df3f1202aecc
StrawberryTea <look@strawberrytea.xyz>
2023-08-27 15:04:31 -0400

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -398,7 +398,7 @@
 (use-package! fireplace
   :defer t :init
   (map! :map +misc-applications-eyecandy-map
-        "f" #'fireplace)
+        "f" #'+fireplace)
   :config
   (map! :map fireplace-mode-map
         "<f6>" #'+fireplace-hydra/body))
