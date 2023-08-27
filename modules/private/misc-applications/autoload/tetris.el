5933589887017465fa700441ecae13c472da61e9
StrawberryTea <look@strawberrytea.xyz>
2023-08-27 17:39:58 -0400

    modules/private/misc-applications/autoload/tetris.el

--- modules/private/misc-applications/autoload/tetris.el
+++ modules/private/misc-applications/autoload/tetris.el
@@ -1,16 +1,20 @@
 ;;; private/misc-applications/autoload/tetris.el -*- lexical-binding: t; -*-
 
 ;;;###autoload (autoload '+tetris-hydra/body "private/misc-applications/autoload/tetris" nil t)
-(defhydra +tetris-hydra (:color pink :hint nil)
-  ("<f6>" nil "Exit" :exit t)
-  ("q" +tetris-quit nil :exit t)
-  ("n" tetris-start-game "Restart game" :column "Misc")
-  ("p" tetris-pause-game "Pause/Unpause game" :column "Misc")
-  ("SPC" tetris-move-bottom "Move bottom" :column "Movement")
-  ("<up>" tetris-rotate-prev "Rotate clockwise" :column "Movement")
-  ("<left>" tetris-move-left "Move left" :column "Movement")
-  ("<right>" tetris-move-right "Move right" :column "Movement")
-  ("a" autotetris-mode "Autotetris" :column "Misc"))
+(eval
+ `(defhydra +tetris-hydra (:color pink :hint nil)
+    ("<f6>" nil "Exit" :exit t)
+    ("q" +tetris-quit nil :exit t)
+    ("n" tetris-start-game "Restart game" :column "Misc")
+    ("p" tetris-pause-game "Pause/Unpause game" :column "Misc")
+    ,@(if (modulep! :editor evil)
+          '(("ZZ" tetris-end-game "Quit" :column "Misc")
+          '(("SPC" tetris-move-bottom "Move bottom" :column "Movement"))))
+    ("<up>" tetris-rotate-prev "Rotate clockwise" :column "Movement")
+    ("<left>" tetris-move-left "Move left" :column "Movement")
+    ("<right>" tetris-move-right "Move right" :column "Movement")
+    ("a" autotetris-mode "Autotetris" :column "Misc"))
+ t)
 
 ;;;###autoload
 (defun +tetris (&optional arg)
