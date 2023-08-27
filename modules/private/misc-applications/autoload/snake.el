71ac997a80bf273c62f3be3a5749cc5d01cc65a0
StrawberryTea <look@strawberrytea.xyz>
2023-08-27 15:56:55 -0400

    modules/private/misc-applications/autoload/snake.el

--- modules/private/misc-applications/autoload/snake.el
+++ modules/private/misc-applications/autoload/snake.el
@@ -10,3 +10,31 @@
   ("<up>" snake-move-up "Move up" :column "Movement")
   ("<left>" snake-move-left "Move left" :column "Movement")
   ("<right>" snake-move-right "Move right" :column "Movement"))
+
+;;;###autoload
+(defun +snake (&optional arg)
+  (interactive "P")
+  (if arg
+    (setq +snake--old-wconf nil)
+  (if (modulep! :ui workspaces)
+      ;; delete current workspace if empty
+      ;; this is useful when mu4e is in the daemon
+      ;; as otherwise you can accumulate empty workspaces
+      (progn
+        (unless (+workspace-buffer-list)
+          (+workspace-delete (+workspace-current-name)))
+        (+workspace-switch +snake-workspace-name t))
+    (setq +snake--old-wconf (current-window-configuration))
+    (delete-other-windows)
+    (switch-to-buffer (doom-fallback-buffer))))
+  (call-interactively #'snake))
+
+;;;###autoload
+(defun +snake-quit ()
+  (interactive)
+  (if (modulep! :ui workspaces)
+    (when (+workspace-exists-p +snake-workspace-name)
+      (+workspace/delete +snake-workspace-name))
+  (when +snake--old-wconf
+    (set-window-configuration +snake--old-wconf)))
+  (kill-buffer "*Snake*"))
