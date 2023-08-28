bdac5c4a2ddb1a55b0aead3473b9d833b9536ec0
StrawberryTea <look@strawberrytea.xyz>
2023-08-28 13:47:53 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -1,23 +1,5 @@
 ;;; private/dired/autoload.el -*- lexical-binding: t; -*-
 
-;;(defun cae-dired-switch-buffer--handle-dirvish (fn)
-;;  (call-interactively fn)
-;;  (when (and (derived-mode-p 'dired-mode)
-;;             (one-window-p)
-;;             (not (window-dedicated-p)))
-;;    (ignore-error user-error
-;;      (dirvish-layout-switch dirvish-default-layout))))
-;;
-;;;;;###autoload
-;;(defun cae-dired-previous-buffer ()
-;;  (interactive)
-;;  (cae-dired-switch-buffer--handle-dirvish #'previous-buffer))
-;;
-;;;;;###autoload
-;;(defun cae-dired-next-buffer ()
-;;  (interactive)
-;;  (cae-dired-switch-buffer--handle-dirvish #'next-buffer))
-
 ;;;###autoload
 (defun cae-dired-jump ()
   (interactive)
