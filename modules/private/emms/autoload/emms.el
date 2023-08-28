770b3ee58f4c4cb57a205d8d3abc7411f8a61ecf
StrawberryTea <look@strawberrytea.xyz>
2023-08-28 13:36:40 -0400

    modules/private/emms/autoload/emms.el

--- modules/private/emms/autoload/emms.el
+++ modules/private/emms/autoload/emms.el
@@ -38,3 +38,18 @@
     ("p" "Playlists" (lambda () (interactive) (dired "/mnt/music/Playlists/")))
     ("a" "Anime Music" (lambda () (interactive) (dired "/mnt/music/Anime Music/")))
     ("m" "EMMS indexed" (lambda () (interactive) (dired emms-source-file-default-directory)))]])
+
+;; These two functions are from https://www.reddit.com/r/emacs/comments/qg2d0k/emms_modeline_shows_full_path_to_the_songs_i_only/
+
+;;;###autoload
+(defun cae-track-title-from-file-name (file)
+  "For using with EMMS description functions. Extracts the track
+title from the file name FILE, which just means a) taking only
+the file component at the end of the path, and b) removing any
+file extension."
+  (with-temp-buffer
+    (save-excursion (insert (file-name-nondirectory (directory-file-name file))))
+    (ignore-error 'search-failed
+      (search-forward-regexp (rx "." (+ alnum) eol))
+      (delete-region (match-beginning 0) (match-end 0)))
+    (buffer-string)))
