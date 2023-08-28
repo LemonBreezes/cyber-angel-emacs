84ad750b7b988dd4624e89b81bcf5a4b5594b495
StrawberryTea <look@strawberrytea.xyz>
2023-08-28 13:47:03 -0400

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -1,26 +1,22 @@
 ;;; private/dired/autoload.el -*- lexical-binding: t; -*-
 
-(defun cae-dired-switch-buffer--handle-dirvish (fn)
-  (when (and (derived-mode-p 'dired-mode)
-             (window-dedicated-p))
-    (let* ((dv (dirvish-curr)) (fn (nth 4 (dv-type dv))))
-      (if fn (funcall fn) (dirvish-kill dv))))
-  (call-interactively fn)
-  (when (and (derived-mode-p 'dired-mode)
-             (one-window-p)
-             (not (window-dedicated-p)))
-    (ignore-error user-error
-      (dirvish-layout-switch dirvish-default-layout))))
-
-;;;###autoload
-(defun cae-dired-previous-buffer ()
-  (interactive)
-  (cae-dired-switch-buffer--handle-dirvish #'previous-buffer))
-
-;;;###autoload
-(defun cae-dired-next-buffer ()
-  (interactive)
-  (cae-dired-switch-buffer--handle-dirvish #'next-buffer))
+;;(defun cae-dired-switch-buffer--handle-dirvish (fn)
+;;  (call-interactively fn)
+;;  (when (and (derived-mode-p 'dired-mode)
+;;             (one-window-p)
+;;             (not (window-dedicated-p)))
+;;    (ignore-error user-error
+;;      (dirvish-layout-switch dirvish-default-layout))))
+;;
+;;;;;###autoload
+;;(defun cae-dired-previous-buffer ()
+;;  (interactive)
+;;  (cae-dired-switch-buffer--handle-dirvish #'previous-buffer))
+;;
+;;;;;###autoload
+;;(defun cae-dired-next-buffer ()
+;;  (interactive)
+;;  (cae-dired-switch-buffer--handle-dirvish #'next-buffer))
 
 ;;;###autoload
 (defun cae-dired-jump ()
