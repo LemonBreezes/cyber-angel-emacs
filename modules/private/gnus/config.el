46d6b8e17f22ce4e428ed22635cd6372240c39d7
LemonBreezes <look@strawberrytea.xyz>
2023-08-30 18:15:47 -0500

    modules/private/gnus/config.el

--- modules/private/gnus/config.el
+++ modules/private/gnus/config.el
@@ -130,7 +130,7 @@
 (use-package! gnus-sum
   :hook (gnus-select-group . gnus-group-set-timestamp)
   :config
-  (when (cae-display-graphic-p)
+  (when cae-tty-disable-unicode
     ;; Pretty marks
     (setq! gnus-sum-thread-tree-false-root nil
            gnus-sum-thread-tree-single-indent nil
