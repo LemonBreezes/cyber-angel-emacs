2fab2165d65e2bdd46c5039336f59cf8f391cc75
LemonBreezes <look@strawberrytea.xyz>
2023-08-30 01:04:16 -0500

    modules/private/dired/config.el

--- modules/private/dired/config.el
+++ modules/private/dired/config.el
@@ -7,7 +7,8 @@
 
 (after! dired
   ;; Use parallel versions of comression programs.
-  ;; Install `pigz', `pbzip2', `pixz', `plzip' for parallel decompression.
+  ;; Install `pigz', `pbzip2', `pixz', `plzip', and `lzop' for parallel decompression.
+  ;; Install `dpkg',
   (setq dired-compress-file-alist
         '(("\\.gz\\'" . "pigz -9f %i")
           ("\\.bz2\\'" . "pbzip2 -9f %i")
