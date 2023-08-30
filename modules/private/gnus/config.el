ad70ce5c793f0e2a11b9678f9e205bfc430adeaa
LemonBreezes <look@strawberrytea.xyz>
2023-08-30 18:59:58 -0500

    modules/private/gnus/config.el

--- modules/private/gnus/config.el
+++ modules/private/gnus/config.el
@@ -115,14 +115,12 @@
                       ("emacs" "nntp+Gmane:gmane.emacs.bugs" "nntp+Gmane:gmane.emacs.diffs" "nntp+Gmane:gmane.emacs.devel"
                        "nntp+Gmane:gmane.emacs.gnus.general" "nntp+Gmane:gmane.emacs.gnus.user"
                        "nntp+Gmane:gmane.emacs.orgmode")
-                      ("Junk" "nnhackernews:ask" "nnhackernews:job" "nnhackernews:show")
                       ("Gnus"))
    gnus-topic-topology '(("Gnus" visible)
                          (("misc" visible))
                          (("emacs" visible nil nil))
                          (("fastmail" visible nil nil))
-                         (("hackernews" visible))
-                         (("Junk" invisible))))
+                         (("hackernews" visible))))
   (map! :map gnus-group-mode-map
         "<f6>" #'cae-gnus-group-cheatsheet/body))
 
