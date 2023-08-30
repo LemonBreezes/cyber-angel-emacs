1e3afcb3ce1ca80a5bbe138fcd55a178264eeefc
LemonBreezes <look@strawberrytea.xyz>
2023-08-30 18:48:36 -0500

    lisp/cae-evil.el

--- lisp/cae-evil.el
+++ lisp/cae-evil.el
@@ -79,12 +79,12 @@
       evil-vsplit-window-right t
       evil-split-window-below t)
 
-(after! evil-embrace
-  (setq evil-embrace-show-help-p t)
-  (cl-pushnew ?\C-f evil-embrace-evil-surround-keys)
-  ;; I prefer using `C-f' for prefix functions everywhere over a DWIM-style `f'.
-  (add-hook! (lisp-mode emacs-lisp-mode clojura-mode racket-mode hy-mode)
-    #'+evil-embrace-lisp-mode-hook-h))
+;;(after! evil-embrace
+;;  (setq evil-embrace-show-help-p t)
+;;  (cl-pushnew ?\C-f evil-embrace-evil-surround-keys)
+;;  ;; I prefer using `C-f' for prefix functions everywhere over a DWIM-style `f'.
+;;  (remove-hook! (lisp-mode emacs-lisp-mode clojura-mode racket-mode hy-mode)
+;;    #'+evil-embrace-lisp-mode-hook-h))
 
 (use-package! evil-owl
   :hook (doom-first-input . evil-owl-mode))
