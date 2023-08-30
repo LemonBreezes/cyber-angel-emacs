f4ed12ef77f1810ff8b0043e91281ae978339b83
LemonBreezes <look@strawberrytea.xyz>
2023-08-30 18:16:48 -0500

    modules/private/holy/config.el

--- modules/private/holy/config.el
+++ modules/private/holy/config.el
@@ -168,7 +168,7 @@
   :config
   (setq anzu-mode-lighter ""
         anzu-replace-threshold 50
-        anzu-replace-to-string-separator (if (cae-display-graphic-p) " → " " -> ")))
+        anzu-replace-to-string-separator (if cae-tty-disable-unicode " -> " " → ")))
 
 (use-package! isearch-mb
   :after-call isearch-mode-hook
