95515cec91817f1cb2f63093739094dbeece527d
LemonBreezes <look@strawberrytea.xyz>
2023-08-31 10:49:40 -0500

    modules/private/org/config.el

--- modules/private/org/config.el
+++ modules/private/org/config.el
@@ -16,9 +16,9 @@
     (when (org-at-heading-p)
       (cae-org-set-created-timestamp))))
 
-(advice-add #'+org-init-keybinds-h :after
-            (cae-defun cae-org-fixup-doom-keybindings ()
-              (remove-hook 'org-tab-first-hook #'+org-indent-maybe-h)))
+;;(advice-add #'+org-init-keybinds-h :after
+;;            (cae-defun cae-org-fixup-doom-keybindings ()
+;;              (remove-hook 'org-tab-first-hook #'+org-indent-maybe-h)))
 
 (use-package! org-appear
   :defer t :init
