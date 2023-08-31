edbfe80e1b5466037f1d5f564a3249cf0af457df
LemonBreezes <look@strawberrytea.xyz>
2023-08-31 01:36:21 -0500

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -74,6 +74,7 @@
       [C-i] #'doom/dumb-indent
       "C-S-i" #'doom/dumb-dedent
       "<escape>" #'keyboard-quit
+      "<f7>" #'er/expand-region
       [remap doom/sudo-this-file] #'cae-toggle-sudo
       (:after transient
        (:map transient-map
