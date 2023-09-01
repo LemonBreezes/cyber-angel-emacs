0d2133a7fd159929225fc8e750ee1e25c50bf996
LemonBreezes <look@strawberrytea.xyz>
2023-09-01 15:45:11 -0500

    org/silly-loop.el

---
+++ org/silly-loop.el
@@ -0,0 +1,9 @@
+;;; org/silly-loop.el -*- lexical-binding: t; -*-
+
+(defun silly-loop (n)
+  "Return the time, in seconds, to run N iterations of a loop."
+  (let ((t1 (float-time)))
+    (while (> (setq n (1- n)) 0))
+    (- (float-time) t1)))
+
+(silly-loop 50000000)
