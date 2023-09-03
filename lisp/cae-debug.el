a7e1118c75a720d08e3935149d74b3e15cc1cd13
LemonBreezes <look@strawberrytea.xyz>
2023-09-03 00:32:40 -0500

    lisp/cae-debug.el

--- lisp/cae-debug.el
+++ lisp/cae-debug.el
@@ -64,4 +64,3 @@ normally have their errors suppressed."
 ;; Use this with `vertico--exhibit' for example to debug completion tables.
 ;; Copied from here:
 ;; https://gist.github.com/jdtsmith/1fbcacfe677d74bbe510aec80ac0050c.
-
