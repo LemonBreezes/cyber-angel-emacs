ab2f3ce22a53f385e176c5e84729311544ce6de4
LemonBreezes <look@strawberrytea.xyz>
2023-09-03 13:09:48 -0500

    modules/private/misc-applications/autoload/empv.el

--- modules/private/misc-applications/autoload/empv.el
+++ modules/private/misc-applications/autoload/empv.el
@@ -1,12 +1,8 @@
 ;;; private/misc-applications/autoload/empv.el -*- lexical-binding: t; -*-
 
-;;;###autoload
-(defun +empv-lazy-load-keymap ()
-  (interactive)
-  (require 'empv)
-  (setq unread-command-events
-        (listify-key-sequence
-         (kbd (concat +misc-applications-prefix
-                      +misc-applications-music-prefix
-                      "m"))))
-  (cae-which-key-show-map 'empv-map))
+;;;###autoload (autoload '+empv-keymap "private/misc-applications/autoload/empv" nil t)
+(defun +empv-keymap () (interactive))
+(hercules-def
+ :toggle-funs #'+empv-keymap
+ :keymap 'empv-map
+ :package 'empv)
