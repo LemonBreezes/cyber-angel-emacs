ba0842177ea60febfa6ce009571b07c44651b1c7
LemonBreezes <look@strawberrytea.xyz>
2023-09-04 14:39:37 -0500

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -384,3 +384,16 @@ also marks comment with leading whitespace"
                  (looking-at "[[:space:]]"))
         (forward-char -1))
       (forward-char 1))))
+
+;;;###autoload
+(defun cae-auto-sudoedit-maybe-h ()
+  (unless (let ((path (or (buffer-file-name) list-buffers-directory)))
+            (string= (file-attribute-user-id
+                      (file-attributes path 'string))
+                     (if (and (featurep 'tramp)
+                              (tramp-tramp-file-p path))
+                         (tramp-get-remote-uid (tramp-dissect-file-name path)
+                                               'string)
+                       (user-login-name))))
+    (require 'auto-sudoedit)
+    (auto-sudoedit)))
