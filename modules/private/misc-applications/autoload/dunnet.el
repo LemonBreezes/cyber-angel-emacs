c54ea313e00a8c0484fb842c60689b87f0fab45e
LemonBreezes <look@strawberrytea.xyz>
2023-09-03 20:55:57 -0500

    modules/private/misc-applications/autoload/dunnet.el

--- modules/private/misc-applications/autoload/dunnet.el
+++ modules/private/misc-applications/autoload/dunnet.el
@@ -6,13 +6,7 @@
   (if arg
       (setq +dunnet--old-wconf nil)
     (if (modulep! :ui workspaces)
-        ;; delete current workspace if empty
-        ;; this is useful when mu4e is in the daemon
-        ;; as otherwise you can accumulate empty workspaces
-        (progn
-          (unless (+workspace-buffer-list)
-            (+workspace-delete (+workspace-current-name)))
-          (+workspace-switch +dunnet-workspace-name t))
+        (+workspace-switch +dunnet-workspace-name t)
       (setq +dunnet--old-wconf (current-window-configuration))
       (delete-other-windows)
       (switch-to-buffer (doom-fallback-buffer))))
