2f0e6d22bf84f55cd4f5bc5ec05db34aacaffddb
LemonBreezes <look@strawberrytea.xyz>
2023-09-04 01:06:25 -0500

    modules/private/exwm/startup-programs/mpv.el

--- modules/private/exwm/startup-programs/mpv.el
+++ modules/private/exwm/startup-programs/mpv.el
@@ -1,1 +1,12 @@
 ;;; startup/mpv.el -*- lexical-binding: t; -*-
+
+(defvar startup/mpv-workspace "mpv")
+
+(defun startup/manage-chromium ()
+  (when (and (stringp exwm-class-name)
+             (string-match-p "mpv" exwm-class-name))
+    (unless (string= (+workspace-current-name) startup/mpv-workspace)
+      (previous-buffer))
+    (unless (+workspace-exists-p startup/mpv-workspace)
+      (+workspace-new startup/mpv-workspace)
+      (set-persp-parameter 'dont-save-to-file t (persp-get-by-name startup/mpv-workspace)))))
