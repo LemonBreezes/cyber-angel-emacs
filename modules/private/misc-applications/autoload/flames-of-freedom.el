f90658387f49a0c390fcb0d2a2fe9f0dd8b69d67
LemonBreezes <look@strawberrytea.xyz>
2023-09-03 20:56:25 -0500

    modules/private/misc-applications/autoload/flames-of-freedom.el

--- modules/private/misc-applications/autoload/flames-of-freedom.el
+++ modules/private/misc-applications/autoload/flames-of-freedom.el
@@ -5,24 +5,18 @@
   (interactive "P")
   (if arg
     (setq +flames-of-freedom--old-wconf nil)
-  (if (modulep! :ui workspaces)
-      ;; delete current workspace if empty
-      ;; this is useful when mu4e is in the daemon
-      ;; as otherwise you can accumulate empty workspaces
-      (progn
-        (unless (+workspace-buffer-list)
-          (+workspace-delete (+workspace-current-name)))
-        (+workspace-switch +flames-of-freedom-workspace-name t))
-    (setq +flames-of-freedom--old-wconf (current-window-configuration))
-    (delete-other-windows)
-    (switch-to-buffer (doom-fallback-buffer))))
+    (if (modulep! :ui workspaces)
+        (+workspace-switch +flames-of-freedom-workspace-name t)
+      (setq +flames-of-freedom--old-wconf (current-window-configuration))
+      (delete-other-windows)
+      (switch-to-buffer (doom-fallback-buffer))))
   (call-interactively #'flames-of-freedom-default))
 
 (defun +flames-of-freedom-quit ()
   (if (modulep! :ui workspaces)
-    (when (+workspace-exists-p +flames-of-freedom-workspace-name)
-      (+workspace/delete +flames-of-freedom-workspace-name))
-  (when +flames-of-freedom--old-wconf
-    (set-window-configuration +flames-of-freedom--old-wconf))))
+      (when (+workspace-exists-p +flames-of-freedom-workspace-name)
+        (+workspace/delete +flames-of-freedom-workspace-name))
+    (when +flames-of-freedom--old-wconf
+      (set-window-configuration +flames-of-freedom--old-wconf))))
 
 (advice-add #'flames-of-freedom-default :after #'+flames-of-freedom-quit)
