0e11c504630501bb38a5c27df1dd16e688f8dc19
LemonBreezes <look@strawberrytea.xyz>
2023-09-03 20:55:26 -0500

    modules/private/misc-applications/autoload/snow.el

--- modules/private/misc-applications/autoload/snow.el
+++ modules/private/misc-applications/autoload/snow.el
@@ -6,13 +6,7 @@
   (if arg
     (setq +snow--old-wconf nil)
   (if (modulep! :ui workspaces)
-      ;; delete current workspace if empty
-      ;; this is useful when mu4e is in the daemon
-      ;; as otherwise you can accumulate empty workspaces
-      (progn
-        (unless (+workspace-buffer-list)
-          (+workspace-delete (+workspace-current-name)))
-        (+workspace-switch +snow-workspace-name t))
+      (+workspace-switch +snow-workspace-name t)
     (setq +snow--old-wconf (current-window-configuration))
     (delete-other-windows)
     (switch-to-buffer (doom-fallback-buffer))))
@@ -26,8 +20,8 @@
 (defun +snow-quit ()
   (interactive)
   (if (modulep! :ui workspaces)
-    (when (+workspace-exists-p +snow-workspace-name)
-      (+workspace/delete +snow-workspace-name))
-  (when +snow--old-wconf
-    (set-window-configuration +snow--old-wconf)))
+      (when (+workspace-exists-p +snow-workspace-name)
+        (+workspace/delete +snow-workspace-name))
+    (when +snow--old-wconf
+      (set-window-configuration +snow--old-wconf)))
   (kill-buffer "*snow*"))
