bdbc58a038b226ba4f0d05dfe8932da0e2bb292b
LemonBreezes <look@strawberrytea.xyz>
2023-09-04 16:41:41 -0500

    config.el

--- config.el
+++ config.el
@@ -58,7 +58,7 @@
   (remove-hook 'tty-setup-hook #'doom-init-clipboard-in-tty-emacs-h))
 
 ;; This is necessary for Tramp to work. This regexp is specific to my machines.
-(setq tramp-shell-prompt-pattern "\\(?:^\\|\\)[^]#-%>]*#?[]#-%>][[:blank:]]*")
+(setq tramp-shell-prompt-pattern "\\(?:^\\|\\)[^]\n#-%>]*#?[]#-%>][[:blank:]]*")
 
 
 ;;; UI
