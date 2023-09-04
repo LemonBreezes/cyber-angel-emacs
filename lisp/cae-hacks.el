5482c6a548f658aae779c79550a496007cc32c60
LemonBreezes <look@strawberrytea.xyz>
2023-09-03 23:55:21 -0500

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -169,10 +169,6 @@ It is meant to be used as a `post-gc-hook'."
                cae-keyboard-remap-hydra-hint))
   (defalias sym #'identity))
 
-;; For some reason, this variable is not being set. It's safe to set it here as
-;; it should be overwritten by Doom later anyways.
-(setq evil-undo-system 'undo-fu)
-
 ;; For some reason I got a void variable error in `helm-system-packages' for
 ;; this.
 (defvar helm-marked-buffer-name "*helm marked*")
