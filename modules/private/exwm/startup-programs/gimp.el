7ab43b33f9342110a55ac2f7d545b34655f39fa1
LemonBreezes <look@strawberrytea.xyz>
2023-09-05 16:51:56 -0500

    modules/private/exwm/startup-programs/gimp.el

--- modules/private/exwm/startup-programs/gimp.el
+++ modules/private/exwm/startup-programs/gimp.el
@@ -2,46 +2,29 @@
 
 (defvar startup/gimp-process nil)
 (defvar startup/gimp-executable (executable-find "gimp"))
-(defvar startup/gimp-workspace "GIMP")
 
 (defun startup/start-gimp (&optional arg)
   (when startup/gimp-executable
     (setq startup/gimp-process
           (start-process "gimp"
                          " *startup/gimp*"
-                         startup/gimp-executable))
-    (when arg (+workspace-switch startup/gimp-workspace t))))
+                         startup/gimp-executable))))
 
 (defun startup/kill-gimp (&optional arg)
   (interactive "p")
   (when (process-live-p startup/gimp-process)
-    (kill-process startup/gimp-process))
-  (when (and arg (+workspace-exists-p startup/gimp-workspace))
-    (when (string= startup/gimp-workspace
-                   (+workspace-current-name))
-      (+workspace/other))
-    (+workspace-delete startup/gimp-workspace)))
+    (kill-process startup/gimp-process)))
 
 (defun startup/restart-gimp (&optional arg)
   (interactive "p")
   (startup/kill-gimp)
   (startup/start-gimp arg))
 
-(defun startup/manage-gimp ()
-  (when (and (stringp exwm-class-name)
-             (string-match-p "gimp" exwm-class-name))
-    (unless (string= (+workspace-current-name) startup/gimp-workspace)
-      (previous-buffer))
-    (unless (+workspace-exists-p startup/gimp-workspace)
-      (+workspace-new startup/gimp-workspace)
-      (set-persp-parameter 'dont-save-to-file t (persp-get-by-name startup/gimp-workspace)))))
-
 (defun startup/select-gimp ()
   (interactive)
   (unless (process-live-p startup/gimp-process)
     (startup/start-gimp))
-  (+workspace-switch startup/gimp-workspace t)
-  (+workspace-switch-to-exwm-buffer-maybe))
+  (+workspace-switch startup/gimp-workspace t))
 
 (map! :map +startup-applications-map
       :prefix "g"
