7082e688fbb59492717e6a1f6cb627763b3d2e53
LemonBreezes <look@strawberrytea.xyz>
2023-09-05 17:00:54 -0500

    modules/private/exwm/startup-programs/pavucontrol.el

--- modules/private/exwm/startup-programs/pavucontrol.el
+++ modules/private/exwm/startup-programs/pavucontrol.el
@@ -9,34 +9,17 @@
     (setq startup/pavucontrol-process
           (start-process "pavucontrol"
                          " *startup/pavucontrol*"
-                         startup/pavucontrol-executable))
-    (when arg (+workspace-switch startup/pavucontrol-workspace t)
-          (set-persp-parameter 'dont-save-to-file t
-                               (persp-get-by-name startup/pavucontrol-workspace)))))
+                         startup/pavucontrol-executable))))
 
 (defun startup/kill-pavucontrol (&optional arg)
   (interactive "p")
   (when (process-live-p startup/pavucontrol-process)
-    (kill-process startup/pavucontrol-process))
-  (when (and arg (+workspace-exists-p startup/pavucontrol-workspace))
-    (when (string= startup/pavucontrol-workspace
-                   (+workspace-current-name))
-      (+workspace/other))
-    (+workspace-delete startup/pavucontrol-workspace)))
+    (kill-process startup/pavucontrol-process)))
 
 (defun startup/restart-pavucontrol (&optional arg)
   (interactive "p")
   (startup/kill-pavucontrol)
   (startup/start-pavucontrol arg))
-
-(defun startup/manage-pavucontrol ()
-  (when (and (stringp exwm-class-name)
-             (string-match-p "pavucontrol" exwm-class-name))
-    (unless (string= (+workspace-current-name) startup/pavucontrol-workspace)
-      (previous-buffer))
-    (unless (+workspace-exists-p startup/pavucontrol-workspace)
-      (+workspace-new startup/pavucontrol-workspace)
-      (set-persp-parameter 'dont-save-to-file t (persp-get-by-name startup/pavucontrol-workspace)))))
 
 (defun startup/select-pavucontrol ()
   (interactive)
@@ -48,10 +31,7 @@
                     (string-match-p "pavucontrol" class)))
                 (doom-buffer-list)))
     (startup/restart-pavucontrol))
-  (+workspace-switch startup/pavucontrol-workspace t)
-  (set-persp-parameter 'dont-save-to-file t
-                       (persp-get-by-name startup/pavucontrol-workspace))
-  (+workspace-switch-to-exwm-buffer-maybe))
+  (+workspace-switch startup/pavucontrol-workspace t))
 
 (map! :map +startup-applications-map
       :prefix "p"
@@ -64,4 +44,3 @@
     "p r" "Restart Pavucontrol"
     "p s" "Select Pavucontrol"
     "p x" "Kill Pavucontrol"))
-(add-hook 'exwm-manage-finish-hook #'startup/manage-pavucontrol)
