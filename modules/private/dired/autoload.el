6a65fe58a611da68f12eb61071c00eaf825a65ce
LemonBreezes <look@strawberrytea.xyz>
2023-09-04 19:46:10 -0500

    modules/private/dired/autoload.el

--- modules/private/dired/autoload.el
+++ modules/private/dired/autoload.el
@@ -67,3 +67,25 @@
     (let* ((dv (dirvish-curr)) (fn (nth 4 (dv-type dv))))
       (if fn (funcall fn) (dirvish-kill dv))))
   (apply oldfun args))
+
+
+;;;###autoload
+(defun cae-dired-consult-jump-a (oldfun pos)
+  (+log pos)
+  (when (consp pos) (setq pos (car pos)))
+  (when (and (markerp pos)
+             (derived-mode-p 'dired-mode))
+    (when-let ((file (buffer-file-name (marker-buffer pos))))
+      (progn
+        ;; Check if file is in a different directory and if so change to it
+        (when-let ((dir (file-name-directory file)))
+          (unless (file-equal-p dir default-directory)
+            (funcall oldfun dir)))
+        ;; If not a directory, kill Dirvish and find the file
+        (unless (file-directory-p file)
+          ;; Copied from `dirvish-find-entry-a'
+          (let* ((dv (dirvish-curr)) (fn (nth 4 (dv-type dv))))
+            (if fn (funcall fn) (dirvish-kill dv)))
+          (funcall oldfun file wildcards)))))
+  ;; If not in Dired mode, find the file as usual
+  (funcall oldfun pos))
