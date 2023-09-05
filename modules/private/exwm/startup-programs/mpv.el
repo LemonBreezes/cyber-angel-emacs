4098d21606a28ee2a6dee9e81bdb7de128234c7d
LemonBreezes <look@strawberrytea.xyz>
2023-09-05 16:58:29 -0500

    modules/private/exwm/startup-programs/mpv.el

--- modules/private/exwm/startup-programs/mpv.el
+++ modules/private/exwm/startup-programs/mpv.el
@@ -2,23 +2,9 @@
 
 (defvar startup/mpv-workspace "mpv")
 
-(defun startup/manage-mpv ()
-  (when (and (stringp exwm-class-name)
-             (string-match-p "mpv" exwm-class-name))
-    (unless (string= (+workspace-current-name) startup/mpv-workspace)
-      (previous-buffer))
-    (unless (+workspace-exists-p startup/mpv-workspace)
-      (+workspace-new startup/mpv-workspace))))
-
 (defun startup/mpv-kill-mpv ()
   (interactive)
   (when (+workspace-exists-p startup/mpv-workspace)
     (+workspace-delete startup/mpv-workspace)))
 
-(defun startup/mpv-setup-workspace (persp _)
-  (unless (eq (persp-get-by-name startup/mpv-workspace) :nil)
-    (set-persp-parameter 'dont-save-to-file t (persp-get-by-name startup/mpv-workspace))))
-
-(add-hook 'exwm-manage-finish-hook #'startup/manage-mpv)
 (advice-add #'empv-exit :after #'startup/mpv-kill-mpv)
-(add-hook 'persp-created-functions #'startup/mpv-setup-workspace)
