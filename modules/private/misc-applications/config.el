e203e3874012a4ee0b6841b3193b426527210120
LemonBreezes <look@strawberrytea.xyz>
2023-09-05 22:06:09 -0500

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -257,18 +257,6 @@
     (which-key-add-keymap-based-replacements +misc-applications-system-map
       "s" "system packages")))
 
-(use-package! paradox
-  :defer t :init
-  (map! :map +misc-applications-system-map
-        "e" #'paradox-list-packages)
-  (after! which-key
-    (which-key-add-keymap-based-replacements +misc-applications-system-map
-      "e" "emacs packages"))
-  :config
-  (paradox-enable)
-  (map! :map paradox-menu-mode-map
-        "<f6>" #'cae-paradox-menu-quick-help))
-
 (use-package! pulseaudio-control
   :when (and (eq system-type 'gnu/linux)
              (executable-find "pactl"(and (eq system-type 'gnu/linux)
