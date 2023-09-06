a94d525f7696c20b7c8c056c8419a5a7cf156ac9
LemonBreezes <look@strawberrytea.xyz>
2023-09-06 12:36:23 -0500

    modules/private/exwm/autoload/exwm.el

--- modules/private/exwm/autoload/exwm.el
+++ modules/private/exwm/autoload/exwm.el
@@ -4,7 +4,6 @@
 (defvar +exwm-refocus-application--delay exwm-input--update-focus-interval)
 (defvar +exwm-refocus-application--timer nil)
 (defvar +exwm-refocus-application--last-time 0)
-
 ;;;###autoload
 (defun +exwm-refocus-application (&rest _)
   "Refocus input for the currently selected EXWM buffer, if any."
@@ -18,21 +17,18 @@
   (when (derived-mode-p 'exwm-mode)
     (setq +exwm-refocus-application--message (current-message))
     (let ((state (bound-and-true-p evil-state)))
-      (ignore state)
       (minibuffer-with-setup-hook
-          #'+exwm-refocus-application-minibuffer-quit-timer
+          (lambda ()
+            (run-at-time +exwm-refocus-application--delay nil
+             (lambda ()
+               (run-at-time
+                0.0 nil
+                (lambda ()
+                  (minibuffer-message "")
+                  (pcase state
+                    ('insert (exwm-evil-core-insert))
+                    ('normal (exwm-evil-core-normal))
+                    (_ nil))))
+               (when (minibufferp)
+                 (throw 'exit nil)))))
         (read-string "")))))
-
-(defun +exwm-refocus-application-minibuffer-quit-timer ()
-  (run-at-time +exwm-refocus-application--delay nil
-               (lambda ()
-                 (run-at-time
-                  0.0 nil
-                  (lambda ()
-                    (minibuffer-message "")
-                    (pcase state
-                      ('insert (exwm-evil-core-insert))
-                      ('normal (exwm-evil-core-normal))
-                      (_ nil)))
-                  (when (minibufferp)
-                    (throw 'exit nil))))))
