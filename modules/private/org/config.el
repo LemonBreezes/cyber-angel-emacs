9714d38b2dca69dbb178681ac5be628cb0dadea6
LemonBreezes <look@strawberrytea.xyz>
2023-09-06 15:36:04 -0500

    modules/private/org/config.el

--- modules/private/org/config.el
+++ modules/private/org/config.el
@@ -75,26 +75,30 @@
   (define-key worf-mode-map (kbd "S-<iso-lefttab>") (lookup-key org-mode-map (kbd "<backtab>")))
   (define-key worf-mode-map (kbd "<tab>") (lookup-key org-mode-map (kbd "<tab>")))
   (worf-define-key worf-mode-map (kbd "n") #'worf-new/body)
-  (advice-add #'worf-property
-              :after
-              (cae-defun cae-worf-property-a ()
-                ;; Jump infront of the property drawer
-                (let ((parent (org-element-property :parent (org-element-at-point))))
-                  (when (eq 'property-drawer (car parent))
-                    (goto-char (org-element-property :begin parent))))))
-  (advice-add #'worf-down
-              :around
-              ;; Skip over vimish folds
-              (cae-defun cae-worf-skip-vimish-fold-forward-a (oldfun arg)
-                (let ((point-max (-some->> (and (require 'vimish-fold nil t)
-                                                (overlays-at (point)))
-                                   (-filter #'vimish-fold--vimish-overlay-p)
-                                   (-map #'overlay-end)
-                                   (apply #'max)))
-                      (point (point)))
-                  (when point-max
-                    (setq arg (1+ arg)))
-                  (funcall oldfun arg)
-                  (when (<= (point) point-max)
-                    (goto-char point)))))
+  ;; So E is the same as % in my layout because E is not bound in
+  ;; `worf-mode-map'. I can use E to set the priority of the current heading.
+  (worf-define-key worf-mode-map "E" #'org-priority)
+
+  ;;(advice-add #'worf-property
+  ;;            :after
+  ;;            (cae-defun cae-worf-property-a ()
+  ;;              ;; Jump infront of the property drawer
+  ;;              (let ((parent (org-element-property :parent (org-element-at-point))))
+  ;;                (when (eq 'property-drawer (car parent))
+  ;;                  (goto-char (org-element-property :begin parent))))))
+  ;;(advice-add #'worf-down
+  ;;            :around
+  ;;            ;; Skip over vimish folds
+  ;;            (cae-defun cae-worf-skip-vimish-fold-forward-a (oldfun arg)
+  ;;              (let ((point-max (-some->> (and (require 'vimish-fold nil t)
+  ;;                                              (overlays-at (point)))
+  ;;                                 (-filter #'vimish-fold--vimish-overlay-p)
+  ;;                                 (-map #'overlay-end)
+  ;;                                 (apply #'max)))
+  ;;                    (point (point)))
+  ;;                (when point-max
+  ;;                  (setq arg (1+ arg)))
+  ;;                (funcall oldfun arg)
+  ;;                (when (<= (point) point-max)
+  ;;                  (goto-char point)))))
   (advice-add #'worf-add :after #'cae-org-set-created-timestamp))
