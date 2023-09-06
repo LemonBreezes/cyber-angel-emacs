ec22dde85104408f10137aabdbd6bb26e4b10151
LemonBreezes <look@strawberrytea.xyz>
2023-09-06 17:36:54 -0500

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -71,7 +71,7 @@
       "C-x x o" #'ov-clear
       "C-x P" #'pop-to-buffer
       "M-Z" #'zap-up-to-char
-      [C-i] #'doom/dumb-indent
+      ;;[C-i] #'doom/dumb-indent
       [remap dabbrev-expand] #'hippie-expand
       "C-S-i" #'doom/dumb-dedent
       "<escape>" #'keyboard-quit
