cd03b29f5902edc9043250b41603d42358f261d8
LemonBreezes <look@strawberrytea.xyz>
2023-09-06 12:37:29 -0500

    modules/private/exwm/autoload/exwm.el

--- modules/private/exwm/autoload/exwm.el
+++ modules/private/exwm/autoload/exwm.el
@@ -4,6 +4,8 @@
 (defvar +exwm-refocus-application--delay exwm-input--update-focus-interval)
 (defvar +exwm-refocus-application--timer nil)
 (defvar +exwm-refocus-application--last-time 0)
+(defvar +exwm-refocus-application--last-state nil)
+
 ;;;###autoload
 (defun +exwm-refocus-application (&rest _)
   "Refocus input for the currently selected EXWM buffer, if any."
@@ -16,19 +18,21 @@
 (defun +exwm-refocus-application--timer ()
   (when (derived-mode-p 'exwm-mode)
     (setq +exwm-refocus-application--message (current-message))
-    (let ((state (bound-and-true-p evil-state)))
+    (let ((+exwm-refocus-application--last-state (bound-and-true-p evil-state)))
       (minibuffer-with-setup-hook
-          (lambda ()
-            (run-at-time +exwm-refocus-application--delay nil
-             (lambda ()
-               (run-at-time
-                0.0 nil
-                (lambda ()
-                  (minibuffer-message "")
-                  (pcase state
-                    ('insert (exwm-evil-core-insert))
-                    ('normal (exwm-evil-core-normal))
-                    (_ nil))))
-               (when (minibufferp)
-                 (throw 'exit nil)))))
+          #'+exwm-refocus-application-minibuffer-quit-timer
         (read-string "")))))
+
+(defun +exwm-refocus-application-minibuffer-quit-timer ()
+  (run-at-time +exwm-refocus-application--delay nil
+               (lambda ()
+                 (run-at-time
+                  0.0 nil
+                  (lambda ()
+                    (minibuffer-message "")
+                    (pcase +exwm-refocus-application--last-state
+                      ('insert (exwm-evil-core-insert))
+                      ('normal (exwm-evil-core-normal))
+                      (_ nil))))
+                 (when (minibufferp)
+                   (throw 'exit nil)))))
