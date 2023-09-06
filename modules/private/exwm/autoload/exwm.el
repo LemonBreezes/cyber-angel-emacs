b9abc306cfc7afdcbfa27d86d9aa2ca3662b0c71
LemonBreezes <look@strawberrytea.xyz>
2023-09-06 11:36:36 -0500

    modules/private/exwm/autoload/exwm.el

--- modules/private/exwm/autoload/exwm.el
+++ modules/private/exwm/autoload/exwm.el
@@ -2,6 +2,7 @@
 
 (defvar +exwm-refocus-application--message nil)
 (defvar +exwm-refocus-application--delay (+ exwm-input--update-focus-interval 0.011))
+(defvar +exwm-refocus-application--timer nil)
 
 ;;;###autoload
 (defun +exwm-refocus-application (&rest _)
@@ -12,7 +13,6 @@
 (defun +exwm-refocus-application--timer ()
   (when (derived-mode-p 'exwm-mode)
     (setq +exwm-refocus-application--message (current-message))
-    (advice-add #'+exwm-refocus-application :override #'ignore)
     (let ((state (bound-and-true-p evil-state)))
       (add-transient-hook! 'minibuffer-setup-hook
         (run-at-time +exwm-refocus-application--delay nil
@@ -21,7 +21,6 @@
                         0.0 nil
                         (lambda ()
                           (minibuffer-message +exwm-refocus-application--message)
-                          (advice-remove #'+exwm-refocus-application #'ignore)
                           (pcase state
                             ('insert (exwm-evil-core-insert))
                             ('normal (exwm-evil-core-normal))
