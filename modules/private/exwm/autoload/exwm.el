cb44c2db8075e89d69323233cdf8b6277fe2c476
LemonBreezes <look@strawberrytea.xyz>
2023-09-06 12:34:01 -0500

    modules/private/exwm/autoload/exwm.el

--- modules/private/exwm/autoload/exwm.el
+++ modules/private/exwm/autoload/exwm.el
@@ -19,11 +19,9 @@
     (setq +exwm-refocus-application--message (current-message))
     (let ((state (bound-and-true-p evil-state)))
       (ignore state)
-      (add-transient-hook! 'minibuffer-setup-hook
-        #'+exwm-refocus-application-minibuffer-quit-timer))
-    (minibuffer-with-setup-hook
-        #'+exwm-refocus-application-minibuffer-quit-timer
-        (read-string ""))))
+      (minibuffer-with-setup-hook
+          #'+exwm-refocus-application-minibuffer-quit-timer
+        (read-string "")))))
 
 (defun +exwm-refocus-application-minibuffer-quit-timer ()
   (run-at-time +exwm-refocus-application--delay nil
