1d0795cbe5f5fa9b388a2b797f63c205a4e3f594
LemonBreezes <look@strawberrytea.xyz>
2023-09-08 15:02:22 -0500

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -56,7 +56,8 @@
     "T" "timers"))
 (after! timer-list
   (map! :map timer-list-mode-map
-        "<f6>" #'+timer-list-hydra/body
+        "<f6>" #'cae-cheatsheets-timer-list
+        :n "<f6>" #'cae-cheatsheets-evil-timer-list
         :n "gr" #'revert-buffer))
 (map! :map process-menu-mode-map
       "<f6>" #'+list-processes-hydra/body
