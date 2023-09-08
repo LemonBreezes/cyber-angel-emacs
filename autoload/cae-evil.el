6c3c75759e13b068bfa3f4b4716d59c906dc7d4f
LemonBreezes <look@strawberrytea.xyz>
2023-09-07 19:00:23 -0500

    autoload/cae-evil.el

--- autoload/cae-evil.el
+++ autoload/cae-evil.el
@@ -113,22 +113,3 @@
       (with-current-buffer buffer
         (org-mode)
         (setq-local doom-real-buffer-p t)))))
-
-;;;###autoload (autoload '+evil-cp-wrap-next-double-quotes "autoload/cae-evil" nil t)
-(evil-define-command +evil-cp-wrap-next-double-quotes (count)
-  "Wraps the next COUNT sexps inside curly braces. If the point
-is inside a symbol, that symbol is treated as the first sexp to
-wrap.
-
-When called with \\[universal-argument], wraps the current
-enclosing form and the next N forms, where N is the count for how
-many times the \\[universal-argument] was invoked."
-  (interactive "<c>")
-  (setq count (or count 1))
-  (if (consp current-prefix-arg)
-      (let ((count (evil-cp-universal-invoke-arg-count)))
-        (save-excursion
-          (sp-backward-up-sexp)
-          (evil-cp--wrap-next "\"" count))
-        (evil-cp--backward-up-list))
-    (evil-cp--wrap-next "\"" count)))
