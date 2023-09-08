7257400c7c16eb62e5d604cef010fca3c4981044
LemonBreezes <look@strawberrytea.xyz>
2023-09-08 14:52:06 -0500

    modules/private/misc-applications/autoload/fireplace.el

--- modules/private/misc-applications/autoload/fireplace.el
+++ modules/private/misc-applications/autoload/fireplace.el
@@ -1,14 +1,5 @@
 ;;; private/misc-applications/autoload/fireplace.el -*- lexical-binding: t; -*-
 
-;;;###autoload (autoload '+fireplace-hydra/body "private/misc-applications/autoload/fireplace" nil t)
-(defhydra +fireplace-hydra (:color pink :hint nil)
-  ("<f6>" nil "Exit" :exit t)
-  ("q" +fireplace-quit nil :exit t)
-  ("C-*" fireplace-toggle-smoke "Toggle smoke")
-  ("C-+" fireplace-up "Fireplace up")
-  ("C--" fireplace-down "Fireplace down")
-  ("C-=" fireplace-toggle-sound "Toggle sound"))
-
 ;;;###autoload
 (defun +fireplace (&optional arg)
   (interactive "P")
