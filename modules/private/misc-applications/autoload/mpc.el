ec4a2464403ad7d8cd29e3dca5e4ac691b48d5d0
LemonBreezes <look@strawberrytea.xyz>
2023-09-08 14:58:41 -0500

    modules/private/misc-applications/autoload/mpc.el

--- modules/private/misc-applications/autoload/mpc.el
+++ modules/private/misc-applications/autoload/mpc.el
@@ -1,26 +1,5 @@
 ;;; private/misc-applications/autoload/mpc.el -*- lexical-binding: t; -*-
 
-;;;###autoload (autoload '+mpc-hydra/body "private/misc-applications/autoload/mpc" nil t)
-(eval
- `(defhydra +mpc-hydra (:color pink :hint nil)
-    ("<f6>" nil "Exit" :exit t)
-    ("q" +mpc-quit nil :exit t)
-    ("<" mpc-prev "Previous song" :column "Navigate")
-    (">" mpc-next "Next song" :column "Navigate")
-    (,(if (modulep! :editor evil) "t" "s") mpc-toggle-play "Toggle play" :column "Toggle")
-    ,@(when (modulep! :editor evil)
-        '(("r" mpc-toggle-repeat "Toggle repeat" :column "Toggle")
-          ("s" mpc-toggle-shuffle "Toggle shuffle" :column "Toggle")
-          ("c" mpc-toggle-consume "Toggle consume" :column "Toggle")
-          ("p" mpc-playlist "Show playlist" :column "Playlist")
-          ("a" mpc-playlist-add "Add to playlist" :column "Playlist")
-          ("x" mpc-play-at-point "Play at point" :column "Playlist")))
-    ("p" mpc-pause "Pause" :column "Toggle")
-    ("g" mpc-seek-current "Seek current" :column "Navigate")
-    ("o" mpc-goto-playing-song "Goto playing song" :column "Movement"))
- t)
-
-
 (defvar +mpc--wconf nil)
 (defvar +mpc-buf-pos-alist nil)
 
