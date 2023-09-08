49a11efa5292bd755eefc2ef3ac527cf7267ab4e
LemonBreezes <look@strawberrytea.xyz>
2023-09-08 14:53:41 -0500

    modules/private/misc-applications/autoload/somafm.el

--- modules/private/misc-applications/autoload/somafm.el
+++ modules/private/misc-applications/autoload/somafm.el
@@ -9,14 +9,3 @@
   (if (cae-display-graphic-p)
       (call-interactively #'somafm)
     (call-interactively #'somafm-by-completion)))
-
-;;;###autoload (autoload '+somafm-hydra/body "private/misc-applications/autoload/somafm" nil t)
-(eval
- `(defhydra +somafm-hydra (:color pink :hint nil)
-    ("<f6>" nil "Exit" :exit t)
-    ("q" quit-window nil :exit t)
-    ("RET" somafm--play "Play")
-    ("l" somafm--sort "Sort")
-    ("s" somafm--stop "Stop")
-    (,(if (modulep! :editor evil) "r" "g") somafm--refresh-and-show-channels-buffer "Refresh"))
- t)
