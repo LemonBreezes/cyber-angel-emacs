89a1065c58e3de2f2fb008aa7cbdb994a0eb6f94
LemonBreezes <look@strawberrytea.xyz>
2023-09-08 13:58:17 -0500

    autoload/cae-cheatsheets.el

--- autoload/cae-cheatsheets.el
+++ autoload/cae-cheatsheets.el
@@ -1,23 +1,5 @@
 ;;; autoload/cae-cheatsheets.el -*- lexical-binding: t; -*-
 
-;;;###autoload (autoload 'cae-embark-collect-cheatsheet-hydra/body "autoload/cae-cheatsheets" nil t)
-(defhydra cae-embark-collect-cheatsheet-hydra (:color pink :foreign-keys run)
-  ("a" embark-act "Act" :column "Act")
-  ("A" embark-act-all "Act on all" :column "Act")
-  ("E" embark-export "Export" :column "Act")
-  ("m" embark-collect-mark "Mark" :column "Act")
-  ("s" isearch-forward "Search forward" :column "Navigate")
-  ;;("{" outline-previous-heading "Previous heading" :column "Navigate")
-  ;;("}" outline-next-heading "Next heading" :column "Navigate")
-  ("u" embark-collect-unmark "Unmark" :column "Act")
-  ("U" embark-collect-unmark-all "Unmark all" :column "Act")
-  ("t" embark-collect-toggle-marks "Toggle marks" :column "Act")
-  ("M-a" embark-collect-direct-action-minor-mode "Toggle direct action" :column "Act")
-  ("S" tabulated-list-sort "Sort" :column "Misc")
-  ;;("M-<left>" tabulated-list-previous-column "Previous column" :column "Navigate")
-  ;;("M-<right>" tabulated-list-next-column "Next column" :column "Navigate")
-  ("q" nil "Exit" :exit t :column nil))
-
 ;;;###autoload (autoload 'cae-vertico-cheatsheet-hydra/body "autoload/cae-cheatsheets" nil t)
 (defhydra cae-vertico-cheatsheet-hydra (:color pink :foreign-keys run)
   ("C-c ;" embark-export "Export")
