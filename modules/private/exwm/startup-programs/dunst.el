640fd0578f9782eddaa2d0adbf3e1dcbbe3d9289
LemonBreezes <look@strawberrytea.xyz>
2023-09-08 16:19:41 -0500

    modules/private/exwm/startup-programs/dunst.el

--- modules/private/exwm/startup-programs/dunst.el
+++ modules/private/exwm/startup-programs/dunst.el
@@ -26,23 +26,27 @@
 
   (add-hook 'circadian-after-load-theme-hook #'startup/restart-dunst)
 
-  (map! :leader
-        :prefix +startup-prefix
-        (:prefix"n"
-         :desc "Restart Dunst" "r" #'startup/restart-dunst
-         :desc "Kill Dunst" "x" #'startup/kill-dunst
-         :desc "Close notification" "c"
-         (cmd! (let ((shell-file-name "/bin/sh"))
-                 (quiet! (shell-command "dunstctl close" nil nil))))
-         :desc "History pop" "h"
-         (cmd! (let ((shell-file-name "/bin/sh"))
-                 (quiet! (shell-command "dunstctl history-pop" nil nil))))
-         :desc "Close all notifications" "C"
-         (cmd! (let ((shell-file-name "/bin/sh"))
-                 (quiet! (shell-command "dunstctl close-all" nil nil))))
-         :desc "Show context menu" "."
-         (cmd! (let ((shell-file-name "/bin/sh"))
-                 (quiet! (shell-command "dunstctl context" nil nil))))))
+  (map! :map +startup-applications-map
+        (:prefix "n"
+         "r" #'startup/restart-dunst
+         "x" #'startup/kill-dunst
+         "c" (cmd! (let ((shell-file-name "/bin/sh"))
+                     (quiet! (shell-command "dunstctl close" nil nil))))
+         "h" (cmd! (let ((shell-file-name "/bin/sh"))
+                     (quiet! (shell-command "dunstctl history-pop" nil nil))))
+         "C" (cmd! (let ((shell-file-name "/bin/sh"))
+                     (quiet! (shell-command "dunstctl close-all" nil nil))))
+         "." (cmd! (let ((shell-file-name "/bin/sh"))
+                     (quiet! (shell-command "dunstctl context" nil nil))))))
+  (after! which-key
+    (which-key-add-keymap-based-replacements +startup-applications-map
+                                             "n" "Dunst"
+                                             "n ." "Show context menu"
+                                             "n C" "Close all notifications"
+                                             "n c" "Close notification"
+                                             "n h" "History pop"
+                                             "n r" "Restart Dunst"
+                                             "n x" "Kill Dunst"))
 
   (if (process-live-p startup/dunst-process)
       (startup/restart-dunst)
