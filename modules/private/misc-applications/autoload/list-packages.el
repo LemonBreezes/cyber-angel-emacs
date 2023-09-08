6efe9fdbb09e7cd66669e62c105b867b1e6c00e4
LemonBreezes <look@strawberrytea.xyz>
2023-09-08 13:07:22 -0500

    modules/private/misc-applications/autoload/list-packages.el

--- modules/private/misc-applications/autoload/list-packages.el
+++ modules/private/misc-applications/autoload/list-packages.el
@@ -1,16 +1,6 @@
 ;;; private/misc-applications/autoload/list-packages.el -*- lexical-binding: t; -*-
 
 ;;;###autoload (autoload '+list-packages-hydra/body "private/misc-applications/autoload/list-packages" nil t)
-(eval ;; TODO
- `(defhydra +list-packages-hydra (:color pink :hint nil)
-    ("<f6>" nil "Exit" :exit t)
-    ("q" quit-window nil :exit t)
-    ("S" tabulated-list-sort "Sort")
-    ("?" package-menu-describe-package "Describe package")
-    ("H ")
-    )
-      t)
-
 (defhydra +list-packages-hydra (:color pink :hint nil)
   ("<f6>" nil "Exit" :exit t)
   ("q" quit-window :exit t)
