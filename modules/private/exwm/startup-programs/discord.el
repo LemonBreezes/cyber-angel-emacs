6fc0830c51fac89ba71c6cd6821b2a9e3eddba38
LemonBreezes <look@strawberrytea.xyz>
2023-09-07 19:48:30 -0500

    modules/private/exwm/startup-programs/discord.el

--- modules/private/exwm/startup-programs/discord.el
+++ modules/private/exwm/startup-programs/discord.el
@@ -51,7 +51,8 @@
   :before-while #'elcord--start-idle
   (process-live-p startup/discord-process))
 
-(map! :map +startup-applications-map :prefix "d"
+(map! :map +startup-applications-map
+      :prefix "d"
       "r" #'startup/restart-discord
       "s" #'startup/select-discord
       "x" #'startup/kill-discord)
