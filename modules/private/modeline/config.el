a785dff5f0436371a7d565185ae9f75cffded0aa
LemonBreezes <look@strawberrytea.xyz>
2023-09-09 01:17:59 -0500

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -1,10 +1,14 @@
 ;;; private/modeline/config.el -*- lexical-binding: t; -*-
 
-(add-hook! 'doom-first-file-hook
-  (setq-default mode-line-format
-                (cl-subst '(:eval (breadcrumb-project-crumbs))
-                          'mode-line-buffer-identification
-                          (default-value 'mode-line-format))))
+;;(add-hook! 'doom-first-file-hook
+;;  (setq-default mode-line-format
+;;                (cl-subst '(:eval (breadcrumb-project-crumbs))
+;;                          'mode-line-buffer-identification
+;;                          (default-value 'mode-line-format))))
+
+(use-package! breadcrumb
+  :defer t :init
+  (add-hook 'doom-first-buffer-hook #'breadcrumb-mode))
 
 (when (modulep! :editor evil)
   (setq evil-mode-line-format
