70ea8c481db997875fae3cc1add583625eadca15
LemonBreezes <look@strawberrytea.xyz>
2023-09-09 14:28:07 -0500

    modules/private/lisp/autoload/eval.el

--- modules/private/lisp/autoload/eval.el
+++ modules/private/lisp/autoload/eval.el
@@ -9,16 +9,8 @@
     (setq edebug-previous-result
           (edebug-safe-prin1-to-string previous-value))))
 
-(advice-add #'edebug-compute-previous-result
-            :around
-            #'adviced:edebug-compute-previous-result)
-
 (defun adviced:edebug-previous-result (_ &rest r)
   "Adviced `edebug-previous-result'."
   (eros--make-result-overlay edebug-previous-result
     :where (point)
     :duration eros-eval-result-duration))
-
-(advice-add #'edebug-previous-result
-            :around
-            #'adviced:edebug-previous-result)
