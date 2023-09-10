2c5c738776000798eb83fbdfac29f39bd37293d3
LemonBreezes <look@strawberrytea.xyz>
2023-09-10 15:51:16 -0500

    modules/private/exwm/+auto-persp.el

--- modules/private/exwm/+auto-persp.el
+++ modules/private/exwm/+auto-persp.el
@@ -145,16 +145,18 @@ buffers of that class."
 
   (add-hook 'exwm-floating-setup-hook #'exwm--disable-floating)
 
-  (persp-def-auto-persp "EXWM"
-                        :parameters '((dont-save-to-file . t))
-                        :hooks '(exwm-manage-finish-hook)
-                        :dyn-env '(after-switch-to-buffer-functions ;; prevent recursion
-                                   (persp-add-buffer-on-find-file nil)
-                                   persp-add-buffer-on-after-change-major-mode)
-                        :switch 'window
-                        :predicate #'+exwm-persp--predicate
-                        :after-match #'+exwm-persp--after-match
-                        :get-name #'+exwm-persp--get-name)
+  (run-at-time 1 nil
+               (lambda ()
+                 (persp-def-auto-persp "EXWM"
+                                       :parameters '((dont-save-to-file . t))
+                                       :hooks '(exwm-manage-finish-hook)
+                                       :dyn-env '(after-switch-to-buffer-functions ;; prevent recursion
+                                                  (persp-add-buffer-on-find-file nil)
+                                                  persp-add-buffer-on-after-change-major-mode)
+                                       :switch 'window
+                                       :predicate #'+exwm-persp--predicate
+                                       :after-match #'+exwm-persp--after-match
+                                       :get-name #'+exwm-persp--get-name)))
 
   (advice-add #'+workspace-switch :after #'+exwm-persp--focus-workspace-app)
 
