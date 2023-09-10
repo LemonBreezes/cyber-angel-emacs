3aa26ad96e4155bb55bf970190b2241c7e76824f
LemonBreezes <look@strawberrytea.xyz>
2023-09-09 20:52:27 -0500

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -122,10 +122,10 @@
   :config
   (after! cae-cheatsheets
     (let ((custom-filters
-           '(("R" (cmd! () (elfeed-search-set-filter "@6-months-ago +reddit")) "reddit" :column "Custom filters")
-             ("E" (cmd! () (elfeed-search-set-filter "@6-months-ago +emacs")) "emacs" :column "Custom filters")
-             ("Y" (cmd! () (elfeed-search-set-filter "@6-months-ago +tube")) "youtube" :column "Custom filters")
-             ("*" (cmd! () (elfeed-search-set-filter "@6-months-ago +star")) "star" :column "Custom filters"))))
+           `(("R" ,(cmd! () (elfeed-search-set-filter "@6-months-ago +reddit")) "reddit" :column "Custom filters")
+             ("E" ,(cmd! () (elfeed-search-set-filter "@6-months-ago +emacs")) "emacs" :column "Custom filters")
+             ("Y" ,(cmd! () (elfeed-search-set-filter "@6-months-ago +tube")) "youtube" :column "Custom filters")
+             ("*" ,(cmd! () (elfeed-search-set-filter "@6-months-ago +star")) "star" :column "Custom filters"))))
       (dolist (hydra '(cae-cheatsheets-elfeed-hydra cae-cheatsheets-evil-elfeed-hydra))
         (eval `(defhydra+ ,hydra () ,@custom-filters)))
       (cl-loop for (key . filter) in custom-filters do
