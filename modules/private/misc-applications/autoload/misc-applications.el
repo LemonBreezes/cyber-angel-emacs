4e10d0c25e3552c6e2b9e4992de29451f60f025e
LemonBreezes <look@strawberrytea.xyz>
2023-09-10 15:02:41 -0500

    modules/private/misc-applications/autoload/misc-applications.el

--- modules/private/misc-applications/autoload/misc-applications.el
+++ modules/private/misc-applications/autoload/misc-applications.el
@@ -3,6 +3,8 @@
 ;;;###autoload
 (defun +misc-applications-hide-cursor-h ()
   (setq-local cursor-type nil)
+  (when (boundp 'hl-line-mode)
+    (hl-line-mode +1))
   (when (featurep 'evil)
     (setq-local evil-normal-state-cursor '(bar . 0)
                 evil-insert-state-cursor '(bar . 0)
