3b8e007ea8ba4be1b57a7cb0071b2312b08d776f
LemonBreezes <look@strawberrytea.xyz>
2023-09-10 20:45:34 -0500

    modules/private/exwm/+auto-persp.el

--- modules/private/exwm/+auto-persp.el
+++ modules/private/exwm/+auto-persp.el
@@ -145,23 +145,16 @@ buffers of that class."
 
   (add-hook 'exwm-floating-setup-hook #'exwm--disable-floating)
 
-  (run-at-time 1 nil
-               (lambda ()
-                 (advice-add #'+workspace/display :override #'ignore)
-                 (unwind-protect
-                     (persp-def-auto-persp "EXWM"
-                                           :parameters '((dont-save-to-file . t))
-                                           :hooks '(exwm-manage-finish-hook)
-                                           :dyn-env '(after-switch-to-buffer-functions ;; prevent recursion
-                                                      (persp-add-buffer-on-find-file nil)
-                                                      persp-add-buffer-on-after-change-major-mode)
-                                           :switch 'window
-                                           :predicate #'+exwm-persp--predicate
-                                           :after-match #'+exwm-persp--after-match
-                                           :get-name #'+exwm-persp--get-name)
-                   (+workspace-switch (car (+workspace-list-names)))
-                   (advice-remove #'+workspace/display #'ignore)
-                   (+workspace/display))))
+  (persp-def-auto-persp "EXWM"
+                        :parameters '((dont-save-to-file . t))
+                        :hooks '(exwm-manage-finish-hook)
+                        :dyn-env '(after-switch-to-buffer-functions ;; prevent recursion
+                                   (persp-add-buffer-on-find-file nil)
+                                   persp-add-buffer-on-after-change-major-mode)
+                        :switch 'window
+                        :predicate #'+exwm-persp--predicate
+                        :after-match #'+exwm-persp--after-match
+                        :get-name #'+exwm-persp--get-name)
 
   (advice-add #'+workspace-switch :after #'+exwm-persp--focus-workspace-app)
 
