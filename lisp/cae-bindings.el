15fdddc9b4f934c7ddc17c08a9849cff02ce14d1
LemonBreezes <look@strawberrytea.xyz>
2023-09-12 12:39:37 -0500

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -283,4 +283,5 @@
 
 (when (modulep! :private exwm)
   (map! "s-c" (cmd! () (start-process "chrome" nil "google-chrome-stable"))
-        "s-f" (cmd! () (start-process "flameshot" nil "flameshot" "gui"))))
+        "s-f" (cmd! () (start-process "flameshot" nil "flameshot" "gui"))
+        "s-t" (cmd! () (start-process "teams" nil "teams-for-linux"))))
