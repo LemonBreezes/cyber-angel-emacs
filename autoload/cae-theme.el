a853c56dfe1bf39e63a491709f303d1c0bf6ff59
LemonBreezes <look@strawberrytea.xyz>
2023-09-13 14:15:27 -0500

    autoload/cae-theme.el

--- autoload/cae-theme.el
+++ autoload/cae-theme.el
@@ -21,3 +21,24 @@
              (>= (cl-second now) (cl-second sunset)))
         (and (= (cl-first now) (cl-first sunrise))
              (< (cl-second now) (cl-second sunrise))))))
+
+;;;###autoload
+(defun cae-theme-export-using-pywal ()
+  (when (and (executable-find "python")
+             (executable-find "wal")
+             (require 'theme-magic nil t)
+             (require 'ewal nil t))
+    (let (;; If we're in a pyenv directory, we might accidentally run the virtual
+          ;; version of Python instead of the user's root version. To fix this, we
+          ;; temporarily change to the user's dir.
+          (default-directory "~/"))
+      (set-process-sentinel
+       (apply #'start-process
+              "cae-theme-magic"
+              nil
+              "python"
+              theme-magic--pywal-python-script
+              (theme-magic--auto-extract-16-colors))
+       (lambda (_proc event)
+         (when (string= event "finished\n")
+           (ewal-load-colors)))))))
