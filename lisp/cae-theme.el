7eac91b9909ade854b06e5ec5034936cee78721a
LemonBreezes <look@strawberrytea.xyz>
2023-09-13 11:18:34 -0500

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -135,21 +135,21 @@
   :init (setq ewal-use-built-in-always-p nil
               ewal-use-built-in-on-failure-p t
               ewal-built-in-palette "sexy-material"))
-;;(use-package! ewal-spacemacs-themes
-;;  :init (progn
-;;          (setq spacemacs-theme-underline-parens t
-;;                my:rice:font (font-spec
-;;                              :family "Source Code Pro"
-;;                              :weight 'semi-bold
-;;                              :size 11.0))
-;;          (show-paren-mode +1)
-;;          (global-hl-line-mode)
-;;          (set-frame-font my:rice:font nil t)
-;;          (add-to-list  'default-frame-alist
-;;                        `(font . ,(font-xlfd-name my:rice:font))))
-;;  :config (progn
-;;            (load-theme 'ewal-spacemacs-modern t)
-;;            (enable-theme 'ewal-spacemacs-modern)))
+(use-package! ewal-spacemacs-themes
+  :init (progn
+          (setq spacemacs-theme-underline-parens t
+                my:rice:font (font-spec
+                              :family "Source Code Pro"
+                              :weight 'semi-bold
+                              :size 11.0))
+          (show-paren-mode +1)
+          (global-hl-line-mode)
+          (set-frame-font my:rice:font nil t)
+          (add-to-list  'default-frame-alist
+                        `(font . ,(font-xlfd-name my:rice:font))))
+  :config (progn
+            (load-theme 'ewal-spacemacs-modern t)
+            (enable-theme 'ewal-spacemacs-modern)))
 (use-package! ewal-evil-cursors
   :after (ewal-spacemacs-themes)
   :config (ewal-evil-cursors-get-colors
