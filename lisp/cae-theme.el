2c5534180a2885b88e95bf6ce3c5ea291bab8277
LemonBreezes <look@strawberrytea.xyz>
2023-09-13 11:22:18 -0500

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -134,27 +134,10 @@
 (use-package! ewal
   :init (setq ewal-use-built-in-always-p nil
               ewal-use-built-in-on-failure-p t
-              ewal-built-in-palette "sexy-material"))
-(use-package! ewal-spacemacs-themes
-  :init (progn
-          (setq spacemacs-theme-underline-parens t
-                my:rice:font (font-spec
-                              :family "Source Code Pro"
-                              :weight 'semi-bold
-                              :size 11.0))
-          (show-paren-mode +1)
-          (global-hl-line-mode)
-          (set-frame-font my:rice:font nil t)
-          (add-to-list  'default-frame-alist
-                        `(font . ,(font-xlfd-name my:rice:font))))
-  :config (progn
-            (load-theme 'ewal-spacemacs-modern t)
-            (enable-theme 'ewal-spacemacs-modern)))
+              ewal-built-in-palette "sexy-material")
+  :config
+  (ewal-load-colors))
 (use-package! ewal-evil-cursors
   :after (ewal-spacemacs-themes)
   :config (ewal-evil-cursors-get-colors
            :apply t :spaceline t))
-(use-package! spaceline
-  :after (ewal-evil-cursors winum)
-  :init (setq powerline-default-separator nil)
-  :config (spaceline-spacemacs-theme))
