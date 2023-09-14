8731bd951b647367b4f8422d899e3a10ebba2df9
LemonBreezes <look@strawberrytea.xyz>
2023-09-14 16:28:21 -0500

    autoload/cae-corfu.el

--- autoload/cae-corfu.el
+++ autoload/cae-corfu.el
@@ -38,3 +38,12 @@
            if (or (eq major-mode (buffer-local-value 'major-mode buf))
                   (< (buffer-size buf) (* 1 1024 1024)))
          collect buf))
+
+;;;###autoload
+(defun corfu-send-shell (&rest _)
+  "Send completion candidate when inside comint/eshell."
+  (cond
+   ((and (derived-mode-p 'eshell-mode) (fboundp 'eshell-send-input))
+    (eshell-send-input))
+   ((and (derived-mode-p 'comint-mode)  (fboundp 'comint-send-input))
+    (comint-send-input))))
