c4729f2979280280f2b7742998010c14e2f8d8fe
LemonBreezes <look@strawberrytea.xyz>
2023-09-15 11:44:51 -0500

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -144,7 +144,7 @@ using the tab-width variable."
                                     (regexp-quote actual-indent) "" line)))
              stripped-indent)
          line))
-               lines "\n")))
+     lines "\n")))
 
 ;;;###autoload
 (defun cae-copy-for-reddit ()
@@ -318,7 +318,7 @@ This is the format used on Reddit for code blocks."
           ((eq this-command 'cae-avy-symbol-at-point)
            (er/mark-symbol))
           (t (if (fboundp 'eri/expand-region)
-                  (eri/expand-region 1)
+                 (eri/expand-region 1)
                (er/expand-region 1))))
     (call-interactively action))
   (run-at-time 0.0 nil
