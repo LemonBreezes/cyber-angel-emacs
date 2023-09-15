8fb9913f39355bb36fe57bff6109a001779a2236
LemonBreezes <look@strawberrytea.xyz>
2023-09-15 11:53:49 -0500

    config.el

--- config.el
+++ config.el
@@ -434,9 +434,7 @@
               (t ,@'(vertico-flat-mode)))
             vertico-multiform-commands
             `((cae-embark-act-with-completing-read
-               ,@(if (cae-display-graphic-p)
-                     '(grid posframe)
-                   '(grid)))))))
+               ,(alist-get 'embark-keybinding vertico-multiform-categories))))))
 
   ;; Use Emacs as the default editor for shell commands.
   (when (cae-display-graphic-p)
