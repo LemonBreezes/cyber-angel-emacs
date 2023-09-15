5e1a4255bc2b72ddb733f7d5f883e741ed35f98d
LemonBreezes <look@strawberrytea.xyz>
2023-09-15 18:23:14 -0500

    lisp/cae-helm.el

--- lisp/cae-helm.el
+++ lisp/cae-helm.el
@@ -1,3 +1,8 @@
 ;;; lisp/cae-helm.el -*- lexical-binding: t; -*-
 
-(advice-add #'helm-packages-install :around #'cae-always-yes-a)
+(advice-add #'helm-packages-install :around
+        (cae-defun cae-helm-packages-install-and-require-a (oldfun candidate)
+          (let ((pkgs (helm-marked-candidates)))
+          (cae-always-yes-a oldfun candidate)
+          (dolist (pkg pkgs)
+            (require pkg)))))
