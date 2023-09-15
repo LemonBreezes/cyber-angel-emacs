84c9da1c97316a4fe4e02dc6692f2f70a9e63361
LemonBreezes <look@strawberrytea.xyz>
2023-09-15 12:23:18 -0500

    config.el

--- config.el
+++ config.el
@@ -258,7 +258,7 @@
       (set-popup-rule! (regexp-quote embark--verbose-indicator-buffer)
         :size #'+popup-shrink-to-fit :side 'bottom :ttl t)
       (set-popup-rule! "^\\*Embark Export: "
-        :size #'+popup-shrink-to-fit :side 'right :ttl 0))
+        :size #'cae-popup-resize-help-buffer :side 'right :ttl 0))
     (after! elfeed
       (set-popup-rule! (format "^%s$" (regexp-quote elfeed-log-buffer-name))
         :size '+popup-shrink-to-fit :side 'right :select nil :quit t :ttl nil))
