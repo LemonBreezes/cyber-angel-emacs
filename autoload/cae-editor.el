8f2d2ade051f0e3cdc76bf8d215e05a51cf10493
LemonBreezes <look@strawberrytea.xyz>
2023-09-15 14:55:05 -0500

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -405,3 +405,24 @@ also marks comment with leading whitespace"
 (defun cae-embrace-with-prefix-function ()
   (let ((fname (read-string "Function: ")))
     (cons (format "(%s " (or fname "")) ")")))
+
+(defun cae-modeline--rotate-word-at-point (rotate-function)
+  (save-excursion
+    (when-let ((beg (car-safe (bounds-of-thing-at-point 'symbol))))
+      (goto-char beg))
+    (skip-syntax-forward "^w" (line-end-position))
+    (condition-case err
+        (call-interactively rotate-function)
+        (error
+         (skip-syntax-backward "^w" (line-beginning-position))
+         (call-interactively rotate-function)))))
+
+;;;###autoload
+(defun cae-modeline-rotate-forward-word-at-point ()
+  (interactive)
+  (cae-modeline--rotate-word-at-point #'parrot-rotate-next-word-at-point))
+
+;;;###autoload
+(defun cae-modeline-rotate-backward-word-at-point ()
+  (interactive)
+  (cae-modeline--rotate-word-at-point #'parrot-rotate-prev-word-at-point))
