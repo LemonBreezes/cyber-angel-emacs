4b8f5f7d0e596150df3b638d6b611bd8cdb77be7
LemonBreezes <look@strawberrytea.xyz>
2023-09-15 14:26:03 -0500

    modules/private/gnus/config.el

--- modules/private/gnus/config.el
+++ modules/private/gnus/config.el
@@ -40,16 +40,7 @@
                                    (nntp "Eternal September"
                                          (nntp-address "news.eternal-september.org")
                                          (nntp-authinfo-user "StrawberryTea"))
-                                   (nnhackernews "")
-                                   ;;(nnimap "Fastmail"
-                                   ;;        (nnimap-inbox "INBOX")
-                                   ;;        (nnimap-address "imap.fastmail.com")
-                                   ;;        (nnimap-server-port 993)
-                                   ;;        (nnimap-stream ssl)
-                                   ;;        (nnimap-streaming t)
-                                   ;;        (nnimap-expunge never)
-                                   ;;        (nnimap-split-methods default))
-                                   )
+                                   (nnhackernews ""))
    gnus-registry-ignored-groups '(("nntp" t) ("^INBOX" t))
    gnus-signature-separator '("^-- $" "^-- *$" "^_____+$")
    gnus-uncacheable-groups "^nnml"
@@ -116,7 +107,7 @@
                        "nntp+Gmane:gmane.emacs.gnus.patches" "nntp+Gmane:gmane.emacs.gnus.announce"
                        "nntp+Gmane:gmane.emacs.gnus.general")
                       ("orgmode" "nntp+Gmane:gmane.emacs.orgmode") ("misc")
-                      ("fastmail" "nnimap+fastmail:INBOX" "nnimap+fastmail:Sent") ("hackernews" "nnhackernews:news")
+                      ("hackernews" "nnhackernews:news")
                       ("emacs" "nntp+Gmane:gmane.linux.gentoo.announce" "nntp+Gmane:gmane.emacs.help"
                        "nntp+Gmane:gmane.emacs.diffs" "nntp+Gmane:gmane.emacs.bugs"
                        "nntp+Gmane:gmane.emacs.devel" "nntp+Gmane:gmane.emacs.announce")
@@ -125,7 +116,7 @@
    gnus-topic-topology '(("Root" visible) (("misc" visible))
                          (("emacs" visible nil nil) (("orgmode" visible nil nil))
                           (("gnus" visible nil nil)) (("emms" invisible nil nil)))
-                         (("fastmail" visible nil nil)) (("hackernews" visible))))
+                         (("hackernews" visible))))
   (map! :map gnus-group-mode-map
         "<f6>" #'cae-gnus-group-cheatsheet/body
         :n "Tm" #'gnus-topic-move-group))
@@ -191,44 +182,6 @@
 
 (use-package! spam
   :after gnus)
-
-(use-package! bbdb
-  :after gnus :init
-  (add-hook 'message-mode-hook
-            (function (lambda()
-                        (local-set-key (kbd "<tab>") 'bbdb-complete-mail))))
-  :defer t :config
-  (require 'bbdb-com)
-  (require 'bbdb-gnus)
-  (bbdb-initialize 'message 'gnus)
-  (bbdb-mua-auto-update-init 'message 'gnus)
-  (setq bbdb-mua-pop-up nil)
-  (setq bbdb-allow-duplicates t)
-  (setq bbdb-pop-up-window-size 5)
-  (setq bbdb-ignore-redundant-mails t)
-  (setq bbdb-update-records-p 'create)
-  (setq bbdb-mua-interactive-action '(create . query))
-  (setq bbdb-mua-auto-update-p 'create)
-  (add-hook 'mail-setup-hook 'bbdb-mail-aliases)
-  (add-hook 'message-setup-hook 'bbdb-mail-aliases)
-  (add-hook 'bbdb-notice-mail-hook 'bbdb-auto-notes)
-  (add-hook 'kill-emacs-hook 'bbdb-save)
-  (setq bbdb-completion-display-record t
-        bbdb-default-area-code nil
-        bbdb-add-aka t
-        bbdb-add-name nil
-        bbdb-add-mails t
-        bbdb-ignore-message-alist '(("Newsgroup" . ".*")))
-  (defalias 'bbdb-y-or-n-p #'(lambda (prompt) t))
-  (setq bbdb-auto-notes-rules
-        '(("Newsgroups" ("[^,]+" newsgroups 0))
-          ("Subject" (".*" last-subj 0 t))
-          ("User-Agent" (".*" mailer 0))
-          ("X-Mailer" (".*" mailer 0))
-          ("Organization" (".*" organization 0))
-          ("X-Newsreader" (".*" mailer 0))
-          ("X-Face" (".+" face 0 'replace))
-          ("Face" (".+" face 0 'replace)))))
 
 (use-package! nnhackernews
   :defer t :config
