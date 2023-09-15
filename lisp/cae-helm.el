5df9645cc143754b537548904a06c0d8e4ef2a0e
LemonBreezes <look@strawberrytea.xyz>
2023-09-15 18:24:04 -0500

    lisp/cae-helm.el

--- lisp/cae-helm.el
+++ lisp/cae-helm.el
@@ -1,8 +1,11 @@
 ;;; lisp/cae-helm.el -*- lexical-binding: t; -*-
 
+;; It's super annoying to have to confirm the package
 (advice-add #'helm-packages-install :around
             (cae-defun cae-helm-packages-install-and-require-a (oldfun candidate)
               (let ((pkgs (helm-marked-candidates)))
                 (cae-always-yes-a oldfun candidate)
                 (dolist (pkg pkgs)
-                  (require pkg)))))
+                  (condition-case err
+                      (require pkg)
+                    (message "Failed to load %s: %s" pkg err))))))
