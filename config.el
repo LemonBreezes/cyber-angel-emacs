0be583b720fa550f69913fda0e661e229c2c4a12
StrawberryTea <look@strawberrytea.xyz>
2023-09-16 22:36:15 -0500

    config.el

--- config.el
+++ config.el
@@ -44,7 +44,7 @@
 (unless (memq system-type '(cygwin windows-nt ms-dos))
   ;; Previously I used Iosevka Comfy and size 18.
   (setq doom-font (font-spec :family "Iosevka Term SS15" :size 18)
-        doom-variable-pitch-font (font-spec :family "Iosevka Comfy Duo"
+        doom-variable-pitch-font (font-spec :family "Iosevka Aile"
                                             :size 18)))
 
 ;; Do not break my clipboard in SSH sessions.
