e3e8518f2d54aa126db6ec761ff1fb4d3d3b5fd1
StrawberryTea <look@strawberrytea.xyz>
2023-09-18 01:00:16 -0500

    .

--- init.el
+++ init.el
@@ -98,7 +98,7 @@
        (:if (not (memq system-type '(cygwin windows-nt ms-dos))) tty +osc)
 
        :config
-       (default +bindings)
+       (default +bindings +smartparens)
 
        :email
        (mu4e +org)
