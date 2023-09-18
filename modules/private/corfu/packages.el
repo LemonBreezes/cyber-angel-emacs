37582cb719b553b10835f180321cfd90c1ea4321
StrawberryTea <look@strawberrytea.xyz>
2023-09-18 10:52:28 -0500

    .

--- modules/private/corfu/packages.el
+++ modules/private/corfu/packages.el
@@ -1,15 +1,17 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; completion/corfu/packages.el
 
+;; -*- no-byte-compile: t; -*-
+;;; completion/corfu/packages.el
+
 (package! corfu :recipe (:files ("*.el" "extensions/*.el")))
 (package! cape)
 (when (modulep! +icons)
+  (package! svg-lib)
   (package! kind-icon))
-(when (modulep! :os tty)
-  (package! corfu-terminal))
 (when (modulep! +orderless)
   (package! orderless))
-(package! yasnippet-capf)
+(when (modulep! :os tty)
+  (package! corfu-terminal))
 (when (modulep! :editor snippets)
-  (package! yasnippet-capf :recipe
-    (:host github :repo "LuigiPiucco/yasnippet-capf")))
+  (package! yasnippet-capf))
