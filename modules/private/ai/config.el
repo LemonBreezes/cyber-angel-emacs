212dc973c80e21204ca61c929785c399b7e8e3b9
StrawberryTea <look@strawberrytea.xyz>
2023-09-21 23:16:34 -0500

    modules/private/ai/config.el

--- modules/private/ai/config.el
+++ modules/private/ai/config.el
@@ -26,6 +26,8 @@
   (map! :map org-ai-global-mode-map
         :prefix ("C-c M-a" . "org-ai")
         "b" #'cae-ai-org-ai-on-buffer)
+  (map! :nv "g1" #'cae-evil-org-ai-on-region
+        :nv "g!" #'cae-ai-org-ai-on-buffer)
   (map! :map org-ai-mode-map
         [remap org-ai-kill-region-at-point] #'cae-ai-org-ai-kill-region-at-point)
   (defvar org-ai-global-mode-prefix-map
