6f8af4e7598f07be12c43af694477890dec7727f
StrawberryTea <look@strawberrytea.xyz>
2023-09-21 23:43:00 -0500

    config.el

--- config.el
+++ config.el
@@ -898,7 +898,35 @@
       (add-to-list 'parrot-rotate-dict '(:rot ("kbd" "cae-keyboard-kbd")))
       (add-to-list 'parrot-rotate-dict '(:rot ("+log" "message")))
       (add-to-list 'parrot-rotate-dict '(:rot ("backtrace!" "unbacktrace!")))
-      (add-to-list 'parrot-rotate-dict '(:rot ("enabled" "disabled"))))))
+      (add-to-list 'parrot-rotate-dict '(:rot ("enabled" "disabled")))))
+
+  (use-package! string-inflection
+    :commands (string-inflection-all-cycle
+               string-inflection-toggle
+               string-inflection-camelcase
+               string-inflection-lower-camelcase
+               string-inflection-kebab-case
+               string-inflection-underscore
+               string-inflection-capital-underscore
+               string-inflection-upcase)
+    :init
+    (map! :leader :prefix-map ("c~" . "naming convention")
+          :desc "cycle" "~" #'string-inflection-all-cycle
+          :desc "toggle" "t" #'string-inflection-toggle
+          :desc "CamelCase" "c" #'string-inflection-camelcase
+          :desc "downCase" "d" #'string-inflection-lower-camelcase
+          :desc "kebab-case" "k" #'string-inflection-kebab-case
+          :desc "under_score" "_" #'string-inflection-underscore
+          :desc "Upper_Score" "u" #'string-inflection-capital-underscore
+          :desc "UP_CASE" "U" #'string-inflection-upcase)
+    (after! evil
+      (evil-define-operator evil-operator-string-inflection (beg end _type)
+        "Define a new evil operator that cycles symbol casing."
+        :move-point nil
+        (interactive "<R>")
+        (string-inflection-all-cycle)
+        (setq evil-repeat-info '([?g ?~])))
+      (define-key evil-normal-state-map (kbd "g~") 'evil-operator-string-inflection))))
 
 
 ;;; Autocompletion
