9cea453eb658180d93527520422bbc4587faf676
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 16:09:45 -0500

    lisp/cae-ansi-color.el

--- lisp/cae-ansi-color.el
+++ lisp/cae-ansi-color.el
@@ -1,11 +1,2 @@
 ;;; lisp/cae-ansi-color.el -*- lexical-binding: t; -*-
 
-(add-hook 'text-mode-hook
-          (cae-defun cae-ansi-color-text-mode-h ()
-            (unless (derived-mode-p 'org-mode)
-              ;; Apply ANSI color codes
-              (with-silent-modifications
-                (ansi-color-apply-on-region (point-min) (point-max) t)))))
-
-(setenv "TERM" "xterm-256color")
-(setq compilation-environment '("TERM=xterm-256color"))
