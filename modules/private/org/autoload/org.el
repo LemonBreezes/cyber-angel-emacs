55340871ecab545543bcb886e4352500997bd105
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 18:42:43 -0500

    modules/private/org/autoload/org.el

--- modules/private/org/autoload/org.el
+++ modules/private/org/autoload/org.el
@@ -26,7 +26,7 @@ from running."
  :transient t)
 
 ;;;###autoload
-(defun +org-export-remove-zero-width-space (text _backend _info)
+(defun cae-org-export-remove-zero-width-space (text _backend _info)
   "Remove zero width spaces from TEXT."
   (unless (org-export-derived-backend-p 'org)
     (replace-regexp-in-string "\u200B" "" text)))
