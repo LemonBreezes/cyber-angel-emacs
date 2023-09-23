a9766dad42187643e54dafbbebab96325a24d3f3
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 18:20:44 -0500

    autoload/cae-email.el

--- autoload/cae-email.el
+++ autoload/cae-email.el
@@ -6,3 +6,21 @@
   (if (bound-and-true-p +notmuch-workspace-name)
       (+workspace/delete +notmuch-workspace-name)
     (call-interactively #'notmuch-bury-or-kill-this-buffer)))
+
+;;;###autoload
+(defun cae-org-msg-goto-body (&optional end)
+  "Go to either the beginning or the end of the body.
+END can be the symbol top, bottom, or nil to toggle."
+  (interactive)
+  (let ((initial-pos (point)))
+    (org-msg-goto-body)
+    (when (or (eq end 'top)
+              (and (or (eq initial-pos (point)) ; Already at bottom
+                       (<= initial-pos ; Above message body
+                           (save-excursion
+                             (message-goto-body)
+                             (point))))
+                   (not (eq end 'bottom))))
+      (message-goto-body)
+      (search-forward (format org-msg-greeting-fmt
+                              (concat " " (org-msg-get-to-name)))))))
