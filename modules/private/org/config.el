b16be4419455c00b3b9c424c3d655eebd1959d7e
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 18:39:10 -0500

    modules/private/org/config.el

--- modules/private/org/config.el
+++ modules/private/org/config.el
@@ -153,3 +153,16 @@
             'all-the-icons
           'unicode))
   (org-ol-tree-ui--update-icon-set))
+
+(when (modulep! :editor evil)
+  (after! org
+    (map! :map org-mode-map
+          :nie "M-SPC M-SPC" (cmd! (insert "\u200B"))))
+
+  (defun +org-export-remove-zero-width-space (text _backend _info)
+    "Remove zero width spaces from TEXT."
+    (unless (org-export-derived-backend-p 'org)
+      (replace-regexp-in-string "\u200B" "" text)))
+
+  (after! ox
+    (add-to-list 'org-export-filter-final-output-functions #'+org-export-remove-zero-width-space t)))
