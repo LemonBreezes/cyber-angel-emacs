3bdbc83e422c8049ad1764980c6eba169de96c30
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 18:39:50 -0500

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -282,9 +282,7 @@
           "M-RET" #'cae-evil-org-insert-heading
           "M-S-RET" #'cae-evil-org-insert-todo-heading
           "M-<return>" #'org-insert-heading
-          "M-S-<return>" #'org-insert-todo-heading
-          :localleader
-          "l f" #'+org-insert-file-link)))
+          "M-S-<return>" #'org-insert-todo-heading)))
 
 (defmacro cae-exwm-app-runner (app-name app-title)
   `(lambda (arg)
