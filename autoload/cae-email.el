bf9af70b94205e0b7b7ba8df52eddad0121ae7fa
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 18:29:33 -0500

    autoload/cae-email.el

--- autoload/cae-email.el
+++ autoload/cae-email.el
@@ -6,3 +6,10 @@
   (if (bound-and-true-p +notmuch-workspace-name)
       (+workspace/delete +notmuch-workspace-name)
     (call-interactively #'notmuch-bury-or-kill-this-buffer)))
+
+;;;###autoload
+(defun cae-org-msg-goto-body-when-replying (compose-type &rest _)
+  "Call `+org-msg-goto-body' when the current message is a reply."
+  (when (and (derived-mode-p 'org-msg-edit-mode)
+             (eq compose-type 'reply))
+    (org-msg-goto-body)))
