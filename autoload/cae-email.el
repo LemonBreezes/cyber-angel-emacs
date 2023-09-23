37347f5b87a03c297bcd7fecf7c02ece22f8c2a0
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 18:29:04 -0500

    autoload/cae-email.el

--- autoload/cae-email.el
+++ autoload/cae-email.el
@@ -8,27 +8,9 @@
     (call-interactively #'notmuch-bury-or-kill-this-buffer)))
 
 ;;;###autoload
-(defun cae-org-msg-goto-body (&optional end)
-  "Go to either the beginning or the end of the body.
-END can be the symbol top, bottom, or nil to toggle."
-  (interactive)
-  (let ((initial-pos (point)))
-    (org-msg-goto-body)
-    (when (or (eq end 'top)
-              (and (or (eq initial-pos (point)) ; Already at bottom
-                       (<= initial-pos ; Above message body
-                           (save-excursion
-                             (message-goto-body)
-                             (point))))
-                   (not (eq end 'bottom))))
-      (message-goto-body)
-      (and org-msg-greeting-fmt
-           (search-forward (format org-msg-greeting-fmt
-                                   (concat " " (org-msg-get-to-name))))))))
-
-;;;###autoload
 (defun cae-org-msg-goto-body-when-replying (compose-type &rest _)
   "Call `+org-msg-goto-body' when the current message is a reply."
   (when (and (derived-mode-p 'org-msg-edit-mode)
              (eq compose-type 'reply))
+    (+log "EHHJ")
     (cae-org-msg-goto-body)))
