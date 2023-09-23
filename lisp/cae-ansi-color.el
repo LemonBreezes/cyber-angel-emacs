432e4885ac3bd91a6ff20ad10c4dc5b1b60dced0
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 15:46:01 -0500

    lisp/cae-ansi-color.el

--- lisp/cae-ansi-color.el
+++ lisp/cae-ansi-color.el
@@ -1,8 +1,8 @@
 ;;; lisp/cae-ansi-color.el -*- lexical-binding: t; -*-
 
-(add-hook! text-mode
-  (unless (derived-mode-p 'org-mode)
-    ;; Apply ANSI color codes
-    (with-silent-modifications
-      (ansi-color-apply-on-region (point-min) (point-max) t))))
-                                        ;
+(add-hook 'text-mode-hook
+          (cae-defun cae-ansi-color-text-mode-h ()
+            (unless (derived-mode-p 'org-mode)
+              ;; Apply ANSI color codes
+              (with-silent-modifications
+                (ansi-color-apply-on-region (point-min) (point-max) t)))))
