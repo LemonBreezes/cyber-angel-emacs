8e70dcc69af946364fec59e056dc706bac338fc5
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 16:45:05 -0500

    lisp/cae-dashboard.el

--- lisp/cae-dashboard.el
+++ lisp/cae-dashboard.el
@@ -1,30 +1,24 @@
 ;;; lisp/cae-dashboard.el -*- lexical-binding: t; -*-
 
-(defun +doom-dashboard-setup-modified-keymap ()
-  (setq +doom-dashboard-mode-map (make-sparse-keymap))
-  (map! :map +doom-dashboard-mode-map
-        :desc "Find file" :ng "f" #'find-file
-        :desc "Recent files" :ng "r" #'consult-recent-file
-        :desc "Reload last session" :ng "R" #'doom/quickload-session
-        :desc "Config dir" :ng "C" #'doom/open-private-config
-        :desc "Open config.org" :ng "c" (cmd! (find-file (expand-file-name "config.org" doom-user-dir)))
-        :desc "Open org-mode root" :ng "O" (cmd! (find-file (expand-file-name "lisp/org/" doom-user-dir)))
-        :desc "Open dotfile" :ng "." (cmd! (doom-project-find-file "~/.config/"))
-        :desc "Notes (roam)" :ng "n" #'org-roam-node-find
-        :desc "Switch buffer" :ng "b" #'+vertico/switch-workspace-buffer
-        :desc "Switch buffers (all)" :ng "B" #'consult-buffer
-        :desc "IBuffer" :ng "i" #'ibuffer
-        :desc "Previous buffer" :ng "p" #'previous-buffer
-        :desc "Set theme" :ng "t" #'consult-theme
-        :desc "Quit" :ng "Q" #'save-buffers-kill-terminal
-        :desc "Show keybindings" :ng "h" (cmd!
-                                          (if (and (featurep 'evil) (evil-normal-state-p))
-                                              (cae-show-normal-state-bindings)
-                                            (which-key-show-keymap '+doom-dashboard-mode-map)))))
-
-(add-transient-hook! #'+doom-dashboard-mode (+doom-dashboard-setup-modified-keymap))
-(add-transient-hook! #'+doom-dashboard-mode :append (+doom-dashboard-setup-modified-keymap))
-(add-hook! 'doom-init-ui-hook :append (+doom-dashboard-setup-modified-keymap))
+(map! :map +doom-dashboard-mode-map
+      :desc "Find file" :ng "f" #'find-file
+      :desc "Recent files" :ng "r" #'consult-recent-file
+      :desc "Reload last session" :ng "R" #'doom/quickload-session
+      :desc "Config dir" :ng "C" #'doom/open-private-config
+      :desc "Open config.org" :ng "c" (cmd! (find-file (expand-file-name "config.org" doom-user-dir)))
+      :desc "Open org-mode root" :ng "O" (cmd! (find-file (expand-file-name "lisp/org/" doom-user-dir)))
+      :desc "Open dotfile" :ng "." (cmd! (doom-project-find-file "~/.config/"))
+      :desc "Notes (roam)" :ng "n" #'org-roam-node-find
+      :desc "Switch buffer" :ng "b" #'+vertico/switch-workspace-buffer
+      :desc "Switch buffers (all)" :ng "B" #'consult-buffer
+      :desc "IBuffer" :ng "i" #'ibuffer
+      :desc "Previous buffer" :ng "p" #'previous-buffer
+      :desc "Set theme" :ng "t" #'consult-theme
+      :desc "Quit" :ng "Q" #'save-buffers-kill-terminal
+      :desc "Show keybindings" :ng "h" (cmd!
+                                        (if (and (featurep 'evil) (evil-normal-state-p))
+                                            (cae-show-normal-state-bindings)
+                                          (which-key-show-keymap '+doom-dashboard-mode-map))))
 
 (map! :leader :desc "Dashboard" "d" #'+doom-dashboard/open)
 
