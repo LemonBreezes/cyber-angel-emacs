e22aff595ca9df7c110647f97baa0420f6cbecaa
StrawberryTea <look@strawberrytea.xyz>
2023-09-24 01:11:45 -0500

    lisp/cae-projectile.el

--- lisp/cae-projectile.el
+++ lisp/cae-projectile.el
@@ -81,7 +81,7 @@
           "." #'+default/search-project-for-symbol-at-point
           "x" #'doom/open-project-scratch-buffer
           "X" #'doom/switch-to-project-scratch-buffer)
-    (setq project-switch-commands #'find-file)
+    (setq project-switch-commands #'consult-fd)
     (advice-add #'project-switch-project :before
                 (cae-defun cae-project-switch-action-a (dir)
                   (+workspaces-switch-to-project-h dir)))))
