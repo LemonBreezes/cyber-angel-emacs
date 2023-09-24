a46d9a0f81e055d9ae10424d8981541bb127e264
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 20:32:25 -0500

    lisp/cae-dashboard.el

--- lisp/cae-dashboard.el
+++ lisp/cae-dashboard.el
@@ -1,5 +1,11 @@
 ;;; lisp/cae-dashboard.el -*- lexical-binding: t; -*-
 
+(defun cae-dashboard-help ()
+  (interactive)
+  (if (and (featurep 'evil) (evil-normal-state-p))
+      (cae-show-normal-state-bindings)
+    (which-key-show-keymap '+doom-dashboard-mode-map)))
+
 (map! :map +doom-dashboard-mode-map
       :desc "Find file" :ng "f" #'find-file
       :desc "Recent files" :ng "r" #'consult-recent-file
@@ -20,10 +26,7 @@
       :desc "Previous buffer" :ng "p" #'previous-buffer
       :desc "Set theme" :ng "t" #'consult-theme
       :desc "Quit" :ng "Q" #'save-buffers-kill-terminal
-      :desc "Show keybindings" :ng "h" (cmd!
-                                        (if (and (featurep 'evil) (evil-normal-state-p))
-                                            (cae-show-normal-state-bindings)
-                                          (which-key-show-keymap '+doom-dashboard-mode-map))))
+      :desc "Show keybindings" :ng "h" #'cae-dashboard-help)
 
 (map! :leader :desc "Dashboard" "d" #'+doom-dashboard/open)
 
