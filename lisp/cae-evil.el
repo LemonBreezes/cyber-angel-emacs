537e5bc630083ee9db2872aeb45abd20220c300b
StrawberryTea <look@strawberrytea.xyz>
2023-09-23 22:17:12 -0500

    lisp/cae-evil.el

--- lisp/cae-evil.el
+++ lisp/cae-evil.el
@@ -172,8 +172,6 @@
           "h" #'evil-i-syntax
           :map evil-outer-text-objects-map
           "h" #'evil-a-syntax)))
-:config
-(define-key evil-outer-text-objects-map "x" #'evil-textobj-syntax-wordhfa)
 
 ;;Local Variables:
 ;;eval: (unless (modulep! :editor evil) (remove-hook 'write-file-functions #'eval-buffer t))
