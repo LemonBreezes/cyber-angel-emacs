08775441ba32bb96f78262476323022d31d9ae65
StrawberryTea <look@strawberrytea.xyz>
2023-09-25 16:40:06 -0500

    modules/private/ai/autoload/ai.el

--- modules/private/ai/autoload/ai.el
+++ modules/private/ai/autoload/ai.el
@@ -1,11 +1,18 @@
 ;;; private/ai/autoload/ai.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-ai-org-ai-on-buffer ()
+(defun cae-org-ai-on-buffer ()
   (interactive)
   (save-mark-and-excursion
       (mark-whole-buffer)
       (call-interactively #'org-ai-on-region)))
+
+;;;###autoload
+(defun cae-org-ai-on-region-or-buffer ()
+  (interactive)
+  (if (region-active-p)
+      (call-interactively #'org-ai-on-region)
+    (call-interactively #'cae-org-ai-on-buffer)))
 
 ;;;###autoload
 (defun cae-ai-org-ai-kill-region-at-point ()
