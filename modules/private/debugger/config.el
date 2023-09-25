eb28e9f2e5ca1279fb8114a2293cf82d05057dcc
StrawberryTea <look@strawberrytea.xyz>
2023-09-25 01:31:11 -0500

    modules/private/debugger/config.el

--- modules/private/debugger/config.el
+++ modules/private/debugger/config.el
@@ -2,14 +2,10 @@
 
 (when (and (modulep! :tools lsp)
            (modulep! :tools debugger +lsp))
-  (after! lsp-mode
-    (map! :map lsp-mode-map
-          "<f6>" #'cae-debugger-dap-hydra/body))
   (after! dap-ui
     (remove-hook 'dap-ui-mode-hook #'dap-ui-controls-mode)
     (map! :map dap-ui-repl-mode-map
-          [remap comint-delchar-or-maybe-eof] #'cae-debugger-quit-or-delete-or-send-eof
-          "<f6>" #'cae-debugger-dap-hydra/body))
+          [remap comint-delchar-or-maybe-eof] #'cae-debugger-quit-or-delete-or-send-eof))
   (after! dap-mode
     (remove-hook 'dap-stopped-hook #'+dap-running-session-mode)
     (setq dap-debug-restart-keep-session nil
