cf31b27861ededd31db2d406eab40856f2e87708
StrawberryTea <look@strawberrytea.xyz>
2023-09-26 12:32:16 -0500

    modules/private/org/packages.el

--- modules/private/org/packages.el
+++ modules/private/org/packages.el
@@ -5,7 +5,6 @@
 (package! org-inline-pdf)
 (package! language-detection)
 (package! org-appear)
-(package! org-ol-tree :recipe (:host github :repo "Townk/org-ol-tree"))
 
 (when (modulep! +pretty)
   (package! org-tidy :recipe (:host github :repo "jxq0/org-tidy"))
