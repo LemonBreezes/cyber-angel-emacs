ce6b065875f8e951d4f15ea2aaca5b3dede1f335
StrawberryTea <look@strawberrytea.xyz>
2023-09-27 13:42:54 -0500

    config.el

--- config.el
+++ config.el
@@ -496,7 +496,8 @@
 
     ;; Since it doesn't though, I just set it to `t' and scroll up manually if
     ;; there are errors.
-    (setq compilation-scroll-output t))
+    (setq compilation-scroll-output t
+          compilation-environment '("LANG=C" "TERM=dumb")))
 
 
   (after! tramp
