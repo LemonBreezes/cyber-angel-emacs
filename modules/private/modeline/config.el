e198252d21b8ba8b180758c02a41b5e22c48823d
StrawberryTea <look@strawberrytea.xyz>
2023-09-29 12:38:55 -0500

    modules/private/modeline/config.el

--- modules/private/modeline/config.el
+++ modules/private/modeline/config.el
@@ -214,5 +214,4 @@
   (backtrace! mu4e-modeline-mode)
   (when (modulep! :email mu4e)
     (after! mu4e-modeline
-      (setq mu4e-modeline-show-global t)
-      (mu4e-modeline-mode +1))))
+      (setq mu4e-modeline-show-global t))))
