f691b78c5f6a43ae06ffe2a687e4046130bef2d7
StrawberryTea <look@strawberrytea.xyz>
2023-09-29 12:46:01 -0500

    config.el

--- config.el
+++ config.el
@@ -1264,6 +1264,10 @@
 
   (when (modulep! :email mu4e)
     (map! [remap compose-mail] #'+mu4e/compose))
+  (after! mu4e-vars
+    (setq mu4e-modeline-support t))
+  (after! mu4e-modeline
+    (setq mu4e-modeline-show-global t))
   (after! mu4e
     (setq mu4e-contexts
           `(,(make-mu4e-context
