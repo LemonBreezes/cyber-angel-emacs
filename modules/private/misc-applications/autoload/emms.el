cb03f2a2fb72978ce0c7e94cd3fecc1c906246e0
StrawberryTea <look@strawberrytea.xyz>
2023-09-30 00:36:08 -0500

    modules/private/misc-applications/autoload/emms.el

--- modules/private/misc-applications/autoload/emms.el
+++ modules/private/misc-applications/autoload/emms.el
@@ -84,8 +84,9 @@ rather than the whole path."
                (+ (length (string-replace "%s" "" emms-mode-line-format))
                   (min (length song)
                        emms-mode-line-cycle-max-width))))
-        (setq emms-mode-line-string
-              (replace-regexp-in-string "\\s-+" " " emms-mode-line-string))
+        (and emms-mode-line-string
+             (setq emms-mode-line-string
+                   (replace-regexp-in-string "\\s-+" " " emms-mode-line-string)))
       (let* ((suffix (cadr (split-string emms-mode-line-format "%s")))
              (width (cae-variable-pitch-width emms-mode-line-string))
              (l (length emms-mode-line-string))
