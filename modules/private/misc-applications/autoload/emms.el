a20498e6879cded1baf5f8c4ea455d7e95698c7b
StrawberryTea <look@strawberrytea.xyz>
2023-09-29 23:52:54 -0500

    modules/private/misc-applications/autoload/emms.el

--- modules/private/misc-applications/autoload/emms.el
+++ modules/private/misc-applications/autoload/emms.el
@@ -37,7 +37,7 @@
 ;; The following two functions are from
 ;; https://www.reddit.com/r/emacs/comments/qg2d0k/emms_modeline_shows_full_path_to_the_songs_i_only/
 
-(defun cae-track-title-from-file-name (file)
+(defun +track-title-from-file-name (file)
   "For using with EMMS description functions. Extracts the track
 title from the file name FILE, which just means a) taking only
 the file component at the end of the path, and b) removing any
@@ -50,7 +50,7 @@ file extension."
     (buffer-string)))
 
 ;;;###autoload
-(defun cae-emms-track-description (track)
+(defun +emms-track-description (track)
   "Return a description of TRACK, for EMMS, but try to cut just
 the track name from the file name, and just use the file name too
 rather than the whole path."
@@ -68,3 +68,30 @@ rather than the whole path."
           ((eq (emms-track-type track) 'file)
            (cae-track-title-from-file-name (emms-track-name track)))
           (t (emms-track-simple-description track)))))
+
+
+(defvar emms-mode-line-string-pixel-length-max-alist nil)
+(defvar emms-mode-line-song-pixel-length-max-hash-table (make-hash-table :test 'equal))
+
+;;;###autoload
+(defun +emms-mode-line-cycle-valign (&rest _)
+  (let* ((song (or emms-mode-line-cycle--title
+                  (funcall emms-mode-line-cycle-current-title-function))))
+    (unless (or (not emms-mode-line-string)
+                (> (length emms-mode-line-string)
+                   (+ (length (string-replace "%s" "" emms-mode-line-format))
+                      (min (length song)
+                           emms-mode-line-cycle-max-width))))
+      (let* ((suffix (cadr (split-string emms-mode-line-format "%s")))
+             (width (cae-variable-pitch-width emms-mode-line-string))
+             (l (length emms-mode-line-string))
+             (padding (max (- (max (setf (alist-get l emms-mode-line-string-pixel-length-max-alist)
+                                         (max (alist-get l emms-mode-line-string-pixel-length-max-alist 0)
+                                              width))
+                                   (or (gethash )))
+                              width 0))))
+        (setq emms-mode-line-string
+              (concat (string-remove-suffix suffix emms-mode-line-string)
+                      (propertize " "
+                                  'display `(space :width (,padding)))
+                      suffix))))))
