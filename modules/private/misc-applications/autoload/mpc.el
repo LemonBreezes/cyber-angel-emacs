eb2357818a9f6b3418e08558d1a8bd74dd5c3bfa
StrawberryTea <look@strawberrytea.xyz>
2023-09-30 15:45:39 -0500

    modules/private/misc-applications/autoload/mpc.el

--- modules/private/misc-applications/autoload/mpc.el
+++ modules/private/misc-applications/autoload/mpc.el
@@ -19,8 +19,7 @@
     (call-interactively #'mpc))
   (let ((inhibit-redisplay t))
     (when (and +mpc--prev-buf (get-buffer-window +mpc--prev-buf))
-      (select-window (get-buffer-window +mpc--prev-buf)))
-    (+mpc-jump-to-previous-position))
+      (select-window (get-buffer-window +mpc--prev-buf))))
   (setq +mpc--wconf (current-window-configuration)))
 
 ;;;###autoload
