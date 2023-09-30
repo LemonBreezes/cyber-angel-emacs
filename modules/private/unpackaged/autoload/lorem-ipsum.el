9855bea060ffe9926e9ee4d2c3e62460ed104d6a
StrawberryTea <look@strawberrytea.xyz>
2023-09-30 17:51:30 -0500

    modules/private/unpackaged/autoload/lorem-ipsum.el

---
+++ modules/private/unpackaged/autoload/lorem-ipsum.el
@@ -0,0 +1,118 @@
+;;; private/unpackaged/autoload/lorem-ipsum.el -*- lexical-binding: t; -*-
+
+(defcustom unpackaged/lorem-ipsum-overlay-exclude nil
+  "List of regexps to exclude from `unpackaged/lorem-ipsum-overlay'."
+  :type '(repeat regexp))
+
+;;;###autoload
+(cl-defun unpackaged/lorem-ipsum-overlay (&key replace-p use-map-p)
+  "Overlay all text in current buffer with \"lorem ipsum\" text.
+When called again, remove overlays.  Useful for taking
+screenshots without revealing buffer contents.
+
+If REPLACE-P is non-nil (interactively, with prefix and prompt),
+replace buffer contents rather than overlaying them.  When a
+buffer is very large and would have so many overlays that
+performance would be prohibitively slow, you may replace the
+buffer contents instead.  (Of course, be careful about saving the
+buffer after replacing its contents.)
+
+If USE-MAP-P is non-nil (interactively, with prefix and prompt),
+all instances of a real word are replaced with the same word;
+otherwise, each instance of a real word is replaced with a random
+word (further obscuring the text).
+
+Each piece of non-whitespace text in the buffer is compared with
+regexps in `unpackaged/lorem-ipsum-overlay-exclude', and ones
+that match are not overlaid.  Note that the regexps are compared
+against the entire non-whitespace token, up-to and including the
+preceding whitespace, but only the alphabetic part of the token
+is overlaid.  For example, in an Org buffer, a line that starts
+with:
+
+  #+TITLE: unpackaged.el
+
+could be matched against the exclude regexp (in `rx' syntax):
+
+  (rx (or bol bos blank) \"#+\" (1+ alnum) \":\" (or eol eos blank))
+
+And the line would be overlaid like:
+
+  #+TITLE: parturient.et"
+  (interactive (when current-prefix-arg
+                 (list :replace-p (yes-or-no-p "Replace contents (or just overlay)? ")
+                       :use-map-p (yes-or-no-p "Map words (or be completely random)? "))))
+  (require 'lorem-ipsum)
+  (let ((ovs (overlays-in (point-min) (point-max))))
+    (if (cl-loop for ov in ovs
+                 thereis (overlay-get ov :lorem-ipsum-overlay))
+        ;; Remove overlays.
+        (dolist (ov ovs)
+          (when (overlay-get ov :lorem-ipsum-overlay)
+            (delete-overlay ov)))
+      ;; Add overlays.
+      (let ((lorem-ipsum-words (--> lorem-ipsum-text
+                                    (-flatten it) (apply #'concat it)
+                                    (split-string it (rx (or space punct)) 'omit-nulls)))
+            (case-fold-search nil)
+            (map (make-hash-table :test #'equal)))
+        (cl-labels ((overlay-group (group)
+                                   (let* ((beg (match-beginning group))
+                                          (end (match-end group))
+                                          (replacement-word (if use-map-p
+                                                                (lorem-word* (match-string-no-properties group))
+                                                              (lorem-word (match-string-no-properties group))))
+                                          (ov (make-overlay beg end)))
+                                     (when replacement-word
+                                       (overlay-put ov :lorem-ipsum-overlay t)
+                                       (overlay-put ov 'display replacement-word))))
+                    (replace-group (group)
+                                   (let* ((beg (match-beginning group))
+                                          (end (match-end group))
+                                          (replacement-word (if use-map-p
+                                                                (lorem-word* (match-string-no-properties group))
+                                                              (lorem-word (match-string-no-properties group)))))
+                                     (when replacement-word
+                                       (setf (buffer-substring beg end) replacement-word))))
+                    (lorem-word (word)
+                                (if-let* ((matches (lorem-matches (length word))))
+                                    (apply-case word (downcase (seq-random-elt matches)))
+                                  ;; Word too long: compose one.
+                                  (apply-case word (downcase (compose-word (length word))))))
+                    (lorem-word* (word)
+                                 (or (gethash word map)
+                                     (puthash word
+                                              (if-let ((matches (lorem-matches (length word))))
+                                                  (apply-case word (downcase (seq-random-elt matches)))
+                                                ;; Word too long: compose one.
+                                                (apply-case word (downcase (compose-word (length word)))))
+                                              map)))
+                    (lorem-matches (length &optional (comparator #'=))
+                                   (cl-loop for liw in lorem-ipsum-words
+                                            when (funcall comparator (length liw) length)
+                                            collect liw))
+                    (apply-case (source target)
+                                (cl-loop for sc across-ref source
+                                         for tc across-ref target
+                                         when (not (string-match-p (rx lower) (char-to-string sc)))
+                                         do (setf tc (string-to-char (upcase (char-to-string tc)))))
+                                target)
+                    (compose-word (length)
+                                  (cl-loop while (> length 0)
+                                           for word = (seq-random-elt (lorem-matches length #'<=))
+                                           concat word
+                                           do (cl-decf length (length word)))))
+          (save-excursion
+            (goto-char (point-min))
+            (while (re-search-forward (rx (group (1+ (or bol bos blank (not alpha)))
+                                                 (0+ (not (any alpha blank)))
+                                                 (group (1+ alpha))
+                                                 (0+ (not (any alpha blank)))))
+                                      nil t)
+              (unless (cl-member (match-string-no-properties 0) unpackaged/lorem-ipsum-overlay-exclude
+                                 :test (lambda (string regexp)
+                                         (string-match-p regexp string)))
+                (if replace-p
+                    (replace-group 2)
+                  (overlay-group 2)))
+              (goto-char (match-end 2)))))))))
