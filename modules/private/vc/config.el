46b20d8946323db3bbe7a723c53f5ffe06b34202
StrawberryTea <look@strawberrytea.xyz>
2023-10-01 14:23:50 -0500

    modules/private/vc/config.el

--- modules/private/vc/config.el
+++ modules/private/vc/config.el
@@ -163,4 +163,17 @@
 (use-package! consult-gh-embark
   :after (consult-gh embark))
 
+(use-package! helm-ls-git
+  :when (or (modulep! :completion helm)
+            (modulep! :private helm))
+  :init
+  ()
+  :config
+  (setq helm-ls-git-auto-checkout t
+        helm-ls-git-fuzzy-match t
+        helm-ls-git-status-command (if (modulep! :tools magit)
+                                       'magit-status-setup-buffer
+                                     'vc-dir)
+        helm-ls-git-log-max-commits "500"))
+
 (use-package! igist :defer t)           ; TODO
