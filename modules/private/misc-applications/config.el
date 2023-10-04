91357430c1d7b41067c3790adecb3f874b6b9e13
StrawberryTea <look@strawberrytea.xyz>
2023-10-04 00:09:29 -0500

    modules/private/misc-applications/config.el

--- modules/private/misc-applications/config.el
+++ modules/private/misc-applications/config.el
@@ -714,7 +714,6 @@
         empv-base-directory +misc-applications-music-dir
         empv-audio-dir +misc-applications-music-dir
         empv-video-dir +misc-applications-videos-dir
-        empv-allow-insecure-connections t
         empv-playlist-dir +misc-applications-music-dir)
   (add-hook 'empv-init-hook #'empv-override-quit-key)
   (aio-defun cae-empv-set-invidious-instance ()
