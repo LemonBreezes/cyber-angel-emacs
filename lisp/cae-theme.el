896827bc5087c6a518e597ade60be1ff152ae8f1
StrawberryTea <look@strawberrytea.xyz>
2023-10-07 11:08:34 -0500

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -30,7 +30,7 @@
   (after! org-modern
     ;; These features interfere with the heading backgrounds.
     (setq org-modern-tag nil
-          org-modern-todo-faces nil))
+          org-modern-todo nil))
 
   (use-package! backline
     :when cae-theme-extend-heading-faces
