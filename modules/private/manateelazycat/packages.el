64fb8d33838e44608f8152402f5f6544df0e6e53
StrawberryTea <look@strawberrytea.xyz>
2023-10-07 23:09:22 -0500

    modules/private/manateelazycat/packages.el

--- modules/private/manateelazycat/packages.el
+++ modules/private/manateelazycat/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/manateelazycat/packages.el
 
-(package! holo-layer :recipe (:host github :repo "manateelazycat/holo-layer" :files ("*")))
+(package! holo-layer :recipe (:host github :repo "manateelazycat/holo-layer" :files ("*.el" "*.py" "plugin")))
