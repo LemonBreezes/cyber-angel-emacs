de445500476be471d0ab8f3d6d87dbc64b10e389
StrawberryTea <look@strawberrytea.xyz>
2023-10-07 23:07:08 -0500

    modules/private/manateelazycat/config.el

--- modules/private/manateelazycat/config.el
+++ modules/private/manateelazycat/config.el
@@ -1,1 +1,5 @@
 ;;; private/manateelazycat/config.el -*- lexical-binding: t; -*-
+
+(use-package! holo-layer
+  :defer t :config
+  (setq holo-layer-build-dir (concat doom-local-dir "straight/repos/holo-layer/")))
