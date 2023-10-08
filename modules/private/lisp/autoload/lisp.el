879bf07779a9d339ac01f5e49d881d7cffb86edf
StrawberryTea <look@strawberrytea.xyz>
2023-10-08 18:24:27 -0500

    modules/private/lisp/autoload/lisp.el

--- modules/private/lisp/autoload/lisp.el
+++ modules/private/lisp/autoload/lisp.el
@@ -51,8 +51,3 @@
               :keymap 'lispy-mode-map
               :transient t
               :flatten t)
-
-;;;###autoload
-(defun cae-lispy-embark-cheatsheet ()
-  (interactive)
-  (embark-bindings-in-keymap lispy-mode-map))
