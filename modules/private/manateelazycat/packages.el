c2a76096606e291aa8299ba2c575a3dc494d64c2
StrawberryTea <look@strawberrytea.xyz>
2023-10-08 00:26:08 -0500

    modules/private/manateelazycat/packages.el

--- modules/private/manateelazycat/packages.el
+++ modules/private/manateelazycat/packages.el
@@ -3,5 +3,3 @@
 
 ;; Did not work for me at all. Black screen whenever I use `holo-layer-enable'.
 ;;(package! holo-layer :recipe (:host github :repo "manateelazycat/holo-layer" :files ("*.el" "*.py" "plugin" "icon_cache" "swaymsg-treefetch" "resources")))
-
-(package! awesome-tray)
