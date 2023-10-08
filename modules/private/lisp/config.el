614f9c4a1e01d3e38d3f32b0b4a3c5b4fabcbb0b
StrawberryTea <look@strawberrytea.xyz>
2023-10-08 16:20:40 -0500

    modules/private/lisp/config.el

--- modules/private/lisp/config.el
+++ modules/private/lisp/config.el
@@ -100,7 +100,6 @@
 
 (when (modulep! :editor lispy)
   (after! lispy
-    (define-key lispy-mode-map (kbd "<f6>") #'cae-lispy-cheat-sheet/body)
     (add-hook 'doom-escape-hook
               (cae-defun cae-lispy-clear-iedit-h ()
                 (when (bound-and-true-p iedit-mode)
