1992c9921d24bd5950bf962000d06a357ac22280
StrawberryTea <look@strawberrytea.xyz>
2023-10-08 00:11:48 -0500

    modules/private/manateelazycat/packages.el

--- modules/private/manateelazycat/packages.el
+++ modules/private/manateelazycat/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/manateelazycat/packages.el
 
-;; Did not work for me at all. Black screen
+;; Did not work for me at all. Black screen whenever I use `holo-layer-enable'
 ;;(package! holo-layer :recipe (:host github :repo "manateelazycat/holo-layer" :files ("*.el" "*.py" "plugin" "icon_cache" "swaymsg-treefetch" "resources")))
