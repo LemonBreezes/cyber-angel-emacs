46141b4fdeb298fb309c5b0bd5627bd62ba6fd32
StrawberryTea <look@strawberrytea.xyz>
2023-10-08 18:46:02 -0500

    modules/private/lisp/autoload/lisp.el

--- modules/private/lisp/autoload/lisp.el
+++ modules/private/lisp/autoload/lisp.el
@@ -45,14 +45,14 @@
           ("Variables" "^\\s-*(\\(def\\(?:c\\(?:onst\\(?:ant\\)?\\|ustom\\)\\|ine-symbol-macro\\|parameter\\|var\\(?:-local\\)?\\)\\)\\s-+\\(\\(?:\\sw\\|\\s_\\|\\\\.\\)+\\)" 2)
           ("Types" "^\\s-*(\\(cl-def\\(?:struct\\|type\\)\\|def\\(?:class\\|face\\|group\\|ine-\\(?:condition\\|error\\|widget\\)\\|package\\|struct\\|t\\(?:\\(?:hem\\|yp\\)e\\)\\)\\)\\s-+'?\\(\\(?:\\sw\\|\\s_\\|\\\\.\\)+\\)" 2))))
 
-;;;###autoload
-(defun cae-lispy-which-key-cheatsheet () (interactive))
-(hercules-def :toggle-funs #'cae-lispy-cheatsheet
-              :keymap 'lispy-mode-map
-              :transient t
-              :flatten t)
-
-;;;###autoload
-(defun cae-lispy-embark-cheatsheet ()
-  (interactive)
-  (embark-bindings-in-keymap lispy-mode-map))
+;;;;;###autoload
+;;(defun cae-lispy-which-key-cheatsheet () (interactive))
+;;(hercules-def :toggle-funs #'cae-lispy-cheatsheet
+;;              :keymap 'lispy-mode-map
+;;              :transient t
+;;              :flatten t)
+;;
+;;;;;###autoload
+;;(defun cae-lispy-embark-cheatsheet ()
+;;  (interactive)
+;;  (embark-bindings-in-keymap lispy-mode-map))
