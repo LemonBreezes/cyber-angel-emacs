8db5ea79fdc098fe5dd0db4bc3c104065678bdfe
StrawberryTea <look@strawberrytea.xyz>
2023-10-09 17:02:12 -0500

    lisp/cae-visible-scrolling.el

---
+++ lisp/cae-visible-scrolling.el
@@ -0,0 +1,21 @@
+;;; lisp/cae-visible-scrolling.el -*- lexical-binding: t; -*-
+
+(advice-add #'evil-scroll-up :before
+            (cae-defun cae-evil-scroll-up-with-hint-a (count)
+              (unless (= (line-beginning-position) (point-min))
+                (let ((count (evil--get-scroll-count count)))
+                  (save-excursion
+                    (move-to-window-line (if (< count 0)
+                                             -1
+                                           0))
+                    (funcall scrollkeeper-guideline-fn))))))
+
+(advice-add #'evil-scroll-down :before
+            (cae-defun cae-evil-scroll-up-with-hint-a (count)
+              (unless (= (line-beginning-position) (point-min))
+                (let ((count (evil--get-scroll-count count)))
+                  (save-excursion
+                    (move-to-window-line (if (< count 0)
+                                             0
+                                           -1))
+                    (funcall scrollkeeper-guideline-fn))))))
