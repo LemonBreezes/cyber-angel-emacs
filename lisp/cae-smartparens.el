74686789f3d2551a141d0722222586390696aaf7
StrawberryTea <look@strawberrytea.xyz>
2023-10-08 23:33:27 -0500

    lisp/cae-smartparens.el

--- lisp/cae-smartparens.el
+++ lisp/cae-smartparens.el
@@ -106,7 +106,8 @@
       (define-key smartparens-mode-map (kbd (car binding)) (cdr binding))
       (when (modulep! :editor lispy)
         (after! lispy
-          (unless (lookup-key lispy-mode-map (kbd (car binding)))
-            (define-key lispy-mode-map (kbd (car binding)) (cdr binding)))))))
+          (if (lookup-key lispy-mode-map (kbd (car binding)))
+              (+log (lookup-key lispy-mode-map (kbd (car binding))))
+              (define-key lispy-mode-map (kbd (car binding)) (cdr binding)))))))
 
   (map! :map smartparens-mode-map "C-M-?" #'cae-sp-which-key-cheatsheet))
