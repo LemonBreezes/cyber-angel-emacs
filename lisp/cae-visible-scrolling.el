c231bcdec02b81b5de2b75cbe01c5c1a1e8ff7a2
StrawberryTea <look@strawberrytea.xyz>
2023-10-09 17:06:03 -0500

    lisp/cae-visible-scrolling.el

--- lisp/cae-visible-scrolling.el
+++ lisp/cae-visible-scrolling.el
@@ -1,14 +1,14 @@
 ;;; lisp/cae-visible-scrolling.el -*- lexical-binding: t; -*-
 
 (advice-add #'evil-scroll-down :before
-            (cae-defun cae-evil-scroll-up-with-hint-a (count)
+            (cae-defun cae-evil-scroll-down-with-hint-a (count)
               (unless (= (line-end-position) (point-max))
                 (require 'scrollkeeper)
                 (let ((count (evil--get-scroll-count count)))
                   (save-excursion
                     (move-to-window-line (if (< count 0)
-                                             -1
-                                           0))
+                                             0
+                                           -1))
                     (funcall scrollkeeper-guideline-fn))))))
 
 (advice-add #'evil-scroll-up :before
@@ -18,6 +18,6 @@
                 (let ((count (evil--get-scroll-count count)))
                   (save-excursion
                     (move-to-window-line (if (< count 0)
-                                             0
-                                           -1))
+                                             -1
+                                           0))
                     (funcall scrollkeeper-guideline-fn))))))
