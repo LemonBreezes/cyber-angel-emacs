50f89199f0dbb5b937fa15a5f15cdd48ec2a193c
StrawberryTea <look@strawberrytea.xyz>
2023-10-10 11:39:54 -0500

    init.el

--- init.el
+++ init.el
@@ -35,6 +35,8 @@
 (setq evil-undo-system 'undo-fu
       evil-want-C-u-scroll t
       evil-want-C-u-delete t
+      ;; I use `C-w' to yank words into the minibuffer. So to maintain a single
+      ;; keybinding for deleting words, I use `C-<backspace>'.
       evil-want-C-w-delete nil)
 
 (doom! :completion
