18c53f9d6a09618e828d33d32385c741cdc772fd
StrawberryTea <look@strawberrytea.xyz>
2023-10-10 17:36:17 -0500

    modules/cae/misc-applications/config.el

--- modules/cae/misc-applications/config.el
+++ modules/cae/misc-applications/config.el
@@ -704,7 +704,6 @@
   (require 'elfeed-tube)
   (add-to-list 'empv-mpv-args "--ytdl-format=best")
   (add-to-list 'empv-mpv-args "--save-position-on-quit")
-  (empv-observe 'metadata (lambda (data) (message "Metadata changed, new metadata is: %s" data)))
   (setq empv-reset-playback-speed-on-quit t
         empv-base-directory +misc-applications-music-dir
         empv-audio-dir +misc-applications-music-dir
