5a252a4c5a14d140b3521a864b8df78c23227fa4
StrawberryTea <look@strawberrytea.xyz>
2023-10-10 12:14:11 -0500

    config.el

--- config.el
+++ config.el
@@ -390,8 +390,8 @@
   ;; Was reading this
   ;; https://github.com/link0ff/emacs-init/blob/1fc141e20092cc357f2c6021626635e8ac067b8c/emacs.custom.el.
   (setq delete-by-moving-to-trash t
-        ;; These are slow but not being able to recover files can be really
-        ;; painful too.
+        ;; These are slow but not being able to recover deleted files can be
+        ;; really painful too.
         remote-file-name-inhibit-delete-by-moving-to-trash nil
         remote-file-name-inhibit-auto-save nil
         remote-file-name-inhibit-auto-save-visited nil
