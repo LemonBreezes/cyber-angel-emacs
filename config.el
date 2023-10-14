583ff827ee0f288dc7e8162e3845ad3cc39e3c3e
StrawberryTea <look@strawberrytea.xyz>
2023-10-14 14:08:02 -0500

    config.el

--- config.el
+++ config.el
@@ -402,7 +402,7 @@
         remote-file-name-inhibit-auto-save t
         remote-file-name-inhibit-auto-save-visited t
 
-        large-file-warning-threshold 100000000 ; Multiply Doom's number by 10.
+        large-file-warning-threshold 100000000 ; ~100 MB
         yank-pop-change-selection t
         global-mark-ring-max 1024
         history-delete-duplicates t
