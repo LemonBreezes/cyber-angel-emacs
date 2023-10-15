564821b9513961f41b0931e995e2e7869be8282e
StrawberryTea <look@strawberrytea.xyz>
2023-10-15 16:27:41 -0500

    autoload/cae-corfu.el

--- autoload/cae-corfu.el
+++ autoload/cae-corfu.el
@@ -53,10 +53,3 @@
   (interactive)
   (cape-interactive #'lsp-completion-at-point))
 
-;;;###autoload
-(defun cae-yasnippet-capf ()
-  (if (or (not (char-before))
-          (eq (char-syntax (char-before))
-              ?\s))
-      (call-interactively #'+default/insert-snippet)
-    (call-interactively #'yasnippet-capf)))
