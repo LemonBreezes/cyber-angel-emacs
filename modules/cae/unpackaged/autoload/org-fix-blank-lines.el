03e3067630db4b10d5de3ff53cb29214da6d2c46
StrawberryTea <look@strawberrytea.xyz>
2023-10-16 17:21:29 -0500

    modules/cae/unpackaged/autoload/org-fix-blank-lines.el

--- modules/cae/unpackaged/autoload/org-fix-blank-lines.el
+++ modules/cae/unpackaged/autoload/org-fix-blank-lines.el
@@ -49,7 +49,7 @@ exist after each headings's drawers."
 (defun cae-unpackaged-org-fix-all-blank-lines ()
   (unless (and (buffer-file-name)
                (file-in-directory-p (buffer-file-name) doom-emacs-dir)
-               (string-match-p "doom-emacs" (project-name (project-current))))
+               (string-match-p "doom.*emacs" (project-name (project-current))))
     (ignore-errors (cae-unpackaged-org-fix-blank-lines '(4)))))
 
 ;;;###autoload
