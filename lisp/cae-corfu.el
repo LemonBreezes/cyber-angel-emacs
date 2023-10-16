2ecc122fdf6127cd14efe49e1a2ca90c3e1401a3
StrawberryTea <look@strawberrytea.xyz>
2023-10-15 21:24:39 -0500

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -28,14 +28,14 @@
 
 (after! orderless
   (when (modulep! :completion corfu +split-char)
-      ;; So Orderless splits the string into components and then determines the
-      ;; matching style for each component. This is all regexp stuff.
-      (progn (setq orderless-component-separator
-                   (cae-orderless-escapable-split-fn ?,))
-             (after! corfu
-               (setq corfu-separator ?,)
-               (map! :map corfu-map
-                     "," #'corfu-insert-separator)))))
+    ;; So Orderless splits the string into components and then determines the
+    ;; matching style for each component. This is all regexp stuff.
+    (progn (setq orderless-component-separator
+                 (cae-orderless-escapable-split-fn ?,))
+           (after! corfu
+             (setq corfu-separator ?,)
+             (map! :map corfu-map
+                   "," #'corfu-insert-separator)))))
 
 (add-hook 'minibuffer-setup-hook #'cae-corfu-enable-in-minibuffer-h)
 (add-hook 'minibuffer-exit-hook #'corfu-quit)
