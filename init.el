3fdfd60552a97fcf2f07caa27af4edd125752536
StrawberryTea <look@strawberrytea.xyz>
2023-10-18 14:59:35 -0500

    init.el

--- init.el
+++ init.el
@@ -35,107 +35,107 @@
 
 (setq evil-undo-system 'undo-fu)
 
-(doom! :completion
-       (vertico +icons)
-       (corfu +tng +orderless)
+;; (doom! :completion
+;;        (vertico +icons)
+;;        (corfu +tng +orderless)
 
-       :ui
-       doom-dashboard
-       hl-todo
-       (hydra +childframe)
-       (ligatures +extra +iosevka)
-       ;;nav-flash
-       (popup +defaults)
-       (vc-gutter +pretty +diff-hl)
-       vi-tilde-fringe
-       workspaces
-       (window-select +numbers)
-       ophints
-       treemacs
-       zen
+;;        :ui
+;;        doom-dashboard
+;;        hl-todo
+;;        (hydra +childframe)
+;;        (ligatures +extra +iosevka)
+;;        ;;nav-flash
+;;        (popup +defaults)
+;;        (vc-gutter +pretty +diff-hl)
+;;        vi-tilde-fringe
+;;        workspaces
+;;        (window-select +numbers)
+;;        ophints
+;;        treemacs
+;;        zen
 
-       :editor
-       file-templates
-       format
-       snippets
-       multiple-cursors
-       fold
-       (evil +everywhere)
-       lispy
+;;        :editor
+;;        file-templates
+;;        format
+;;        snippets
+;;        multiple-cursors
+;;        fold
+;;        (evil +everywhere)
+;;        lispy
 
-       :emacs
-       undo
-       ibuffer
-       vc
-       (dired +icons)
+;;        :emacs
+;;        undo
+;;        ibuffer
+;;        vc
+;;        (dired +icons)
 
-       :term
-       eshell
-       (:if (not (memq system-type '(cygwin windows-nt ms-dos))) vterm)
+;;        :term
+;;        eshell
+;;        (:if (not (memq system-type '(cygwin windows-nt ms-dos))) vterm)
 
-       :checkers
-       (syntax +childframe)
-       ;;spell
-       ;;grammar
+;;        :checkers
+;;        (syntax +childframe)
+;;        ;;spell
+;;        ;;grammar
 
-       :tools
-       (debugger +lsp)
-       direnv
-       editorconfig
-       (eval +overlay)
-       (lookup +dictionary +docsets +offline)
-       (magit +forge)
-       (lsp +peek)
-       tree-sitter
-       docker
-       pdf
+;;        :tools
+;;        (debugger +lsp)
+;;        direnv
+;;        editorconfig
+;;        (eval +overlay)
+;;        (lookup +dictionary +docsets +offline)
+;;        (magit +forge)
+;;        (lsp +peek)
+;;        tree-sitter
+;;        docker
+;;        pdf
 
-       :lang
-       (emacs-lisp +tree-sitter)
-       (org +roam2 +tree-sitter +dragndrop)
-       (cc +lsp +tree-sitter)
-       (web +lsp +tree-sitter)
-       (sh +lsp +tree-sitter)
-       (nix +lsp +tree-sitter)
-       (solidity +lsp +tree-sitter)
-       (lua +lsp +tree-sitter +fennel)
-       (data +lsp +tree-sitter)
-       (latex +lsp +tree-sitter +cdlatex +latexmk +fold)
-       (yaml +lsp +tree-sitter)
-       (json +lsp +tree-sitter)
-       (javascript +lsp +tree-sitter)
-       (python +lsp +tree-sitter)
+;;        :lang
+;;        (emacs-lisp +tree-sitter)
+;;        (org +roam2 +tree-sitter +dragndrop)
+;;        (cc +lsp +tree-sitter)
+;;        (web +lsp +tree-sitter)
+;;        (sh +lsp +tree-sitter)
+;;        (nix +lsp +tree-sitter)
+;;        (solidity +lsp +tree-sitter)
+;;        (lua +lsp +tree-sitter +fennel)
+;;        (data +lsp +tree-sitter)
+;;        (latex +lsp +tree-sitter +cdlatex +latexmk +fold)
+;;        (yaml +lsp +tree-sitter)
+;;        (json +lsp +tree-sitter)
+;;        (javascript +lsp +tree-sitter)
+;;        (python +lsp +tree-sitter)
 
-       :os
-       (:if IS-MAC macos)
-       (:if (not (memq system-type '(cygwin windows-nt ms-dos))) tty +osc)
+;;        :os
+;;        (:if IS-MAC macos)
+;;        (:if (not (memq system-type '(cygwin windows-nt ms-dos))) tty +osc)
 
-       :config
-       (default +bindings +smartparens)
+;;        :config
+;;        (default +bindings +smartparens)
 
-       :email
-       (mu4e +org)
-       ;;(notmuch +org)
+;;        :email
+;;        (mu4e +org)
+;;        ;;(notmuch +org)
 
-       :app
-       (rss +org)
+;;        :app
+;;        (rss +org)
 
-       :cae
-       ;;holy
-       helm
-       debugger
-       ;;(dirvish +icons +dirvish)
-       eshell
-       exwm
-       dired
-       lisp
-       misc-applications
-       modeline
-       unpackaged
-       vc
-       org
-       ai
-       gnus)
+;;        :cae
+;;        ;;holy
+;;        helm
+;;        debugger
+;;        ;;(dirvish +icons +dirvish)
+;;        eshell
+;;        exwm
+;;        dired
+;;        lisp
+;;        misc-applications
+;;        modeline
+;;        unpackaged
+;;        vc
+;;        org
+;;        ai
+;;        gnus)
 
 ;;Local Variables:
 ;;eval: (when (featurep 'lispy) (lispy-mode -1))
