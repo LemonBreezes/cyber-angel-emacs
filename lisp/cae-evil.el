84bf02e37238904c0aae73fa4b870fd3445befed
StrawberryTea <look@strawberrytea.xyz>
2023-10-18 19:58:26 -0500

    lisp/cae-evil.el

--- lisp/cae-evil.el
+++ lisp/cae-evil.el
@@ -164,8 +164,10 @@
   (map! (:prefix "C-x"
          :i "C-c" #'copilot-complete
          :i "C-f" #'cape-file
-         :i "C-s" (cmd! () (let ((consult-yasnippet-use-thing-at-point t))
-                             (call-interactively #'+default/insert-snippet)))
+         :i "C-s" (cmd! ()
+                        (require 'consult-yasnippet)
+                        (let ((consult-yasnippet-use-thing-at-point t))
+                          (call-interactively #'+default/insert-snippet)))
          (:after cape
           :i "C-l" (cape-interactive-capf (cape-capf-super #'cape-history #'cape-line)))
          :i "C-]" #'cape-lsp
