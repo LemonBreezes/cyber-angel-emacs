2bcd7c184e385691e2c1f7e8b06b2f1b538de7d7
StrawberryTea <look@strawberrytea.xyz>
2023-10-19 14:06:24 -0500

    lisp/cae-visible-scrolling.el

--- lisp/cae-visible-scrolling.el
+++ lisp/cae-visible-scrolling.el
@@ -8,26 +8,36 @@
 
   ;; These advices don't support scrolling in multiple steps but that is okay
   ;; with me.
-  (defun cae-evil-scroll-with-hint-a (point-fn edge-fn line-fn)
-    (unless (funcall edge-fn (funcall point-fn) (funcall line-fn))
+  (defadvice! cae-evil-scroll-down-with-hint-a (count &rest _)
+    :before #'evil-scroll-down
+    (unless (= (line-end-position) (point-max))
       (require 'scrollkeeper)
       (let ((count (evil--get-scroll-count count)))
         (save-excursion
           (move-to-window-line (if (< count 0) 0 -1))
           (funcall scrollkeeper-guideline-fn)))))
 
-  (defadvice! cae-evil-scroll-down-with-hint-a (count &rest _)
-    :before #'evil-scroll-down
-    (cae-evil-scroll-with-hint-a #'line-end-position #'= #'point-max))
-
   (defadvice! cae-evil-scroll-up-with-hint-a (count &rest _)
     :before #'evil-scroll-up
-    (cae-evil-scroll-with-hint-a #'line-beginning-position #'= #'point-min))
+    (unless (= (line-beginning-position) (point-min))
+      (require 'scrollkeeper)
+      (let ((count (evil--get-scroll-count count)))
+        (save-excursion
+          (move-to-window-line (if (< count 0) -1 0))
+          (funcall scrollkeeper-guideline-fn)))))
 
   (defadvice! evil-scroll-page-up-with-hint-a (count &rest _)
     :before #'evil-scroll-page-up
-    (cae-evil-scroll-with-hint-a #'abs #'>= 1))
+    (unless (> (abs count) 1)
+      (require 'scrollkeeper)
+      (save-excursion
+        (move-to-window-line (if (< count 0) -1 0))
+        (funcall scrollkeeper-guideline-fn))))
 
   (defadvice! evil-scroll-page-down-with-hint-a (count &rest _)
     :before #'evil-scroll-page-down
-    (cae-evil-scroll-with-hint-a #'abs #'>= 1)))
+    (unless (> (abs count) 1)
+      (require 'scrollkeeper)
+      (save-excursion
+        (move-to-window-line (if (< count 0) 0 -1))
+        (funcall scrollkeeper-guideline-fn)))))
