fadfe3e4d81ead324fa9b8440daa4ca33c79c5a3
StrawberryTea <look@strawberrytea.xyz>
2023-10-19 14:13:56 -0500

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -142,13 +142,12 @@
 (when (modulep! :ui popup)
   (unless (boundp 'cae-fix-popup-other-keybinding-idle-timer)
     (defvar cae-fix-popup-other-keybinding-idle-timer nil)
+    (defun cae-fix-popup-other-keybinding ()
+      (define-key ctl-x-map "p" nil)
+      (map! :map ctl-x-map
+            "p" #'+popup/other))
     (setq cae-fix-popup-other-keybinding-idle-timer
-          (run-with-idle-timer
-           3 t
-           (cae-defun cae-fix-popup-other-keybinding ()
-             (define-key ctl-x-map "p" nil)
-             (map! :map ctl-x-map
-                   "p" #'+popup/other))))))
+          (run-with-idle-timer 3 t #'cae-fix-popup-other-keybinding))))
 
 (advice-add #'persp-set-keymap-prefix :override #'ignore)
 
