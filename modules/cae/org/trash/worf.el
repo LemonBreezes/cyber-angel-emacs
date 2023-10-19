0a934c796fc7947c3ce286407a2fd5e6f3ed0bc8
StrawberryTea <look@strawberrytea.xyz>
2023-10-19 14:15:25 -0500

    modules/cae/org/trash/worf.el

--- modules/cae/org/trash/worf.el
+++ modules/cae/org/trash/worf.el
@@ -12,26 +12,24 @@
   ;; redundant keybindings seem to fix it.
   (define-key worf-mode-map (kbd "S-<iso-lefttab>") (lookup-key org-mode-map (kbd "<backtab>")))
   (define-key worf-mode-map (kbd "<tab>") (lookup-key org-mode-map (kbd "<tab>")))
-  (advice-add #'worf-property
-              :after
-              (cae-defun cae-worf-property-a ()
-                ;; Jump infront of the property drawer
-                (let ((parent (org-element-property :parent (org-element-at-point))))
-                  (when (eq 'property-drawer (car parent))
-                    (goto-char (org-element-property :begin parent))))))
-  (advice-add #'worf-down
-              :around
-              ;; Skip over vimish folds
-              (cae-defun cae-worf-skip-vimish-fold-forward-a (oldfun arg)
-                (let ((point-max (-some->> (and (require 'vimish-fold nil t)
-                                                (overlays-at (point)))
-                                   (-filter #'vimish-fold--vimish-overlay-p)
-                                   (-map #'overlay-end)
-                                   (apply #'max)))
-                      (point (point)))
-                  (when point-max
-                    (setq arg (1+ arg)))
-                  (funcall oldfun arg)
-                  (when (<= (point) point-max)
-                    (goto-char point)))))
+  (defadvice! cae-worf-property-a ()
+    :after #'worf-property
+    ;; Jump infront of the property drawer
+    (let ((parent (org-element-property :parent (org-element-at-point))))
+      (when (eq 'property-drawer (car parent))
+        (goto-char (org-element-property :begin parent)))))
+  ;; Skip over vimish folds
+  (defadvice! cae-worf-skip-vimish-fold-forward-a (oldfun arg)
+    :around #'worf-down
+    (let ((point-max (-some->> (and (require 'vimish-fold nil t)
+                                    (overlays-at (point)))
+                       (-filter #'vimish-fold--vimish-overlay-p)
+                       (-map #'overlay-end)
+                       (apply #'max)))
+          (point (point)))
+      (when point-max
+        (setq arg (1+ arg)))
+      (funcall oldfun arg)
+      (when (<= (point) point-max)
+        (goto-char point))))
   (advice-add #'worf-add :after #'cae-org-set-created-timestamp))
