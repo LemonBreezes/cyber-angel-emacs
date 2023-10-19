dc52408029985a826c5a989f8b5fe1c4a4944cdb
StrawberryTea <look@strawberrytea.xyz>
2023-10-19 14:13:21 -0500

    lisp/cae-helm.el

--- lisp/cae-helm.el
+++ lisp/cae-helm.el
@@ -2,11 +2,11 @@
 
 ;; It's super annoying to have to confirm the package installations and then
 ;; manually require the package as well. Let's skip those parts.
-(advice-add #'helm-packages-install :around
-            (cae-defun cae-helm-packages-install-and-require-a (oldfun candidate)
-              (let ((pkgs (helm-marked-candidates)))
-                (cae-always-yes-a oldfun candidate)
-                (dolist (pkg pkgs)
-                  (condition-case err
-                      (require pkg)
-                    (message "Failed to load %s: %s" pkg err))))))
+(defadvice! cae-helm-packages-install-and-require-a (oldfun candidate)
+  :around #'helm-packages-install
+  (let ((pkgs (helm-marked-candidates)))
+  (cae-always-yes-a oldfun candidate)
+  (dolist (pkg pkgs)
+    (condition-case err
+        (require pkg)
+      (message "Failed to load %s: %s" pkg err)))))
