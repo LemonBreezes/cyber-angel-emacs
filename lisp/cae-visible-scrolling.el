ec6946fef6ecca737e161d730a8cfbdea06d77d3
StrawberryTea <look@strawberrytea.xyz>
2023-10-19 14:10:18 -0500

    lisp/cae-visible-scrolling.el

--- lisp/cae-visible-scrolling.el
+++ lisp/cae-visible-scrolling.el
@@ -8,36 +8,37 @@
 
   ;; These advices don't support scrolling in multiple steps but that is okay
   ;; with me.
+  (defun cae-shared-scroll-with-hint-a (count line-fn count-fn)
+    (require 'scrollkeeper)
+    (let ((count (funcall count-fn count)))
+      (save-excursion
+        (move-to-window-line (funcall line-fn count))
+        (funcall scrollkeeper-guideline-fn))))
+
   (defadvice! cae-evil-scroll-down-with-hint-a (count &rest _)
     :before #'evil-scroll-down
     (unless (= (line-end-position) (point-max))
-      (require 'scrollkeeper)
-      (let ((count (evil--get-scroll-count count)))
-        (save-excursion
-          (move-to-window-line (if (< count 0) 0 -1))
-          (funcall scrollkeeper-guideline-fn)))))
+      (cae-shared-scroll-with-hint-a count
+                                     (lambda (count) (if (< count 0) 0 -1))
+                                     #'evil--get-scroll-count)))
 
   (defadvice! cae-evil-scroll-up-with-hint-a (count &rest _)
     :before #'evil-scroll-up
     (unless (= (line-beginning-position) (point-min))
-      (require 'scrollkeeper)
-      (let ((count (evil--get-scroll-count count)))
-        (save-excursion
-          (move-to-window-line (if (< count 0) -1 0))
-          (funcall scrollkeeper-guideline-fn)))))
+      (cae-shared-scroll-with-hint-a count
+                                     (lambda (count) (if (< count 0) -1 0))
+                                     #'evil--get-scroll-count)))
 
   (defadvice! evil-scroll-page-up-with-hint-a (count &rest _)
     :before #'evil-scroll-page-up
     (unless (> (abs count) 1)
-      (require 'scrollkeeper)
-      (save-excursion
-        (move-to-window-line (if (< count 0) -1 0))
-        (funcall scrollkeeper-guideline-fn))))
+      (cae-shared-scroll-with-hint-a count
+                                     (lambda (count) (if (< count 0) -1 0))
+                                     #'identity)))
 
   (defadvice! evil-scroll-page-down-with-hint-a (count &rest _)
     :before #'evil-scroll-page-down
     (unless (> (abs count) 1)
-      (require 'scrollkeeper)
-      (save-excursion
-        (move-to-window-line (if (< count 0) 0 -1))
-        (funcall scrollkeeper-guideline-fn)))))
+      (cae-shared-scroll-with-hint-a count
+                                     (lambda (count) (if (< count 0) 0 -1))
+                                     #'identity))))
