32972a425968ea6a06840d5b6073f80585f5fed9
StrawberryTea <look@strawberrytea.xyz>
2023-10-19 15:08:31 -0500

    lisp/cae-lib.el

--- lisp/cae-lib.el
+++ lisp/cae-lib.el
@@ -1,11 +1,5 @@
 ;;; lisp/cae-lib.el -*- lexical-binding: t; -*-
 
-(defmacro cae-defun (name arglist &optional docstring &rest body)
-  "`defun' but guaranteed return the created function."
-  (declare (doc-string 3) (indent 2))
-  `(progn (defun ,name ,arglist ,docstring ,@body)
-          #',name))
-
 (defun cae-ignore-errors-a (fun &rest args)
   "Ignore errors in FUN with ARGS."
   (ignore-errors (apply fun args)))
