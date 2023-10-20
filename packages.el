5a80c4b2774c684ff8d9147e4e8d28f7725e8e3a
StrawberryTea <look@strawberrytea.xyz>
2023-10-19 19:28:15 -0500

    packages.el

--- packages.el
+++ packages.el
@@ -19,6 +19,12 @@
     (:host github :repo "LemonBreezes/evil-org-mode")))
 (package! anzu :recipe
   (:host github :repo "LemonBreezes/anzu" :branch "expose-anzu-update-timer-to-user"))
+(when (and (modulep! :completion corfu)
+           (modulep! :editor snippets))
+  (package! yasnippet-capf :recipe
+    (:host github
+     :repo "LemonBreezes/yasnippet-capf"
+     :branch "allow-interactive-completion-without-prefix")))
 
 (unless (modulep! :config default +smartparens)
   (disable-packages! smartparens))
