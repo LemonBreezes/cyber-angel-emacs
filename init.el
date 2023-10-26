0b798f7cb66d82d54a6f1ed3df9a63ce9adfc000
StrawberryTea <look@strawberrytea.xyz>
2023-10-25 20:59:57 -0500

    init.el

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
 
 ;; I added these to help with debugging my config. It's easier to toggle these
 ;; than to comment out large sections of my config.
-(let ((p t))
+(let ((p nil))
   (defvar cae-init-ui-enabled-p (and p t))
   (defvar cae-init-tools-enabled-p (and p t))
   (defvar cae-init-editor-enabled-p (and p t))
