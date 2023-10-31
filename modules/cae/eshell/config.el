6db2c4bf1ee96ed7d2344efa928a1e59a259faa8
StrawberryTea <look@strawberrytea.xyz>
2023-10-31 00:01:51 -0500

    modules/cae/eshell/config.el

--- modules/cae/eshell/config.el
+++ modules/cae/eshell/config.el
@@ -90,17 +90,6 @@
     (setq epe-show-local-working-directory t
           epe-show-git-status-extended t)))
 
-;; Don't leave me with unbalanced delimiters.
-(defadvice! cae-eshell-kill-input-with-delimiters-a ()
-  :after #'eshell-kill-input
-  (while (and (not (eq (point) (pos-eol)))
-              (condition-case _
-                  (scan-sexps (pos-bol) (pos-eol))
-                (scan-error t)))
-    (delete-char 1))
-  (unless (eq (char-syntax (char-before)) ?\s)
-    (insert-char ?\s)))
-
 (after! esh-module
   (add-to-list 'eshell-modules-list 'eshell-elecslash))
 
