9b7e17759c61e04392eaf57df42f8788980473ca
StrawberryTea <look@strawberrytea.xyz>
2023-11-07 10:33:31 -0600

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -39,6 +39,25 @@
     (after! markdown-mode
       (setq markdown-fontify-whole-heading-line t))))
 
+;; Prevent heading backgrounds from being overwritten.
+(when cae-theme-extend-heading-faces
+  (setq-hook! 'lua-mode-hook
+    lsp-semantic-token-faces '(;;("comment" . lsp-face-semhl-comment)
+                               ("keyword" . lsp-face-semhl-keyword)
+                               ("string" . lsp-face-semhl-string) ("number" . lsp-face-semhl-number)
+                               ("regexp" . lsp-face-semhl-regexp) ("operator" . lsp-face-semhl-operator)
+                               ("namespace" . lsp-face-semhl-namespace) ("type" . lsp-face-semhl-type)
+                               ("struct" . lsp-face-semhl-struct) ("class" . lsp-face-semhl-class)
+                               ("interface" . lsp-face-semhl-interface) ("enum" . lsp-face-semhl-enum)
+                               ("typeParameter" . lsp-face-semhl-type-parameter)
+                               ("function" . lsp-face-semhl-function) ("method" . lsp-face-semhl-method)
+                               ("member" . lsp-face-semhl-member) ("property" . lsp-face-semhl-property)
+                               ("event" . lsp-face-semhl-event) ("macro" . lsp-face-semhl-macro)
+                               ("variable" . lsp-face-semhl-variable) ("parameter" . lsp-face-semhl-parameter)
+                               ("label" . lsp-face-semhl-label) ("enumConstant" . lsp-face-semhl-constant)
+                               ("enumMember" . lsp-face-semhl-constant) ("dependent" . lsp-face-semhl-type)
+                               ("concept" . lsp-face-semhl-interface))))
+
 (defun cae-theme-customize-faces-h (_)
   (after! org
     ;; This is how I like my ellipsis to look. Subtle.
