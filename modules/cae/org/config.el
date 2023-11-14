fae729db5f25c862c9caae311d5d2e1e0f9b5df6
StrawberryTea <look@strawberrytea.xyz>
2023-11-14 16:22:11 -0600

    modules/cae/org/config.el

--- modules/cae/org/config.el
+++ modules/cae/org/config.el
@@ -8,7 +8,10 @@
 (defvar +org-exit-src-code-hook nil
   "Hook run just before exiting a org source block buffer.")
 (advice-add #'org-edit-src-exit :before #'cae-org-run-exit-src-code-hooks)
-(add-hook '+org-exit-src-code-hook #'ws-butler-trim-eob-lines)
+(add-hook! '+org-exit-src-code-hook
+  (defun +org-exit-src-trim-eob-lines-h ()
+    (unless (eq this-command 'org-return)
+      (ws-butler-trim-eob-lines))))
 
 (advice-add #'org-insert-heading :after #'cae-org-set-created-timestamp)
 (add-hook! 'org-capture-mode-hook
