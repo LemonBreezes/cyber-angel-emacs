1bcb0124dbeec9f940903b61981e6d8b51dd042e
StrawberryTea <look@strawberrytea.xyz>
2023-11-27 11:56:09 -0500

    modules/cae/exwm/config.el

--- modules/cae/exwm/config.el
+++ modules/cae/exwm/config.el
@@ -176,23 +176,6 @@ expansion occurs within the parent Emacs session.")
   (use-package! exwm-mff
     :defer t :init (add-hook 'exwm-init-hook #'exwm-mff-mode))
 
-  (defadvice! +exwm-input--fake-last-command ()
-    :override #'exwm-input--fake-last-command
-    (setq last-command #'exwm-input--noop)
-    (run-hooks 'pre-command-hook)
-    (run-hooks 'post-command-hook)
-    (condition-case hook-error
-        (progn
-          (run-hooks 'pre-command-hook)
-          (run-hooks 'post-command-hook))
-      ((error)
-       (exwm--log "Error occurred while running command hooks: %s\n\nBacktrace:\n\n%s"
-                  hook-error
-                  (with-temp-buffer
-                    (setq-local standard-output (current-buffer))
-                    (backtrace)
-                    (buffer-string))))))
-
   (when (modulep! :editor evil +everywhere)
     (load! "+evil"))
 
