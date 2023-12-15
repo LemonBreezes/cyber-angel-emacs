ff19f380536e797e180ae3341c67967cccc60feb
StrawberryTea <look@strawberrytea.xyz>
2023-12-15 14:57:45 -0600

    modules/cae/modeline/packages.el

--- modules/cae/modeline/packages.el
+++ modules/cae/modeline/packages.el
@@ -2,9 +2,7 @@
 ;;; private/modeline/packages.el
 
 (package! minions)
-(if (modulep! +pretty)
-    (package! nyan-mode)
-  (package! mlscroll))
+(package! nyan-mode)
 (package! anzu)
 (when (modulep! :editor evil)
   (package! evil-anzu))
