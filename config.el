9ad1c6c076aaf10e8ec3b700a51a82d95c7c316f
StrawberryTea <look@strawberrytea.xyz>
2023-12-15 12:57:05 -0600

    config.el

--- config.el
+++ config.el
@@ -739,7 +739,7 @@
           ispell-dictionary "en_US"
           ispell-help-in-bufferp 'electric)
     (when-let* ((nixos-aspell-dir "/run/current-system/sw/lib/aspell")
-               (file-exists-p nixos-aspell-dir))
+                (file-exists-p nixos-aspell-dir))
       (setq ispell-aspell-data-dir nixos-aspell-dir
             ispell-aspell-dict-dir nixos-aspell-dir)))
 
