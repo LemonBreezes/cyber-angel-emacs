5659a51067abd97cf2a9464a09ecc481881e3a47
StrawberryTea <look@strawberrytea.xyz>
2023-12-15 15:25:22 -0600

    modules/cae/modeline/config.el

--- modules/cae/modeline/config.el
+++ modules/cae/modeline/config.el
@@ -214,6 +214,7 @@
 
   (add-hook 'doom-first-file-hook #'column-number-mode)
 
+  ;; This package could use some improvements or a fork.
   (use-package! nyan-mode
     :when (cae-display-graphic-p)
     :defer t :init
