62ab12a96eb98653eea7f3e566f5d2c52f048776
StrawberryTea <look@strawberrytea.xyz>
2023-12-17 08:54:58 -0600

    modules/cae/modeline/packages.el

--- modules/cae/modeline/packages.el
+++ modules/cae/modeline/packages.el
@@ -7,3 +7,5 @@
 (package! anzu)
 (when (modulep! :editor evil)
   (package! evil-anzu))
+(when (modulep! +wakatime)
+  (package! wakatime-ui :recipe (:host github :repo "Artawower/wakatime-ui.el")))
