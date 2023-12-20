0a30113231559e7d6fb8f1039c15bd99e8712183
StrawberryTea <look@strawberrytea.xyz>
2023-12-20 09:59:15 -0600

    init.el

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+(when (daemonp)
+  (kill-emacs))
+
 (add-to-list 'safe-local-variable-directories doom-user-dir)
 (add-to-list 'safe-local-variable-directories doom-emacs-dir)
 
