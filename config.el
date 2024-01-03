2021ee2cb931a72175ab09a931064ebc9230f351
StrawberryTea <look@strawberrytea.xyz>
2024-01-03 05:06:56 -0600

    config.el

--- config.el
+++ config.el
@@ -1042,8 +1042,8 @@
 ;;; Autocompletion
 
 (when cae-init-autocompletion-enabled-p
-  ;;(when (modulep! :completion corfu)
-  ;;  (load! "lisp/cae-corfu"))
+  (when (modulep! :completion corfu)
+    (load! "lisp/cae-corfu"))
 
   (setq ido-save-directory-list-file (concat doom-cache-dir "ido.last"))
   (after! ido
