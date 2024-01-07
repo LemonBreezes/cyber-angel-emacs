605903c8c4bde1e50c0ceaa8f5aaa995b48b5bc9
StrawberryTea <look@strawberrytea.xyz>
2024-01-07 13:52:07 -0600

    config.el

--- config.el
+++ config.el
@@ -5,6 +5,7 @@
 ;;; Stuff that should not be disabled.
 
 (load! "lisp/cae-tty")
+(load! "lisp/cae-keyboard")
 (load! "lisp/cae-bindings")
 (load! "lisp/cae-multi")                ;Run parallel Emacs instances.
 (load! "lisp/cae-smartparens")          ;Allow Smartparens to be disabled. This
