ca88eb79682b30baf7dd97de2d326671c956e53f
StrawberryTea <look@strawberrytea.xyz>
2024-01-08 07:09:57 -0600

    modules/cae/vc/packages.el

--- modules/cae/vc/packages.el
+++ modules/cae/vc/packages.el
@@ -7,7 +7,6 @@
 (package! vc-backup :recipe (:repo "LemonBreezes/vc-backup"))
 (package! magit-stats)
 (package! embark-vc)
-(package! igist) ; TODO
 (package! blamer)
 (when (modulep! :completion vertico)
   (package! consult-gh :recipe (:host github :repo "armindarvish/consult-gh" :branch "main")))
