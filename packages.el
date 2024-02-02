1c88d4cfcda3f8a20ed83848c2a7232353788424
StrawberryTea <look@strawberrytea.xyz>
2024-02-02 06:57:47 -0600

    packages.el

--- packages.el
+++ packages.el
@@ -18,8 +18,6 @@
            (modulep! :lang org))
   (package! evil-org :recipe
     (:host github :repo "LemonBreezes/evil-org-mode")))
-(package! anzu :recipe
-  (:host github :repo "LemonBreezes/anzu" :branch "expose-anzu-update-timer-to-user"))
 (package! helpful :recipe
   (:host github :repo "LemonBreezes/helpful" :branch "fix-scan-sexps-error"))
 
