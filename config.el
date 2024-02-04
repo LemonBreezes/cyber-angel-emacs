ca674cf4a553f5eeb8b4d9082cc647ef89e05fc6
StrawberryTea <look@strawberrytea.xyz>
2024-02-03 22:16:00 -0600

    config.el

--- config.el
+++ config.el
@@ -389,7 +389,9 @@
                    browse-url-generic-args (when (eq (user-uid) 0)
                                              '("--no-sandbox")))))
           ((executable-find "firefox-bin")
-           (setq browse-url-generic-program "firefox-bin"))))
+           (setq browse-url-generic-program "firefox-bin"))
+          ((executable-find "firefox-beta")
+           (setq browse-url-generic-program "firefox-beta"))))
 
   (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
   (add-to-list 'doom-large-file-excluded-modes 'nov-mode)
