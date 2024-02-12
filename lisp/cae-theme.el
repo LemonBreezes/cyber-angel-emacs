0d7c692e36458eb11cee8309914f83b8b0dff522
StrawberryTea <look@strawberrytea.xyz>
2024-02-12 07:24:58 -0600

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -2,7 +2,7 @@
 
 (defvar cae-theme-enable-modeline-bell t)
 (defvar cae-theme-extend-heading-faces t)
-(defvar cae-theme-export-theme-with-pywal nil)
+(defvar cae-theme-export-theme-with-pywal t)
 
 (add-hook 'enable-theme-functions #'cae-theme-customize-faces-h)
 
