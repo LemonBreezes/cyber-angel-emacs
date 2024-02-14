eb5dd2f2ff6e2e98cf37de317cb70d424f717e06
StrawberryTea <look@strawberrytea.xyz>
2024-02-14 15:01:22 -0600

    lisp/cae-projectile.el

--- lisp/cae-projectile.el
+++ lisp/cae-projectile.el
@@ -10,7 +10,7 @@
         (projectile-discover-projects-in-search-path))
 
       ;; Projectile's caching of commands is really annoying because if I change
-      ;; the command, the cache doesn't get updated.
+      ;; the commands, the cache doesn't get updated.
       (setq projectile-project-enable-cmd-caching nil)
 
       (map! :leader :prefix "p"
