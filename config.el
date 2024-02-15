867149ff60b191271439e3ec1b7feffa117bfc7f
StrawberryTea <look@strawberrytea.xyz>
2024-02-14 19:27:23 -0600

    config.el

--- config.el
+++ config.el
@@ -295,16 +295,16 @@
     (map! :map messages-buffer-mode-map :n "q" #'quit-window))
 
   (when (modulep! :ui workspaces)
-  (defadvice! cae-which-key-show-workspace (orig-fun &rest pages-obj)
-    "Show my workspaces in the echo thingy"
-    :around #'which-key--process-page
-    (let ((out (apply orig-fun pages-obj)))
-      (if (not (string-match-p "Display tab bar" (car out)))
-          out
-        (cons (car out)
-              (lambda ()
-                (funcall (cdr out))
-                (which-key--echo (concat (current-message) " " (+workspace--tabline)))))))))
+    (defadvice! cae-which-key-show-workspace (orig-fun &rest pages-obj)
+      "Show my workspaces in the echo thingy"
+      :around #'which-key--process-page
+      (let ((out (apply orig-fun pages-obj)))
+        (if (not (string-match-p "Display tab bar" (car out)))
+            out
+          (cons (car out)
+                (lambda ()
+                  (funcall (cdr out))
+                  (which-key--echo (concat (current-message) " " (+workspace--tabline)))))))))
 
   ;; Lower the default popup delay.
   (after! tooltip
