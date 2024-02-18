87737bb0b9e1dcd081e59a12cfb2ecddc5b8ffbe
StrawberryTea <look@strawberrytea.xyz>
2024-02-17 19:05:02 -0600

    config.el

--- config.el
+++ config.el
@@ -226,6 +226,8 @@
         ("^\\*exwm" :ignore t)
         ("^\\*lsp-ui-imenu\\*$" :size cae-popup-shrink-to-fit :select t :quit t
          :side right :ttl 0)
+        ("^\\*Ilist\\*$" :size cae-popup-shrink-to-fit :select t :quit t
+         :side right :ttl 0)
         ("^\\*difftastic git diff\\*$" :size +popup-shrink-to-fit
          :select t :quit t :side bottom :ttl 0)
         ("^\\*Pp Eval Output\\*" :size +popup-shrink-to-fit
