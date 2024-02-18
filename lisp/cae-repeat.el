c8ac82f8fda8f864c18dbea6afae4d4fd3653c5e
StrawberryTea <look@strawberrytea.xyz>
2024-02-17 23:05:39 -0600

    lisp/cae-repeat.el

--- lisp/cae-repeat.el
+++ lisp/cae-repeat.el
@@ -42,6 +42,18 @@
        "]" +workspace/switch-right))
    t)
 
+  (define-repeat-map winum
+    ("0" winum-select-window-0-or-10
+     "1" winum-select-window-1
+     "2" winum-select-window-2
+     "3" winum-select-window-3
+     "4" winum-select-window-4
+     "5" winum-select-window-5
+     "6" winum-select-window-6
+     "7" winum-select-window-7
+     "8" winum-select-window-8
+     "9" winum-select-window-9))
+
   (define-repeat-map cae-dired-jump
     ("-" cae-dired-jump))
 
