0318479890464ce1acd1c841b3a0741e62a0cb00
StrawberryTea <look@strawberrytea.xyz>
2024-02-19 19:13:29 -0600

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -5,7 +5,8 @@
 (after! cape
   (setq cape-dabbrev-check-other-buffers t))
 (after! corfu
-  (setq tab-first-completion 'eol)
+  (setq corfu-auto-delay 0.05
+        tab-first-completion 'eol)
   (after! corfu-quick
     (setq corfu-quick1 (cae-keyboard-kbd "asdfgh")
           corfu-quick2 (cae-keyboard-kbd "jkluionm")))
