2ed30b6467c35db68c18b0c93b095518f2b672ae
StrawberryTea <look@strawberrytea.xyz>
2024-02-19 23:05:01 -0600

    modules/cae/ai/config.el

--- modules/cae/ai/config.el
+++ modules/cae/ai/config.el
@@ -86,7 +86,7 @@
   (setq copilot--base-dir
         (expand-file-name ".local/straight/repos/copilot.el/" doom-emacs-dir)
         copilot-max-char 1000000
-        copilot-idle-delay 0.5)
+        copilot-idle-delay 0)
   ;; Model our Copilot interface after Fish completions.
   (map! :map copilot-completion-map
         "<right>" #'copilot-accept-completion
