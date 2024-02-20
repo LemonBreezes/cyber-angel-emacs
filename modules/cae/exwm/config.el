f48dcc8ee8d4f4571397c5c600175a8fdf46c6fa
StrawberryTea <look@strawberrytea.xyz>
2024-02-19 18:10:47 -0600

    modules/cae/exwm/config.el

--- modules/cae/exwm/config.el
+++ modules/cae/exwm/config.el
@@ -112,7 +112,7 @@
       (defun cae-disable-track-mouse-h ()
         (setq track-mouse
               (and (derived-mode-p 'special-mode)
-                t))))
+                   t))))
 
     ;; Nested Emacs sessions break when `exwm-xim' is used.
     ;; Configure emacs input methods in all X windows.
