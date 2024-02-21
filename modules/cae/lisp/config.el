1ec6bdc04b24ab0ea96cb18cefd0987e087d3759
StrawberryTea <look@strawberrytea.xyz>
2024-02-20 22:50:15 -0600

    modules/cae/lisp/config.el

--- modules/cae/lisp/config.el
+++ modules/cae/lisp/config.el
@@ -1,27 +1,27 @@
 ;;; private/lisp/config.el -*- lexical-binding: t; -*-
 
-;;(use-package! nameless
-;;  :defer t
-;;  ;; `nameless-mode' can cause buffers to become garbled when used in a
-;;  ;; terminal.
-;;  :when (cae-display-graphic-p)
-;;  :init
-;;  (add-hook 'emacs-lisp-mode-hook #'nameless-mode)
-;;  (after! elisp-mode
-;;    (map! :map emacs-lisp-mode-map
-;;          :localleader
-;;          "n" #'nameless-mode))
-;;  :config
-;;  (setq nameless-private-prefix t
-;;        nameless-global-aliases '()))
-;;
-;;(defadvice! +syntax--fix-elisp-flymake-load-path (orig-fn &rest args)
-;;  "Set load path for elisp byte compilation Flymake backend"
-;;  :around #'elisp-flymake-byte-compile
-;;  (let ((elisp-flymake-byte-compile-load-path
-;;         (append elisp-flymake-byte-compile-load-path load-path)))
-;;    (apply orig-fn args)))
-;;
+(use-package! nameless
+  :defer t
+  ;; `nameless-mode' can cause buffers to become garbled when used in a
+  ;; terminal.
+  :when (cae-display-graphic-p)
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'nameless-mode)
+  (after! elisp-mode
+    (map! :map emacs-lisp-mode-map
+          :localleader
+          "n" #'nameless-mode))
+  :config
+  (setq nameless-private-prefix t
+        nameless-global-aliases '()))
+
+(defadvice! +syntax--fix-elisp-flymake-load-path (orig-fn &rest args)
+  "Set load path for elisp byte compilation Flymake backend"
+  :around #'elisp-flymake-byte-compile
+  (let ((elisp-flymake-byte-compile-load-path
+         (append elisp-flymake-byte-compile-load-path load-path)))
+    (apply orig-fn args)))
+
 ;;;; Show `eros' overlays for `edebug' results.
 ;;(when (modulep! :tools eval +overlay)
 ;;  (defun cae-edebug-compute-previous-result-a (_ &rest r)
