176ec50334cef69b1424a98e68a083413637df03
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 14:56:09 -0600

    vanilla-emacs-configs/cape-tree-sitter-faces-bug.el

--- vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
+++ vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
@@ -3,3 +3,12 @@
 (let ((default-directory (file-name-directory load-file-name)))
   (when (file-exists-p (expand-file-name "default.el" default-directory))
     (load-file (expand-file-name "default.el" default-directory))))
+
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
+(straight-use-package 'tree-sitter-hl)
+
+(require 'cc-mode)
+(scratch-buffer)
+(c-mode)
+(insert "int main() { return 0; ")
