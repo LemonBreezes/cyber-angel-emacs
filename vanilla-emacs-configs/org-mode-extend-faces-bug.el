ad29564a37a832c6bf6a50d7350fa38ff6165681
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 12:52:40 -0600

    vanilla-emacs-configs/org-mode-extend-faces-bug.el

--- vanilla-emacs-configs/org-mode-extend-faces-bug.el
+++ vanilla-emacs-configs/org-mode-extend-faces-bug.el
@@ -5,6 +5,7 @@
     (load-file (expand-file-name "default.el" default-directory))))
 
 (require 'org)
+;;
 (load-theme 'leuven t)
 
 (dolist (face '(org-level-1 org-level-2 org-level-3 org-level-4 org-level-5
