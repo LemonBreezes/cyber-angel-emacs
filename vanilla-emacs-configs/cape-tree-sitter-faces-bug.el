42e18ea0916556fd39b79e78b0f4108ef75b8c3c
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 14:59:30 -0600

    vanilla-emacs-configs/cape-tree-sitter-faces-bug.el

--- vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
+++ vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
@@ -12,6 +12,6 @@
 (c-mode)
 (tree-sitter-hl-mode)
 (insert "int main() { return 0; ")
-(message "%s %s"
+(message "get-text-property: %s | get-pos-property: %s"
          (get-text-property (point) 'face)
          (get-pos-property (point) 'face))
