75a7ce12b7c5ec4a47f8aee2489ba9923496f9eb
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 16:49:49 -0600

    vanilla-emacs-configs/cape-tree-sitter-faces-bug.el

--- vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
+++ vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
@@ -49,6 +49,50 @@ previously nil, it will be set to VALUE, not (list VALUE)."
                  (get-pos-property start prop)))
       (setq start next))))
 
+(defun tree-sitter-hl--highlight-region (beg end &optional loudly)
+  "Highlight the region (BEG . END).
+This is intended to be used as a buffer-local override of
+`font-lock-fontify-region-function'.
+
+If LOUDLY is non-nil, print debug messages."
+  (tsc--save-context
+    (let ((inhibit-point-motion-hooks t)
+          (cursor-intangible-mode t))
+      ;; Extend the region to be highlighted, so that it is not too wastefully
+      ;; small. Then extend it again, based on some heuristic, for querying, to
+      ;; avoid certain pathological cases. This is partially analogous to the
+      ;; extension done by `font-lock-default-fontify-region'.
+      (let ((hl-region `(,beg . ,end))
+            (query-region `(,beg . ,end)))
+        (tree-sitter-hl--extend-regions hl-region query-region)
+        (setf `(,beg . ,end) hl-region)
+        (tsc--query-cursor-set-byte-range tree-sitter-hl--query-cursor
+                                         (position-bytes (car query-region))
+                                         (position-bytes (cdr query-region))))
+      (let* ((root-node (tsc-root-node tree-sitter-tree))
+             (captures  (tsc--query-cursor-captures-1
+                         tree-sitter-hl--query-cursor
+                         tree-sitter-hl--query
+                         root-node
+                         #'tsc--buffer-substring-no-properties)))
+        ;; TODO: Handle quitting.
+        (with-silent-modifications
+          (font-lock-unfontify-region beg end)
+          ;; TODO: Consider giving certain combinations of highlight names their
+          ;; own faces. For example, it might be desirable for fontification of
+          ;; a node that matches both "constructor" and "variable" to be
+          ;; different from the union of "constructor fontification" and
+          ;; "variable fontification".
+          (mapc #'tree-sitter-hl--highlight-capture captures)
+          ;; This should primarily be for keywords added through
+          ;; `font-lock-add-keywords' (minor modes and end users).
+          (when (and tree-sitter-hl-use-font-lock-keywords
+                     font-lock-set-defaults)
+            (font-lock-fontify-keywords-region beg end loudly)))
+        ;; We may have highlighted more, but are only reasonably sure about
+        ;; HL-REGION.
+        `(jit-lock-bounds ,beg . ,end)))))
+
 (require 'cc-mode)
 (scratch-buffer)
 (insert "int main() { return 0; // comment")
