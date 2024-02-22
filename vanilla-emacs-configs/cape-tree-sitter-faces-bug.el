8ca849ffdb1b4330cfc21532ee462c0578e32509
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 16:46:05 -0600

    vanilla-emacs-configs/cape-tree-sitter-faces-bug.el

--- vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
+++ vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
@@ -49,47 +49,6 @@ previously nil, it will be set to VALUE, not (list VALUE)."
                  (get-pos-property start prop)))
       (setq start next))))
 
-(defun tree-sitter-hl--setup ()
-  "Set up `tree-sitter-hl' in the current buffer.
-This assumes both `tree-sitter-mode' and `font-lock-mode' were already enabled."
-  ;; TODO: If there's an error, disable `tree-sitter-hl--extra-patterns-list'
-  ;; and retry.
-  (when (tree-sitter-hl--ensure-query)
-    (unless tree-sitter-hl--query-cursor
-      (setq tree-sitter-hl--query-cursor (tsc-make-query-cursor))
-      ;; Invalidate the buffer, only if we were actually disabled previously.
-      ;;(tree-sitter-hl--invalidate)
-      )
-    ;; TODO: Override `font-lock-extend-after-change-region-function', or hook
-    ;; into `jit-lock-after-change-extend-region-functions' directly. For that to
-    ;; work, we need to make sure `tree-sitter--after-change' runs before
-    ;; `jit-lock-after-change'.
-    ;;(add-hook 'tree-sitter-after-change-functions
-    ;;          #'tree-sitter-hl--invalidate
-    ;;          nil :local)
-    ;; TODO: Figure out how to properly integrate with `jit-lock-mode' directly,
-    ;; without relying on `font-lock-mode'. Among other things, it would enable
-    ;; highlighting without setting `font-lock-defaults'. At the moment,
-    ;; `font-lock-mode' somehow helps with making sure that fontification is
-    ;; updated in-time, instead of eventually.
-    (add-function :around (local 'font-lock-fontify-region-function)
-                  #'tree-sitter-hl--highlight-region-with-fallback)
-    ;;(tree-sitter-hl--minimize-font-lock-keywords)
-    ;; XXX: We used to have a hack that calls`font-lock-turn-on-thing-lock',
-    ;; which allows turning on tree-based syntax highlighting by temporarily
-    ;; binding `major-mode', even though such a major mode may not be installed,
-    ;; or does not exist. For example:
-    ;;
-    ;;     (let ((major-mode 'go-mode)) (tree-sitter-hl-mode))
-    ;;
-    ;; However, if `font-lock-mode' is subsequently disabled, because
-    ;; `font-lock-turn-off-thing-lock' does not properly clean up the local
-    ;; value of `font-lock-ensure-function', calling `font-lock-ensure' will
-    ;; signal an error. For example, this happens when org-mode's code blocks
-    ;; are highlighted). Therefore, we disabled that hack. See
-    ;; https://github.com/emacs-tree-sitter/elisp-tree-sitter/issues/74
-    ))
-
 (require 'cc-mode)
 (scratch-buffer)
 (insert "int main() { return 0; // comment")
@@ -97,6 +56,7 @@ This assumes both `tree-sitter-mode' and `font-lock-mode' were already enabled."
 (setq tree-sitter-hl-enable-query-region-extension t)
 (tree-sitter-hl-mode)
 (tree-sitter-hl--highlight-region (point-min) (point-max))
+(message "HELLO2: %s %s" (current-buffer) (window-buffer))
 (message "HELLO: get-text-property: %s | get-pos-property: %s"
          (get-text-property (point-max) 'face)
          (get-pos-property (point-max) 'face))
