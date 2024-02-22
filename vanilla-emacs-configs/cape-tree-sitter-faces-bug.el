5385e5a686c8eff3ec25f0af4cb5d4e01e14167e
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 15:04:32 -0600

    vanilla-emacs-configs/cape-tree-sitter-faces-bug.el

--- vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
+++ vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
@@ -1,8 +1,18 @@
 ;;; vanilla-emacs-configs/cape-tree-sitter-faces-bug.el -*- lexical-binding: t; -*-
 
-(let ((default-directory (file-name-directory load-file-name)))
-  (when (file-exists-p (expand-file-name "default.el" default-directory))
-    (load-file (expand-file-name "default.el" default-directory))))
+;; Bootstrap straight
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+                                    'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'tree-sitter)
 (straight-use-package 'tree-sitter-langs)
