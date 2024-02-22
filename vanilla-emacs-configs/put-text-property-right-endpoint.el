782da693596602421acc9ce0f41b8fc0b5bdbe20
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 17:03:12 -0600

    vanilla-emacs-configs/put-text-property-right-endpoint.el

--- vanilla-emacs-configs/put-text-property-right-endpoint.el
+++ vanilla-emacs-configs/put-text-property-right-endpoint.el
@@ -5,7 +5,5 @@
 (font-lock-mode -1)
 (put-text-property (point-min) (point-max) 'face '(:foreground "red"))
 (message "%s %s %s %s"
-         (get-text-property (point-min) 'face)
          (get-text-property (point-max) 'face)
-         (get-pos-property (point-min) 'face)
          (get-pos-property (point-max) 'face))
