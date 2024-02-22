b80b7f08f1a0b4abc749d1b6d4729d6e778a856e
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 16:29:51 -0600

    vanilla-emacs-configs/cape-tree-sitter-faces-bug.el

--- vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
+++ vanilla-emacs-configs/cape-tree-sitter-faces-bug.el
@@ -36,16 +36,14 @@ previously nil, it will be set to VALUE, not (list VALUE)."
            (setq prev (list prev)))
       (unless (listp prev)
         (setq prev (list prev)))
-      (unless (memq value prev)
-        (put-text-property start next prop
-                           ;; Reduce GC pressure by not making a list if it's
-                           ;; just a single face.
-                           (if prev
-                               (append prev (list value))
-                             value)
-                           object))
-      (setq start next)))
-  (message "%s" (get-text-property (point-max) 'face)))
+      (put-text-property start next prop
+                         ;; Reduce GC pressure by not making a list if it's
+                         ;; just a single face.
+                         (if prev
+                             (append prev (list value))
+                           value)
+                         object)
+      (setq start next))))
 
 (require 'cc-mode)
 (scratch-buffer)
