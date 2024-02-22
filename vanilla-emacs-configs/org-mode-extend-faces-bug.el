08a52566b745caaef01627c13d1f7df9fe8bfce8
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 12:07:34 -0600

    vanilla-emacs-configs/org-mode-extend-faces-bug.el

--- vanilla-emacs-configs/org-mode-extend-faces-bug.el
+++ vanilla-emacs-configs/org-mode-extend-faces-bug.el
@@ -8,3 +8,8 @@
 (dolist (face '(org-level-1 org-level-2 org-level-3 org-level-4 org-level-5
                 org-level-6 org-level-7 org-level-8))
   (set-face-attribute face nil :extend t))
+
+(scratch-buffer)
+(org-mode)
+;; insert an org document with 2 levels of headings and 4 headings total
+(insert "* Heading 1\n** Heading 2\n** Heading 3\n** Heading 4\n")
