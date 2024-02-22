46cff7d27759363742ac11366a086f59eba1ab6f
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 17:03:30 -0600

    vanilla-emacs-configs/put-text-property-right-endpoint.el

--- vanilla-emacs-configs/put-text-property-right-endpoint.el
+++ vanilla-emacs-configs/put-text-property-right-endpoint.el
@@ -4,6 +4,6 @@
 (insert "hello world")
 (font-lock-mode -1)
 (put-text-property (point-min) (point-max) 'face '(:foreground "red"))
-(message "%s %s %s %s"
+(message "get-text-property: %s, get-pos-property: %s"
          (get-text-property (point-max) 'face)
          (get-pos-property (point-max) 'face))
