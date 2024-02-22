937335072b87fc0bd6ab7d84ab5ecc3c94639f7b
StrawberryTea <look@strawberrytea.xyz>
2024-02-22 12:15:37 -0600

    vanilla-emacs-configs/org-mode-extend-faces-bug.el

--- vanilla-emacs-configs/org-mode-extend-faces-bug.el
+++ vanilla-emacs-configs/org-mode-extend-faces-bug.el
@@ -15,4 +15,52 @@
 (setq org-fontify-whole-heading-line t)
 (scratch-buffer)
 (org-mode)
-(insert-file-contents "/home/st/org/roam/20200912110941-infty_2_reading_group.org")
+(insert-file-contents "#+title: (Infty, 2) Reading Group
+#+ROAM_TAGS: \"Quasicategories\"
+
+* [2020-09-11 Fri] Links passed around in today's meeting
+:PROPERTIES:
+:CREATED_TIME: [2020-09-12 Sat 11:10]
+:END:
+
+** [[https://arxiv.org/abs/2006.07997][Internal enriched categories]]
+:PROPERTIES:
+:CREATED_TIME: [2020-09-12 Sat 11:11]
+:END:
+
+** [[https://www.i2m.univ-amu.fr/perso/dimitri.ara/files/qcatsup.pdf][Higher quasi-categories vs higher Rezk spaces]]
+:PROPERTIES:
+:CREATED_TIME: [2020-09-12 Sat 11:12]
+:END:
+
+** [[https://arxiv.org/pdf/1712.06469.pdf][∞-operads as analytic monads]]
+:PROPERTIES:
+:CREATED_TIME: [2020-09-12 Sat 11:13]
+:END:
+
+** [[https://arxiv.org/pdf/2002.01037.pdf][On lax transformations, adjunctions, and monads in (∞,2)-categories]]
+:PROPERTIES:
+:CREATED_TIME: [2020-09-12 Sat 11:14]
+:END:
+
+** [[https://acmbl.github.io/straight_slides.pdf][A modular proof of the straightening theorem]]
+:PROPERTIES:
+:CREATED_TIME: [2020-09-20 Sun 13:20]
+:END:
+
+* [2020-09-18 Fri 14:29] Links passed around
+:PROPERTIES:
+:CREATED_TIME: [2020-09-18 Fri 14:29]
+:END:
+
+** [[https://arxiv.org/abs/1502.06526][Twisted TQFTs]]
+:PROPERTIES:
+:CREATED_TIME: [2020-09-18 Fri 14:29]
+:END:
+
+** [[https://arxiv.org/pdf/2003.11757.pdf][Lax Gray tensor product for 2-categories]]
+:PROPERTIES:
+:CREATED_TIME: [2020-09-18 Fri 14:30]
+:END:
+
+")
