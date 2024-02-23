ac39ec0cfcc17aebebc7ca5f10de1600d6031265
StrawberryTea <look@strawberrytea.xyz>
2024-02-23 09:00:47 -0600

    vanilla-emacs-configs/put-text-property-right-endpoint.el

--- vanilla-emacs-configs/put-text-property-right-endpoint.el
+++ vanilla-emacs-configs/put-text-property-right-endpoint.el
@@ -1,9 +1,9 @@
 ;;; vanilla-emacs-configs/put-text-property-right-endpoint.el -*- lexical-binding: t; -*-
 
 (scratch-buffer)
-(insert "hello world")
+(insert "hello world ")
 (font-lock-mode -1)
-(put-text-property (point-min) (point-max) 'face '(:foreground "red"))
+(put-text-property (point-min) (length "hello world") 'face '(:foreground "red"))
 (message "get-text-property: %s, get-pos-property: %s"
          (get-text-property (point-max) 'face)
          (get-pos-property (point-max) 'face))
