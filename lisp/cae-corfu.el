c4947e47d10476109614ab87e106b88d3b09dcd5
StrawberryTea <look@strawberrytea.xyz>
2024-02-23 18:03:50 -0600

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -25,20 +25,6 @@
   (map! :map corfu-map
         [remap corfu-quit] #'cae-corfu-quit))
 
-;;(after! lsp-completion
-;;  ;; Do not try to configure `company-capf' for LSP.
-;;  (setq lsp-completion-provider nil))
-
-;; Do not make us type RET twice with Corfu.
-;;(defun corfu--maybe-return-filter (cmd)
-;;  (if (eq corfu--index -1) (corfu-quit) cmd))
-;;(after! corfu
-;;  (keymap-set corfu-map "RET" `(menu-item "corfu-maybe-return" corfu-insert
-;;                                :filter corfu--maybe-return-filter))
-;;  (keymap-set
-;;   corfu-map "<return>" `(menu-item "corfu-maybe-return" corfu-insert
-;;                          :filter corfu--maybe-return-filter)))
-
 ;; Wildcard separator
 (defvar +orderless-wildcard-character ?,
   "A character used as a wildcard in Corfu for fuzzy autocompletion. If you
