bf73fc00236540776f65ae34183c546dd8fb0f95
StrawberryTea <look@strawberrytea.xyz>
2024-02-26 22:34:32 -0600

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -482,3 +482,28 @@ mark the string and call `edit-indirect-region' with it."
     ;; Stop AI from freezing Emacs while Emacs is waiting for the AI to respond.
     (set-process-sentinel proc nil))
   (call-interactively #'kill-current-buffer))
+
+;;;###autoload
+(defun mu4e-compose-goto-top ()
+  "Go to the beginning of the message or buffer.
+Go to the beginning of the message or, if already there, go to the
+beginning of the buffer."
+  (interactive)
+  (let ((old-position (point)))
+    (message-goto-body)
+    (when (equal (point) old-position)
+      (beginning-of-buffer))))
+
+;;;###autoload
+(defun mu4e-compose-goto-bottom ()
+  "Go to the end of the message or buffer.
+Go to the end of the message (before signature) or, if already there, go to the
+end of the buffer."
+  (interactive)
+  (let ((old-position (point))
+        (message-position (save-excursion (message-goto-body) (point))))
+    (end-of-buffer)
+    (when (re-search-backward "^-- $" message-position t)
+      (previous-line))
+    (when (equal (point) old-position)
+      (end-of-buffer))))
