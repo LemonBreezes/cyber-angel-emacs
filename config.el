56bb1b2fe929da32c7110f5aacb06a5bca2ea2c3
StrawberryTea <look@strawberrytea.xyz>
2024-02-28 11:11:09 -0600

    config.el

--- config.el
+++ config.el
@@ -105,7 +105,9 @@
   (autoload 'minibuffer-depth-setup "mb-depth")
   (add-hook 'minibuffer-setup-hook  #'minibuffer-depth-setup)
 
-  ;; Just my guess for how big this number should be.
+  ;; Just my guess for how big this number should be. Call
+  ;; `cae-set-jit-lock-chunk-size-to-optimal' on a few different files to get an
+  ;; idea.
   (setq jit-lock-chunk-size 2500)
 
   ;; I don't like `hl-line-mode' globally because it sometimes conflicts with
