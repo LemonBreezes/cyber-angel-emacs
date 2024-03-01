adbc91a62f4c892d17cc969927826c4c8883cda0
StrawberryTea <look@strawberrytea.xyz>
2024-03-01 13:47:42 -0600

    autoload/cae-debug.el

--- autoload/cae-debug.el
+++ autoload/cae-debug.el
@@ -1,7 +1,7 @@
 ;;; autoload/cae-debug.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun search-in-dabbrev-buffers (search-string)
+(defun cae-search-in-dabbrev-buffers (search-string)
   "Search for SEARCH-STRING in all buffers returned by dabbrev--select-buffers."
   (let ((buffers (dabbrev--select-buffers)))
     (multi-occur buffers search-string)))
