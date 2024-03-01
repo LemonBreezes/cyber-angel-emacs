5300299bb0abca0952739949a5555f7bd40d0cf3
StrawberryTea <look@strawberrytea.xyz>
2024-02-29 22:23:35 -0600

    autoload/cae-benchmark.el

---
+++ autoload/cae-benchmark.el
@@ -0,0 +1,18 @@
+;;; autoload/cae-benchmark.el -*- lexical-binding: t; -*-
+
+(defun cae-compute-optimal-jit-lock-chunk-size ()
+  "Calculate an estimated optimal value for `jit-lock-chunk-size' based on window dimensions."
+  (save-excursion
+    (goto-char (window-start))
+    ;; Calculate optimal chunk size: window height * average line length (with spacing)
+    ;; Then, increase the estimated size by 10% to provide a buffer.
+    (floor (* 1.1 (window-height)
+              (floor (/ (line-end-position) (line-number-at-pos)))
+              (1+ (or line-spacing 0))))))
+
+;;;###autoload
+(defun cae-set-jit-lock-chunk-size-to-optimal ()
+  "Set `jit-lock-chunk-size' to the computed optimal value."
+  (interactive)
+  (set-variable 'jit-lock-chunk-size (compute-optimal-jit-lock-chunk-size))
+  (message "jit-lock-chunk-size set to optimal value of %d" jit-lock-chunk-size))
