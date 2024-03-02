01803fbeaddf48d03a11138194063ecf6df5587d
StrawberryTea <look@strawberrytea.xyz>
2024-03-02 09:25:23 -0600

    modules/cae/eww/packages.el

--- modules/cae/eww/packages.el
+++ modules/cae/eww/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; cae/eww/packages.el
 
-(package! eww-plz (recipe :fetcher github :repo "9viz/eww-plz.el"))
+(package! eww-plz :recipe (:fetcher github :repo "9viz/eww-plz.el"))
