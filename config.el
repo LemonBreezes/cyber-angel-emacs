8a9cb254a7bda11a9cdbc59c31a6b2816fbbaedb
StrawberryTea <look@strawberrytea.xyz>
2024-03-02 16:35:15 -0600

    config.el

--- config.el
+++ config.el
@@ -638,14 +638,15 @@
 
   ;; Loading `tramp-sh' is slow, so we have this hook load auto-sudoedit if we
   ;; need to use sudo on a file before `tramp-sh' is loaded.
-  (add-hook 'find-file-hook #'cae-auto-sudoedit-maybe-h -1)
-  (use-package! auto-sudoedit
-    :after tramp-sh :config
-    (remove-hook 'find-file-hook #'cae-auto-sudoedit-maybe-h)
-    (defadvice! cae-auto-sudoedit-file-local-name-a (oldfun dir buffer setup)
-      :around #'dirvish-data-for-dir
-      (funcall oldfun (tramp-file-local-name dir) buffer setup))
-    (auto-sudoedit-mode +1)))
+  ;;(add-hook 'find-file-hook #'cae-auto-sudoedit-maybe-h -1)
+  ;;(use-package! auto-sudoedit
+  ;;  :after tramp-sh :config
+  ;;  (remove-hook 'find-file-hook #'cae-auto-sudoedit-maybe-h)
+  ;;  (defadvice! cae-auto-sudoedit-file-local-name-a (oldfun dir buffer setup)
+  ;;    :around #'dirvish-data-for-dir
+  ;;    (funcall oldfun (tramp-file-local-name dir) buffer setup))
+  ;;  (auto-sudoedit-mode +1))
+  )
 
 
 ;;; Editor
