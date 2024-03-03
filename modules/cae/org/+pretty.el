f0aa2c448a14c183dce506ff46b01e5ebd82c248
StrawberryTea <look@strawberrytea.xyz>
2024-03-02 21:23:51 -0600

    modules/cae/org/+pretty.el

--- modules/cae/org/+pretty.el
+++ modules/cae/org/+pretty.el
@@ -22,7 +22,8 @@
           ("WAIT" :inverse-video t :inherit +org-todo-onhold)
           ("[?]"  :inverse-video t :inherit +org-todo-onhold)
           ("KILL" :inverse-video t :inherit +org-todo-cancel)
-          ("NO"   :inverse-video t :inherit +org-todo-cancel))
+          ("NO"   :inverse-video t :inherit +org-todo-cancel)
+          ("COMMENT" :inverse-video t :inherit org-special-keyword))
         org-modern-footnote
         (cons nil (cadr org-script-display))
         org-modern-block-fringe nil
