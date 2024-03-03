1efa2a987aa7adde54c588f94399973055407f45
StrawberryTea <look@strawberrytea.xyz>
2024-03-03 15:09:19 -0600

    vanilla-emacs-configs/org-mode-extend-faces-bug-3.el

---
+++ vanilla-emacs-configs/org-mode-extend-faces-bug-3.el
@@ -0,0 +1,30 @@
+;;; vanilla-emacs-configs/org-mode-extend-faces-bug-3.el -*- lexical-binding: t; -*-
+
+;; Bootstrap straight
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+                                    'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'org)
+
+(require 'org)
+(load-theme 'leuven t)
+
+(dolist (face '(org-level-1 org-level-2 org-level-3 org-level-4 org-level-5
+                org-level-6 org-level-7 org-level-8))
+  (set-face-attribute face nil :extend t))
+
+(setq org-fontify-whole-heading-line t)
+(scratch-buffer)
+(org-mode)
+(insert "<<<radio>>>\n* [2020-09-11 Fri] Headline 1\n
+* COMMENT Headline 2")
