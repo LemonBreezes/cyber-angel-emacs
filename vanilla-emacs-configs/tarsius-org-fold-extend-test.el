45a94e7172a19c12f47eccc3d34b83c1883e50da
StrawberryTea <look@strawberrytea.xyz>
2024-03-04 20:23:08 -0600

    vanilla-emacs-configs/tarsius-org-fold-extend-test.el

--- vanilla-emacs-configs/tarsius-org-fold-extend-test.el
+++ vanilla-emacs-configs/tarsius-org-fold-extend-test.el
@@ -1,7 +1,7 @@
 ;;; vanilla-emacs-configs/tarsius-org-fold-extend-test.el -*- lexical-binding: t; -*-
 
-(require 'cl)
-(setq load-path (remove-if (lambda (x) (string-match-p "org$" x)) load-path))
+(require 'cl-lib)
+(setq load-path (cl-remove-if (lambda (x) (string-match-p "org$" x)) load-path))
 (add-to-list 'load-path "/home/st/.config/emacs/.local/straight/build-30.0.50/org/lisp/")
 (setq org-fontify-whole-heading-line t)
 (require 'org)
