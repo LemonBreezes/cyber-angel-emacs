979c99bfb7bc87d364ddce8b0624b494c6658732
StrawberryTea <look@strawberrytea.xyz>
2024-03-04 21:18:21 -0600

    vanilla-emacs-configs/org-mode-extend-faces-bug-4.el

--- vanilla-emacs-configs/org-mode-extend-faces-bug-4.el
+++ vanilla-emacs-configs/org-mode-extend-faces-bug-4.el
@@ -9,9 +9,9 @@
 (require 'org)
 (load-theme 'leuven t)
 
-;;(dolist (face '(org-level-1 org-level-2 org-level-3 org-level-4 org-level-5
-;;                org-level-6 org-level-7 org-level-8))
-;;  (set-face-attribute face nil :extend t))
+(dolist (face '(org-level-1 org-level-2 org-level-3 org-level-4 org-level-5
+                org-level-6 org-level-7 org-level-8))
+  (set-face-attribute face nil :extend t))
 
 (setq org-fontify-whole-heading-line t)
 (setq org-startup-folded t)
