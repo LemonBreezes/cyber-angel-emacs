46dec8fbc3fb461a4d72c62b8d701cbbf74cd7e5
StrawberryTea <look@strawberrytea.xyz>
2024-03-04 20:20:37 -0600

    vanilla-emacs-configs/tarsius-org-fold-extend-test.el

--- vanilla-emacs-configs/tarsius-org-fold-extend-test.el
+++ vanilla-emacs-configs/tarsius-org-fold-extend-test.el
@@ -1,8 +1,9 @@
 ;;; vanilla-emacs-configs/tarsius-org-fold-extend-test.el -*- lexical-binding: t; -*-
 
-(add-to-list 'load-path "/home/st/.config/emacs/.local/straight/build-30.0.50/org/lisp/")
 (setq org-fontify-whole-heading-line t)
 (require 'org-loaddefs)
+(use-package org
+  :load-path "/home/st/.config/emacs/.local/straight/build-30.0.50/org/lisp/")
 (load-theme 'leuven t)
 
 (with-current-buffer (get-buffer-create "*demo*")
