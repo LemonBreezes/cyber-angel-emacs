9ebb54c2977d5f29e45c91a08eff4f932be6542f
StrawberryTea <look@strawberrytea.xyz>
2024-03-04 20:55:16 -0600

    vanilla-emacs-configs/org-mode-extend-faces-bug-4.el

---
+++ vanilla-emacs-configs/org-mode-extend-faces-bug-4.el
@@ -0,0 +1,19 @@
+;;; vanilla-emacs-configs/org-mode-extend-faces-bug-4.el -*- lexical-binding: t; -*-
+
+(let ((default-directory (file-name-directory load-file-name)))
+  (when (file-exists-p (expand-file-name "default.el" default-directory))
+    (load-file (expand-file-name "default.el" default-directory))))
+
+(straight-use-package 'org)
+
+(require 'org)
+(load-theme 'leuven t)
+
+(dolist (face '(org-level-1 org-level-2 org-level-3 org-level-4 org-level-5
+                org-level-6 org-level-7 org-level-8))
+  (set-face-attribute face nil :extend t))
+
+(setq org-fontify-whole-heading-line t)
+(scratch-buffer)
+(org-mode)
+(insert "* foo\n** bar\ntest\n** baz\ntest\n\n")
