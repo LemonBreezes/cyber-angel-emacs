372b7ebd4aa18cdc53f12a4c8842b6433ff84fe6
StrawberryTea <look@strawberrytea.xyz>
2024-03-04 20:21:05 -0600

    vanilla-emacs-configs/tarsius-org-fold-extend-test.el

--- vanilla-emacs-configs/tarsius-org-fold-extend-test.el
+++ vanilla-emacs-configs/tarsius-org-fold-extend-test.el
@@ -1,7 +1,6 @@
 ;;; vanilla-emacs-configs/tarsius-org-fold-extend-test.el -*- lexical-binding: t; -*-
 
 (setq org-fontify-whole-heading-line t)
-(require 'org-loaddefs)
 (use-package org
   :load-path "/home/st/.config/emacs/.local/straight/build-30.0.50/org/lisp/")
 (load-theme 'leuven t)
