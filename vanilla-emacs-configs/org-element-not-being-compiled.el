6d1f6b0223bffb9a6b6f1165398d6b02ca042dcb
StrawberryTea <look@strawberrytea.xyz>
2024-03-04 22:44:24 -0600

    vanilla-emacs-configs/org-element-not-being-compiled.el

--- vanilla-emacs-configs/org-element-not-being-compiled.el
+++ vanilla-emacs-configs/org-element-not-being-compiled.el
@@ -1,9 +1,18 @@
 ;;; vanilla-emacs-configs/org-element-not-being-compiled.el -*- lexical-binding: t; -*-
 
+;; Bootstrap straight
+(defvar bootstrap-version)
 
-(let ((default-directory (file-name-directory load-file-name)))
-  (when (file-exists-p (expand-file-name "default.el" default-directory))
-    (load-file (expand-file-name "default.el" default-directory))))
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+                                    'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'org)
 (straight-use-package 'vertico)
