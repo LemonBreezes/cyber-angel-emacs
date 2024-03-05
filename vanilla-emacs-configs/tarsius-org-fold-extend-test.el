7c3a1d8b46831d2eb2b501825499e243c3dec648
StrawberryTea <look@strawberrytea.xyz>
2024-03-04 20:44:14 -0600

    vanilla-emacs-configs/tarsius-org-fold-extend-test.el

--- vanilla-emacs-configs/tarsius-org-fold-extend-test.el
+++ vanilla-emacs-configs/tarsius-org-fold-extend-test.el
@@ -1,15 +1,29 @@
 ;;; vanilla-emacs-configs/tarsius-org-fold-extend-test.el -*- lexical-binding: t; -*-
 
-(require 'cl-lib)
-(add-to-list 'load-path "/home/st/.config/emacs/.local/straight/build-30.0.50/org")
-(setq org-fontify-whole-heading-line t)
-(require 'org-autoloads)
+;; Bootstrap straight
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+                                    'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'org)
+
 (require 'org)
 (load-theme 'leuven t)
 
-(with-current-buffer (get-buffer-create "*demo*")
-  (org-mode)
-  (erase-buffer)
-  (save-excursion
-    (insert "* foo\n** bar\ntest\n** baz\ntest\n\n"))
-  (pop-to-buffer (current-buffer)))
+(dolist (face '(org-level-1 org-level-2 org-level-3 org-level-4 org-level-5
+                org-level-6 org-level-7 org-level-8))
+  (set-face-attribute face nil :extend t))
+
+(setq org-fontify-whole-heading-line t)
+(scratch-buffer)
+(org-mode)
+(insert "* foo\n** bar\ntest\n** baz\ntest\n\n")
