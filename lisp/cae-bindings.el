be78671cd761e93c7ecbb17d757d9b0911bc4f93
StrawberryTea <look@strawberrytea.xyz>
2024-03-06 23:59:28 -0600

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -139,7 +139,9 @@
 (define-key resize-window-repeat-map "_" #'shrink-window)
 
 ;; Fix the TAB key
-(map! :i "<tab>" nil                    ; This seems like a weird bug.
+(map! :i "<tab>" nil                    ; It seems like a weird bug in Doom that
+                                        ; this was bound to
+                                        ; `completion-at-point'.
       (:after cc-mode
        :map c-mode-base-map
        "TAB" #'indent-for-tab-command))
