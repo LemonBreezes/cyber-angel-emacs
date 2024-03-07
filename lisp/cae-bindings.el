bdc0524934acedbf8efcbe9ace38ad75d15fe182
StrawberryTea <look@strawberrytea.xyz>
2024-03-06 23:42:21 -0600

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -103,7 +103,8 @@
       ;; indentation, and expanding snippets.
       [C-i] #'doom/dumb-indent
       "C-S-i" #'doom/dumb-dedent
-      ;; For some reason this key was getting overriden by `completion-at-point'.
+      ;; For some reason this key was getting overriden by
+      ;; `completion-at-point'.
       :gi "<tab>" #'indent-for-tab-command
       (:when (and (modulep! :editor snippets)
                   (modulep! :completion corfu +tng))
