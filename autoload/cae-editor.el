9c0f55e58218f9b0e683dfb8e93301f0252e1d71
StrawberryTea <look@strawberrytea.xyz>
2024-03-09 11:28:20 -0600

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -286,45 +286,6 @@ This is the format used on Reddit for code blocks."
     (yank-indent--post-command-hook)))
 
 ;;;###autoload
-(defun cae-embark-act ()
-  (interactive)
-  (require 'embark)
-  (let ((embark-cycle-key (key-description (this-command-keys))))
-    (call-interactively 'embark-act)))
-
-;;;###autoload
-(defun cae-avy-do (action pt)
-  (save-mark-and-excursion
-    (goto-char pt)
-    (cond ((or (eq avy-command 'avy-goto-line)
-               (memq this-command '(avy-goto-line-above
-                                    avy-goto-line-below)))
-           (progn (goto-char (line-beginning-position))
-                  (set-mark (point))
-                  (goto-char (line-end-position))))
-          ((eq this-command 'cae-avy-symbol-at-point)
-           (er/mark-symbol))
-          (t (if (fboundp 'eri/expand-region)
-                 (eri/expand-region 1)
-               (er/expand-region 1))))
-    (call-interactively action))
-  (run-at-time 0.0 nil
-               (lambda ()
-                 (cae-pop-mark)
-                 (when (bound-and-true-p hl-line-mode)
-                   (hl-line-highlight))
-                 (when (bound-and-true-p beacon-mode)
-                   (beacon-blink)))))
-
-;;;###autoload
-(defalias 'cae-avy-action-embark-act
-  (apply-partially #'cae-avy-do #'embark-act))
-
-;;;###autoload
-(defalias 'cae-avy-action-kill
-  (apply-partially #'cae-avy-do #'cae-kill-region))
-
-;;;###autoload
 (defun cae-kill-region ()
   (interactive)
   (call-interactively #'kill-region)
