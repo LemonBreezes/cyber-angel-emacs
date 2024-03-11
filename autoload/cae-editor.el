1c89ab84d3a944df8f147b58cc2695551e821897
StrawberryTea <look@strawberrytea.xyz>
2024-03-11 00:30:54 -0500

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -487,3 +487,55 @@ jumping backwards."
                         ;; a list already, append if not already there
                         (cons old-file (remove old-file current-history))))
                  cae--sibling-file-history)))))
+
+;;;###autoload
+(defun cae-randomize-region (beg end)
+  (interactive
+   (if (use-region-p)
+       (list (region-beginning) (region-end)) ; use the current region if it is active
+     (list (point-min) (point-max)))) ; if no region is active, use the whole buffer
+  ;; Do not shuffle the first line in dired-mode.
+  (when (and (derived-mode-p 'dired-mode)
+             (= (line-number-at-pos beg) 1))
+    (goto-char beg)
+    (setq beg (line-beginning-position 2)))
+  (let ((inhibit-read-only t))
+    (if (> beg end)
+        (let (mid) (setq mid end end beg beg mid)))
+    (save-excursion
+      ;; put beg at the start of a line and end and the end of one --
+      ;; the largest possible region which fits this criteria
+      (goto-char beg)
+      (or (bolp) (forward-line 1))
+      (setq beg (point))
+      (goto-char end)
+      ;; the test for bolp is for those times when end is on an empty
+      ;; line; it is probably not the case that the line should be
+      ;; included in the reversal; it isn't difficult to add it
+      ;; afterward.
+      (or (and (eolp) (not (bolp)))
+          (progn (forward-line -1) (end-of-line)))
+      (setq end (point-marker))
+      (let ((strs (shuffle-list
+                   (split-string (buffer-substring-no-properties beg end)
+                                 "\n"))))
+        (delete-region beg end)
+        (dolist (str strs)
+          (insert (concat str "\n"))))))
+  (when (bound-and-true-p nerd-icons-dired-mode)
+    (nerd-icons-dired--refresh)))
+
+(defun cae-shuffle-list (list)
+  "Randomly permute the elements of LIST.
+All permutations equally likely."
+  (let ((i 0)
+  j
+  temp
+  (len (length list)))
+    (while (< i len)
+      (setq j (+ i (random (- len i))))
+      (setq temp (nth i list))
+      (setcar (nthcdr i list) (nth j list))
+      (setcar (nthcdr j list) temp)
+      (setq i (1+ i))))
+  list)
