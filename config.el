274eafbc84d7cae20bef4a5db92722b8687c7456
StrawberryTea <look@strawberrytea.xyz>
2024-03-11 11:22:07 -0500

    config.el

--- config.el
+++ config.el
@@ -196,11 +196,11 @@
               (list "--:\\\\${}+@-Z_[:alpha:]~*?#" "" "")))))
 
   ;; Do not spam me with warnings while using `org-ai'.
-  ;;(unless init-file-debug
-  ;;  (add-hook! 'doom-first-file-hook :append
-  ;;    (defun cae-warnings-initialize ()
-  ;;      (setq warning-minimum-level :error
-  ;;            warning-minimum-log-level :error))))
+  (unless init-file-debug
+    (add-hook! 'doom-first-file-hook :append
+      (defun cae-warnings-initialize ()
+        (setq warning-minimum-level :error
+              warning-minimum-log-level :error))))
 
   (after! alert
     (setq alert-default-style 'libnotify))
