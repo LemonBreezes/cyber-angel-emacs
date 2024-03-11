e427c301b5e0fd9d2bafe29f7d0aa52bd9ffee6e
StrawberryTea <look@strawberrytea.xyz>
2024-03-11 00:40:10 -0500

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -89,6 +89,7 @@
       "C-x x c" #'cae-edit-indirect-dwim
       "C-x M-o" #'ace-swap-window
       "C-x M-t" #'transpose-frame
+      "C-x j" #'cae-jump-to-random-line-end
       ;;"C-x x N" #'cae-make-new-buffer ;TODO Check if this is necessary next
                                         ;time I disable Evil.
       "C-x x o" #'ov-clear
