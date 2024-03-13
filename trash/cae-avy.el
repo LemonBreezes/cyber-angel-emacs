191d0923e080047237e614ccc741663db5049f21
StrawberryTea <look@strawberrytea.xyz>
2024-03-13 14:47:47 -0500

    trash/cae-avy.el

--- trash/cae-avy.el
+++ trash/cae-avy.el
@@ -1,13 +1,6 @@
 ;;; trash/cae-avy.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-embark-act ()
-  (interactive)
-  (require 'embark)
-  (let ((embark-cycle-key (key-description (this-command-keys))))
-    (call-interactively 'embark-act)))
-
-;;;###autoload
 (defun cae-avy-do (action pt)
   (save-mark-and-excursion
     (goto-char pt)
