5943cc2ecd6dcc5db01f5fa257d98bcd0088bd97
StrawberryTea <look@strawberrytea.xyz>
2024-03-17 14:00:21 -0500

    lisp/cae-repeat.el

--- lisp/cae-repeat.el
+++ lisp/cae-repeat.el
@@ -68,103 +68,107 @@
   (define-repeat-map cae-jump-to-random-line-end
     ("j" cae-jump-to-random-line-end))
 
-  (define-repeat-map org-priority
-    ("u" org-priority-up
-     "d" org-priority-down))
+  (define-repeat-map git-rebase-move-line
+    ("j" git-rebase-move-line-down)
+    ("k" git-rebase-move-line-up))
 
-  (define-repeat-map org-ai-kill-region-at-point
-    ("DEL" org-ai-kill-region-at-point))
-  (define-repeat-map cae-org-ai-kill-region-at-point
-    ("DEL" cae-org-ai-kill-region-at-point))
+    (define-repeat-map org-priority
+      ("u" org-priority-up
+       "d" org-priority-down))
 
-  ;; Currently part of `other-window' repeat map
-  (define-repeat-map winner
-    ("u" winner-undo
-     "r" winner-redo
-     "U" winner-redo))
+    (define-repeat-map org-ai-kill-region-at-point
+      ("DEL" org-ai-kill-region-at-point))
+    (define-repeat-map cae-org-ai-kill-region-at-point
+      ("DEL" cae-org-ai-kill-region-at-point))
 
-  (define-repeat-map cae-buffer-navigation-repeat-map
-    ("<left>" cae-dired-previous-buffer
-     "<right>" cae-dired-next-buffer))
+    ;; Currently part of `other-window' repeat map
+    (define-repeat-map winner
+      ("u" winner-undo
+       "r" winner-redo
+       "U" winner-redo))
 
-  (define-repeat-map set-mark-command
-    ("SPC" set-mark-command))
+    (define-repeat-map cae-buffer-navigation-repeat-map
+      ("<left>" cae-dired-previous-buffer
+       "<right>" cae-dired-next-buffer))
 
-  (define-repeat-map pop-global-mark
-    ("C-@" pop-global-mark))
+    (define-repeat-map set-mark-command
+      ("SPC" set-mark-command))
 
-  ;; Now I can scroll with `v' and reverse directions with `V'! Amazing!
-  (define-repeat-map scroll-up-command
-    ("v" scroll-up-command)
-    (:exit "V" scroll-down-command))
-  (define-repeat-map scroll-down-command
-    ("v" scroll-down-command)
-    (:exit "V" scroll-up-command))
+    (define-repeat-map pop-global-mark
+      ("C-@" pop-global-mark))
 
-  (define-repeat-map multiple-cursors-mark
-    ("n" mc/mark-next-like-this
-     "p" mc/mark-previous-like-this
-     "N" mc/unmark-next-like-this
-     "P" mc/unmark-previous-like-this))
+    ;; Now I can scroll with `v' and reverse directions with `V'! Amazing!
+    (define-repeat-map scroll-up-command
+      ("v" scroll-up-command)
+      (:exit "V" scroll-down-command))
+    (define-repeat-map scroll-down-command
+      ("v" scroll-down-command)
+      (:exit "V" scroll-up-command))
 
-  (define-repeat-map scroll-other-window
-    ("v" scroll-other-window)
-    (:exit "V" scroll-other-window-down))
-  (define-repeat-map scroll-other-window-down
-    ("v" scroll-other-window-down)
-    (:exit "V" scroll-other-window))
+    (define-repeat-map multiple-cursors-mark
+      ("n" mc/mark-next-like-this
+       "p" mc/mark-previous-like-this
+       "N" mc/unmark-next-like-this
+       "P" mc/unmark-previous-like-this))
 
-  (define-repeat-map vertico-scroll-up
-    ("v" vertico-scroll-up)
-    (:exit "V" vertico-scroll-down))
-  (define-repeat-map vertico-scroll-down
-    ("v" vertico-scroll-down)
-    (:exit "V" vertico-scroll-up))
+    (define-repeat-map scroll-other-window
+      ("v" scroll-other-window)
+      (:exit "V" scroll-other-window-down))
+    (define-repeat-map scroll-other-window-down
+      ("v" scroll-other-window-down)
+      (:exit "V" scroll-other-window))
 
-  (when (modulep! :editor evil)
-    (define-repeat-map evil-window-next
-      ("w" evil-window-next
-       "W" evil-window-prev))
+    (define-repeat-map vertico-scroll-up
+      ("v" vertico-scroll-up)
+      (:exit "V" vertico-scroll-down))
+    (define-repeat-map vertico-scroll-down
+      ("v" vertico-scroll-down)
+      (:exit "V" vertico-scroll-up))
 
-    (define-repeat-map evil-scroll
-      ("d" evil-scroll-down
-       "u" evil-scroll-up))
+    (when (modulep! :editor evil)
+      (define-repeat-map evil-window-next
+        ("w" evil-window-next
+         "W" evil-window-prev))
 
-    (define-repeat-map evil-scroll-page
-      ("f" evil-scroll-page-down
-       "b" evil-scroll-page-up))
+      (define-repeat-map evil-scroll
+        ("d" evil-scroll-down
+         "u" evil-scroll-up))
 
-    (define-repeat-map evil-scroll-line
-      ("e" evil-scroll-line-down
-       "y" evil-scroll-line-up))
+      (define-repeat-map evil-scroll-page
+        ("f" evil-scroll-page-down
+         "b" evil-scroll-page-up))
 
-    (define-repeat-map evil-complete-line
-      ("C-p" evil-complete-previous-line
-       "C-n" evil-complete-next-line))
+      (define-repeat-map evil-scroll-line
+        ("e" evil-scroll-line-down
+         "y" evil-scroll-line-up))
 
-    (define-repeat-map evil-paste-pop
-      ("n" evil-paste-pop-next
-       "p" evil-paste-pop))
+      (define-repeat-map evil-complete-line
+        ("C-p" evil-complete-previous-line
+         "C-n" evil-complete-next-line))
 
-    (define-repeat-map tab-bar-switch-next-tab
-      ("t" tab-bar-switch-to-next-tab
-       "T" tab-bar-switch-to-prev-tab)))
+      (define-repeat-map evil-paste-pop
+        ("n" evil-paste-pop-next
+         "p" evil-paste-pop))
 
-  (defun cae-repeat-ignore-when-hydra-active-a ()
-    (bound-and-true-p hydra-curr-map))
+      (define-repeat-map tab-bar-switch-next-tab
+        ("t" tab-bar-switch-to-next-tab
+         "T" tab-bar-switch-to-prev-tab)))
 
-  (advice-add #'repeat-post-hook :before-until
-              #'cae-repeat-ignore-when-hydra-active-a)
+    (defun cae-repeat-ignore-when-hydra-active-a ()
+      (bound-and-true-p hydra-curr-map))
 
-  (after! outline
-    (map! :map outline-navigation-repeat-map
-          "RET" #'outline-toggle-children)
-    (put #'outline-toggle-children 'repeat-map 'outline-navigation-repeat-map))
+    (advice-add #'repeat-post-hook :before-until
+                #'cae-repeat-ignore-when-hydra-active-a)
 
-  (autoload 'embark-verbose-indicator "embark")
-  (autoload 'which-key--create-buffer-and-show "which-key")
+    (after! outline
+      (map! :map outline-navigation-repeat-map
+            "RET" #'outline-toggle-children)
+      (put #'outline-toggle-children 'repeat-map 'outline-navigation-repeat-map))
 
-  ;; This is so that my repeat maps are reloaded when I save this file.
-  (when cae-config-finished-loading
-    (ignore-errors (repeat-mode -1))
-    (ignore-errors (repeat-mode 1))))
+    (autoload 'embark-verbose-indicator "embark")
+    (autoload 'which-key--create-buffer-and-show "which-key")
+
+    ;; This is so that my repeat maps are reloaded when I save this file.
+    (when cae-config-finished-loading
+      (ignore-errors (repeat-mode -1))
+      (ignore-errors (repeat-mode 1))))
