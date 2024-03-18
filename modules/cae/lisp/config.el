135551764c64fc33ece19accd00b537952a46356
StrawberryTea <look@strawberrytea.xyz>
2024-03-17 19:27:38 -0500

    modules/cae/lisp/config.el

--- modules/cae/lisp/config.el
+++ modules/cae/lisp/config.el
@@ -37,10 +37,9 @@
   (map! :map lispy-mode-map
         [remap lispy-newline-and-indent-plain] #'cae-lisp-newline-and-indent))
 
-;; Use Emacs Lisp mode for dir-locals files. This will probably make them slower
-;; to load but it will make them easier to edit.
-;;(add-to-list 'auto-mode-alist (cons (regexp-quote dir-locals-file)
-;;                                    'emacs-lisp-mode))
+;; Use Emacs Lisp mode for dir-locals files. This will make them easier to edit.
+(add-to-list 'auto-mode-alist (cons (regexp-quote dir-locals-file)
+                                    'emacs-lisp-mode))
 
 ;; Use my own Imenu expression instead of Doom's. For mine, I do not count
 ;; comments like ";; This code does ..." as sections.
