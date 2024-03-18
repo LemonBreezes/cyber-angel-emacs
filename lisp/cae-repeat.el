a5ac8cecd16e049d0e248f89164ee69cd07822f2
StrawberryTea <look@strawberrytea.xyz>
2024-03-17 23:59:43 -0500

    lisp/cae-repeat.el

--- lisp/cae-repeat.el
+++ lisp/cae-repeat.el
@@ -39,8 +39,8 @@
        "8" +workspace/switch-to-7
        "9" +workspace/switch-to-8
        "[" +workspace/switch-left
-       "]" +workspace/switch-right)
-      (:exit "`" +workspace/other))
+       "]" +workspace/switch-right
+       "`" +workspace/other))
    t)
 
   (define-repeat-map winum
