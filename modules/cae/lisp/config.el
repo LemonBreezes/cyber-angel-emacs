34330145505f12c8374594b98af38a32072be9dd
StrawberryTea <look@strawberrytea.xyz>
2024-03-17 19:26:49 -0500

    modules/cae/lisp/config.el

--- modules/cae/lisp/config.el
+++ modules/cae/lisp/config.el
@@ -37,7 +37,8 @@
   (map! :map lispy-mode-map
         [remap lispy-newline-and-indent-plain] #'cae-lisp-newline-and-indent))
 
-;; Use Emacs Lisp mode for dir-locals files.
+;; Use Emacs Lisp mode for dir-locals files. This will probably make them slower
+;; to load but it will make them easier to edit.
 (add-to-list 'auto-mode-alist (cons (regexp-quote dir-locals-file)
                                     'emacs-lisp-mode))
 
