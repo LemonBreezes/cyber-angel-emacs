e9f81e770082a164989515606674bd03bcf736bb
StrawberryTea <look@strawberrytea.xyz>
2024-03-21 17:06:10 -0500

    autoload/cae-corfu.el

--- autoload/cae-corfu.el
+++ autoload/cae-corfu.el
@@ -25,15 +25,6 @@
            (popon-live-p corfu-terminal--popon))))
 
 ;;;###autoload
-(defun corfu-send-shell (&rest _)
-  "Send completion candidate when inside comint/eshell."
-  (cond
-   ((and (derived-mode-p 'eshell-mode) (fboundp 'eshell-send-input))
-    (eshell-send-input))
-   ((and (derived-mode-p 'comint-mode)  (fboundp 'comint-send-input))
-    (comint-send-input))))
-
-;;;###autoload
 (defun cae-cape-lsp ()
   (interactive)
   (cond ((and (modulep! :tools lsp)
