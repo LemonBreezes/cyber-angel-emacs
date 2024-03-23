bade938e43fc501d27b6987495884b1a35068690
StrawberryTea <look@strawberrytea.xyz>
2024-03-23 10:57:46 -0500

    init.el

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
 
 ;; I added these to help with debugging my config. It's easier to toggle these
 ;; than to comment out large sections of my config.
-(defvar cae-exwm-disabled-p nil)
+(defvar cae-exwm-disabled-p t)
 (defvar cae-init-core-enabled-p t)
 (let ((p t))
   (defvar cae-init-ui-enabled-p (and p t))
