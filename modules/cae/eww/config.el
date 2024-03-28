5f001dadd8634e94d1bc07e0f2aacd2f5979dc05
StrawberryTea <look@strawberrytea.xyz>
2024-03-28 13:02:44 -0500

    .

--- modules/cae/eww/config.el
+++
@@ -1,4 +0,0 @@
-;;; cae/eww/config.el -*- lexical-binding: t; -*-
-
-(after! eww
-  (require 'eww-plz))
--- modules/cae/eww/packages.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; cae/eww/packages.el
-
-(package! eww-plz :recipe (:host github :repo "9viz/eww-plz.el"))
