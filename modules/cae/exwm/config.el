cc1a41a049e49ea4bd901e045e1a974d6dfa3724
StrawberryTea <look@strawberrytea.xyz>
2024-03-31 17:19:37 -0500

    modules/cae/exwm/config.el

--- modules/cae/exwm/config.el
+++ modules/cae/exwm/config.el
@@ -185,9 +185,9 @@
   (use-package! exwm-mff
     :defer t :init (add-hook 'exwm-init-hook #'exwm-mff-mode))
 
-  (use-package! i3bar
-    :when (modulep! +i3bar)
-    :defer t)
+  ;;(use-package! i3bar
+  ;;  :when (modulep! +i3bar)
+  ;;  :defer t)
 
   (when (modulep! :editor evil +everywhere)
     (load! "+evil"))
