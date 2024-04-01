45912c433d5ec6c393feccb7704ad0120daefdee
StrawberryTea <look@strawberrytea.xyz>
2024-04-01 08:37:47 -0500

    modules/cae/misc-applications/autoload/emms.el

--- modules/cae/misc-applications/autoload/emms.el
+++ modules/cae/misc-applications/autoload/emms.el
@@ -39,8 +39,16 @@
      (lambda () (interactive)
        (require 'emms)
        (emms-player-mpd-sync-from-mpd)
+       (let ((file (or (emms-track-get
+                        (emms-playlist-current-selected-track) 'name)
+                       (doom-store-get :last-emms-song)))))
        (dired-jump nil (emms-track-get
                         (emms-playlist-current-selected-track) 'name))))]])
+
+(add-hook! 'kill-emacs-hook
+  (defun +emms-store-last-playing-song ()
+    (doom-store-put :last-emms-song
+                    (emms-track-get (emms-playlist-current-selected-track) 'name))))
 
 ;; The following two functions are from
 ;; https://www.reddit.com/r/emacs/comments/qg2d0k/emms_modeline_shows_full_path_to_the_songs_i_only/
