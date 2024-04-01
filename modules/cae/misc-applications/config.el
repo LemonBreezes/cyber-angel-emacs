ca9aec33756702ef8b8dc75ecba65c00958fa8c7
StrawberryTea <look@strawberrytea.xyz>
2024-04-01 09:25:24 -0500

    modules/cae/misc-applications/config.el

--- modules/cae/misc-applications/config.el
+++ modules/cae/misc-applications/config.el
@@ -791,7 +791,8 @@
     (require 'emms-player-mpd)
     (setq emms-setup-default-player-list '(emms-player-mpd)
           emms-player-list '(emms-player-mpd)
-          emms-info-functions '(emms-info-mpd emms-info-native emms-info-exiftool)))
+          emms-info-functions '(emms-info-mpd emms-info-native emms-info-exiftool))
+    (emms-player-mpd-connect))
   (after! emms-browser
     (map! :map emms-browser-mode-map
           :ng "q" #'+emms-quit
