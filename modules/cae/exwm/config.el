063c0bce73f63ac03036c86ac6f1210f43b9f0f8
StrawberryTea <look@strawberrytea.xyz>
2024-03-31 22:53:07 -0500

    modules/cae/exwm/config.el

--- modules/cae/exwm/config.el
+++ modules/cae/exwm/config.el
@@ -189,8 +189,7 @@
     (cae-when-none-of-these-processes-running
      ("polybar")
      (use-package! i3bar
-       :defer t
-       :config
+       :defer t :config
        (setq! tab-bar-show t
               tab-bar-format '(tab-bar-format-tabs        ; Optional: Remove to _only_ display the bar.
                                tab-bar-format-align-right ; Optional: Remove to align left.
