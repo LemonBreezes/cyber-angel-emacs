89f4cd2f35e81f500589306a9f62066ad1ff1e7e
StrawberryTea <look@strawberrytea.xyz>
2024-04-02 00:52:54 -0500

    vanilla-emacs-configs/mc-test.el

--- vanilla-emacs-configs/mc-test.el
+++ vanilla-emacs-configs/mc-test.el
@@ -1,5 +1,19 @@
 ;;; vanilla-emacs-configs/mc-test.el -*- lexical-binding: t; -*-
 
+;; Bootstrap straight
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+                                    'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (straight-use-package 'multiple-cursors)
 
 (require 'multiple-cursors)
