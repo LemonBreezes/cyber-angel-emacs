bea92f08ab683cba6109b8b19b8596fb8d647c41
StrawberryTea <look@strawberrytea.xyz>
2024-04-05 17:09:35 -0500

    config.el

--- config.el
+++ config.el
@@ -191,7 +191,7 @@
           doom-modeline-major-mode-icon nil
           doom-modeline-minor-modes nil))
 
-  (defvar-keymap doom-leader-GitHub-map)
+  (defvar-keymap doom-leader-GitHub-map) ; Silence byte-compiler.
   (after! which-key
     (setq which-key-ellipsis "..."
           which-key-idle-delay 0.5
