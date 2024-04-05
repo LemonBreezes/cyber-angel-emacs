25f304f71d81fd23b09f3a2fde69d9b6abae2074
StrawberryTea <look@strawberrytea.xyz>
2024-04-05 15:22:11 -0500

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -9,7 +9,8 @@
 (after! cape
   (setq cape-dabbrev-check-other-buffers t))
 (after! corfu
-  (setq corfu-auto-delay 0.05)
+  (setq corfu-auto-delay 0.05
+        corfu-on-exact-match 'quit)
   (after! corfu-quick
     (setq corfu-quick1 (cae-keyboard-kbd "asdfgh")
           corfu-quick2 (cae-keyboard-kbd "jkluionm"))))
