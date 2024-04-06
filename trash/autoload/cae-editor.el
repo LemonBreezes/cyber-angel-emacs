48e9f4aff4797b05f40f0a6643082b1a62b8f749
StrawberryTea <look@strawberrytea.xyz>
2024-04-05 19:07:36 -0500

    trash/autoload/cae-editor.el

---
+++ trash/autoload/cae-editor.el
@@ -0,0 +1,21 @@
+;;; trash/autoload/cae-editor.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun cae-toggle-sudo ()
+  "Toggle sudo access for the current file."
+  (interactive)
+  (let* ((file (or buffer-file-name
+                   (when (or (derived-mode-p 'dired-mode)
+                             (derived-mode-p 'wdired-mode))
+                     default-directory)))
+         (file-localname (file-remote-p file 'localname))
+         (tramp-prefix (and file-localname
+                            (string-remove-suffix file-localname file)))
+         (sudo-prefix (format "/sudo:root@%s:" (file-remote-p file 'host)))
+         (p (point)))
+    (if (string-suffix-p sudo-prefix tramp-prefix)
+        (find-file (concat (string-remove-suffix sudo-prefix tramp-prefix)
+                           (tramp-file-local-name file)))
+      (doom/sudo-this-file))
+    (unless (buffer-modified-p)
+      (goto-char p))))
