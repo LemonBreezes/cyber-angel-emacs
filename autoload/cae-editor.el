74307c26ea1930f1dbf57b4cbd966356a5bd1c0a
StrawberryTea <look@strawberrytea.xyz>
2024-04-05 19:07:20 -0500

    autoload/cae-editor.el

--- autoload/cae-editor.el
+++ autoload/cae-editor.el
@@ -26,26 +26,6 @@
 ;;  (funcall orig-func buffer-or-name))
 ;;
 
-;;;###autoload
-(defun cae-toggle-sudo ()
-  "Toggle sudo access for the current file."
-  (interactive)
-  (let* ((file (or buffer-file-name
-                   (when (or (derived-mode-p 'dired-mode)
-                             (derived-mode-p 'wdired-mode))
-                     default-directory)))
-         (file-localname (file-remote-p file 'localname))
-         (tramp-prefix (and file-localname
-                            (string-remove-suffix file-localname file)))
-         (sudo-prefix (format "/sudo:root@%s:" (file-remote-p file 'host)))
-         (p (point)))
-    (if (string-suffix-p sudo-prefix tramp-prefix)
-        (find-file (concat (string-remove-suffix sudo-prefix tramp-prefix)
-                           (tramp-file-local-name file)))
-      (doom/sudo-this-file))
-    (unless (buffer-modified-p)
-      (goto-char p))))
-
 (defun cae-strip-top-level-indentation (str)
   "Strip the top-level leading indentation for every line in STR.
 The least indented line will have 0 leading whitespace. Convert tabs to spaces
