21cce5f9abb216369885ded0490b78e362e41cd3
StrawberryTea <look@strawberrytea.xyz>
2024-04-07 07:57:37 -0500

    .

---
+++ dir-local-files/home-dir.el
@@ -0,0 +1,18 @@
+((nil
+  . ((eval
+      . (progn
+          ;; Do not render `blamer' hints since we use `git-auto-commit-mode'.
+          (setq-local blamer--block-render-p t)
+
+          ;; Automatically commit saved files to Git and push them to the
+          ;; remote.
+          (when (and (buffer-file-name)
+                     (require 'git-auto-commit-mode nil t)
+                     (require 'vc-git nil t)
+                     (eq (vc-backend (buffer-file-name)) 'Git)
+                     (file-equal-p (vc-git-root (buffer-file-name))
+                                   "~/"))
+            (setq-local gac-automatically-add-new-files-p nil)
+            (setq-local gac-automatically-push-p t)
+            (git-auto-commit-mode 1))
+          )))))
---
+++ modules/cae/haskell/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; cae/haskell/packages.el
+
+(package! consult-hoogle)
