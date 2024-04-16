ba533daebb4dd67b007ce1e5db8f2240be4be49a
StrawberryTea <look@strawberrytea.xyz>
2024-04-16 09:17:37 -0500

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -158,9 +158,3 @@ It is meant to be used as a `post-gc-hook'."
 ;;  (defun cae-catch-buffers-out-of-sync-h ()
 ;;    (unless (eq (current-buffer) (window-buffer))
 ;;      (message "Buffer out of sync: %s" (buffer-name)))))
-
-;; I am currently debugging EXWM.
-(setq exwm-debug t)
-
-;; Debugging another issue.
-(cae-debug-toggle-debugging-function-on-hidden-errors #'url-expand-file-name)
