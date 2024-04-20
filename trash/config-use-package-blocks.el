c3b83f3c7f1525faa8bbc326c0f647b623720000
StrawberryTea <look@strawberrytea.xyz>
2024-04-20 18:45:21 -0500

    trash/config-use-package-blocks.el

--- trash/config-use-package-blocks.el
+++ trash/config-use-package-blocks.el
@@ -112,7 +112,10 @@
          (lambda (keyword)
            (cl-destructuring-bind (regexp . face) keyword
              (string= "'[^']*'" regexp)))
-         syslog-font-lock-keywords)))
+         syslog-font-lock-keywords)
+        ;; I don't use syslog notes.
+        syslog-note-thing #'ignore))
+(advice-add #'syslog-load-notes :override #'ignore)
 (add-hook 'syslog-mode-hook #'cae-apply-ansi-color-to-buffer-h)
 (add-to-list 'auto-mode-alist '("/var/log.*\\'" . syslog-mode))
 (add-to-list 'auto-mode-alist '("\\.log\\'" . syslog-mode))
