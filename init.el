760913610ca060482a13a0ee24db71d824879b45
StrawberryTea <look@strawberrytea.xyz>
2024-04-21 22:52:06 -0500

    init.el

--- init.el
+++ init.el
@@ -15,14 +15,14 @@
   (after! vterm
     (setq vterm-shell interactive-shell)))
 
-;;(when (and (>= (num-processors) 32)
-;;           (> (car (memory-info))
-;;              (* 180 1024 1024)))
-;;  (let ((cae-gc-cons-threshold (* 128 1024 1024 1024)))
-;;    (setq gcmh-high-cons-threshold cae-gc-cons-threshold
-;;          consult--gc-threshold cae-gc-cons-threshold
-;;          cae-hacks-gc-cons-threshold cae-gc-cons-threshold
-;;          +lsp--default-gcmh-high-cons-threshold cae-gc-cons-threshold)))
+(when (and (>= (num-processors) 32)
+           (> (car (memory-info))
+              (* 180 1024 1024)))
+  (let ((cae-gc-cons-threshold (* 128 1024 1024 1024)))
+    (setq gcmh-high-cons-threshold cae-gc-cons-threshold
+          consult--gc-threshold cae-gc-cons-threshold
+          cae-hacks-gc-cons-threshold cae-gc-cons-threshold
+          +lsp--default-gcmh-high-cons-threshold cae-gc-cons-threshold)))
 (setq doom-incremental-idle-timer 0.25)
 
 (setq native-comp-async-jobs-number (num-processors)
