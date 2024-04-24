f4b1020c7b3087cba1d709b15584f91fb7ba709c
StrawberryTea <look@strawberrytea.xyz>
2024-04-24 16:04:10 -0500

    modules/cae/exwm/packages.el

--- modules/cae/exwm/packages.el
+++ modules/cae/exwm/packages.el
@@ -8,6 +8,3 @@
   (package! exwm-evil
     :recipe (:host github :repo "LemonBreezes/exwm-evil"))
   (package! exwm-firefox-evil))
-(when (modulep! +i3bar)
-  (package! i3bar :recipe
-    (:host github :repo "Stebalien/i3bar.el")))
