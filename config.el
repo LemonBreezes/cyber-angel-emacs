13a0a15fbb03d91e9fb6ae581d759eca5c3c20fc
StrawberryTea <look@strawberrytea.xyz>
2024-04-24 15:40:48 -0500

    config.el

--- config.el
+++ config.el
@@ -431,7 +431,7 @@
      ("polybar" "xmobar" "lemonbar" "dzen2" "i3bar" "i3status")
      (not (display-graphic-p))
      (use-package! i3bar
-       :delay 0.1
+       :defer 0.1 :config
        (setq! tab-bar-format '(tab-bar-format-tabs ; Optional: Remove to _only_ display the bar.
                                tab-bar-format-align-right ; Optional: Remove to align left.
                                tab-bar-format-global)
