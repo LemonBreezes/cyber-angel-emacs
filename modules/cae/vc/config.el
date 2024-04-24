652c8f17ee9d6e513c5925d867333377f2f8660e
StrawberryTea <look@strawberrytea.xyz>
2024-04-24 13:36:42 -0500

    modules/cae/vc/config.el

--- modules/cae/vc/config.el
+++ modules/cae/vc/config.el
@@ -105,16 +105,6 @@
 (use-package! embark-vc
   :after embark)
 
-(use-package! blamer
-  :defer t :init
-  (map! :leader :desc "Blamer" "tB" #'blamer-mode)
-  :config
-  (setq blamer-idle-time 0.3
-        blamer-avatar-folder (concat doom-cache-dir "avatars/"))
-  (setq blamer-author-formatter "%s ")
-  (setq blamer-datetime-formatter "[%s]")
-  (setq blamer-commit-formatter " ● %s"))
-
 (use-package! consult-gh
   :defer t :when (modulep! :completion vertico)
   :commands (consult-gh-search-repos
