a9b72447d08edf6bdbb250dfb8a9d8276b6fa6ea
StrawberryTea <look@strawberrytea.xyz>
2024-04-24 08:07:09 -0500

    autoload/cae-benchmark.el

--- autoload/cae-benchmark.el
+++ autoload/cae-benchmark.el
@@ -1,13 +1,31 @@
 ;;; autoload/cae-benchmark.el -*- lexical-binding: t; -*-
 
+(defun cae-average-line-length-excluding-empty-lines ()
+  "Compute the average line length in the current buffer, excluding empty lines."
+  (interactive)
+  (let (res)
+    (save-excursion
+      (goto-char (point-min))
+      (let ((total-length 0)
+            (line-count 0))
+        (while (not (eobp))             ; Iterate until the end of the buffer
+          (unless (looking-at "^$")     ; Check if the line is empty
+            (setq total-length (+ total-length (- (line-end-position) (line-beginning-position))))
+            (setq line-count (1+ line-count)))
+          (forward-line 1))             ; Move to the next line
+        (setq res (if (eq line-count 0) 0 (/ (float total-length) line-count)))))
+    (if (called-interactively-p)
+        (message "Average line length (excluding empty lines) is %.02f" res)
+      res)))
+
 (defun cae-compute-optimal-jit-lock-chunk-size ()
   "Calculate an estimated optimal value for `jit-lock-chunk-size' based on window dimensions."
   (save-excursion
     (goto-char (window-start))
     ;; Calculate optimal chunk size: window height * average line length (with spacing)
     ;; Then, increase the estimated size by 10% to provide a buffer.
-    (floor (* 1.1 (window-height)
-              (floor (/ (line-end-position) (line-number-at-pos)))
+    (floor (* (window-height)
+              (cae-average-line-length-excluding-empty-lines)
               (1+ (or line-spacing 0))))))
 
 ;;;###autoload
