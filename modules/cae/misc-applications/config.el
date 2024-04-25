7f22aedb189324964ee8ef06369799407102ba2a
StrawberryTea <look@strawberrytea.xyz>
2024-04-24 20:32:43 -0500

    modules/cae/misc-applications/config.el

--- modules/cae/misc-applications/config.el
+++ modules/cae/misc-applications/config.el
@@ -97,59 +97,6 @@
         (expand-file-name "alarm-clock.cache" doom-cache-dir))
   (alarm-clock--turn-autosave-on))
 
-(use-package! elfeed
-  :when (modulep! :app rss)
-  :defer t :init
-  (map! :map +misc-applications-random-map
-        "r" #'=rss)
-  (after! which-key
-    (which-key-add-keymap-based-replacements +misc-applications-random-map
-      "r" "RSS"))
-  :config
-  (setq elfeed-search-title-max-width 100)
-  (defun cae-elfeed-set-filter (tag period)
-    (lambda ()
-      (interactive)
-      (elfeed-search-set-filter (concat "@" period "-ago" (if tag (concat " +" tag) "")))))
-  (let ((custom-filters
-         `(("R" ,(cae-elfeed-set-filter "reddit" "6-months") "Reddit feeds" :column "Custom filters")
-           ("E" ,(cae-elfeed-set-filter "emacs" "6-months") "Emacs feeds" :column "Custom filters")
-           ("Y" ,(cae-elfeed-set-filter "tube" "6-months") "Youtube feeds" :column "Custom filters")
-           ("*" ,(cae-elfeed-set-filter "star" "6-months") "Starred feeds" :column "Custom filters")
-           ("a" ,(cae-elfeed-set-filter "" "6-months") "All feeds" :column "Custom filters")
-           ("T" ,(cae-elfeed-set-filter "" "1-day") "Today's feeds" :column "Custom filters"))))
-    (cl-loop for (key filter desc . rest) in custom-filters do
-             (after! elfeed
-               (map! :map elfeed-search-mode-map
-                     :desc desc :ng key filter))))
-  (after! recentf
-    (push elfeed-db-directory recentf-exclude))
-  (map! :map elfeed-show-mode-map
-        :ng "o" #'link-hint-open-link
-        :map elfeed-search-mode-map
-        [remap elfeed-search-quit-window] #'+elfeed-quit
-        :n "b" #'elfeed-search-browse-url
-        :n "m" #'elfeed-toggle-star
-        :n "F" #'elfeed-tube-fetch
-        :n "l" #'+elfeed-toggle-log-buffer
-        :ng "t" #'mark-whole-buffer)
-
-  (use-package! elfeed-tube
-    :config
-    (elfeed-tube-setup)
-    (map! :map elfeed-show-mode-map
-          "F" #'elfeed-tube-fetch
-          [remap save-buffer] #'elfeed-tube-save
-          :map elfeed-search-mode-map
-          "F" #'elfeed-tube-fetch
-          [remap save-buffer] #'elfeed-tube-save)
-    (use-package elfeed-tube-mpv
-      :when (executable-find "mpv")
-      :config
-      (map! :map elfeed-show-mode-map
-            "C-c C-f" #'elfeed-tube-mpv-follow-mode
-            "C-c C-w" #'elfeed-tube-mpv-where))))
-
 (use-package! pomm
   :defer t :init
   (map! :map +misc-applications-random-map
