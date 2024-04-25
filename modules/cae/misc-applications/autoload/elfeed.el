1fb94ca9aa70ece5937857ae1ba8b03c30fa3afc
StrawberryTea <look@strawberrytea.xyz>
2024-04-24 20:33:59 -0500

    modules/cae/misc-applications/autoload/elfeed.el

--- modules/cae/misc-applications/autoload/elfeed.el
+++ modules/cae/misc-applications/autoload/elfeed.el
@@ -1,19 +1,2 @@
 ;;; private/misc-applications/autoload/elfeed.el -*- lexical-binding: t; -*-
 
-;;;###autoload
-(defun +elfeed-quit ()
-  (interactive)
-  (elfeed-db-save)
-  (+workspace/delete +rss-workspace-name)
-  (when (buffer-live-p elfeed-log-buffer-name)
-    (kill-buffer elfeed-log-buffer-name)))
-
-;;;###autoload
-(defun +elfeed-toggle-log-buffer ()
-  (interactive)
-  (if (get-buffer-window elfeed-log-buffer-name)
-      (delete-window (get-buffer-window elfeed-log-buffer-name))
-    (pop-to-buffer elfeed-log-buffer-name)
-    (local-set-key (kbd "q") #'quit-window)
-    (when (featurep 'evil)
-      (evil-local-set-key 'normal (kbd "q") #'quit-window))))
