6b7241811589fe72de831c07225908eec71102b4
StrawberryTea <look@strawberrytea.xyz>
2024-04-25 20:18:40 -0500

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -11,9 +11,9 @@
 (defadvice! cae-run-theme-hook-h (_)
   :after #'consult-theme
   (run-hooks 'doom-load-theme-hook))
-;;(add-hook! 'circadian-after-load-theme-hook
-;;  (defun cae-run-theme-hook-h (_)
-;;    (run-hooks 'doom-load-theme-hook)))
+(add-hook! 'circadian-after-load-theme-hook
+  (defun cae-run-theme-hook-h (_)
+    (run-hooks 'doom-load-theme-hook)))
 
 ;; Make Org headlines respect the heading backgrounds.
 (when cae-theme-extend-heading-faces
