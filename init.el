af05450ec0f5fc23709b724061af621be4ab3f61
StrawberryTea <look@strawberrytea.xyz>
2024-04-30 11:16:33 -0500

    init.el

--- init.el
+++ init.el
@@ -141,7 +141,7 @@
        (rss +org)
 
        :cae
-                                        ;helm ;Currently broken on Emacs30.
+       ;;helm ;Currently broken on Emacs30.
        debugger
        eshell
        exwm
