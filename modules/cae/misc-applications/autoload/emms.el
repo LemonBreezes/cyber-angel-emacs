39512261f050a6f8df231d3da95fd34005c283a9
StrawberryTea <look@strawberrytea.xyz>
2024-05-01 13:00:59 -0500

    modules/cae/misc-applications/autoload/emms.el

--- modules/cae/misc-applications/autoload/emms.el
+++ modules/cae/misc-applications/autoload/emms.el
@@ -44,7 +44,6 @@
        (dired (expand-file-name "Artists" emms-source-file-default-directory))))
     ("l" "Longplays"
      (lambda () (interactive)
-       t
        (require 'emms)
        (dired (expand-file-name "Longplays" emms-source-file-default-directory))))
     ("j" "Currently playing"
