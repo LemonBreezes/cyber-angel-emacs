4b5266575678285e803068569742d2258189cf2d
StrawberryTea <look@strawberrytea.xyz>
2024-05-09 23:28:35 -0500

    modules/cae/misc-applications/config.el

--- modules/cae/misc-applications/config.el
+++ modules/cae/misc-applications/config.el
@@ -658,10 +658,10 @@
         empv-video-dir +misc-applications-videos-dir
         empv-playlist-dir +misc-applications-music-dir)
   (aio-defun cae-empv-set-invidious-instance ()
-             (setq empv-invidious-instance
-                   (concat "https://"
-                           (aio-await (elfeed-tube--get-invidious-url))
-                           "/api/v1")))
+    (setq empv-invidious-instance
+          (concat "https://"
+                  (aio-await (elfeed-tube--get-invidious-url))
+                  "/api/v1")))
   (cae-empv-set-invidious-instance)
   (after! embark
     (empv-embark-initialize-extra-actions)))
