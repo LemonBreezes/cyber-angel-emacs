7ef1d17f804509b47fb64f772cf29aac08881d87
StrawberryTea <look@strawberrytea.xyz>
2024-05-10 18:14:43 -0500

    init.el

--- init.el
+++ init.el
@@ -47,8 +47,8 @@
   (defvar cae-init-autocompletion-enabled-p (and p t))
   (defvar cae-init-text-enabled-p (and p t))
   (defvar cae-init-email-enabled-p (and p t))
-  (defvar cae-init-term-enabled-p (and p nil))
-  (defvar cae-init-languages-enabled-p (and p nil)))
+  (defvar cae-init-term-enabled-p (and p t))
+  (defvar cae-init-languages-enabled-p (and p t)))
 
 (setq evil-undo-system 'undo-fu)
 
