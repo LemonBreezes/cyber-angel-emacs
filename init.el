db9f4c4aa71d5ee30a53b1d992844d2b342c09c6
StrawberryTea <look@strawberrytea.xyz>
2024-05-10 21:49:28 -0500

    init.el

--- init.el
+++ init.el
@@ -40,9 +40,9 @@
 ;; than to comment out large sections of my config.
 (defvar cae-init-core-enabled-p t)
 (defvar cae-keyboard-remaps-enabled-p t)
-(let ((p nil))
+(let ((p t))
   (defvar cae-init-ui-enabled-p (and p t))
-  (defvar cae-init-tools-enabled-p (and p nil))
+  (defvar cae-init-tools-enabled-p (and p t))
   (defvar cae-init-editor-enabled-p (and p t))
   (defvar cae-init-autocompletion-enabled-p (and p t))
   (defvar cae-init-text-enabled-p (and p t))
