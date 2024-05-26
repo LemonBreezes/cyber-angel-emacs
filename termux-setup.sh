3c6f90f61c7da47325e793901aed01be3c4173e0
StrawberryTea <look@strawberrytea.xyz>
2024-05-26 04:27:12 -0500

    termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
 pkg install python lzop pigz plzip unrar zip tree python fd ripgrep gdb gh openssh git wakatime-cli termux-api nodejs poppler ffmpeg libvterm cmake aspell aspell-en autoconf direnv
+# unless emacs/files is symbolically linked to termux home, do that
