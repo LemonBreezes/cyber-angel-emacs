22674c981231e6d371010da1f004b8a4dbe28bba
StrawberryTea <look@strawberrytea.xyz>
2024-05-26 04:05:54 -0500

    init.el

--- init.el
+++ init.el
@@ -1,5 +1,10 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+(when (string-equal system-type "android")  ;; Add Termux binaries to PATH environment
+  (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
+    (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
+    (setq exec-path (append exec-path (list termuxpath)))))
+
 ;; This is so that I don't accidentally start Emacs as a daemon.
 (when (daemonp) (kill-emacs))
 
