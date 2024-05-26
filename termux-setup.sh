88c8f6a71b12da9cbea4c0bee27576a5880396a3
StrawberryTea <look@strawberrytea.xyz>
2024-05-26 03:56:18 -0500

    termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-pkg install python lzop pigz plzip unrar zip tree python fd ripgrep gdb gh openssh git wakatime-cli termux-api nodejs
+pkg install python lzop pigz plzip unrar zip tree python fd ripgrep gdb gh openssh git wakatime-cli termux-api nodejs poppler ffmpeg libvterm cmake
