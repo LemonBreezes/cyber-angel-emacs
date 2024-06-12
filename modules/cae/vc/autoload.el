998fcfb56f6b0d1b71e6571daec0223e790a1149
StrawberryTea <look@strawberrytea.xyz>
2024-06-12 00:00:38 -0500

    modules/cae/vc/autoload.el

--- modules/cae/vc/autoload.el
+++ modules/cae/vc/autoload.el
@@ -40,27 +40,24 @@ Meant to be used like:
 
 ;;;###autoload
 (defun cae-magit-add-PR-fetch-ref (&optional remote-name)
-  "If refs/pull is not defined on a GH repo, define it.
+  "If refs/pull is not defined on a GH repo, define it for all remotes.
 
-If REMOTE-NAME is not specified, it defaults to the `remote' set
-for the \"main\" or \"master\" branch."
-  (let* ((remote-name (or remote-name
-                          (magit-get "branch" "upstream" "remote")
-                          (magit-get "branch" "main" "remote")
-                          (magit-get "branch" "master" "remote")))
-         (remote-url (magit-get "remote" remote-name "url"))
-         (fetch-refs (and (stringp remote-url)
-                          (string-match "github" remote-url)
-                          (magit-get-all "remote" remote-name "fetch")))
-         ;; https://oremacs.com/2015/03/11/git-tricks/
-         ;; TODO Make this apply to all remotes and work for GitLab as well.
-         (fetch-address (format "+refs/pull/*/head:refs/remotes/%s/pr/*" remote-name)))
-    (when fetch-refs
-      (unless (member fetch-address fetch-refs)
-        (magit-git-string "config"
-                          "--add"
-                          (format "remote.%s.fetch" remote-name)
-                          fetch-address)))))
+If REMOTE-NAME is specified, it targets only that remote."
+  (let ((remotes (if remote-name
+                     (list remote-name)
+                   (magit-list-remotes))))  ;; Get a list of all remote names
+    (dolist (remote remotes)
+      (let* ((remote-url (magit-get "remote" remote "url"))
+             (fetch-refs (and (stringp remote-url)
+                              (string-match "github" remote-url)
+                              (magit-get-all "remote" remote "fetch")))
+             (fetch-address (format "+refs/pull/*/head:refs/remotes/%s/pr/*" remote)))
+        (when fetch-refs
+          (unless (member fetch-address fetch-refs)
+            (magit-git-string "config"
+                              "--add"
+                              (format "remote.%s.fetch" remote)
+                              fetch-address)))))))
 
 ;;;###autoload
 (defun consult-gh-fork-current-repo (&optional name remote remote-name)
