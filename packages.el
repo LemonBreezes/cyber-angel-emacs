ae00316bbdb6103687f3b4229a52133c02f9d745
StrawberryTea <look@strawberrytea.xyz>
2024-06-21 11:54:59 -0500

    packages.el

--- packages.el
+++ packages.el
@@ -64,7 +64,7 @@
 (package! indent-bars :recipe (:host github :repo "jdtsmith/indent-bars"))
 (package! backline)
 (package! xterm-color)
-(package! casual)
+;; (package! casual)
 
 ;; cae-theme.el
 (package! modus-themes)
