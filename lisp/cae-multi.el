2da2d3100cb1781e7047a240ec22ae0917673dd0
StrawberryTea <look@strawberrytea.xyz>
2024-06-28 23:48:04 -0500

    lisp/cae-multi.el

--- lisp/cae-multi.el
+++ lisp/cae-multi.el
@@ -35,13 +35,12 @@
 (after! calendar
   (setq diary-file (concat cae-multi-secrets-dir "diary")))
 
+(setq gac-automatically-add-new-files-p nil
+      gac-automatically-push-p t)
 (use-package! git-auto-commit-mode
   :defer t :init
   (autoload 'gac--after-save "git-auto-commit-mode")
   :config
-  (setq gac-automatically-add-new-files-p nil
-        gac-automatically-push-p t
-        gac-debounce-interval 1.0)
   (setq-hook! 'git-auto-commit-mode-hook
     backup-inhibited t)
 
