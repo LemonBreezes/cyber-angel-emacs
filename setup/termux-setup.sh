abde60acff4a28a2956377caeb7af409ebb986ba
StrawberryTea <look@strawberrytea.xyz>
2024-06-28 22:20:02 -0500

    setup/termux-setup.sh

--- setup/termux-setup.sh
+++ setup/termux-setup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
 pkg install python lzop pigz plzip unrar zip tree python fd ripgrep-all gdb gh openssh git wakatime-cli termux-api nodejs poppler ffmpeg libvterm cmake aspell aspell-en autoconf direnv emacs git mu isync texlive-bin
+
+# Directory where the org directory will be linked
+if [ -e ~/org ]; then
+  echo "Error: ~/org already exists."
+else
+  ln -s /storage/emulated/0/org ~/org
+  echo "Symbolic link created: ~/org -> /storage/emulated/0/org"
+fi
+
 # unless emacs/files is symbolically linked to termux home, do that
 # The TSC dynamic module is being obtained compiled for X86_64 instead of AARCH64 which is what my Pixel phone supports.
