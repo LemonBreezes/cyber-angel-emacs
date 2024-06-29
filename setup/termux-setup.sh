a819a52abe94230e32dc138fff85f6b137788e0d
StrawberryTea <look@strawberrytea.xyz>
2024-06-28 22:45:33 -0500

    setup/termux-setup.sh

--- setup/termux-setup.sh
+++ setup/termux-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-pkg install python lzop pigz plzip unrar zip tree python fd ripgrep-all gdb gh openssh git wakatime-cli termux-api nodejs poppler ffmpeg libvterm cmake aspell aspell-en autoconf direnv emacs git mu isync texlive-bin automake
+pkg install python lzop pigz plzip unrar zip tree python fd ripgrep-all gdb gh openssh git wakatime-cli termux-api nodejs poppler ffmpeg libvterm cmake aspell aspell-en autoconf direnv emacs git mu isync texlive-bin automake build-essential
 
 # Directory where the org directory will be linked
 if [ -e ~/org ]; then
