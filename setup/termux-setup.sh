5f2fcb479e6fd245cf08e859ead488dced070173
StrawberryTea <look@strawberrytea.xyz>
2024-07-01 11:15:33 -0500

    setup/termux-setup.sh

--- setup/termux-setup.sh
+++ setup/termux-setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-pkg install python lzop pigz plzip unrar zip tree python fd ripgrep-all gdb gh openssh git wakatime-cli termux-api nodejs poppler ffmpeg libvterm cmake aspell aspell-en autoconf direnv emacs git mu isync texlive-bin automake build-essential x11-repo termux-x11-nightly
+pkg install python lzop pigz plzip unrar zip tree python fd ripgrep-all gdb gh openssh git wakatime-cli termux-api nodejs poppler ffmpeg libvterm cmake aspell aspell-en autoconf direnv emacs git mu isync texlive-bin automake build-essential x11-repo
+pkg install termux-x11-nightly
 
 # unless emacs/files is symbolically linked to termux home, do that
 # The TSC dynamic module is being obtained compiled for X86_64 instead of AARCH64 which is what my Pixel phone supports.
