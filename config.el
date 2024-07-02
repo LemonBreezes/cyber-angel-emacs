89dc093537aa0a14ad0364b378eb346f826b1da0
StrawberryTea <look@strawberrytea.xyz>
2024-07-01 19:32:21 -0500

    .

--- config.el
+++ config.el
@@ -25,6 +25,7 @@
 
   (load! "lisp/cae-tty")
   (load! "lisp/cae-bindings")
+  (load! "lisp/cae-multi")              ;Run parallel Emacs instances.
   (load! "lisp/cae-smartparens")        ;Allow Smartparens to be disabled. This
                                         ;is also our Smartparens configuration.
   (load! "lisp/cae-projectile")         ;Allow Projectile to be disabled. This
