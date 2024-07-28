464579d576e037a872682e33e694af945f3aec76
StrawberryTea <look@strawberrytea.xyz>
2024-07-27 20:37:20 -0500

    init.el

--- init.el
+++ init.el
@@ -2,10 +2,10 @@
 
 ;; I added these to help with debugging my config. It's easier to toggle these
 ;; than to comment out large sections of my config.
-(defvar cae-init-preamble-enabled-p nil)
-(defvar cae-init-core-enabled-p nil)
-(defvar cae-keyboard-remaps-enabled-p nil)
-(let ((p nil))
+(defvar cae-init-preamble-enabled-p t)
+(defvar cae-init-core-enabled-p t)
+(defvar cae-keyboard-remaps-enabled-p t)
+(let ((p t))
   (defvar cae-init-ui-enabled-p (and p t))
   (defvar cae-init-tools-enabled-p (and p t))
   (defvar cae-init-editor-enabled-p (and p t))
