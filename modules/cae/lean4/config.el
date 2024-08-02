f7e5904f6de56d9c01e1c784258a25827dec7d6b
StrawberryTea <look@strawberrytea.xyz>
2024-08-02 11:52:52 -0500

    modules/cae/lean4/config.el

--- modules/cae/lean4/config.el
+++ modules/cae/lean4/config.el
@@ -5,8 +5,8 @@
     (sp-local-pair "/-" "-/")
     (sp-local-pair "`" "`")
     (sp-local-pair "{" nil
-             :post-handlers '(("||\n[i]" "RET") ("| " "SPC"))
-             :unless '(sp-point-before-word-p sp-point-before-same-p))
+                   :post-handlers '(("||\n[i]" "RET") ("| " "SPC"))
+                   :unless '(sp-point-before-word-p sp-point-before-same-p))
     (sp-local-pair "«" "»")
     (sp-local-pair "⟨" "⟩")
     (sp-local-pair "⟨ " " ⟩")
