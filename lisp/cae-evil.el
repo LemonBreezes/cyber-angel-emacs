66526f2f3cce3ff4596f5610259f7b826acdd035
StrawberryTea <look@strawberrytea.xyz>
2024-08-03 15:07:53 -0500

    lisp/cae-evil.el

--- lisp/cae-evil.el
+++ lisp/cae-evil.el
@@ -1,6 +1,7 @@
 ;;; lisp/cae-evil.el -*- lexical-binding: t; -*-
 
 (setq +evil-want-move-window-to-wrap-around t)
+(setq evil-want-C-w-delete nil)
 
 ;; I use a split keyboard and map backspace to my left thumb key.
 ;;(lookup-key evil-normal-state-map doom-localleader-key)
