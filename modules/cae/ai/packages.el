a392efd2c89337715b73c867b6e0f9c3950a5afe
StrawberryTea <look@strawberrytea.xyz>
2024-08-25 06:14:03 -0500

    .

--- modules/cae/ai/packages.el
+++ modules/cae/ai/packages.el
@@ -6,6 +6,6 @@
 (package! whisper :recipe (:host github :repo "natrys/whisper.el"))
 (package! chatgpt-shell)
 (package! dall-e-shell)
-(package! copilot)
+(package! copilot :recipe (:host github :repo "copilot-emacs/copilot.el"))
 (package! gptel)
 (package! magit-gptcommit)
