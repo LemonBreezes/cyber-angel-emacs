a69b73ed704a04784bb9cca9f752f1e840d45ccd
StrawberryTea <look@strawberrytea.xyz>
2024-08-28 16:24:33 -0400

    trash/cae-evil.el

--- trash/cae-evil.el
+++ trash/cae-evil.el
@@ -7,3 +7,19 @@
           "h" #'evil-i-syntax
           :map evil-outer-text-objects-map
           "h" #'evil-a-syntax)))
+
+(use-package! harpoon
+  :defer t :init
+  (map! :leader
+        "1" #'harpoon-go-to-1
+        "2" #'harpoon-go-to-2
+        "3" #'harpoon-go-to-3
+        "4" #'harpoon-go-to-4
+        "5" #'harpoon-go-to-5
+        "6" #'harpoon-go-to-6
+        "7" #'harpoon-go-to-7
+        "8" #'harpoon-go-to-8
+        "9" #'harpoon-go-to-9)
+  (after! which-key
+    (add-to-list 'which-key-replacement-alist
+                 '(("" . "harpoon-go-to-[0-9]+") . ignore))))
