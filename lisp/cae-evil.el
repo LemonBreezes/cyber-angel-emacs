95e41184734959499a8319a5f912e705245a706c
StrawberryTea <look@strawberrytea.xyz>
2024-08-28 16:24:25 -0400

    lisp/cae-evil.el

--- lisp/cae-evil.el
+++ lisp/cae-evil.el
@@ -235,22 +235,6 @@
 (use-package! evil-owl
   :hook (doom-first-input . evil-owl-mode))
 
-(use-package! harpoon
-  :defer t :init
-  (map! :leader
-        "1" #'harpoon-go-to-1
-        "2" #'harpoon-go-to-2
-        "3" #'harpoon-go-to-3
-        "4" #'harpoon-go-to-4
-        "5" #'harpoon-go-to-5
-        "6" #'harpoon-go-to-6
-        "7" #'harpoon-go-to-7
-        "8" #'harpoon-go-to-8
-        "9" #'harpoon-go-to-9)
-  (after! which-key
-    (add-to-list 'which-key-replacement-alist
-                 '(("" . "harpoon-go-to-[0-9]+") . ignore))))
-
 (after! evil-snipe
   (cl-pushnew #'calendar-mode evil-snipe-disabled-modes))
 
