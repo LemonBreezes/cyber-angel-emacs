fe94fbd0ca0df285668a1a505326d7639d2108e9
StrawberryTea <look@strawberrytea.xyz>
2024-09-03 11:00:30 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -80,10 +80,10 @@
 (package! crontab-mode)
 (package! inputrc-mode)
 (package! ebuild-mode :built-in t)
-(package! tokei) ;I use this for counting lines of code in
-()               ;my projects.
-(package! font-lock-studio) ;I don't use this often, but it's useful
-()                          ;for debugging font-lock issues.
+(package! tokei)                        ;I use this for counting lines of code in
+                                        ;my projects.
+(package! font-lock-studio)             ;I don't use this often, but it's useful
+                                        ;for debugging font-lock issues.
 (package! show-font :recipe ;Preview fonts
   (:host github :repo "protesilaos/show-font"))
 (package! eff)
