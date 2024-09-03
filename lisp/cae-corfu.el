20487b2a4c9b46d2c5721cfb47b31d2048399e18
StrawberryTea <look@strawberrytea.xyz>
2024-09-02 22:23:20 -0400

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -15,6 +15,13 @@
     (setq corfu-quick1 (cae-keyboard-kbd "asdfgh")
           corfu-quick2 (cae-keyboard-kbd "jkluionm"))))
 
+
+(remove-hook 'minibuffer-setup-hook #'+corfu-add-cape-dabbrev-h)
+(add-hook! minibuffer-setup
+  (defun +corfu-add-cape-dabbrev-for-minibuffer-h ()
+    (when (+corfu-enable-in-minibuffer-p)
+      (add-hook 'completion-at-point-functions #'cape-dabbrev 40 t))))
+
 ;; Wildcard separator
 (defvar +orderless-wildcard-character ?,
   "A character used as a wildcard in Corfu for fuzzy autocompletion. If you
