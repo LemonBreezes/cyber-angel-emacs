b42fd4ba1d9d9c7b8c5f9f919cf2221a5e86712c
StrawberryTea <look@strawberrytea.xyz>
2024-09-03 11:00:35 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -80,8 +80,8 @@
 (package! crontab-mode)
 (package! inputrc-mode)
 (package! ebuild-mode :built-in t)
-(package! tokei)                        ;I use this for counting lines of code in
-                                        ;my projects.
+(package! tokei)                        ;I use this for counting lines of code
+                                        ;in my projects.
 (package! font-lock-studio)             ;I don't use this often, but it's useful
                                         ;for debugging font-lock issues.
 (package! show-font :recipe ;Preview fonts
