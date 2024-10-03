7bca50c9b4aac2af10547942d15e4642d0b16448
StrawberryTea <look@strawberrytea.xyz>
2024-10-02 23:34:06 -0500

    modules/cae/eshell/config.el

--- modules/cae/eshell/config.el
+++ modules/cae/eshell/config.el
@@ -71,11 +71,6 @@
     (setq epe-show-local-working-directory t
           epe-show-git-status-extended t)))
 
-(when (modulep! :lang python)
-  (after! esh-opt
-    (when (require 'virtualenvwrapper nil t)
-      (venv-initialize-eshell))))
-
 ;; Don't leave me with unbalanced delimiters.
 (defadvice! cae-eshell-kill-input-with-delimiters-a ()
   :after #'eshell-kill-input
