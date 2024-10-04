df8234e101cc8321caeba6dfc2a1d03314368cc9
StrawberryTea <look@strawberrytea.xyz>
2024-10-04 00:49:31 -0500

    modules/cae/ai/packages.el

--- modules/cae/ai/packages.el
+++ modules/cae/ai/packages.el
@@ -1,11 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/ai/packages.el
 
-(package! org-ai)
-(package! greader)
-(package! whisper :recipe (:host github :repo "natrys/whisper.el"))
-(package! chatgpt-shell)
-(package! dall-e-shell)
 (package! copilot :recipe (:host github :repo "copilot-emacs/copilot.el"))
 (package! gptel)
 (package! magit-gptcommit)
