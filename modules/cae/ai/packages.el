5a80022eafcdc033cc0fd2d68bce8777416a84d0
StrawberryTea <look@strawberrytea.xyz>
2024-10-07 22:02:40 -0400

    modules/cae/ai/packages.el

--- modules/cae/ai/packages.el
+++ modules/cae/ai/packages.el
@@ -4,6 +4,7 @@
 (package! copilot :recipe (:host github :repo "copilot-emacs/copilot.el"))
 (package! gptel)
 (package! elysium :recipe (:host github :repo "lanceberge/elysium"))
+(package! aider :recipe (:host github :repo "tninja/aider.el"))
 (package! magit-gptcommit)
 (package! chatgpt-shell)
 (package! dall-e-shell)
