92bcceeafdf2b116cfac9a288ca3576099ee7e1e
StrawberryTea <look@strawberrytea.xyz>
2024-10-07 20:01:00 -0400

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -104,8 +104,7 @@
       "<escape>" #'keyboard-quit
       (:when (modulep! :cae ai)
        "<f2>" #'gptel-send
-       "<f5>" #'elysium-query
-       )
+       "<f5>" #'elysium-query)
       "<f6>" #'embrace-commander
       "<f7>" #'eri/expand-region
       "C-<f1>" #'try
