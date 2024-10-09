5838d4331a373ef0a05f8b10daf2efb6de13c051
StrawberryTea <look@strawberrytea.xyz>
2024-10-09 19:47:33 -0400

    modules/cae/ai/config.el

--- modules/cae/ai/config.el
+++ modules/cae/ai/config.el
@@ -18,7 +18,7 @@
 
 (use-package! aider
   :defer t :config
-  (setq aider-args `("--model" "--o1-preview")))
+  (setq aider-args '("--model" "--o1-preview")))
 
 (use-package! elysium
   :defer t :autoload (elysium-query)
