5c67fed6a370b016141a26703a23d514d47fe3bb
StrawberryTea <look@strawberrytea.xyz>
2024-10-09 20:02:40 -0400

    modules/cae/ai/config.el

--- modules/cae/ai/config.el
+++ modules/cae/ai/config.el
@@ -29,7 +29,7 @@
 (defvar llm-refactoring-provider nil)
 (after! llm
   (require 'llm-openai)
-  (setq llm-refactoring-provider (make-llm-openai :chat-model "o1-preview" :key (cae-secrets-get-openai-api-key))
+  (setq llm-refactoring-provider (make-llm-openai :chat-model "gpt-4o" :key (cae-secrets-get-openai-api-key))
         magit-gptcommit-llm-provider llm-refactoring-provider
         llm-warn-on-nonfree nil))
 
