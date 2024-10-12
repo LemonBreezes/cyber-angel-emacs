7003e018826daa070c4a1b54ad61b8b411cf3a86
StrawberryTea <look@strawberrytea.xyz>
2024-10-11 19:38:44 -0500

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -165,3 +165,28 @@ It is meant to be used as a `post-gc-hook'."
 ;;  :around #'straight-use-recipes
 ;;  (unless (eq 'nongnu-elpa (car recipe))
 ;;    (funcall fn recipe)))
+
+
+(defun cae-hacks-chatgpt-shell--make-payload (history)
+  "Create the request payload from HISTORY."
+  (setq history
+        (vconcat ;; Vector for json
+         (chatgpt-shell--user-assistant-messages
+          (last history
+                (chatgpt-shell--unpaired-length
+                 (if (functionp chatgpt-shell-transmitted-context-length)
+                     (funcall chatgpt-shell-transmitted-context-length
+                              (chatgpt-shell-model-version) history)
+                   chatgpt-shell-transmitted-context-length))))))
+  ;; TODO: Use `chatgpt-shell-make-request-data'.
+  (let ((request-data `((model . ,(chatgpt-shell-model-version))
+                        (messages . ,(if (chatgpt-shell-system-prompt)
+                                         history
+                                       history)))))
+    (when chatgpt-shell-model-temperature
+      (push `(temperature . ,chatgpt-shell-model-temperature) request-data))
+    (when chatgpt-shell-streaming
+      (push `(stream . t) request-data))
+    request-data))
+
+(advice-add #'chatgpt-shell--make-payload :override #'cae-hacks-chatgpt-shell--make-payload)
