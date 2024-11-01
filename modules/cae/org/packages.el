a426d332d892d1072b6fa31a825090a0eb631048
StrawberryTea <look@strawberrytea.xyz>
2024-11-01 01:01:48 -0400

    modules/cae/org/packages.el

--- modules/cae/org/packages.el
+++ modules/cae/org/packages.el
@@ -1,6 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/org/packages.el
 
+(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
+
 (package! org-rich-yank)
 (package! org-inline-pdf)
 (package! language-detection)
