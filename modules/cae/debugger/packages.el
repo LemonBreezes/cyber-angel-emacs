328e9d6749adc00db5153eb2ea1004a0b8150819
StrawberryTea <look@strawberrytea.xyz>
2024-11-01 01:02:21 -0400

    modules/cae/debugger/packages.el

--- modules/cae/debugger/packages.el
+++ modules/cae/debugger/packages.el
@@ -1,6 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; cae/debugger/packages.el
 
+(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
+
 (when (and (modulep! :tools lsp +eglot)
            (modulep! :tools debugger +lsp))
   (package! dape))
