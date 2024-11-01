ac6a44096d926ee2f0693a53d5c7528fc9900f75
StrawberryTea <look@strawberrytea.xyz>
2024-11-01 10:33:58 -0400

    .

---
+++ modules/cae/dired/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; cae/dired/packages.el
+
+(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
---
+++ modules/cae/gnus/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; cae/gnus/packages.el
+
+(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
