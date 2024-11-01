4939a9b262d8261b393a023e1d410ae1dd0000a0
StrawberryTea <look@strawberrytea.xyz>
2024-11-01 00:54:16 -0400

    modules/cae/ai/packages.el

--- modules/cae/ai/packages.el
+++ modules/cae/ai/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/ai/packages.el
 
-(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-Lib"))
+(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
 
 (package! copilot :recipe (:host github :repo "copilot-emacs/copilot.el"))
 (package! gptel)
