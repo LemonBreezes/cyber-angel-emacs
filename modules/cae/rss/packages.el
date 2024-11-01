838f1542daf0a570c0f67e1cbdc98f6486e4c887
StrawberryTea <look@strawberrytea.xyz>
2024-11-01 01:02:00 -0400

    modules/cae/rss/packages.el

--- modules/cae/rss/packages.el
+++ modules/cae/rss/packages.el
@@ -1,5 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; cae/rss/packages.el
 
+(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
+
 (package! elfeed-tube)
 (package! elfeed-tube-mpv)
