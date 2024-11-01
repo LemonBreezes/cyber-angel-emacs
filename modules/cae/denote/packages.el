e1a51ad97ac3f7134f02fb1e9a35f7c15dcd9d01
StrawberryTea <look@strawberrytea.xyz>
2024-11-01 00:59:05 -0400

    modules/cae/denote/packages.el

--- modules/cae/denote/packages.el
+++ modules/cae/denote/packages.el
@@ -1,6 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; cae/denote/packages.el
 
+(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
+
 (package! denote)
 (package! denote-explorer :recipe (:host github :repo "pprevos/denote-explore"))
 (package! denote-menu)
