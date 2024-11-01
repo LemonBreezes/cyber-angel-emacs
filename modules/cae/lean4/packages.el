3d924cbfd43b0f2d7dc58c66320588334bcfbb96
StrawberryTea <look@strawberrytea.xyz>
2024-11-01 01:00:36 -0400

    modules/cae/lean4/packages.el

--- modules/cae/lean4/packages.el
+++ modules/cae/lean4/packages.el
@@ -1,6 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/lean4/packages.el
 
+(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
+
 (package! lean4-mode
   :recipe (:host github
 	   :repo "leanprover/lean4-mode"
