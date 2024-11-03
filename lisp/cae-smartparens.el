2d084bc3c559b79cf93b1dd036a42d345662e254
StrawberryTea <look@strawberrytea.xyz>
2024-11-03 15:28:02 -0500

    lisp/cae-smartparens.el

--- lisp/cae-smartparens.el
+++ lisp/cae-smartparens.el
@@ -49,9 +49,9 @@
                   ((if (listp fs)
                        (cl-loop for f in fs thereis (memq f (list ,@faces)))
                      (memq fs (list ,@faces))))))))
-  (defalias 'cae-sp-in-src-block-p (cae-sp-in-faces-p 'org-block))
-  (defalias 'cae-sp-in-org-table-p (cae-sp-in-faces-p 'org-table))
-  (defalias 'cae-sp-in-org-block-begin-line-p (cae-sp-in-faces-p 'org-block-begin-line))
+  (fset 'cae-sp-in-src-block-p (cae-sp-in-faces-p 'org-block))
+  (fset 'cae-sp-in-org-table-p (cae-sp-in-faces-p 'org-table))
+  (fset 'cae-sp-in-org-block-begin-line-p (cae-sp-in-faces-p 'org-block-begin-line))
   (after! smartparens
     (sp-local-pair 'org-mode "<<" ">>" :unless
                    '(cae-sp-in-src-block-p cae-sp-in-org-table-p))
