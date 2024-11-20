7af91f8006e73b6d26f02e6a8679d86786a3d627
StrawberryTea <look@strawberrytea.xyz>
2024-11-19 21:42:22 -0600

    init.el

--- init.el
+++ init.el
@@ -45,6 +45,8 @@
   (setq native-comp-async-jobs-number (num-processors)
         native-comp-async-report-warnings-errors 'silent)
 
+  (compile-angel-on-load-mode +1)
+
   (load! "lisp/cae-debug")
   (load! "lisp/cae-hacks")
 
