6ee2fde79f6b7f51f750b7066b61daa1f0b0cb43
StrawberryTea <look@strawberrytea.xyz>
2024-11-20 17:02:24 -0600

    init.el

--- init.el
+++ init.el
@@ -40,7 +40,8 @@
               consult--gc-threshold cae-gc-cons-threshold
               cae-hacks-gc-cons-threshold cae-gc-cons-threshold
               +lsp--default-gcmh-high-cons-threshold cae-gc-cons-threshold))
-    (setq gcmh-auto-idle-delay-factor 20))
+    (setq gcmh-auto-idle-delay-factor 20
+          gcmh-high-cons-threshold #x40000000))
   (setq doom-incremental-idle-timer 0.25)
 
   (setq native-comp-async-jobs-number (num-processors)
