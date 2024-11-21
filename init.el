ad7c26641503af7191da0c82fe2e8d93e3f6405f
StrawberryTea <look@strawberrytea.xyz>
2024-11-21 10:18:57 -0600

    init.el

--- init.el
+++ init.el
@@ -43,8 +43,7 @@
     ;; Slow computer
     (setq gcmh-auto-idle-delay-factor 20
           gcmh-high-cons-threshold #x40000000
-          jit-lock-defer-time 2)
-    (advice-add #'global-diff-hl-mode :override #'ignore))
+          jit-lock-defer-time 2))
   (setq doom-incremental-idle-timer 0.25)
 
   (setq native-comp-async-jobs-number (num-processors)
