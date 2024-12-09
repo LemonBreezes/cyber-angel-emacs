35b771aabd282a91eafdaea22fbc931ab48472ee
StrawberryTea <look@strawberrytea.xyz>
2024-12-09 11:49:01 -0600

    config.el

--- config.el
+++ config.el
@@ -600,8 +600,8 @@
   (add-hook 'vertico-mode-hook #'vertico-mouse-mode)
   (remove-hook 'vertico-mode-hook #'vertico-posframe-mode)
   (after! vertico-multiform
-    (let ((non-condensed-display-p (and (cae-display-graphic-p)
-                                        (>= (frame-width) 120))))
+    (let ((non-condensed-display-p (or (cae-display-graphic-p)
+                                       (>= (frame-width) 120))))
       (setq vertico-multiform-categories
             `((embark-keybinding grid)
               (consult-grep
