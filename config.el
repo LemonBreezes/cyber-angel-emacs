832d4074f2888186b8ad2a4dba7a0d3ecc072853
StrawberryTea <look@strawberrytea.xyz>
2024-12-27 12:18:35 -0600

    config.el

--- config.el
+++ config.el
@@ -626,8 +626,7 @@
     (let* ((posframe-supported-p (and (modulep! :completion vertico +childframe)
                                       (or (cae-display-graphic-p)
                                           (> emacs-major-version 30))))
-           (wide-frame-p ;;(>= (frame-width) 120)
-            t)
+           (wide-frame-p (>= (frame-width) 120))
            (default-view (if wide-frame-p
                              (if posframe-supported-p 'posframe 'vertical)
                            'flat))
