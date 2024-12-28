88fabf4a116298e79c5b161e3c24393938387c6d
StrawberryTea <look@strawberrytea.xyz>
2024-12-28 14:24:02 -0600

    modules/cae/misc-applications/autoload/trashed.el

--- modules/cae/misc-applications/autoload/trashed.el
+++ modules/cae/misc-applications/autoload/trashed.el
@@ -1,7 +1,7 @@
 ;;; private/misc-applications/autoload/trashed.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun +trashed-revert-buffer-a (oldfun)
+(defun cae-trashed-revert-buffer-a (oldfun)
   (when (prog1 (buffer-live-p trashed-buffer)
           (funcall oldfun))
     (revert-buffer)))
