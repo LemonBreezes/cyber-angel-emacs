118ac7690df492cd587af2eab8a8a2c5e071186a
StrawberryTea <look@strawberrytea.xyz>
2024-12-28 14:23:58 -0600

    modules/cae/misc-applications/autoload/tetris.el

--- modules/cae/misc-applications/autoload/tetris.el
+++ modules/cae/misc-applications/autoload/tetris.el
@@ -1,33 +1,33 @@
 ;;; private/misc-applications/autoload/tetris.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun +tetris (&optional arg)
+(defun cae-tetris (&optional arg)
   (interactive "P")
   (if arg
-      (setq +tetris--old-wconf nil)
+      (setq cae-tetris--old-wconf nil)
     (if (modulep! :ui workspaces)
-        (+workspace-switch +tetris-workspace-name t)
-      (setq +tetris--old-wconf (current-window-configuration))
+        (+workspace-switch cae-tetris-workspace-name t)
+      (setq cae-tetris--old-wconf (current-window-configuration))
       (delete-other-windows)
       (switch-to-buffer (doom-fallback-buffer))))
   (let* ((saves-buf (find-file-noselect (expand-file-name "tetris-scores" shared-game-score-directory)))
          (highest-score (with-current-buffer saves-buf
-                          (local-set-key (kbd "q") #'+tetris-quit)
+                          (local-set-key (kbd "q") #'cae-tetris-quit)
                           (when (featurep 'evil)
-                            (evil-local-set-key 'normal (kbd "q") #'+tetris-quit))
+                            (evil-local-set-key 'normal (kbd "q") #'cae-tetris-quit))
                           (buffer-substring-no-properties (goto-char (point-min))
                                                           (pos-eol)))))
     (call-interactively #'tetris)
     (setq-local header-line-format highest-score)))
 
 ;;;###autoload
-(defun +tetris-quit ()
+(defun cae-tetris-quit ()
   (interactive)
   (if (modulep! :ui workspaces)
-      (when (+workspace-exists-p +tetris-workspace-name)
-        (+workspace/kill +tetris-workspace-name))
-    (when +tetris--old-wconf
-      (set-window-configuration +tetris--old-wconf)))
+      (when (+workspace-exists-p cae-tetris-workspace-name)
+        (+workspace/kill cae-tetris-workspace-name))
+    (when cae-tetris--old-wconf
+      (set-window-configuration cae-tetris--old-wconf)))
   (kill-buffer "*Tetris*")
   (when-let* ((saves-buf (get-file-buffer (expand-file-name "tetris-scores" shared-game-score-directory))))
     (with-current-buffer saves-buf
