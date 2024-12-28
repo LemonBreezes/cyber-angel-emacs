0000dad37820e99f3d94cb9f5dcc459d34a9f5bc
StrawberryTea <look@strawberrytea.xyz>
2024-12-28 13:59:40 -0600

    autoload/cae-popup.el

--- autoload/cae-popup.el
+++ autoload/cae-popup.el
@@ -7,16 +7,16 @@
            (enlarge-window (- (frame-width)
                               (window-width win))
                            t)))
-         ((let ((i 0))
-            (cl-loop for win the windows
-                     if (window-in-direction 'right win t)
-                     do (cl-incf i)
-                     finally return (> i 1)))
-          ;; resize the help buffer to 1/3 of the screen
-          (with-selected-window win
-            (enlarge-window (- (/ (frame-width) 3)
-                               (window-width win))
-                            t)))))
+        ((let ((i 0))
+           (cl-loop for win the windows
+                    if (window-in-direction 'right win t)
+                    do (cl-incf i)
+                    finally return (> i 1)))
+         ;; resize the help buffer to 1/3 of the screen
+         (with-selected-window win
+           (enlarge-window (- (/ (frame-width) 3)
+                              (window-width win))
+                           t)))))
 
 ;;;###autoload
 (defun cae-popup-shrink-to-fit (&optional window)
