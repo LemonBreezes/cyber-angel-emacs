a68c1de879681a439087be580533bc5bc2b539c4
StrawberryTea <look@strawberrytea.xyz>
2024-12-28 14:23:29 -0600

    modules/cae/misc-applications/autoload/snow.el

--- modules/cae/misc-applications/autoload/snow.el
+++ modules/cae/misc-applications/autoload/snow.el
@@ -1,28 +1,28 @@
 ;;; private/misc-applications/autoload/snow.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun +snow (&optional arg)
+(defun cae-snow (&optional arg)
   (interactive "P")
   (if arg
-    (setq +snow--old-wconf nil)
+    (setq cae-snow--old-wconf nil)
   (if (modulep! :ui workspaces)
-      (+workspace-switch +snow-workspace-name t)
-    (setq +snow--old-wconf (current-window-configuration))
+      (+workspace-switch cae-snow-workspace-name t)
+    (setq cae-snow--old-wconf (current-window-configuration))
     (let ((ignore-window-parameters t))
       (delete-other-windows))
     (switch-to-buffer (doom-fallback-buffer))))
   (call-interactively #'snow)
   (with-current-buffer "*snow*"
-    (local-set-key (kbd "q") #'+snow-quit)
+    (local-set-key (kbd "q") #'cae-snow-quit)
     (when (featurep 'evil)
-      (evil-local-set-key 'normal (kbd "q") #'+snow-quit))))
+      (evil-local-set-key 'normal (kbd "q") #'cae-snow-quit))))
 
 ;;;###autoload
-(defun +snow-quit ()
+(defun cae-snow-quit ()
   (interactive)
   (if (modulep! :ui workspaces)
-      (when (+workspace-exists-p +snow-workspace-name)
-        (+workspace/kill +snow-workspace-name))
-    (when +snow--old-wconf
-      (set-window-configuration +snow--old-wconf)))
+      (when (+workspace-exists-p cae-snow-workspace-name)
+        (+workspace/kill cae-snow-workspace-name))
+    (when cae-snow--old-wconf
+      (set-window-configuration cae-snow--old-wconf)))
   (kill-buffer "*snow*"))
