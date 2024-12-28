701dea83c8b2cba066867dedeb6b71d4cf2eb0a9
StrawberryTea <look@strawberrytea.xyz>
2024-12-28 14:23:01 -0600

    modules/cae/misc-applications/autoload/mpc.el

--- modules/cae/misc-applications/autoload/mpc.el
+++ modules/cae/misc-applications/autoload/mpc.el
@@ -1,89 +1,89 @@
 ;;; private/misc-applications/autoload/mpc.el -*- lexical-binding: t; -*-
 
-(defvar +mpc--wconf nil)
-(defvar +mpc-buf-pos-alist nil)
-(defvar +mpc--prev-buf nil)
+(defvar cae-mpc--wconf nil)
+(defvar cae-mpc-buf-pos-alist nil)
+(defvar cae-mpc--prev-buf nil)
 
 ;;;###autoload
-(defun +mpc (&optional arg)
+(defun cae-mpc (&optional arg)
   (interactive "P")
-  (setq +mpc--old-wconf (current-window-configuration))
+  (setq cae-mpc--old-wconf (current-window-configuration))
   (let ((ignore-window-parameters t))
     (delete-other-windows))
   (cl-letf (((symbol-function #'goto-char)
              (symbol-function #'ignore))))
-  (if (and +mpc--wconf
+  (if (and cae-mpc--wconf
            (not (cl-find-if-not #'buffer-live-p
                                 (mapcar #'cdr (if mpc-proc (process-get mpc-proc 'buffers))))))
-      (set-window-configuration +mpc--wconf)
+      (set-window-configuration cae-mpc--wconf)
     (call-interactively #'mpc))
   (let ((inhibit-redisplay t))
-    (when (and +mpc--prev-buf (get-buffer-window +mpc--prev-buf))
-      (select-window (get-buffer-window +mpc--prev-buf))
-      (+mpc-jump-to-previous-position)
+    (when (and cae-mpc--prev-buf (get-buffer-window cae-mpc--prev-buf))
+      (select-window (get-buffer-window cae-mpc--prev-buf))
+      (cae-mpc-jump-to-previous-position)
       (hl-line-highlight)
       (mpc-playlist)))
-  (setq +mpc--wconf (current-window-configuration)))
+  (setq cae-mpc--wconf (current-window-configuration)))
 
 ;;;###autoload
-(defun +mpc-quit ()
+(defun cae-mpc-quit ()
   (interactive)
   (dolist (buf (mapcar #'cdr (if mpc-proc (process-get mpc-proc 'buffers))))
-    (setf (alist-get buf +mpc-buf-pos-alist)
+    (setf (alist-get buf cae-mpc-buf-pos-alist)
           (with-current-buffer buf
             (point-marker))))
-  (setq +mpc--prev-buf (buffer-name (current-buffer)))
-  (if +mpc--old-wconf
+  (setq cae-mpc--prev-buf (buffer-name (current-buffer)))
+  (if cae-mpc--old-wconf
       (progn
-        (set-window-configuration +mpc--old-wconf)
-        (setq +mpc--old-wconf nil))
+        (set-window-configuration cae-mpc--old-wconf)
+        (setq cae-mpc--old-wconf nil))
     (mpc-quit)))
 
 ;;;###autoload
-(defun +mpc-reload ()
+(defun cae-mpc-reload ()
   (interactive)
   (let ((inhibit-redisplay t))
     (mpc-quit)
     (mpc)
-    (setq +mpc--old-wconf (current-window-configuration))
+    (setq cae-mpc--old-wconf (current-window-configuration))
     (dolist (buf (mapcar #'cdr (if mpc-proc (process-get mpc-proc 'buffers))))
-      (setf (alist-get buf +mpc-buf-pos-alist)
+      (setf (alist-get buf cae-mpc-buf-pos-alist)
             (with-current-buffer buf
               (point-marker))))))
 
-(defun +mpc-jump-to-previous-position ()
-  (when-let* ((marker (alist-get (window-buffer (selected-window)) +mpc-buf-pos-alist))
+(defun cae-mpc-jump-to-previous-position ()
+  (when-let* ((marker (alist-get (window-buffer (selected-window)) cae-mpc-buf-pos-alist))
               (pos (marker-position marker)))
     (run-at-time 0.02 nil #'goto-char pos)))
 
 ;;;###autoload
-(defun +mpc-other-window-previous ()
+(defun cae-mpc-other-window-previous ()
   (interactive)
   (setf (alist-get (window-buffer (selected-window))
-                   +mpc-buf-pos-alist)
+                   cae-mpc-buf-pos-alist)
         (point-marker))
   (cl-loop do (call-interactively #'other-window-previous)
            until (and (not (string= (buffer-name (current-buffer)) "*MPC-Status*"))
                       (not (string= (buffer-name (current-buffer)) "*MPC-Songs*")))
            finally (goto-char (marker-position
                                (alist-get (window-buffer (selected-window))
-                                          +mpc-buf-pos-alist)))))
+                                          cae-mpc-buf-pos-alist)))))
 
 ;;;###autoload
-(defun +mpc-other-window ()
+(defun cae-mpc-other-window ()
   (interactive)
   (setf (alist-get (window-buffer (selected-window))
-                   +mpc-buf-pos-alist)
+                   cae-mpc-buf-pos-alist)
         (point-marker))
   (cl-loop do (call-interactively #'other-window)
            until (and (not (string= (buffer-name (current-buffer)) "*MPC-Status*"))
                       (not (string= (buffer-name (current-buffer)) "*MPC-Songs*")))
            finally (goto-char (marker-position
                                (alist-get (window-buffer (selected-window))
-                                          +mpc-buf-pos-alist)))))
+                                          cae-mpc-buf-pos-alist)))))
 
 ;;;###autoload
-(defun +mpc-play ()
+(defun cae-mpc-play ()
   (interactive)
   (mpc-select)
   (mpc-play))
