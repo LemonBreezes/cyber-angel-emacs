d5eb3e32cffa9f87fe94988048ec3a815bae5b58
StrawberryTea <look@strawberrytea.xyz>
2024-12-28 14:22:26 -0600

    modules/cae/misc-applications/autoload/misc-applications.el

--- modules/cae/misc-applications/autoload/misc-applications.el
+++ modules/cae/misc-applications/autoload/misc-applications.el
@@ -1,7 +1,7 @@
 ;;; private/misc-applications/autoload/misc-applications.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun +misc-applications-hide-cursor-h ()
+(defun cae-misc-applications-hide-cursor-h ()
   (setq-local cursor-type nil)
   (hl-line-mode +1)
   (setq-local evil-normal-state-cursor '(bar . 0)
