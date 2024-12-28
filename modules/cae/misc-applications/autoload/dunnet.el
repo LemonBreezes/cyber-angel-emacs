26a6c0143b43af901a9f171fb659950e24fc425c
StrawberryTea <look@strawberrytea.xyz>
2024-12-28 14:20:12 -0600

    modules/cae/misc-applications/autoload/dunnet.el

--- modules/cae/misc-applications/autoload/dunnet.el
+++ modules/cae/misc-applications/autoload/dunnet.el
@@ -1,24 +1,24 @@
 ;;; private/misc-applications/autoload/dunnet.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun +dunnet (&optional arg)
+(defun cae-dunnet (&optional arg)
   (interactive "P")
   (if arg
-      (setq +dunnet--old-wconf nil)
+      (setq cae-dunnet--old-wconf nil)
     (if (modulep! :ui workspaces)
-        (+workspace-switch +dunnet-workspace-name t)
-      (setq +dunnet--old-wconf (current-window-configuration))
+        (+workspace-switch cae-dunnet-workspace-name t)
+      (setq cae-dunnet--old-wconf (current-window-configuration))
       (let ((ignore-window-parameters t))
         (delete-other-windows))
       (switch-to-buffer (doom-fallback-buffer))))
   (call-interactively #'dunnet))
 
 ;;;###autoload
-(defun +dunnet-quit ()
+(defun cae-dunnet-quit ()
   (interactive)
   (if (modulep! :ui workspaces)
-      (when (+workspace-exists-p +dunnet-workspace-name)
-        (+workspace/kill +dunnet-workspace-name))
-    (when +dunnet--old-wconf
-      (set-window-configuration +dunnet--old-wconf)))
+      (when (+workspace-exists-p cae-dunnet-workspace-name)
+        (+workspace/kill cae-dunnet-workspace-name))
+    (when cae-dunnet--old-wconf
+      (set-window-configuration cae-dunnet--old-wconf)))
   (kill-buffer "*dungeon*"))
