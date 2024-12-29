04fe23f7ed88db8542a0591b3de3915af13787e8
StrawberryTea <look@strawberrytea.xyz>
2024-12-29 09:11:30 -0600

    dir-local-files/music-dir.el

--- dir-local-files/music-dir.el
+++ dir-local-files/music-dir.el
@@ -4,12 +4,12 @@
           ;; Now that I know about `menu-item' keybindings, I could make this code
           ;; more robust but I'm too lazy to do it for now.
           (when (derived-mode-p 'dired-mode)
-            (when (fboundp #'+emms-quick-access)
-              (local-set-key (kbd "a") #'+emms-quick-access)
+            (when (fboundp #'cae-emms-quick-access)
+              (local-set-key (kbd "a") #'cae-emms-quick-access)
               (when (featurep 'evil)
                 (evil-local-set-key 'normal (kbd "a")
                                     (cmd! () (if buffer-read-only
-                                                 (call-interactively #'+emms-quick-access)
+                                                 (call-interactively #'cae-emms-quick-access)
                                                (call-interactively (lookup-key evil-normal-state-map "a")))))))
             (when (fboundp #'emms-play-dired)
               (local-set-key (kbd "e")
