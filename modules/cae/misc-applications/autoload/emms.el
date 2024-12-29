f378367e7fa43d2b31eb09028494df73d851073e
StrawberryTea <look@strawberrytea.xyz>
2024-12-29 09:54:15 -0600

    modules/cae/misc-applications/autoload/emms.el

--- modules/cae/misc-applications/autoload/emms.el
+++ modules/cae/misc-applications/autoload/emms.el
@@ -55,7 +55,7 @@
             (lambda (&rest _)
               (dired-jump nil (emms-track-get
                                (emms-playlist-current-selected-track) 'name))))
-         (dired (expand-file-name "VGM" cae-misc-applications-music-dir)))))]])
+         "No track is currently playing.")))]])
 
 ;; The following two functions are from
 ;; https://www.reddit.com/r/emacs/comments/qg2d0k/emms_modeline_shows_full_path_to_the_songs_i_only/
