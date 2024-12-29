312ec29555a3301a30efa2c42bc4c38660070625
StrawberryTea <look@strawberrytea.xyz>
2024-12-29 09:42:00 -0600

    modules/cae/misc-applications/autoload/emms.el

--- modules/cae/misc-applications/autoload/emms.el
+++ modules/cae/misc-applications/autoload/emms.el
@@ -113,7 +113,7 @@ rather than the whole path."
                              t)))
       :gi "e" `(menu-item "" nil
                 :filter ,(lambda (&optional _)
-                             (message "HELLOOO")
+                           (message "HELLOOO")
                            (when buffer-read-only
                              (call-interactively #'emms-play-dired)
                              (call-interactively #'emms-shuffle)
