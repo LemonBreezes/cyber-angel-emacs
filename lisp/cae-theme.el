88eb6bfbada96bf9fd5a7cb6f0d26f70b2c94638
StrawberryTea <look@strawberrytea.xyz>
2024-12-30 19:55:51 -0600

    lisp/cae-theme.el

--- lisp/cae-theme.el
+++ lisp/cae-theme.el
@@ -7,7 +7,10 @@
 
 (defvar cae-modus-day-theme 'modus-operandi-tinted)
 (defvar cae-modus-night-theme (if (cae-display-graphic-p)
-                                  'modus-vivendi-tinted
+                                  'modus-vivendi-tinted ;; A little bit more
+                                                        ;; legible in the
+                                                        ;; Windows Terminal by
+                                                        ;; default.
                                 'modus-vivendi-tritanopia))
 (setq doom-theme cae-modus-night-theme)
 
