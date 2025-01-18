3a8e177b9b2faca101c3759113f53c0c812e1464
StrawberryTea <look@strawberrytea.xyz>
2025-01-18 11:55:12 -0500

    config.el

--- config.el
+++ config.el
@@ -417,7 +417,7 @@
     (sublimity-mode +1)
     (require 'sublimity-scroll)
     (setq sublimity-scroll-weight 10
-          sublimity-scroll-drift-length 5))
+          sublimity-scroll-drift-length 10))
 
   (use-package! beacon
     :defer t :init (add-hook 'doom-first-file-hook #'beacon-mode)
