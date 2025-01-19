b3c3edda3198ccb3de0e3bd7328609d86b199c59
StrawberryTea <look@strawberrytea.xyz>
2025-01-18 19:31:55 -0500

    autoload/cae-popup.el

--- autoload/cae-popup.el
+++ autoload/cae-popup.el
@@ -42,4 +42,5 @@ Uses `shrink-window-if-larger-than-buffer'."
               (window-parent window))
       (window-swap-states window (next-window window)))
     (window-swap-states window (next-window window))
+    ;; Move it to the bottom maximized.
     (transpose-frame)))
