fcd0c7a0df83985472ec2d1c066ac01a82c98013
StrawberryTea <look@strawberrytea.xyz>
2025-01-19 09:40:07 -0500

    modules/cae/misc-applications/autoload/minesweeper.el

--- modules/cae/misc-applications/autoload/minesweeper.el
+++ modules/cae/misc-applications/autoload/minesweeper.el
@@ -6,7 +6,8 @@
   (if arg
       (setq cae-minesweeper--old-wconf nil)
     (if (modulep! :ui workspaces)
-        (+workspace-switch cae-minesweeper-workspace-name t)
+        (progn (+workspace-switch cae-minesweeper-workspace-name t)
+               (set-persp-parameter 'dont-save-to-file t cae-minesweeper-workspace-name))
       (setq cae-minesweeper--old-wconf (current-window-configuration))
       (let ((ignore-window-parameters t))
         (delete-other-windows))
