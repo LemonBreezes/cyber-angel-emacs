790cce447e033d553233c38e8bbb5d5457e677b9
StrawberryTea <look@strawberrytea.xyz>
2025-01-19 09:51:46 -0500

    modules/cae/misc-applications/autoload/speed-type.el

--- modules/cae/misc-applications/autoload/speed-type.el
+++ modules/cae/misc-applications/autoload/speed-type.el
@@ -7,7 +7,8 @@
       (setq cae-speed-type--old-wconf nil)
     (if (modulep! :ui workspaces)
         (progn (+workspace-switch cae-speed-type-workspace-name t)
-               (set-persp-parameter 'dont-save-to-file t cae-speed-type-workspace-name))
+               (set-persp-parameter 'dont-save-to-file t
+                                    (+workspace-get cae-speed-type-workspace-name)))
       (setq cae-speed-type--old-wconf (current-window-configuration))
       (let ((ignore-window-parameters t))
         (delete-other-windows))
