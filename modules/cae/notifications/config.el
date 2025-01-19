59f7dc2bdcfd357e9e10a62afca64025916cb09f
StrawberryTea <look@strawberrytea.xyz>
2025-01-18 21:44:53 -0500

    .

---
+++ modules/cae/notifications/config.el
@@ -0,0 +1,27 @@
+;;; cae/notifications/config.el -*- lexical-binding: t; -*-
+
+(use-package! ednc
+  :defer t :init
+  (defun cae-ednc-load-h ()
+    (and (require 'dbus nil t)
+         (ednc-mode +1)))
+  (run-with-idle-timer 1.5 nil #'cae-ednc-load-h)
+  (add-hook 'ednc-notification-presentation-functions #'cae-ednc-show-notification-in-buffer)
+  (defun cae-ednc-stack-notifications (&optional hide)
+    (mapconcat (lambda (notification)
+                 (let ((app-name (ednc-notification-app-name notification)))
+                   (unless (member app-name hide)
+                     (push app-name hide)
+                     (ednc-format-notification notification))))
+               (ednc-notifications) ""))
+  (add-hook 'ednc-notification-presentation-functions
+            (lambda (&rest _) (force-mode-line-update t)))
+  :config
+  (add-to-list 'global-mode-string
+               '((:eval (cae-ednc-stack-notifications))))
+  (map! :map ednc-view-mode-map
+        "n" #'next-line
+        "p" #'previous-line)
+
+  ;; Otherwise `alert-send-notification' will block the UI.
+  (advice-add #'alert-send-notification :around #'cae-ednc-wrap-async-call-process-a))
---
+++ modules/cae/notifications/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; cae/notifications/packages.el
+
+(package! ednc)
