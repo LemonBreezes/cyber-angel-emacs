15e17942bc7e89b55d8624841fe641a4b49852bf
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 15:20:44 -0500

    modules/cae/misc-applications/autoload/speed-type.el

--- modules/cae/misc-applications/autoload/speed-type.el
+++ modules/cae/misc-applications/autoload/speed-type.el
@@ -1,7 +1,7 @@
+;;; private/misc-applications/autoload/speed-type.el -*- lexical-binding: t; -*-
+
 ;;;###autoload (autoload 'cae-speed-type-text "cae/misc-applications/autoload/speed-type" nil t)
 ;;;###autoload (autoload 'cae-speed-type-text-quit "cae/misc-applications/autoload/speed-type" nil t)
-
-;;; private/misc-applications/autoload/speed-type.el -*- lexical-binding: t; -*-
 
 (cae-define-launcher
   cae-speed-type-text
