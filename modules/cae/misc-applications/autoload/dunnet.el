1de10d3664bfc088061ed0e46ad0f5d42e8eb8a6
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 15:23:38 -0500

    modules/cae/misc-applications/autoload/dunnet.el

--- modules/cae/misc-applications/autoload/dunnet.el
+++ modules/cae/misc-applications/autoload/dunnet.el
@@ -1,4 +1,4 @@
-;;; private/misc-applications/autoload/dunnet.el -*- lexical-binding: t; -*-
+;;; cae/misc-applications/autoload/dunnet.el -*- lexical-binding: t; -*-
 
 ;;;###autoload (autoload 'cae-dunnet "cae/misc-applications/autoload/dunnet" nil t)
 ;;;###autoload (autoload 'cae-dunnet-quit "cae/misc-applications/autoload/dunnet" nil t)
