04bc50072d77672f284b1771f0d01c286bb0e7cf
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 15:20:54 -0500

    modules/cae/misc-applications/autoload/tetris.el

--- modules/cae/misc-applications/autoload/tetris.el
+++ modules/cae/misc-applications/autoload/tetris.el
@@ -1,7 +1,7 @@
+;;; private/misc-applications/autoload/tetris.el -*- lexical-binding: t; -*-
+
 ;;;###autoload (autoload 'cae-tetris "cae/misc-applications/autoload/tetris" nil t)
 ;;;###autoload (autoload 'cae-tetris-quit "cae/misc-applications/autoload/tetris" nil t)
-
-;;; private/misc-applications/autoload/tetris.el -*- lexical-binding: t; -*-
 
 (cae-define-launcher
   cae-tetris
