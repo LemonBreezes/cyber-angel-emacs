df882d993f1165cddaa894d08db4579dd6235f6f
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 15:19:14 -0500

    modules/cae/misc-applications/autoload/snow.el

--- modules/cae/misc-applications/autoload/snow.el
+++ modules/cae/misc-applications/autoload/snow.el
@@ -4,13 +4,13 @@
 ;;;###autoload (autoload 'cae-snow-quit "cae/misc-applications/autoload/snow" nil t)
 
 (cae-define-launcher
-  cae-snow
-  :launch-fn #'snow
-  :buffer-name "*snow*"
-  :workspace-name cae-snow-workspace-name
-  :setup-fn
-  (lambda ()
-    (with-current-buffer "*snow*"
-      (local-set-key (kbd "q") #'cae-snow-quit)
-      (when (featurep 'evil)
-        (evil-local-set-key 'normal (kbd "q") #'cae-snow-quit)))))
+ cae-snow
+ :launch-fn #'snow
+ :buffer-name "*snow*"
+ :workspace-name cae-snow-workspace-name
+ :setup-fn
+ (lambda ()
+   (with-current-buffer "*snow*"
+     (local-set-key (kbd "q") #'cae-snow-quit)
+     (when (featurep 'evil)
+       (evil-local-set-key 'normal (kbd "q") #'cae-snow-quit)))))
