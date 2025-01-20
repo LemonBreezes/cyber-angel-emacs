98b7fb1dd07f1b4c0c99106830c8150d869f100a
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 17:27:41 -0500

    modules/cae/misc-applications/autoload/hackernews.el

--- modules/cae/misc-applications/autoload/hackernews.el
+++ modules/cae/misc-applications/autoload/hackernews.el
@@ -1,9 +1,13 @@
 ;;; cae/misc-applications/autoload/hackernews.el -*- lexical-binding: t; -*-
 
-;;;###autoload
-(defun cae-hackernews ()
-  (interactive)
-  (let ((buf (get-buffer "*hackernews top stories*")))
-    (if (buffer-live-p buf)
-        (pop-to-buffer buf)
-      (call-interactively #'hackernews))))
+;;;###autoload (autoload 'cae-hackernews "cae/misc-applications/autoload/hackernews" nil t)
+;;;###autoload (autoload 'cae-hackernews-quit "cae/misc-applications/autoload/hackernews" nil t)
+
+(cae-define-launcher
+ cae-hackernews
+ :launch-fn (lambda ()
+              (let ((buf (get-buffer "*hackernews top stories*")))
+                (if (buffer-live-p buf)
+                    (pop-to-buffer buf)
+                  (call-interactively #'hackernews))))
+ :workspace-name cae-hackernews-workspace-name)
