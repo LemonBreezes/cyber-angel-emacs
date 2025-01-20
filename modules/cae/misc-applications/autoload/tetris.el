7e75f8f3493b16dc0f32534cdc099597e9c72e76
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 15:20:54 -0500

    modules/cae/misc-applications/autoload/tetris.el

--- modules/cae/misc-applications/autoload/tetris.el
+++ modules/cae/misc-applications/autoload/tetris.el
@@ -4,23 +4,23 @@
 ;;;###autoload (autoload 'cae-tetris-quit "cae/misc-applications/autoload/tetris" nil t)
 
 (cae-define-launcher
-  cae-tetris
-  :launch-fn #'tetris
-  :buffer-name "*Tetris*"
-  :workspace-name cae-tetris-workspace-name
-  :setup-fn
-  (lambda ()
-    (let* ((saves-buf (find-file-noselect (expand-file-name "tetris-scores" shared-game-score-directory)))
-           (highest-score (with-current-buffer saves-buf
-                            (local-set-key (kbd "q") #'cae-tetris-quit)
-                            (when (featurep 'evil)
-                              (evil-local-set-key 'normal (kbd "q") #'cae-tetris-quit))
-                            (buffer-substring-no-properties (goto-char (point-min))
-                                                            (line-end-position)))))
-      (setq-local header-line-format highest-score)))
-  :cleanup-fn
-  (lambda ()
-    (when-let ((saves-buf (get-file-buffer (expand-file-name "tetris-scores" shared-game-score-directory))))
-      (with-current-buffer saves-buf
-        (save-buffer)
-        (kill-buffer)))))
+ cae-tetris
+ :launch-fn #'tetris
+ :buffer-name "*Tetris*"
+ :workspace-name cae-tetris-workspace-name
+ :setup-fn
+ (lambda ()
+   (let* ((saves-buf (find-file-noselect (expand-file-name "tetris-scores" shared-game-score-directory)))
+          (highest-score (with-current-buffer saves-buf
+                           (local-set-key (kbd "q") #'cae-tetris-quit)
+                           (when (featurep 'evil)
+                             (evil-local-set-key 'normal (kbd "q") #'cae-tetris-quit))
+                           (buffer-substring-no-properties (goto-char (point-min))
+                                                           (line-end-position)))))
+     (setq-local header-line-format highest-score)))
+ :cleanup-fn
+ (lambda ()
+   (when-let ((saves-buf (get-file-buffer (expand-file-name "tetris-scores" shared-game-score-directory))))
+     (with-current-buffer saves-buf
+       (save-buffer)
+       (kill-buffer)))))
