cb7f05772544bdb99da7414faca22bae503f0860
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 15:19:24 -0500

    modules/cae/misc-applications/autoload/bubbles.el

--- modules/cae/misc-applications/autoload/bubbles.el
+++ modules/cae/misc-applications/autoload/bubbles.el
@@ -4,7 +4,7 @@
 ;;;###autoload (autoload 'cae-bubbles-quit "cae/misc-applications/autoload/bubbles" nil t)
 
 (cae-define-launcher
-  cae-bubbles
-  :launch-fn #'bubbles
-  :buffer-name "*bubbles*"
-  :workspace-name cae-bubbles-workspace-name)
+ cae-bubbles
+ :launch-fn #'bubbles
+ :buffer-name "*bubbles*"
+ :workspace-name cae-bubbles-workspace-name)
