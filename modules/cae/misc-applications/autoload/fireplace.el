b36723b97f110c8c71826f329d72e3523bcfd893
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 15:20:09 -0500

    modules/cae/misc-applications/autoload/fireplace.el

--- modules/cae/misc-applications/autoload/fireplace.el
+++ modules/cae/misc-applications/autoload/fireplace.el
@@ -1,7 +1,7 @@
+;;; private/misc-applications/autoload/fireplace.el -*- lexical-binding: t; -*-
+
 ;;;###autoload (autoload 'cae-fireplace "cae/misc-applications/autoload/fireplace" nil t)
 ;;;###autoload (autoload 'cae-fireplace-quit "cae/misc-applications/autoload/fireplace" nil t)
-
-;;; private/misc-applications/autoload/fireplace.el -*- lexical-binding: t; -*-
 
 (cae-define-launcher
   cae-fireplace
