9437ff45fc0a4507f0d9be7ad8ba7ab42d15a831
StrawberryTea <look@strawberrytea.xyz>
2025-01-20 15:20:44 -0500

    modules/cae/misc-applications/autoload/speed-type.el

--- modules/cae/misc-applications/autoload/speed-type.el
+++ modules/cae/misc-applications/autoload/speed-type.el
@@ -4,7 +4,7 @@
 ;;;###autoload (autoload 'cae-speed-type-text-quit "cae/misc-applications/autoload/speed-type" nil t)
 
 (cae-define-launcher
-  cae-speed-type-text
-  :launch-fn #'speed-type-text
-  :buffer-name "speed-type"
-  :workspace-name cae-speed-type-workspace-name)
+ cae-speed-type-text
+ :launch-fn #'speed-type-text
+ :buffer-name "speed-type"
+ :workspace-name cae-speed-type-workspace-name)
