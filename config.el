5db0132d71649d89539b193eca9c20ad3c17d72b
StrawberryTea <look@strawberrytea.xyz>
2025-01-23 11:47:30 -0500

    config.el

--- config.el
+++ config.el
@@ -1265,12 +1265,11 @@
   (after! comint
     (setq comint-history-isearch 'dwim
           comint-buffer-maximum-size 8192))
-  (after! imenu
-    (setq-hook! 'comint-mode-hook
-      imenu-generic-expression
-      `(("Prompts"
-         ,(concat comint-prompt-regexp "\\(.*\\)")
-         1))))
+  (setq-hook! 'comint-mode-hook
+    imenu-generic-expression
+    `(("Prompts"
+       ,(concat comint-prompt-regexp "\\(.*\\)")
+       1)))
 
   (after! em-term
     ;; Some of the commands I copied from other configurations and will likely
