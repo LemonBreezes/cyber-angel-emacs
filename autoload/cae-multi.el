19bd625f9dfd3874c000b0cb6acee37b379cf243
StrawberryTea <look@strawberrytea.xyz>
2025-01-30 00:23:36 -0500

    autoload/cae-multi.el

--- autoload/cae-multi.el
+++ autoload/cae-multi.el
@@ -35,3 +35,25 @@
       (funcall orig-fun file verbose)
       (setq cae-multi-abbrev--file-mtime mtime
             cae-multi-abbrev--auto-commit-disabled t))))
+
+;;;###autoload
+(defun cae-multi-pull-repositories ()
+  "Pull the shared repositories and handle conflicts."
+  (require 'org)
+  (dolist (repo-dir cae-multi-repositories)
+    (let ((default-directory repo-dir))
+      (when (file-directory-p (concat repo-dir "/.git"))
+        (if (file-exists-p (concat repo-dir "/.git/index.lock"))
+            (message "Git lockfile exists in %s, skipping pull" repo-dir)
+          (with-temp-buffer
+            (let ((exit-code (call-process "git" nil (current-buffer) nil "pull")))
+              (if (/= exit-code 0)
+                  (progn
+                    (message "Git pull failed in %s with exit code %d" repo-dir exit-code)
+                    (display-buffer (current-buffer)))
+                (goto-char (point-min))
+                (if (re-search-forward "CONFLICT" nil t)
+                    (progn
+                      (message "Conflict detected during git pull in %s" repo-dir)
+                      (display-buffer (current-buffer)))
+                  (message "Git pull succeeded in %s" repo-dir))))))))))
