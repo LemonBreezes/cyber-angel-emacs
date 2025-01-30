57cb95ede77131daae76ab017be18546549c4311
StrawberryTea <look@strawberrytea.xyz>
2025-01-30 18:13:00 -0500

    modules/cae/ai/config.el

--- modules/cae/ai/config.el
+++ modules/cae/ai/config.el
@@ -177,3 +177,34 @@
           :n "p" #'gptel-context-previous
           :n "d" #'gptel-context-flag-deletion
           :n "RET" #'gptel-context-visit)))
+
+(use-package! minuet
+  :bind
+  (("M-y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
+   ("M-i" . #'minuet-show-suggestion) ;; use overlay for completion
+
+   :map minuet-active-mode-map
+   ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
+   ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
+   ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
+   ("M-A" . #'minuet-accept-suggestion) ;; accept whole completion
+   ;; Accept the first line of completion, or N lines with a numeric-prefix:
+   ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
+   ("M-a" . #'minuet-accept-suggestion-line)
+   ("M-e" . #'minuet-dismiss-suggestion))
+
+  :init
+  ;; if you want to enable auto suggestion.
+  ;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
+  (add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
+  (add-hook 'text-mode-hook #'minuet-auto-suggestion-mode)
+  (add-hook 'conf-mode-hook #'minuet-auto-suggestion-mode)
+
+  :config
+  (setq minuet-provider 'openai-fim-compatible)
+
+  ;; Required when defining minuet-ative-mode-map in insert/normal states.
+  ;; Not required when defining minuet-active-mode-map without evil state.
+  (add-hook 'minuet-active-mode-hook #'evil-normalize-keymaps)
+
+  (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 256))
