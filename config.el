6259df16f691b0e8b4c9f77c582cbe54fd0198ca
StrawberryTea <look@strawberrytea.xyz>
2025-01-30 13:37:13 -0500

    config.el

--- config.el
+++ config.el
@@ -1235,9 +1235,7 @@
       (rainbow-delimiters-mode -1)))
   (setq-hook! 'comint-mode-hook
     imenu-generic-expression
-    `(("Prompts"
-       ,(concat comint-prompt-regexp "\\(.*\\)")
-       1)))
+    `(("Prompts" ,(concat comint-prompt-regexp "\\(.*\\)") 1)))
 
   (after! em-term
     ;; Some of the commands I copied from other configurations and will likely
