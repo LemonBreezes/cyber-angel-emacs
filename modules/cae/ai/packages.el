e9b6455373cbdb9c435a106e3c4b22ec526dc6d5
StrawberryTea <look@strawberrytea.xyz>
2025-01-30 18:07:30 -0500

    modules/cae/ai/packages.el

--- modules/cae/ai/packages.el
+++ modules/cae/ai/packages.el
@@ -3,9 +3,12 @@
 
 (package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
 
-(package! copilot :recipe (:host github :repo "copilot-emacs/copilot.el"))
 (package! gptel)
 (package! aider :recipe (:host github :repo "tninja/aider.el"))
 (package! magit-gptcommit)
 (package! chatgpt-shell)
 (package! dall-e-shell)
+(when (modulep! +copilot)
+  (package! copilot :recipe (:host github :repo "copilot-emacs/copilot.el")))
+(when (modulep! -copilot)
+  (package!))
