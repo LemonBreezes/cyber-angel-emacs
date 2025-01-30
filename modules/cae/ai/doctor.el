50fee4d0a27958e4fe1f8a6c4f6b2f4f55492a0f
StrawberryTea <look@strawberrytea.xyz>
2025-01-29 22:22:50 -0500

    .

---
+++ modules/cae/ai/doctor.el
@@ -0,0 +1,4 @@
+;;; cae/ai/doctor.el -*- lexical-binding: t; -*-
+
+(unless (or (not (modulep! +copilot)) (executable-find "node"))
+  (warn! "Couldn't find node executable. Copilot code completion is disabled."))
