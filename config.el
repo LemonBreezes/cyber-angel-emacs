91269135700e3a34d6cbebd51561119a9da1bac5
StrawberryTea <look@strawberrytea.xyz>
2025-01-30 23:40:02 -0500

    config.el

--- config.el
+++ config.el
@@ -769,8 +769,8 @@
   (undelete-frame-mode +1)
 
   ;; Automatically deindent items when they are added to the kill ring.
-  (when (fboundp #'kill-ring-deindent-mode)
-    (add-hook 'doom-first-input-hook #'kill-ring-deindent-mode))
+  ;;(when (fboundp #'kill-ring-deindent-mode)
+  ;;  (add-hook 'doom-first-input-hook #'kill-ring-deindent-mode))
 
   (after! paren
     (setq show-paren-context-when-offscreen 'overlay
