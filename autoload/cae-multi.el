cbe2804312f6a2262f45787da4e38004ce737cfe
StrawberryTea <look@strawberrytea.xyz>
2025-01-30 22:09:44 -0500

    autoload/cae-multi.el

--- autoload/cae-multi.el
+++ autoload/cae-multi.el
@@ -21,16 +21,17 @@
   (let ((mtime (nth 5 (file-attributes abbrev-file-name))))
     (if (or (null cae-multi-abbrev--file-mtime)
             (equal mtime cae-multi-abbrev--file-mtime))
-        (progn (funcall orig-fun file verbose)
-               (setq cae-multi-abbrev--file-mtime mtime)
-               (unless file
-                 (if cae-multi-abbrev--auto-commit-disabled
-                     (when (y-or-n-p
-                            (concat "Abbrev file modified since a previous "
-                                    "save. Enable auto-commit?"))
-                       (setq cae-multi-abbrev--auto-commit-disabled nil)
-                       (cae-multi-commit-file abbrev-file-name))
-                   (cae-multi-commit-file abbrev-file-name))))
+        (progn
+          (funcall orig-fun file verbose)
+          (setq cae-multi-abbrev--file-mtime mtime)
+          (unless file
+            (if cae-multi-abbrev--auto-commit-disabled
+                (when (y-or-n-p
+                       (concat "Abbrev file modified since a previous "
+                               "save. Enable auto-commit?"))
+                  (setq cae-multi-abbrev--auto-commit-disabled nil)
+                  (cae-multi-commit-file abbrev-file-name))
+              (cae-multi-commit-file abbrev-file-name))))
       (message (concat "Abbrev file modified since last save. "
                        "Disabling abbrev file auto-commit."))
       (funcall orig-fun file verbose)
@@ -73,43 +74,44 @@
                                (message "Conflict detected during git pull in %s" repo-dir)
                                (display-buffer output-buffer)
                                (setq all-pulls-succeeded nil))
-                           (message "Git pull succeeded in %s" repo-dir))
-                         ;; Start git submodule update --init --recursive
-                         (let ((submodule-process
-                                (start-process
-                                 "git-submodule-update-process"
-                                 output-buffer
-                                 "git" "submodule" "update" "--init" "--recursive")))
-                           (push submodule-process processes)
-                           (set-process-sentinel
-                            submodule-process
-                            (lambda (proc event)
-                              (when (memq (process-status proc) '(exit signal))
-                                (if (/= (process-exit-status proc) 0)
+                           (message "Git pull succeeded in %s" repo-dir))))
+                     ;; Start git submodule update --init --recursive
+                     (let ((submodule-process
+                            (start-process
+                             "git-submodule-update-process"
+                             output-buffer
+                             "git" "submodule" "update" "--init" "--recursive")))
+                       (push submodule-process processes)
+                       (set-process-sentinel
+                        submodule-process
+                        (lambda (proc event)
+                          (when (memq (process-status proc) '(exit signal))
+                            (if (/= (process-exit-status proc) 0)
+                                (progn
+                                  (message "Git submodule update failed in %s" repo-dir)
+                                  ;; Optionally display the output buffer
+                                  (display-buffer output-buffer)
+                                  (setq all-pulls-succeeded nil))
+                              (message "Git submodule update succeeded in %s" repo-dir))
+                            ;; Check for conflicts in submodule update
+                            (with-current-buffer output-buffer
+                              (save-excursion
+                                (goto-char (point-max))
+                                (if (re-search-backward "CONFLICT" nil t)
                                     (progn
-                                      (message "Git submodule update failed in %s" repo-dir)
-                                      ;; Optionally display the output buffer
+                                      (message "Conflict detected during git submodule update in %s" repo-dir)
                                       (display-buffer output-buffer)
                                       (setq all-pulls-succeeded nil))
-                                  (message "Git submodule update succeeded in %s" repo-dir))
-                                ;; Check for conflicts in submodule update
-                                (with-current-buffer output-buffer
-                                  (save-excursion
-                                    (goto-char (point-max))
-                                    (if (re-search-backward "CONFLICT" nil t)
-                                        (progn
-                                          (message "Conflict detected during git submodule update in %s" repo-dir)
-                                          (display-buffer output-buffer)
-                                          (setq all-pulls-succeeded nil))
-                                      (message "Submodules updated successfully in %s" repo-dir))))
-                                ;; When all processes have finished, run 'doom sync' if needed
-                                (when (and (null (delq proc (cl-remove-if #'process-live-p processes)))
-                                           all-pulls-succeeded)
-                                  (cae-multi--run-doom-sync))))))))
+                                  (message "Submodules updated successfully in %s" repo-dir))))
+                            ;; When all processes have finished, run 'doom sync' if needed
+                            (when (and (null (delq proc (cl-remove-if #'process-live-p processes)))
+                                       all-pulls-succeeded)
+                              (cae-multi--run-doom-sync)))))))
                    ;; When all processes have finished, run 'doom sync' if needed
-                   (when (and (null (delq proc (cl-remove-if #'process-live-p processes)))
+                   (when (and (null (delq process (cl-remove-if #'process-live-p processes)))
                               all-pulls-succeeded)
                      (cae-multi--run-doom-sync))))))))))))
+
 (defun cae-multi--run-doom-sync ()
   "Run 'doom sync' asynchronously and redirect output to the output buffer."
   (let ((process
