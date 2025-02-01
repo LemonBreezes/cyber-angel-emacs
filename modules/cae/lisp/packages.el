72b3cd1ff8dfebc62c1b5ade2e3f72635423ec9f
StrawberryTea <look@strawberrytea.xyz>
2025-02-01 10:29:54 -0500

    modules/cae/lisp/packages.el

--- modules/cae/lisp/packages.el
+++ modules/cae/lisp/packages.el
@@ -3,4 +3,6 @@
 
 (package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
 
+(when (modulep! :editor lispy)
+  (package! lispy :recipe (:host github :repo "enzuru/lispy")))
 (package! xr)
