c8be857e2c07d466e099c293fe91c9e0dc55d22c
StrawberryTea <look@strawberrytea.xyz>
2025-02-01 18:32:31 -0500

    modules/cae/lisp/config.el

--- modules/cae/lisp/config.el
+++ modules/cae/lisp/config.el
@@ -30,52 +30,6 @@
   (map! :map lispy-mode-map
         [remap lispy-newline-and-indent-plain] #'cae-lisp-newline-and-indent))
 
-;;;; Use Emacs Lisp mode for dir-locals files. This will make them easier to edit.
-;;(add-to-list 'auto-mode-alist (cons (regexp-quote dir-locals-file)
-;;                                    'emacs-lisp-mode))
-
-;; Use my own Imenu expression instead of Doom's. For mine, I do not count
-;; comments like ";; This code does ..." as sections.
-;;(advice-add #'+emacs-lisp-extend-imenu-h :override #'cae-emacs-lisp-extend-imenu-h)
-
-;;;; Check parens before saving.
-;;(after! smartparens
-;;  (dolist (mode '(cider-repl-mode
-;;                  clojure-mode
-;;                  clojurec-mode
-;;                  clojurescript-mode
-;;                  clojurex-mode
-;;                  clojure-ts-mode
-;;                  clojurescript-ts-mode
-;;                  clojurec-ts-mode
-;;                  common-lisp-mode
-;;                  emacs-lisp-mode
-;;                  eshell-mode
-;;                  fennel-mode
-;;                  fennel-repl-mode
-;;                  geiser-repl-mode
-;;                  gerbil-mode
-;;                  inf-clojure-mode
-;;                  inferior-emacs-lisp-mode
-;;                  inferior-lisp-mode
-;;                  inferior-scheme-mode
-;;                  lisp-interaction-mode
-;;                  lisp-mode
-;;                  monroe-mode
-;;                  racket-mode
-;;                  racket-repl-mode
-;;                  scheme-interaction-mode
-;;                  scheme-mode
-;;                  slime-repl-mode
-;;                  sly-mrepl-mode
-;;                  stumpwm-mode
-;;                  ))
-;;    (add-hook (intern (concat (symbol-name mode) "-hook")) #'cae-lisp-check-parens-before-save-h)))
-;;
-;;;; This fixes aggressive indent's indentation of plists. :)
-;;(after! lisp-mode
-;;  (defalias 'lisp-indent-function '+emacs-lisp-indent-function))
-
 (when (modulep! :editor lispy)
   (after! lispy
     (add-hook! 'doom-escape-hook
