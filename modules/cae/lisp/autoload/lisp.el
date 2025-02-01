2868cb124d3a7e628aefa985995084a421e31f6e
StrawberryTea <look@strawberrytea.xyz>
2025-02-01 18:34:32 -0500

    modules/cae/lisp/autoload/lisp.el

--- modules/cae/lisp/autoload/lisp.el
+++ modules/cae/lisp/autoload/lisp.el
@@ -1,10 +1,6 @@
 ;;; cae/lisp/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun cae-lisp-check-parens-before-save-h ()
-  (add-hook 'write-file-functions #'check-parens nil t))
-
-;;;###autoload
 (defun cae-lisp-newline-and-indent ()
   (interactive)
   (cond ((and (minibufferp nil t)
