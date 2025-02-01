0115f64e3b5f07ed2aa82ebe1d118012eb04093c
StrawberryTea <look@strawberrytea.xyz>
2025-02-01 12:50:22 -0500

    lisp/cae-visual-scrolling.el

--- lisp/cae-visual-scrolling.el
+++ lisp/cae-visual-scrolling.el
@@ -1,5 +1,28 @@
 ;;; lisp/cae-visual-scrolling.el -*- lexical-binding: t; -*-
 
+;;Error: wrong-type-argument (overlayp nil)
+;;  whitespace-point--flush-used(113479)
+;;  font-lock-fontify-keywords-region(113424 113479 nil)
+;;  font-lock-default-fontify-region(113424 113479 nil)
+;;  font-lock-fontify-region(113424 113479)
+;;  #f(compiled-function (fun) #<bytecode 0x1ba31c79f97a0c7f>)(font-lock-fontify-region)
+;;  jit-lock--run-functions(113424 113479)
+;;  jit-lock-fontify-now(113424 114924)
+;;  jit-lock-function(113424)
+;;  scroll-down(19)
+;;  scroll-down-command(19)
+;;  view-scroll-lines(nil t 19 t)
+;;  #<subr View-scroll-half-page-backward>(nil)
+;;  funcall(#<subr View-scroll-half-page-backward> nil)
+;;  (let ((pos (if (eq direction 'down) (1- (window-end)) (window-start))) (pulse-delay 0.1)) (pulse-momentary-highlight-one-line pos 'highlight) (sit-for 0.1) (funcall scroll-fn arg))
+;;  (if (or arg (cl-some #'(lambda (x) (and (boundp x) (symbol-value x))) '(executing-kbd-macro))) (funcall scroll-fn arg) (let ((pos (if (eq direction 'down) (1- (window-end)) (window-start))) (pulse-delay 0.1)) (pulse-momentary-highlight-one-line pos 'highlight) (sit-for 0.1) (funcall scroll-fn arg)))
+;;  cae-scroll-with-highlight(#<subr View-scroll-half-page-backward> up nil)
+;;  #f(lambda (f &optional arg) [t] (cae-scroll-with-highlight f 'up arg))(#<subr View-scroll-half-page-backward> nil)
+;;  apply(#f(lambda (f &optional arg) [t] (cae-scroll-with-highlight f 'up arg)) #<subr View-scroll-half-page-backward> nil)
+;;  View-scroll-half-page-backward(nil)
+;;  funcall-interactively(View-scroll-half-page-backward nil)
+;;  command-execute(View-scroll-half-page-backward)
+
 (defun cae-scroll-with-highlight (scroll-fn direction &optional arg)
   "Highlight the appropriate line before calling SCROLL-FN with ARGS.
 DIRECTION should be 'up or 'down."
