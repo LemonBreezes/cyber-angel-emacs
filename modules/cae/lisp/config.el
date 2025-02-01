ee8f00ba6c190bcaecfeefd86f673a8ae6ed5ab2
StrawberryTea <look@strawberrytea.xyz>
2025-02-01 18:31:47 -0500

    modules/cae/lisp/config.el

--- modules/cae/lisp/config.el
+++ modules/cae/lisp/config.el
@@ -38,40 +38,40 @@
 ;; comments like ";; This code does ..." as sections.
 ;;(advice-add #'+emacs-lisp-extend-imenu-h :override #'cae-emacs-lisp-extend-imenu-h)
 
-;;;; Check parens before saving.
-;;(after! smartparens
-;;  (dolist (mode '(cider-repl-mode
-;;                  clojure-mode
-;;                  clojurec-mode
-;;                  clojurescript-mode
-;;                  clojurex-mode
-;;                  clojure-ts-mode
-;;                  clojurescript-ts-mode
-;;                  clojurec-ts-mode
-;;                  common-lisp-mode
-;;                  emacs-lisp-mode
-;;                  eshell-mode
-;;                  fennel-mode
-;;                  fennel-repl-mode
-;;                  geiser-repl-mode
-;;                  gerbil-mode
-;;                  inf-clojure-mode
-;;                  inferior-emacs-lisp-mode
-;;                  inferior-lisp-mode
-;;                  inferior-scheme-mode
-;;                  lisp-interaction-mode
-;;                  lisp-mode
-;;                  monroe-mode
-;;                  racket-mode
-;;                  racket-repl-mode
-;;                  scheme-interaction-mode
-;;                  scheme-mode
-;;                  slime-repl-mode
-;;                  sly-mrepl-mode
-;;                  stumpwm-mode
-;;                  ))
-;;    (add-hook (intern (concat (symbol-name mode) "-hook")) #'cae-lisp-check-parens-before-save-h)))
-;;
+;; Check parens before saving.
+(after! smartparens
+  (dolist (mode '(cider-repl-mode
+                  clojure-mode
+                  clojurec-mode
+                  clojurescript-mode
+                  clojurex-mode
+                  clojure-ts-mode
+                  clojurescript-ts-mode
+                  clojurec-ts-mode
+                  common-lisp-mode
+                  emacs-lisp-mode
+                  eshell-mode
+                  fennel-mode
+                  fennel-repl-mode
+                  geiser-repl-mode
+                  gerbil-mode
+                  inf-clojure-mode
+                  inferior-emacs-lisp-mode
+                  inferior-lisp-mode
+                  inferior-scheme-mode
+                  lisp-interaction-mode
+                  lisp-mode
+                  monroe-mode
+                  racket-mode
+                  racket-repl-mode
+                  scheme-interaction-mode
+                  scheme-mode
+                  slime-repl-mode
+                  sly-mrepl-mode
+                  stumpwm-mode
+                  ))
+    (add-hook (intern (concat (symbol-name mode) "-hook")) #'cae-lisp-check-parens-before-save-h)))
+
 ;;;; This fixes aggressive indent's indentation of plists. :)
 ;;(after! lisp-mode
 ;;  (defalias 'lisp-indent-function '+emacs-lisp-indent-function))
