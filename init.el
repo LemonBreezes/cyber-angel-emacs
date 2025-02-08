1c0b21bdb4f0e5611794e3b785696377227c8cd0
StrawberryTea <look@strawberrytea.xyz>
2025-02-08 01:12:45 -0500

    init.el

--- init.el
+++ init.el
@@ -36,10 +36,11 @@
   ;; Add hostname to the modeline.
   (if (and global-mode-string (listp global-mode-string))
       (appendq! global-mode-string
-                (list (truncate-string-to-width
-                       (secure-hash 'sha512 system-name) 6)))
+                (list (concat " " (truncate-string-to-width
+                       (secure-hash 'sha512 system-name) 6))))
     (setq global-mode-string
-          (list (truncate-string-to-width (secure-hash 'sha512 system-name) 6))))
+          (list (concat " " (truncate-string-to-width
+                             (secure-hash 'sha512 system-name) 6)))))
 
   ;; Set up the secrets directory and its module path.
   (defvar cae-multi-secrets-dir (expand-file-name "secrets/" doom-user-dir))
