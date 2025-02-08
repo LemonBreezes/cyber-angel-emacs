db6d30bfbaf48819e4ba41921b6c7b7d7f712485
StrawberryTea <look@strawberrytea.xyz>
2025-02-07 22:42:35 -0500

    config.el

--- config.el
+++ config.el
@@ -9,8 +9,6 @@
   (when (file-exists-p (concat cae-multi-secrets-dir "secrets.el"))
     (load! (concat cae-multi-secrets-dir "secrets.el")))
 
-  (defvar cae-multi-org-dir "~/org/")
-
   ;; Ensure this is defined even if its module is not loaded.
   (unless (boundp '+default-minibuffer-maps)
     (defvar +default-minibuffer-maps
