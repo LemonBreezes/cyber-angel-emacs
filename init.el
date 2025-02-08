539cfe57253df5cb0591d949b87993c5bc31530e
StrawberryTea <look@strawberrytea.xyz>
2025-02-08 01:09:29 -0500

    init.el

--- init.el
+++ init.el
@@ -37,158 +37,158 @@
   (if (and global-mode-string (listp global-mode-string))
       (appendq! x
                 (string-truncate-left (secure-hash 'sha512 system-name) 6)
-    (setq global-mode-string
-          (list (string-truncate-left (secure-hash 'sha512 system-name) 6))))
+                (setq global-mode-string
+                      (list (string-truncate-left (secure-hash 'sha512 system-name) 6))))
 
-  ;; Set up the secrets directory and its module path.
-  (defvar cae-multi-secrets-dir (expand-file-name "secrets/" doom-user-dir))
-  (make-directory cae-multi-secrets-dir t)
-  (defvar cae-multi-secrets-modules-dir (concat cae-multi-secrets-dir "modules/"))
-  (make-directory cae-multi-secrets-modules-dir t)
-  (add-to-list 'doom-module-load-path cae-multi-secrets-modules-dir)
+    ;; Set up the secrets directory and its module path.
+    (defvar cae-multi-secrets-dir (expand-file-name "secrets/" doom-user-dir))
+    (make-directory cae-multi-secrets-dir t)
+    (defvar cae-multi-secrets-modules-dir (concat cae-multi-secrets-dir "modules/"))
+    (make-directory cae-multi-secrets-modules-dir t)
+    (add-to-list 'doom-module-load-path cae-multi-secrets-modules-dir)
 
-  ;; This is where I clone Git projects to.
-  (defvar cae-repo-dir "~/src/")
+    ;; This is where I clone Git projects to.
+    (defvar cae-repo-dir "~/src/")
 
-  (when (and (>= (num-processors) 32)
-             (not (eq system-type 'windows-nt))
-             ;; Experimental incremental garbage collector.
-             (not (functionp 'igc-collect))
-             ;; 180GB of RAM.
-             (> (car (memory-info))
-                (* 180 1024 1024)))
-    (let ((cae-gc-cons-threshold (* 128 1024 1024 1024)))
-      (setq gcmh-high-cons-threshold cae-gc-cons-threshold
-            consult--gc-threshold cae-gc-cons-threshold
-            cae-hacks-gc-cons-threshold cae-gc-cons-threshold
-            +lsp--default-gcmh-high-cons-threshold cae-gc-cons-threshold)))
+    (when (and (>= (num-processors) 32)
+               (not (eq system-type 'windows-nt))
+               ;; Experimental incremental garbage collector.
+               (not (functionp 'igc-collect))
+               ;; 180GB of RAM.
+               (> (car (memory-info))
+                  (* 180 1024 1024)))
+      (let ((cae-gc-cons-threshold (* 128 1024 1024 1024)))
+        (setq gcmh-high-cons-threshold cae-gc-cons-threshold
+              consult--gc-threshold cae-gc-cons-threshold
+              cae-hacks-gc-cons-threshold cae-gc-cons-threshold
+              +lsp--default-gcmh-high-cons-threshold cae-gc-cons-threshold)))
 
-  ;; Do not start incrementally loading packages until I am actually AFK.
-  (setq doom-incremental-first-idle-timer 60)
+    ;; Do not start incrementally loading packages until I am actually AFK.
+    (setq doom-incremental-first-idle-timer 60)
 
-  (setq native-comp-async-jobs-number (num-processors)
-        native-comp-async-report-warnings-errors 'silent)
+    (setq native-comp-async-jobs-number (num-processors)
+          native-comp-async-report-warnings-errors 'silent)
 
-  (load! "lisp/cae-debug")
-  (load! "lisp/cae-hacks")
+    (load! "lisp/cae-debug")
+    (load! "lisp/cae-hacks")
 
-  ;; Set a fallback theme.
-  (setq doom-theme 'wheatgrass)
+    ;; Set a fallback theme.
+    (setq doom-theme 'wheatgrass)
 
-  (setq evil-undo-system 'undo-fu)
+    (setq evil-undo-system 'undo-fu)
 
-  ;; Make it easier to debug lazy loading issues.
-  (when init-file-debug (setq doom-incremental-first-idle-timer nil))
+    ;; Make it easier to debug lazy loading issues.
+    (when init-file-debug (setq doom-incremental-first-idle-timer nil))
 
-  ;; I never want to see loading messages.
-  (unless init-file-debug
-    (defadvice! cae-load-ignore-message-a (args) :filter-args #'load
-      (cl-destructuring-bind (file &optional noerror nomessage nosuffix must-suffix) args
-        (list file noerror t nosuffix must-suffix))))
+    ;; I never want to see loading messages.
+    (unless init-file-debug
+      (defadvice! cae-load-ignore-message-a (args) :filter-args #'load
+        (cl-destructuring-bind (file &optional noerror nomessage nosuffix must-suffix) args
+          (list file noerror t nosuffix must-suffix))))
 
-  (setq doom-leader-alt-key "<menu>"
-        doom-localleader-alt-key "<menu> m"))
+    (setq doom-leader-alt-key "<menu>"
+          doom-localleader-alt-key "<menu> m"))
 
-(doom! :completion
-       (vertico +icons +childframe)
-       (corfu +orderless +icons +dabbrev)
+  (doom! :completion
+         (vertico +icons +childframe)
+         (corfu +orderless +icons +dabbrev)
 
-       :ui
-       hl-todo
-       (hydra +childframe)
-       (popup +defaults)
-       (vc-gutter +pretty +diff-hl)
-       vi-tilde-fringe
-       workspaces
-       (window-select +numbers)
-       ophints
-       (treemacs +lsp)
+         :ui
+         hl-todo
+         (hydra +childframe)
+         (popup +defaults)
+         (vc-gutter +pretty +diff-hl)
+         vi-tilde-fringe
+         workspaces
+         (window-select +numbers)
+         ophints
+         (treemacs +lsp)
 
-       :editor
-       file-templates
-       fold
-       (format +lsp)
-       snippets
-       multiple-cursors
-       (evil +everywhere)
-       lispy
+         :editor
+         file-templates
+         fold
+         (format +lsp)
+         snippets
+         multiple-cursors
+         (evil +everywhere)
+         lispy
 
-       :emacs
-       undo
-       ibuffer
-       vc
-       (dired +icons +dirvish)
+         :emacs
+         undo
+         ibuffer
+         vc
+         (dired +icons +dirvish)
 
-       :term
-       eshell
-       (:if (not (memq system-type '(cygwin windows-nt ms-dos))) vterm)
+         :term
+         eshell
+         (:if (not (memq system-type '(cygwin windows-nt ms-dos))) vterm)
 
-       :checkers
-       (syntax +childframe +icons)
+         :checkers
+         (syntax +childframe +icons)
 
-       :tools
-       (debugger +lsp)
-       direnv
-       ;;editorconfig ;; Now built into Emacs.
-       (eval +overlay)
-       (lookup +dictionary +docsets +offline)
-       (magit +forge)
-       (lsp +peek)
-       tree-sitter
-       (docker +lsp)
-       pdf
-       biblio
+         :tools
+         (debugger +lsp)
+         direnv
+         ;;editorconfig ;; Now built into Emacs.
+         (eval +overlay)
+         (lookup +dictionary +docsets +offline)
+         (magit +forge)
+         (lsp +peek)
+         tree-sitter
+         (docker +lsp)
+         pdf
+         biblio
 
-       :lang
-       (emacs-lisp +tree-sitter)
-       (org +roam2 +tree-sitter +dragndrop +journal)
-       (cc +lsp +tree-sitter)
-       (web +lsp +tree-sitter)
-       (sh +lsp +tree-sitter)
-       (nix +lsp +tree-sitter)
-       (lua +lsp +tree-sitter +fennel)
-       (data +lsp +tree-sitter)
-       (latex +lsp +tree-sitter +cdlatex +latexmk +fold)
-       (json +lsp +tree-sitter)
-       (python +lsp +tree-sitter +pyright +cython)
-       (haskell +lsp +tree-sitter)
+         :lang
+         (emacs-lisp +tree-sitter)
+         (org +roam2 +tree-sitter +dragndrop +journal)
+         (cc +lsp +tree-sitter)
+         (web +lsp +tree-sitter)
+         (sh +lsp +tree-sitter)
+         (nix +lsp +tree-sitter)
+         (lua +lsp +tree-sitter +fennel)
+         (data +lsp +tree-sitter)
+         (latex +lsp +tree-sitter +cdlatex +latexmk +fold)
+         (json +lsp +tree-sitter)
+         (python +lsp +tree-sitter +pyright +cython)
+         (haskell +lsp +tree-sitter)
 
-       :os
-       (:if IS-MAC macos)
-       (:if (not (memq system-type '(cygwin windows-nt ms-dos))) tty +osc)
+         :os
+         (:if IS-MAC macos)
+         (:if (not (memq system-type '(cygwin windows-nt ms-dos))) tty +osc)
 
-       :config
-       (default +bindings +smartparens +gnupg)
+         :config
+         (default +bindings +smartparens +gnupg)
 
-       :email
-       mu4e
+         :email
+         mu4e
 
-       :app
-       (rss +org)
-       irc
+         :app
+         (rss +org)
+         irc
 
-       :cae
-       (lean4 +lsp +tree-sitter)
-       (helm +childframe)
-       debugger
-       eshell
-       exwm
-       dired
-       lisp
-       misc-applications
-       (modeline +emms)
-       notifications
-       unpackaged
-       vc
-       org
-       (ai +copilot)
-       gnus
-       rss
-       denote
+         :cae
+         (lean4 +lsp +tree-sitter)
+         (helm +childframe)
+         debugger
+         eshell
+         exwm
+         dired
+         lisp
+         misc-applications
+         (modeline +emms)
+         notifications
+         unpackaged
+         vc
+         org
+         (ai +copilot)
+         gnus
+         rss
+         denote
 
-       :secret
-       root)
+         :secret
+         root)
 
-;;Local Variables:
-;;eval: (when (featurep 'lispy) (lispy-mode -1))
-;;End:
+  ;;Local Variables:
+  ;;eval: (when (featurep 'lispy) (lispy-mode -1))
+  ;;End:
