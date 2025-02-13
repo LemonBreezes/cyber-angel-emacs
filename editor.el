ff281fc9f6971ff3f6a3a433dce2a346009c1881
StrawberryTea <look@strawberrytea.xyz>
2025-02-13 17:55:36 -0500

    .

--- editor.el
+++
@@ -1,8 +0,0 @@
-;;; Editor configuration
-
-(when cae-init-editor-enabled-p
-  (load! "lisp/cae-repeat")
-  (load! "lisp/cae-vlf")
-  (load! "lisp/cae-restore-point")
-  ;; ... (Editor configuration continues) ...
-  (advice-add #'kill-this-buffer :around #'doom-set-jump-a))
