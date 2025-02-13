0443c5b0dac59678af46fbe6c15d5b6daa3c35a2
StrawberryTea <look@strawberrytea.xyz>
2025-02-13 17:55:36 -0500

    .

--- autocompletion.el
+++
@@ -1,9 +0,0 @@
-;;; Autocompletion configuration
-
-(when cae-init-autocompletion-enabled-p
-  (when (modulep! :completion corfu)
-    (load! "lisp/cae-corfu"))
-  (setq ido-save-directory-list-file (concat doom-cache-dir "ido.last"))
-  ;; ... (Autocompletion configuration continues) ...
-  (after! helm
-    (setq helm-split-window-default-side 'right)))
