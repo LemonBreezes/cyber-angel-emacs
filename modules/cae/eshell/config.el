e27d6f6740d58533b6f56f77ce5424704cfe3abf
StrawberryTea <look@strawberrytea.xyz>
2025-02-14 12:17:47 -0500

    modules/cae/eshell/config.el

--- modules/cae/eshell/config.el
+++ modules/cae/eshell/config.el
@@ -139,6 +139,19 @@
   :config
   (eshell-atuin-mode))
 
+(use-package detached
+  :init
+  (detached-init)
+  :bind (;; Replace `async-shell-command' with `detached-shell-command'
+         ([remap async-shell-command] . detached-shell-command)
+         ;; Replace `compile' with `detached-compile'
+         ([remap compile] . detached-compile)
+         ([remap recompile] . detached-compile-recompile)
+         ;; Replace built in completion of sessions with `consult'
+         ([remap detached-open-session] . detached-consult-session))
+  :custom ((detached-show-output-on-attach t)
+           (detached-terminal-data-command system-type)))
+
 ;;Local Variables:
 ;;eval: (unless (modulep! :cae eshell) (remove-hook 'write-file-functions #'eval-buffer t))
 ;;End:
