25008a62c33488863cef4fc046b9a57e6b126ae5
StrawberryTea <look@strawberrytea.xyz>
2025-02-14 12:18:54 -0500

    modules/cae/eshell/doctor.el

--- modules/cae/eshell/doctor.el
+++ modules/cae/eshell/doctor.el
@@ -3,3 +3,6 @@
 (unless (executable-find "node")
   (warn! (concat "Couldn't find atuin executable. "
                  "Atuin will not be used for shell history.")))
+(unless (executable-find "dtach")
+  (warn! (concat "Couldn't find dtach executable. "
+                 "Will not be able to detach shell commands from Emacs.")))
