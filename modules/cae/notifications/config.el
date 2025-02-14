51bb19d92140f1de34f71f66b5586b59e718536a
StrawberryTea <look@strawberrytea.xyz>
2025-02-14 17:57:36 -0500

    modules/cae/notifications/config.el

--- modules/cae/notifications/config.el
+++ modules/cae/notifications/config.el
@@ -2,9 +2,6 @@
 
 (use-package! ednc
   :defer t :init
-  ;; BUG Otherwise `alert-send-notification' will block the UI.
-  (advice-add #'alert-send-notification :around
-              #'cae-ednc-wrap-async-call-process-a)
   (defun cae-ednc-load-h ()
     (and (require 'dbus nil t)
          (not (getenv "INSIDE_EXWM"))   ; In EXWM I prefer using Dunst.
@@ -22,3 +19,12 @@
   (map! :map ednc-view-mode-map
         "n" #'next-line
         "p" #'previous-line))
+
+(use-package! alert
+  :defer t :config
+  ;; BUG Otherwise `alert-send-notification' will block the UI.
+  (advice-add #'alert-send-notification :around
+              #'cae-ednc-wrap-async-call-process-a)
+  (after! alert
+    (setq alert-default-style 'libnotify))
+  )
