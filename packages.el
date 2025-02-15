21ec8fb4a478e246421777c9ec43df37a7d2f26f
StrawberryTea <look@strawberrytea.xyz>
2025-02-15 18:50:58 -0500

    packages.el

--- packages.el
+++ packages.el
@@ -7,7 +7,7 @@
                    hl-line
                    elfeed-goodies
                    mu4e-alert)
-(unpin! consult) ; (void-function consult--process-collection)
+(unpin! consult) ; (void-function consult--process-collection) in consult-gh
 (unpin! helpful) ; Keep it up-to-date for bug fixes.
 (unpin! :lang :tools :emacs :email :input :term :checkers :ui :editor)
 (unpin! smartparens)
