cc2e78550abe471a204bc61572083496dce6269e
StrawberryTea <look@strawberrytea.xyz>
2025-02-14 19:42:16 -0500

    modules/cae/notifications/autoload/ednc.el

---
+++ modules/cae/notifications/autoload/ednc.el
@@ -0,0 +1,52 @@
+;;; cae/misc-applications/autoload/ednc.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun cae-ednc-show-notifications ()
+  (interactive)
+  (require 'ednc)
+  (let ((buf (get-buffer-create ednc-log-name)))
+    (pop-to-buffer buf)))
+
+;;;###autoload
+(defun cae-ednc-show-notification-in-buffer (old new)
+  (let ((name (format "*Notification %d*" (ednc-notification-id (or old new)))))
+    (with-current-buffer (get-buffer-create name)
+      (if new (let ((inhibit-read-only t))
+                (if old (erase-buffer) (ednc-view-mode))
+                (set-buffer-multibyte nil)
+                (insert (ednc-format-notification new t))
+                (display-buffer (current-buffer)))
+        (kill-buffer)))))
+
+;;;###autoload
+(defun cae-ednc-dismiss-all-notifications ()
+  (interactive)
+  (cl-mapcar (lambda (notification)
+               (ednc-dismiss-notification notification))
+             (ednc-notifications)))
+
+;;;###autoload
+(defun cae-ednc-toggle-notifications ()
+  (interactive)
+  (require 'ednc)
+  (cond ((ednc-notifications)
+         (cae-ednc-dismiss-all-notifications))
+        ((get-buffer-window ednc-log-name)
+         (delete-window (get-buffer-window ednc-log-name)))
+        (t (cae-ednc-show-notifications))))
+
+;;;###autoload
+(defun cae-ednc-stack-notifications (&optional hide)
+  (mapconcat (lambda (notification)
+               (let ((app-name (ednc-notification-app-name notification)))
+                 (unless (member app-name hide)
+                   (push app-name hide)
+                   (ednc-format-notification notification))))
+             (ednc-notifications) ""))
+
+;;;###autoload
+(defun cae-ednc-load-h ()
+  (and (require 'dbus nil t)
+       (not (getenv "INSIDE_EXWM"))   ; In EXWM I prefer using Dunst.
+       cae-dbus-notifications-supported-p
+       (ednc-mode +1)))
