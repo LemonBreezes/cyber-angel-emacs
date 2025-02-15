d4b5de07a6eb4056b7d67cd447261e52e8d2225f
StrawberryTea <look@strawberrytea.xyz>
2025-02-15 17:38:08 -0500

    benchmarks/cae-benchmark.el

--- benchmarks/cae-benchmark.el
+++ benchmarks/cae-benchmark.el
@@ -17,37 +17,3 @@
     (if (called-interactively-p)
         (message "Average line length (excluding empty lines) is %.02f" res)
       res)))
-
-(defun cae-compute-optimal-jit-lock-chunk-size ()
-  "Calculate an estimated optimal value for `jit-lock-chunk-size' based on window dimensions."
-  (save-excursion
-    (goto-char (window-start))
-    ;; Calculate optimal chunk size: window height * average line length (with spacing)
-    ;; Then, increase the estimated size by 10% to provide a buffer.
-    (floor (* (window-height)
-              (cae-average-line-length-excluding-empty-lines)
-              (1+ (or line-spacing 0))))))
-
-;;;###autoload
-(defun cae-set-jit-lock-chunk-size-to-optimal ()
-  "Set `jit-lock-chunk-size' to the computed optimal value."
-  (interactive)
-  (set-variable 'jit-lock-chunk-size (cae-compute-optimal-jit-lock-chunk-size))
-  (message "jit-lock-chunk-size set to optimal value of %d" jit-lock-chunk-size))
-
-(defun cae-benchmark-gc (&optional threshold)
-  (let ((start-time (current-time))
-        (gcs-done-old gcs-done)
-        (gc-cons-threshold (or threshold gc-cons-threshold)))
-    ;; Generate garbage until `gc-cons-threshold' is exceeded.
-    (while (>= gcs-done-old gcs-done)
-      (cl-loop for i from 0 to 1000 do
-               (make-string 1000 ?a)
-               (make-vector 1000 0)
-               (make-hash-table :test 'eq)
-               (make-symbol "a")))
-    (message "Garbage collection completed in %.06f seconds"
-             (float-time (time-subtract (current-time) start-time)))))
-
-;;(cae-benchmark-gc (* 32 1024 1024 1024))
-;;"Garbage collection completed in 6.178467 seconds"
