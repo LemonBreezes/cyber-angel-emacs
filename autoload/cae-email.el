7c0a7fd04b0f6868c70935fa274e583aefdb8906
StrawberryTea <look@strawberrytea.xyz>
2025-02-14 22:00:18 -0500

    autoload/cae-email.el

--- autoload/cae-email.el
+++ autoload/cae-email.el
@@ -6,3 +6,8 @@
   (if (bound-and-true-p +notmuch-workspace-name)
       (+workspace/kill +notmuch-workspace-name)
     (call-interactively #'notmuch-bury-or-kill-this-buffer)))
+
+;;;###autoload
+(defun cae-mu-init ()
+  (interactive)
+  (compilation-start "mu init --my-address=look@strawberrytea.xyz --maildir=/home/st/.mail && mu index"))
