90d9d76af4f30c5dc89fd40183556d683c1f6dc3
StrawberryTea <look@strawberrytea.xyz>
2025-02-15 17:38:40 -0500

    .

--- lisp/cae-semantic.el
+++
@@ -1,7 +0,0 @@
-;;; lisp/cae-semantic.el -*- lexical-binding: t; -*-
-
-;; Disable Semantic.
-(defadvice! cae-semantic-disable-a (&rest _)
-  :override #'semantic-mode
-  ;; If something tries to enable semantic-mode, emit a backtrace.
-  (backtrace))
