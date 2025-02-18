a8ad48717502d62226e68505c34081609f990d46
StrawberryTea <look@strawberrytea.xyz>
2025-02-17 21:46:32 -0500

    lisp/cae-evil.el

--- lisp/cae-evil.el
+++ lisp/cae-evil.el
@@ -113,9 +113,8 @@
 ;; Fix the garbage Doom minibuffer keybindings by enabling Evil states there.
 (unless (modulep! :editor evil +hybrid)
   (setq evil-want-minibuffer t)
-  (dolist (map +default-minibuffer-maps)
-    (map! :map map
-          :i "C-y" #'cae-yank-word-to-minibuffer))
+  (define-key! :states 'insert :keymaps +default-minibuffer-maps
+    "C-y"    #'cae-yank-word-to-minibuffer)
   (map! :map isearch-mode-map
         :i "C-y" #'isearch-yank-word-or-char))
 
