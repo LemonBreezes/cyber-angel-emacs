2043bac0dfbe024e621041d2b8c9b4b74842cacc
StrawberryTea <look@strawberrytea.xyz>
2025-02-17 21:27:40 -0500

    .

---
+++ modules/cae/dired/doctor.el
@@ -0,0 +1,12 @@
+;;; cae/dired/doctor.el -*- lexical-binding: t; -*-
+
+;; List of external compression/decompression programs used in the Dired config.
+(defvar cae-dired-compression-programs
+  '("pigz" "pbzip2" "pixz" "zstd" "rar"    ; compression programs used in dired-compress-file-alist
+    "plzip" "lzop" "pax"                  ; additional compression tools in dired-compress-files-alist
+    "uncompress" "dpkg-deb" "dictunzip" "pbunzip2" "unzip" "unzstd" "7z" "unrar") ; decompression commands
+  "List of programs required for Dired compression features.")
+
+(dolist (prog cae-dired-compression-programs)
+  (unless (executable-find prog)
+    (warn! "Couldn't find `%s`. Some compression/decompression features may be disabled." prog)))
