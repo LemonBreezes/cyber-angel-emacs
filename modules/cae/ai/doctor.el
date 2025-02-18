a176dc1151e25cd1b1f9c4ad87de77da2b743f92
StrawberryTea <look@strawberrytea.xyz>
2025-02-17 21:06:34 -0500

    modules/cae/ai/doctor.el

--- modules/cae/ai/doctor.el
+++ modules/cae/ai/doctor.el
@@ -3,13 +3,3 @@
 (unless (or (not (modulep! +copilot)) (executable-find "node"))
   (warn! "Couldn't find node executable. Copilot code completion is disabled."))
 
-;; List of external compression/decompression programs used in the Dired config.
-(defvar cae-dired-compression-programs
-  '("pigz" "pbzip2" "pixz" "zstd" "rar"    ; compression programs used in dired-compress-file-alist
-    "plzip" "lzop" "pax"                  ; additional compression tools in dired-compress-files-alist
-    "uncompress" "dpkg-deb" "dictunzip" "pbunzip2" "unzip" "unzstd" "7z" "unrar") ; decompression commands
-  "List of programs required for Dired compression features.")
-
-(dolist (prog cae-dired-compression-programs)
-  (unless (executable-find prog)
-    (warn! "Dired Compression Warning: `%s` not found. Some compression/decompression features may be disabled." prog)))
