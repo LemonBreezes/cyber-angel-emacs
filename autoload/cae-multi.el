08a9a244ace06eb6fcfd4231b4c3455829b9faab
StrawberryTea <look@strawberrytea.xyz>
2025-02-19 04:15:12 -0500

    autoload/cae-multi.el

--- autoload/cae-multi.el
+++ autoload/cae-multi.el
@@ -176,7 +176,9 @@ Then decrement the pending counter and, if zero, clear the running flag."
                                (lambda () (repo-sync-finished repo-dir))
                                output-buffer
                                verb-level
-                               (lambda () (message "Skipping push error for repository: %s" repo-dir))))
+                               (lambda ()
+                                 (when (>= verb-level 1)
+                                   (message "Skipping push error for repository: %s" repo-dir)))))
              (start-fetch-step (repo-dir)
                                (cae-multi--run-git-process
                                 repo-dir
