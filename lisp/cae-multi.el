771fbe0932e5a4bb671829f33042ccae1ee6fae7
StrawberryTea <look@strawberrytea.xyz>
2025-02-19 03:21:41 -0500

    lisp/cae-multi.el

--- lisp/cae-multi.el
+++ lisp/cae-multi.el
@@ -81,7 +81,7 @@
   "If non-nil, automatically pull repositories when idle.")
 
 (defun cae-multi-sync-repositories-if-idle ()
-  (when (> (current-idle-time) 30)
+  (when (> (time-to-seconds (current-idle-time)) 30)
     (cae-multi-sync-repositories)))
 
 (when cae-multi-enable-auto-pull
