79906138c84a4ac11e629a2cd863ebd760f60963
StrawberryTea <look@strawberrytea.xyz>
2025-02-19 19:21:04 -0500

    autoload/cae-tools.el

--- autoload/cae-tools.el
+++ autoload/cae-tools.el
@@ -35,3 +35,16 @@
     ('eshell-mode (detached-eshell-attach-session session))
     ('shell-mode (detached-shell-attach-session session))
     (t (detached-attach-session session))))
+
+;;;###autoload
+(defun cae-detached-extra-alert-notification (session)
+  "Send an `alert' notification when SESSION becomes inactive."
+  (let ((status (detached-session-status session))
+		(host (detached-session-host-name session)))
+	(alert (detached-session-command session)
+		   :title (pcase status
+					('success (format "Detached finished [%s]" host))
+					('failure (format "Detached failed [%s]" host)))
+		   :severity (pcase status
+					   ('success 'moderate)
+					   ('failure 'high)))))
