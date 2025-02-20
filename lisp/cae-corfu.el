89c24254fe7d165f14dc3531653e1d398238c25c
StrawberryTea <look@strawberrytea.xyz>
2025-02-19 19:39:01 -0500

    lisp/cae-corfu.el

--- lisp/cae-corfu.el
+++ lisp/cae-corfu.el
@@ -115,6 +115,8 @@ This variable needs to be set at the top-level before any `after!' blocks.")
 (setq fish-completion-prefer-bash-completion t)
 
 ;; BUG `corfu-terminal' does not work when using `read-string' in the terminal.
+;; For some reason in the latest update of emacs31, the tty child frame stopped
+;; working.
 ;;(when (> emacs-major-version 30)
 ;;  (advice-add #'corfu--popup-support-p :override #'always)
 ;;  (remove-hook 'corfu-mode-hook #'corfu-terminal-mode))
