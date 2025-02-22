91402cdfc639adf8981fe0e76199f009fdb7a282
StrawberryTea <look@strawberrytea.xyz>
2025-02-22 14:16:41 -0500

    autoload/cae-dir-locals.el

--- autoload/cae-dir-locals.el
+++ autoload/cae-dir-locals.el
@@ -7,12 +7,12 @@
 
 ;; ---[ 1. Cache Lookup & Cleanup ]--------------------------------------
 
-(defun cae-hotloading--cleanup-dir-locals-cache ()
+(defun cae--cleanup-dir-locals-cache ()
   "Remove duplicates and invalid entries from `dir-locals-directory-cache'."
   (setq dir-locals-directory-cache
         (cl-remove-duplicates dir-locals-directory-cache :test #'equal)))
 
-(defun cae-hotloading-dir-locals-cache-lookup (file)
+(defun cae-dir-locals-cache-lookup (file)
   "Return the best matching `dir-locals-directory-cache' entry for FILE,
  ignoring any .dir-locals file checks.
 The returned value is an entry of the form:
@@ -39,45 +39,45 @@ does it attempt to verify cache validity."
 ;; ---[ 2. Reload for All Buffers ]--------------------------------------
 
 ;;;###autoload
-(defun cae-hotloading-reload-all-dir-locals ()
+(defun cae-reload-all-dir-locals ()
   "Reload directory-local variables for all buffers that match any entry in the cache.
 Also resets the projectile cache for each affected directory."
   (interactive)
-  (cae-hotloading--cleanup-dir-locals-cache)
+  (cae--cleanup-dir-locals-cache)
   (let ((dirs (make-hash-table :test 'equal)))
     (dolist (buf (buffer-list))
-      (when-let* ((entry (cae-hotloading-dir-locals-cache-lookup (buffer-file-name buf))))
+      (when-let* ((entry (cae-dir-locals-cache-lookup (buffer-file-name buf))))
         (with-current-buffer buf
           (hack-dir-local-variables-non-file-buffer))
         (puthash (car entry) t dirs)))
     (maphash (lambda (dir _)
-               (cae-hotloading-invalidate-project-cache dir))
+               (cae-invalidate-project-cache dir))
              dirs)))
 
 
 ;; ---[ 3. Reload for Buffers of a Specific Class ]----------------------
 
 ;;;###autoload
-(defun cae-hotloading-reload-dir-locals-for-class (class)
+(defun cae-reload-dir-locals-for-class (class)
   "Reload directory-local variables for all buffers whose dir-local class is CLASS.
 Also resets the projectile cache once for each affected directory."
-  (cae-hotloading--cleanup-dir-locals-cache)
+  (cae--cleanup-dir-locals-cache)
   (let ((dirs (make-hash-table :test 'equal)))
     (dolist (buf (buffer-list))
-      (when-let* ((entry (cae-hotloading-dir-locals-cache-lookup (buffer-file-name buf))))
+      (when-let* ((entry (cae-dir-locals-cache-lookup (buffer-file-name buf))))
         (when (eq (nth 1 entry) class)
           (with-current-buffer buf
             (hack-dir-local-variables-non-file-buffer))
           (puthash (car entry) t dirs))))
     (maphash (lambda (dir _)
-               (cae-hotloading-invalidate-project-cache dir))
+               (cae-invalidate-project-cache dir))
              dirs)))
 
 
 ;; ---[ 4. Invalidate Projectile Cache ]----------------------------------
 
 ;;;###autoload
-(defun cae-hotloading-invalidate-project-cache (project-root)
+(defun cae-invalidate-project-cache (project-root)
   (setq projectile-project-root-cache (make-hash-table :test 'equal))
   (remhash project-root projectile-project-type-cache)
   (remhash project-root projectile-projects-cache)
