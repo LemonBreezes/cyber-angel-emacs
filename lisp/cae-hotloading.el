6c15e45f8cdb9908d0f930c2146e80339fa86290
StrawberryTea <look@strawberrytea.xyz>
2025-02-22 14:15:55 -0500

    lisp/cae-hotloading.el

--- lisp/cae-hotloading.el
+++ lisp/cae-hotloading.el
@@ -61,6 +61,75 @@
 
 (dir-locals-set-directory-class doom-private-dir 'doom)
 
+;; Disable `diff-hl-mode' in my Doom private dir.
+(defadvice! cae-hacks-disable-diff-hl-in-private-config-a (&optional arg)
+  :before-until #'diff-hl-mode
+  (file-in-directory-p default-directory doom-user-dir))
+
+(dir-locals-set-class-variables
+ 'home
+ '((nil
+    . ((eval
+        . (progn
+            ;; Do not render `blamer' hints since we use `git-auto-commit-mode'.
+            (setq-local blamer--block-render-p t)
+
+            ;; Automatically commit saved files to Git and push them to the
+            ;; remote.
+            (when (and (buffer-file-name)
+                       (require 'git-auto-commit-mode nil t)
+                       (require 'vc-git nil t)
+                       (file-equal-p (vc-git-root (buffer-file-name))
+                                     "~/"))
+              (setq-local gac-automatically-add-new-files-p nil)
+              (setq-local gac-automatically-push-p t)
+              (git-auto-commit-mode 1))))))))
+
+(when (eq system-type 'gnu/linux)
+  (dir-locals-set-directory-class (getenv "HOME") 'home))
+
+(dir-locals-set-class-variables
+ 'org
+ '((nil
+    . ((eval . (progn
+                 ;; Avoid rendering blamer hints because we use
+                 ;; git-auto-commit-mode.
+                 (setq-local blamer--block-render-p t)
+
+                 ;; If this file is inside the Org directory and git-auto-commit-mode
+                 ;; is available, enable it with
+                 ;; automatic addition of new Org files and auto-push enabled.
+                 (when (and (buffer-file-name)
+                            (require 'git-auto-commit-mode nil t)
+                            (require 'vc-git nil t)
+                            (file-in-directory-p (buffer-file-name)
+                                                 (expand-file-name cae-multi-org-dir)))
+                   (setq-local gac-automatically-add-new-files-p t)
+                   (setq-local gac-automatically-push-p t)
+                   (git-auto-commit-mode 1))))))))
+
+(dir-locals-set-directory-class (expand-file-name cae-multi-org-dir) 'org)
+
+(dir-locals-set-class-variables
+ 'secrets
+ '((nil
+    . ((eval
+        . (progn
+            ;; Do not render `blamer' hints since we use `git-auto-commit-mode'.
+            (setq-local blamer--block-render-p t)
+
+            ;; Automatically commit saved files to Git and push them to the
+            ;; remote.
+            (when (and (buffer-file-name)
+                       (require 'git-auto-commit-mode nil t)
+                       (require 'vc-git nil t))
+              (setq-local gac-automatically-add-new-files-p nil)
+              (setq-local gac-automatically-push-p t)
+              (git-auto-commit-mode 1))))))))
+
+(dir-locals-set-directory-class cae-multi-secrets-dir 'secrets)
+
+
 ;;Local Variables:
 ;;eval: (cae-hotloading-reload-dir-locals-for-class 'doom)
 ;;End:
