9f7158d99b407493d0ff37fbfee94c641f66d43c
StrawberryTea <look@strawberrytea.xyz>
2025-02-25 16:22:30 -0500

    modules/cae/notifications/config.el

--- modules/cae/notifications/config.el
+++ modules/cae/notifications/config.el
@@ -1,24 +1,18 @@
 ;;; cae/notifications/config.el -*- lexical-binding: t; -*-
 
-(defvar cae-dbus-notifications-supported-p nil
-  "Whether the current system supports D-Bus notifications.")
+;; BUG Otherwise `alert-send-notification' will block the UI.
+(advice-add #'alert-send-notification :around
+            #'cae-notifications-wrap-async-call-process-a)
 
-(defun cae-notifications-setup ()
-  (setq cae-dbus-notifications-supported-p
-        (let ((path "/org/freedesktop/DBus"))
-          (when (require 'dbus nil t)
-            (dbus-ping (subst-char-in-string ?/ ?. (substring path 1)) path))))
-  (if cae-dbus-notifications-supported-p
-      (progn
-        (advice-remove #'alert-send-notification :around
-                       #'cae-notifications-wrap-async-call-process-a)
-        (setq alert-default-style 'libnotify)
-        (and (require 'dbus nil t)
-             (not (getenv "INSIDE_EXWM")) ; In EXWM I prefer using Dunst.
-             cae-dbus-notifications-supported-p
-             (ednc-mode +1)))
-    ;; Use `alert' instead of `notifications-notify'.
-    (advice-add #'notifications-notify :around #'cae-notifications-notify-advice)))
+;;(cae-check-dbus-and-notifications-async
+;; (lambda (dbus-enabled notifications-daemon-present)
+;;   (if dbus-enabled
+;;       (if notifications-daemon-present
+;;           (ignore)
+;;         (ednc-mode +1))
+;;     (setq alert-default-style 'libnotify)
+;;     ;; Use `alert' instead of `notifications-notify'.
+;;     (advice-add #'notifications-notify :around #'cae-notifications-notify-advice))))
 
 (run-with-idle-timer 3 nil #'cae-notifications-setup)
 
@@ -43,17 +37,11 @@
   :defer t :init
   (after! circe-notifications
     (setq circe-notifications-alert-style nil))
-  ;; Some elisp packages use `notifications-notify' directly and provide no easy
-  ;; way to use `alert'.
-  (advice-add 'notifications-notify :around #'cae-notifications-notify-advice)
   :config
   (setq alert-default-style
         (cond ((getenv "WSL_DISTRO_NAME")
                'toast)
               (t 'libnotify)))
-  ;; BUG Otherwise `alert-send-notification' will block the UI.
-  (advice-add #'alert-send-notification :around
-              #'cae-notifications-wrap-async-call-process-a)
 
   (setq alert-default-style
         (cond ((getenv "WSL_DISTRO_NAME")
