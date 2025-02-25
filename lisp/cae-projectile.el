48dd113a9b033b7318d628687c01f437b6e684d8
StrawberryTea <look@strawberrytea.xyz>
2025-02-24 19:22:11 -0500

    lisp/cae-projectile.el

--- lisp/cae-projectile.el
+++ lisp/cae-projectile.el
@@ -32,12 +32,12 @@
 ;; BUG Disable `projectile-root-local' because sometimes it is being used to
 ;; incorrectly set the project root somehow and then setting that incorrect
 ;; value in the cache.
-(setq projectile-project-root-functions
-      '(;;projectile-root-local
-        projectile-root-marked
-        projectile-root-bottom-up
-        projectile-root-top-down
-        projectile-root-top-down-recurring))
+;;(setq projectile-project-root-functions
+;;      '(;;projectile-root-local
+;;        projectile-root-marked
+;;        projectile-root-bottom-up
+;;        projectile-root-top-down
+;;        projectile-root-top-down-recurring))
 
 (if (locate-library "projectile")
 ;;; Projectile configuration
