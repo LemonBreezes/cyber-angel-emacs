c1130c1ee4a0cc5582670cc49007cc4626a29af9
StrawberryTea <look@strawberrytea.xyz>
2025-02-26 11:23:51 -0500

    modules/cae/eshell/autoload/commands.el

--- modules/cae/eshell/autoload/commands.el
+++ modules/cae/eshell/autoload/commands.el
@@ -1,15 +1,20 @@
 ;;; cae/eshell/autoload/commands.el -*- lexical-binding: t; -*-
 
+;;;; Buffer and File Operations
+
 ;;;###autoload
 (defun cae-eshell-buffer-contents (buffer)
-  "Return fontified buffer contents for BUFFER."
+  "Return fontified contents of BUFFER as a string.
+This ensures syntax highlighting is applied before returning the content."
   (with-current-buffer buffer
     (font-lock-ensure (point-min) (point-max))
     (buffer-string)))
 
 ;;;###autoload
 (defun cae-eshell-file-contents (file)
-  "Return fontified file contents for FILE."
+  "Return fontified contents of FILE as a string.
+If FILE is already open in a buffer, use that buffer.
+Otherwise, open FILE temporarily with minimal settings."
   (let ((buffer (get-file-buffer file)))
     (if buffer
         (cae-eshell-buffer-contents buffer)
@@ -26,67 +31,77 @@
 
 ;;;###autoload
 (defun eshell/b (regexp)
-  "Output buffer content of buffer matching REGEXP."
+  "Output content of the first buffer whose name matches REGEXP.
+Returns nil if no matching buffer is found."
   (cl-loop for buf in (buffer-list)
            thereis
            (and (string-match-p regexp (buffer-name buf))
                 (with-current-buffer buf
                   (buffer-substring-no-properties (point-min) (point-max))))))
 
-;; By LemonBreezes https://github.com/LemonBreezes/.doom.d/blob/master/modules/cae/eshell/autoload.el
-;; Originally from Aweshell https://github.com/manateelazycat/aweshell/blob/master/aweshell.el.
 ;;;###autoload
 (defun eshell/hat (&rest files)
-  "Output FILES with highlighting."
+  "Output FILES with syntax highlighting.
+Originally from Aweshell: https://github.com/manateelazycat/aweshell"
   (dolist (f files)
-    (eshell-print (cae-eshell-file-contents f))))
+    (if (file-exists-p f)
+        (eshell-print (cae-eshell-file-contents f))
+      (eshell-error (format "File not found: %s\n" f)))))
 
 ;;;###autoload
 (defun eshell/swap (file1 file2)
-  "Swap the contents of FILE1 and FILE2."
-  (let ((temp (make-temp-name temporary-file-directory)))
+  "Swap the contents of FILE1 and FILE2.
+Both files must exist and be writable."
+  (unless (and (file-exists-p file1) (file-exists-p file2))
+    (error "Both files must exist"))
+  (unless (and (file-writable-p file1) (file-writable-p file2))
+    (error "Both files must be writable"))
+  (let ((temp (make-temp-name (expand-file-name "eshell-swap-" temporary-file-directory))))
     (rename-file file1 temp)
     (rename-file file2 file1)
     (rename-file temp file2)))
 
-;; Made obsolete by `eshell-elecslash'.
-;;;;; https://github.com/dakra/dmacs/blob/master/init.org#eshell
-;;;;;###autoload
-;;(defun eshell/rcd (&optional directory)
-;;  "Like regular 'cd' but don't jump out of a tramp directory.
-;;When on a remote directory with tramp don't jump 'out' of the server.
-;;So if we're connected with sudo to 'remotehost'
-;;'$ rcd /etc' would go to '/sudo:remotehost:/etc' instead of just
-;;'/etc' on localhost."
-;;  (unless (file-remote-p default-directory)
-;;    (error "Not in a remote location"))
-;;  (with-parsed-tramp-file-name default-directory nil
-;;    (eshell/cd
-;;     (tramp-make-tramp-file-name
-;;      method user nil host nil (or directory "") hop))))
+;;;; Directory Navigation
 
-;; https://github.com/howardabrams/hamacs/blob/main/ha-eshell.org
 ;;;###autoload
-(defun eshell/set (&rest args)
-  "Creates a buffer local variables."
-  (dolist (arg-pair (seq-partition args 2))
-    (seq-let (var val) arg-pair
-      (let ((var-sym (make-symbol var)))
-        (set (make-local-variable var-sym) val)))))
+(defun eshell/u (&optional n)
+  "Change to the directory of the Nth most recently used buffer.
+N defaults to 1, meaning the most recently used buffer."
+  (setq n (or n 1))
+  (let* ((buf
+          (cl-find-if (lambda (buf)
+                        (and (get-buffer-window buf)
+                             (not (+popup-buffer-p buf))
+                             (doom-real-buffer-p buf)
+                             (or (buffer-local-value 'default-directory buf)
+                                 (buffer-local-value 'list-buffers-directory buf))
+                             (<= (cl-decf n) 0)))
+                      (cdr (buffer-list)))))
+    (if buf
+        (eshell/cd (or (buffer-local-value 'default-directory buf)
+                       (buffer-local-value 'list-buffers-directory buf)))
+      (eshell-print "No suitable buffer found\n"))))
 
-;; https://github.com/OrgTangle/ntangle/blob/de3ec97c4650cc7b5875ee2614e4c1df5ccb5388/tests/dmacs/init.org#L2435
+;;;###autoload
+(defun eshell/uu ()
+  "Change to the directory of the second most recently used buffer.
+Equivalent to calling `eshell/u' with argument 2."
+  (eshell/u 2))
+
+;;;; File Finding and Searching
+
 ;;;###autoload
 (defun eshell/f (filename &optional dir try-count)
-  "Searches for files matching FILENAME in either DIR or the
-current directory. Just a typical wrapper around the standard
-`find' executable.
+  "Search for files matching FILENAME in DIR or current directory.
+A wrapper around the standard `find' executable with some filters.
 
-Since any wildcards in FILENAME need to be escaped, this wraps
-the shell command.
+If no results are found, it tries up to two more times with wildcard
+patterns around FILENAME.
 
-If not results were found, it calls the `find' executable up to
-two more times, wrapping the FILENAME pattern in wildcat
-matches. This seems to be more helpful to me."
+Arguments:
+  FILENAME: The name pattern to search for
+  DIR: Directory to search in (default: current directory)
+  TRY-COUNT: Internal parameter for recursive calls"
   (let* ((cmd (concat
                (executable-find "find")
                " " (or dir ".")
@@ -107,18 +122,40 @@ matches. This seems to be more helpful to me."
         (eshell/f (concat "*" filename) dir 2))
        (t "")))))
 
-;; https://github.com/OrgTangle/ntangle/blob/de3ec97c4650cc7b5875ee2614e4c1df5ccb5388/tests/dmacs/init.org#L2435
 ;;;###autoload
 (defun eshell/ef (filename &optional dir)
-  "Searches for the first matching filename and loads it into a
-file to edit."
+  "Find and edit the first file matching FILENAME in DIR.
+Uses `eshell/f' to find the file and opens it for editing."
   (let* ((files (eshell/f filename dir))
          (file (car (s-split "\n" files))))
-    (find-file file)))
+    (if (and file (not (string-empty-p file)))
+        (find-file file)
+      (message "No matching files found"))))
 
-;; https://www.n16f.net/blog/clearing-the-eshell-buffer/
+;;;; Utility Commands
+
+;;;###autoload
+(defun eshell/set (&rest args)
+  "Create buffer-local variables from pairs of arguments.
+Example: set var1 value1 var2 value2"
+  (unless (cl-evenp (length args))
+    (error "Arguments must be in pairs"))
+  (dolist (arg-pair (seq-partition args 2))
+    (seq-let (var val) arg-pair
+      (let ((var-sym (make-symbol var)))
+        (set (make-local-variable var-sym) val)))))
+
+;;;###autoload
+(defun eshell/help (cmd)
+  "Get help for command CMD from tldr, man or helpful."
+  (cae-eshell-help-run-help cmd))
+
+;;;; Interactive Commands
+
 ;;;###autoload
 (defun cae-eshell-clear ()
+  "Clear the eshell buffer while preserving current input.
+Based on https://www.n16f.net/blog/clearing-the-eshell-buffer/"
   (interactive)
   (let ((input (eshell-get-old-input)))
     (eshell/clear t)
@@ -126,31 +163,10 @@ file to edit."
     (insert input)))
 
 ;;;###autoload
-(defun eshell/u (&optional n)
-  (setq n (or n 1))
-  (let* ((ignored-bufs)
-         (buf
-          (cl-find-if (lambda (buf)
-                        (and (get-buffer-window)
-                             (not (+popup-buffer-p buf))
-                             (doom-real-buffer-p buf)
-                             (or (buffer-local-value 'default-directory buf)
-                                 (buffer-local-value 'list-buffers-directory buf))
-                             (<= (cl-decf n) 0)))
-                      (cdr (buffer-list)))))
-    ;;(+log buf (buffer-local-value 'major-mode buf))
-    (when buf
-      (eshell/cd (or (buffer-local-value 'default-directory buf)
-                     (buffer-local-value 'list-buffers-directory buf))))))
-
-;;;###autoload
-(defun eshell/uu ()
-  (eshell/u 2))
-
-;; Taken from Aweshell: https://github.com/manateelazycat/aweshell/blob/master/aweshell.el
-;;;###autoload
 (defun cae-sudo-toggle ()
-  "Toggle sudo with current command."
+  "Toggle sudo prefix for the current command.
+If the command starts with sudo, remove it; otherwise, add it.
+Originally from Aweshell: https://github.com/manateelazycat/aweshell"
   (interactive)
   (let ((pt (point))
         (commands (buffer-substring-no-properties
@@ -166,14 +182,9 @@ file to edit."
 
 ;;;###autoload
 (defun cae-eshell-quit-or-delete-char (arg)
-  "Delete a character (ahead of the cursor) or quit eshell if there's nothing to
-delete."
+  "Delete character ahead or quit eshell if at end of line.
+With numeric prefix ARG, delete that many characters."
   (interactive "p")
-  (if (and (eolp))
+  (if (eolp)
       (eshell-life-is-too-much)
     (delete-char arg)))
-
-;;;###autoload
-(defun eshell/help (cmd)
-  "Get help for command from tldr, man or helpful."
-  (+eshell-help-run-help cmd))
