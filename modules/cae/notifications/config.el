35db4ffdad1164db7feb9b30a290b9314ab880be
StrawberryTea <look@strawberrytea.xyz>
2025-02-25 20:15:29 -0500

    modules/cae/notifications/config.el

--- modules/cae/notifications/config.el
+++ modules/cae/notifications/config.el
@@ -4,63 +4,21 @@
 (advice-add #'alert-send-notification :around
             #'cae-notifications-wrap-async-call-process-a)
 
-(defun cae-check-dbus-and-notifications-async (callback)
-  "Asynchronously check if DBus is enabled and if a notifications daemon is running.
-CALLBACK is a function that takes two arguments:
-  (DBUS-ENABLED NOTIFICATIONS-DAEMON-PRESENT).
-
-If DBus isn't enabled, the callback is invoked with nil for both arguments.
-If DBus is enabled, then we issue an asynchronous call to ListNames on DBus
-and check if \"org.freedesktop.Notifications\" is among the registered names."
-  ;; Use a timer to schedule this check asynchronously.
-  (let ((dbus-ok nil))
-    ;; First, try to ping DBus. dbus-ping is synchronous,
-    ;; so we call it inside our asynchronous lambda.
-    (condition-case err
-        (progn
-          (dbus-ping :session "org.freedesktop.DBus")
-          (setq dbus-ok t))
-      (error
-       (setq dbus-ok nil)))
-    (if (not dbus-ok)
-        ;; DBus is not enabled; immediately invoke the callback.
-        (funcall callback nil nil)
-      ;; DBus is enabled; now check for a notifications daemon.
-      (dbus-call-method
-       :session
-       "org.freedesktop.DBus"
-       "/org/freedesktop/DBus"
-       "org.freedesktop.DBus"
-       "ListNames"
-       ;; :reply-function is called with the list of names.
-       :reply-function (lambda (names)
-                         (let ((notif-found (cl-some (lambda (name)
-                                                       (string= name "org.freedesktop.Notifications"))
-                                                     names)))
-                           (funcall callback t (if notif-found t nil))))
-       ;; In case of error in the asynchronous call,
-       ;; assume that the notifications daemon isn't present.
-       :error-function (lambda (error)
-                         (funcall callback t nil))))))
-
-
-;; Test the DBus and notifications functionality
-;; Commented out to prevent automatic execution during loading
-;; (cae-check-dbus-and-notifications-async
-;;  (lambda (dbus-enabled notifications-present)
-;;    (if dbus-enabled
-;;        (message "DBus is enabled. Notifications daemon present? %s" notifications-present)
-;;      (message "DBus does not appear to be enabled."))))
-
-
-(cae-check-dbus-and-notifications-async
- (lambda (dbus-enabled notifications-daemon-present)
-   (if dbus-enabled
-       (progn (setq alert-default-style 'libnotify)
-              (unless notifications-daemon-present
-                (ednc-mode +1)))
-     ;; Use `alert' instead of `notifications-notify'.
-     (advice-add #'notifications-notify :around #'cae-notifications-notify-advice))))
+(advice-add #'notifications-notify :around #'cae-notifications-notify-advice)
+(dbus-call-method-asynchronously
+ :session
+ "org.freedesktop.DBus"
+ "/org/freedesktop/DBus"
+ "org.freedesktop.DBus"
+ "ListNames"
+ (lambda (names)
+   (let ((notif-found (cl-some (lambda (name)
+                                 (string= name "org.freedesktop.Notifications"))
+                               names)))
+     (advice-remove #'notifications-notify #'cae-notifications-notify-advice)
+     (setq alert-default-style 'libnotify)
+     (unless notifications-daemon-present
+       (ednc-mode +1)))))
 
 (use-package! ednc
   :when (eq system-type 'gnu/linux)
