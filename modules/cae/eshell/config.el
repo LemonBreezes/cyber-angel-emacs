ba07f63df1f960a9c6b689cc47c51e05ce70fecf
StrawberryTea <look@strawberrytea.xyz>
2025-02-26 11:23:46 -0500

    modules/cae/eshell/config.el

--- modules/cae/eshell/config.el
+++ modules/cae/eshell/config.el
@@ -2,6 +2,8 @@
 
 (require 'cae-lib)
 
+;;;; EAT Terminal Integration
+
 (use-package! eat
   :defer t :init
   (add-hook 'eshell-load-hook #'eat-eshell-mode)
@@ -12,24 +14,28 @@
         eat-enable-yank-to-terminal t)
 
   ;; Temporarily disable some modes when EAT is active.
-  (add-hook 'eat--eshell-process-running-mode-hook
-            (defun cae-eshell-disable-modes-in-eat-h ()
-              (let ((modes '(corfu-mode eldoc-mode)))
-                (dolist (mode modes)
-                  (when (boundp mode)
-                    (let ((mode-var (intern (concat "cae-eshell--" (symbol-name mode) "-enabled-p"))))
-                      (make-local-variable mode-var)
-                      (if eat--eshell-process-running-mode
-                          (progn (set mode-var (symbol-value mode))
-                                 (funcall mode -1))
-                        (when (symbol-value mode-var)
-                          (funcall mode 1)
-                          (set mode-var nil)))))))))
+  (defun cae-eshell--disable-modes-in-eat ()
+    "Disable certain modes when EAT is running and restore them when it stops."
+    (let ((modes '(corfu-mode eldoc-mode)))
+      (dolist (mode modes)
+        (when (boundp mode)
+          (let ((mode-var (intern (concat "cae-eshell--" (symbol-name mode) "-enabled-p"))))
+            (make-local-variable mode-var)
+            (if eat--eshell-process-running-mode
+                (progn (set mode-var (symbol-value mode))
+                       (funcall mode -1))
+              (when (symbol-value mode-var)
+                (funcall mode 1)
+                (set mode-var nil))))))))
+  
+  (add-hook 'eat--eshell-process-running-mode-hook #'cae-eshell--disable-modes-in-eat)
 
   ;; It's kind of hard to figure out how to exit char mode, so let's give a hint.
   (defadvice! cae-eat-eshell-print-char-mode-hint-a ()
     :after #'eat-eshell-char-mode
     (message "Type M-RET/C-M-m to exit char mode.")))
+
+;;;; Command Handling
 
 ;; Doom overrides `eshell/emacs' with a custom function. I prefer for `emacs'
 ;; to work in Eshell as it does in a terminal.
@@ -43,6 +49,8 @@
   :before #'eshell-send-input
   (expand-abbrev))
 
+;;;; Bookmarks and Buffer Handling
+
 (use-package! eshell-bookmark
   :defer t :init
   (add-hook 'eshell-mode-hook #'eshell-bookmark-setup))
@@ -54,6 +62,8 @@
 (after! esh-arg
   (add-hook 'eshell-parse-argument-hook #'cae-eshell-syntax-buffer-redirect))
 
+;;;; History Management
+
 ;; Filter trivial commands from history.
 (defun cae-eshell-input-filter (str)
   "Filter some trivial commands from the input history."
@@ -62,7 +72,17 @@
            (string-prefix-p "cd " str)
            (string-prefix-p " " str)
            (string-match-p "^[a-zA-Z]\\'" str))))
+
 (setq eshell-input-filter #'cae-eshell-input-filter)
+
+(after! em-hist
+  (setq eshell-history-size nil
+        eshell-history-append t
+        eshell-hist-ignoredups 'erase)
+  (add-to-list 'eshell-expand-input-functions
+               #'eshell-expand-history-references))
+
+;;;; Prompt Configuration
 
 ;; Set the prompt
 (autoload 'epe-theme-lambda "eshell-prompt-extras")
@@ -72,6 +92,8 @@
   (after! eshell-prompt-extras
     (setq epe-show-local-working-directory t
           epe-show-git-status-extended t)))
+
+;;;; Input Handling
 
 ;; Don't leave me with unbalanced delimiters.
 (defadvice! cae-eshell-kill-input-with-delimiters-a ()
@@ -86,21 +108,18 @@
   (unless (eq (char-syntax (char-before)) ?\s)
     (insert-char ?\s)))
 
+;;;; Module Configuration
+
 (after! esh-module
   (add-to-list 'eshell-modules-list 'eshell-elecslash))
 
 (autoload 'eshell-elecslash-initialize "em-elecslash")
 (add-hook 'eshell-mode-hook #'eshell-elecslash-initialize)
 
-(after! em-hist
-  (setq eshell-history-size nil
-        eshell-history-append t
-        eshell-hist-ignoredups 'erase)
-  (add-to-list 'eshell-expand-input-functions
-               #'eshell-expand-history-references))
-
 (after! em-glob
   (setq eshell-glob-splice-results t))
+
+;;;; Documentation and Help
 
 ;; Define an Eshell lookup handler and integrate Man with TLDR.
 (let ((tldr-dir (concat doom-cache-dir "tldr/")))
@@ -112,11 +131,16 @@
           (setq tldr-directory-path ,tldr-dir)
           (require 'tldr)
           (tldr-update-docs))))))
-(set-lookup-handlers! 'eshell-mode :documentation #'+eshell-help-run-help)
+
+(set-lookup-handlers! 'eshell-mode :documentation #'cae-eshell-help-run-help)
+
 (after! man
   (map! :map Man-mode-map :n "x" #'+eshell-man-to-tldr))
+
 (after! tldr
   (map! :map tldr-mode-map :n "x" #'+eshell-tldr-to-man))
+
+;;;; Keybindings
 
 (after! eshell
   (when (modulep! :completion vertico)
@@ -132,6 +156,8 @@
         :i "C-u" #'eshell-kill-input
         :ig "C-d" #'cae-eshell-quit-or-delete-char
         [remap doom/backward-to-bol-or-indent] #'beginning-of-line))
+
+;;;; Atuin Integration
 
 (use-package! eshell-atuin
   :when (executable-find "atuin")
