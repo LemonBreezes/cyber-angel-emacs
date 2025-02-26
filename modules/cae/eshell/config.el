e1e3df60657d9bf557a999694425a4441c56b3a6
StrawberryTea <look@strawberrytea.xyz>
2025-02-26 10:31:28 -0500

    modules/cae/eshell/config.el

--- modules/cae/eshell/config.el
+++ modules/cae/eshell/config.el
@@ -111,10 +111,7 @@
           (add-to-list 'load-path ,(file-name-directory (locate-library "tldr")))
           (setq tldr-directory-path ,tldr-dir)
           (require 'tldr)
-          (tldr-update-docs))
-       ;; Now that I'm confident this code works, I don't need the print output.
-       ;;(lambda (_) (message "tldr docs updated"))
-       ))))
+          (tldr-update-docs))))))
 (set-lookup-handlers! 'eshell-mode :documentation #'+eshell-help-run-help)
 (after! man
   (map! :map Man-mode-map :n "x" #'+eshell-man-to-tldr))
