10bfc14408670b01a1ee944cac2cc87edc742d63
StrawberryTea <look@strawberrytea.xyz>
2025-02-26 14:28:37 -0500

    lisp/cae-repeat.el

--- lisp/cae-repeat.el
+++ lisp/cae-repeat.el
@@ -1,13 +1,10 @@
   ;;; ~/.doom.d/lisp/cae-repeat.el -*- lexical-binding: t; -*-
 
-(use-package! repeat
-  :defer 4.0 :init
-  (advice-add #'repeat-mode :around #'cae-shut-up-a)
 
-  ;; This is so that my repeat maps are reloaded when I save this file.
-  (when (and (featurep 'repeat-mode)
-             cae-config-finished-loading)
-    (ignore-errors (repeat-mode -1)))
+(use-package! repeat
+  :defer t :init
+  (advice-add #'repeat-mode :around #'cae-shut-up-a)
+  (add-hook 'doom-first-input-hook #'repeat-mode)
   :config
   (setq repeat-exit-key "TAB"
         repeat-check-key t
@@ -211,4 +208,7 @@
   (autoload 'embark-verbose-indicator "embark")
   (autoload 'which-key--create-buffer-and-show "which-key")
 
-  (repeat-mode +1))
+  ;; This is so that my repeat maps are reloaded when I save this file.
+  (when cae-config-finished-loading
+    (ignore-errors (repeat-mode -1))
+    (ignore-errors (repeat-mode 1))))
