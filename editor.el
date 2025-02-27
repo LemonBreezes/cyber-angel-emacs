4ecd2dd8648a4e4dc139d2cd02b1ff7a4661f63c
StrawberryTea <look@strawberrytea.xyz>
2025-02-26 23:13:33 -0500

    editor.el

--- editor.el
+++ editor.el
@@ -285,10 +285,7 @@
   (parrot-mode +1))
 
 (use-package! parrot-rotate
-  :defer t :init
-  (map! :n "]r" #'cae-rotate-forward-word-at-point
-        :n "[r" #'cae-rotate-backward-word-at-point)
-  :config
+  :defer t :config
   (after! parrot-rotate
     (setq parrot-rotate-animate-after-rotation t
           parrot-rotate-highlight-after-rotation t
