96f6010be2451b7896bcc6ffd75164d324f538ea
StrawberryTea <look@strawberrytea.xyz>
2025-03-01 16:50:24 -0500

    editor.el

--- editor.el
+++ editor.el
@@ -382,30 +382,3 @@
   (remove-hook 'find-file-hook #'cae-auto-sudoedit-maybe-h)
   (remove-hook 'dired-mode-hook #'cae-auto-sudoedit-maybe-h)
   (auto-sudoedit-mode +1))
-
-;; Copied pretty much verbatim from
-;; https://github.com/tecosaur/emacs-config/blob/master/config.org
-(use-package! vlf-setup
-  :defer t :init
-  (advice-add #'files--ask-user-about-large-file :override #'cae-files--ask-about-large-file-vlf)
-  :config
-  (advice-remove 'abort-if-file-too-large #'ad-Advice-abort-if-file-too-large)
-  (defvar-local cae-vlf-cumulative-linenum '((0 . 0))
-    "An alist keeping track of the cumulative line number.")
-  (add-hook 'vlf-after-chunk-update-hook #'cae-vlf-update-linum)
-
-  ;; Since this only works with absolute line numbers, let's make sure we use them.
-  (setq-hook! 'vlf-mode-hook display-line-numbers t)
-
-  (add-hook! 'vlf-mode-hook (setq-local isearch-wrap-function #'cae-vlf-isearch-wrap))
-  (after! vlf
-    (map-keymap (lambda (k v)
-                  (when (commandp v)
-                    (eval `(map! :map vlf-prefix-map
-                                 :localleader
-                                 ,(if (vectorp k)
-                                      (key-description k)
-                                    (key-description (vector k)))
-                                 ',v)
-                          t)))
-                vlf-mode-map)))
