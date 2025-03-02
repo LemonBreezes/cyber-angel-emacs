7ee77d8bfb5c512ea4880e7d9d72e0c9dc571d31
StrawberryTea <look@strawberrytea.xyz>
2025-03-02 11:33:30 -0500

    modules/cae/exwm/config.el

--- modules/cae/exwm/config.el
+++ modules/cae/exwm/config.el
@@ -77,9 +77,9 @@
     (map! :map exwm-mode-map
           :localleader
           (:prefix ("d" . "debug")
-           "l" #'xcb-debug:clearn
+                   "l" #'xcb-debug:clearn
 
-           "t" #'exwm-debug)
+                   "t" #'exwm-debug)
           "f" #'exwm-layout-toggle-fullscreen
           "h" #'exwm-floating-hide
           "q" #'exwm-input-send-next-key
