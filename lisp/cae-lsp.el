85a013d807069d3198b2c3ebf6de8b2161b9bb69
StrawberryTea <look@strawberrytea.xyz>
2025-03-02 18:10:13 -0500

    lisp/cae-lsp.el

--- lisp/cae-lsp.el
+++ lisp/cae-lsp.el
@@ -111,14 +111,6 @@
     (when (modulep! :lang lua +lsp)
       (add-hook 'fennel-mode-local-vars-hook #'lsp!)))
 
-  (defun cae-eglot-prevent-home-directory-project (orig-fun)
-    "Advice for `eglot--current-project' to prevent using home directory as project."
-    (let ((project (funcall orig-fun)))
-      (when project
-        (let ((root (project-root project)))
-          (when (equal (expand-file-name "~") (expand-file-name root))
-            (error "Eglot aborted: refusing to use home directory as project root"))))
-      project))
   (advice-add 'eglot--current-project :around #'cae-eglot-prevent-home-directory-project)
 
   (use-package! breadcrumb
