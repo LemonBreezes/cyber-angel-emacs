a063c5336ec43f4bedd17052c96035b91e72deef
StrawberryTea <look@strawberrytea.xyz>
2025-03-02 17:41:27 -0500

    lisp/cae-lsp.el

--- lisp/cae-lsp.el
+++ lisp/cae-lsp.el
@@ -94,8 +94,7 @@
 
 (when (modulep! :tools lsp +eglot)
   (use-package! eglot-booster
-    :after eglot
-    :config (eglot-booster-mode +1))
+    :after eglot :config (eglot-booster-mode +1))
 
   (after! eglot
     (setq eglot-sync-connect nil)
