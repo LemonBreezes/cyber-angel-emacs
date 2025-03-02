45074ff5fa111bab95da14b6398facb6948d862e
StrawberryTea <look@strawberrytea.xyz>
2025-03-01 22:06:41 -0500

    modules/cae/vc/autoload.el

--- modules/cae/vc/autoload.el
+++ modules/cae/vc/autoload.el
@@ -80,3 +80,37 @@ if both REMOT and REMOTE-NAME are non-nil, REMOTE-NAME is used as the name of th
           forkrepo))
     (consult-gh-repo-fork)))
 
+;;;###autoload (autoload 'cae-smerge-transient "cae/vc/autoload.el" nil t)
+(defhydra unpackaged/smerge-hydra
+  (:color pink :hint nil :post (smerge-auto-leave))
+  "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+  ("n" smerge-next)
+  ("p" smerge-prev)
+  ("b" smerge-keep-base)
+  ("u" smerge-keep-upper)
+  ("l" smerge-keep-lower)
+  ("a" smerge-keep-all)
+  ("RET" smerge-keep-current)
+  ("\C-m" smerge-keep-current)
+  ("<" smerge-diff-base-upper)
+  ("=" smerge-diff-upper-lower)
+  (">" smerge-diff-base-lower)
+  ("R" smerge-refine)
+  ("E" smerge-ediff)
+  ("C" smerge-combine-with-next)
+  ("r" smerge-resolve)
+  ("k" smerge-kill-current)
+  ("ZZ" (lambda ()
+          (interactive)
+          (save-buffer)
+          (bury-buffer))
+   "Save and bury buffer" :color blue)
+  ("q" nil "cancel" :color blue))
