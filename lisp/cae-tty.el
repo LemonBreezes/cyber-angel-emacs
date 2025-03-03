03e506aada672b1f92cf04a454c20cf13b5c0b12
StrawberryTea <look@strawberrytea.xyz>
2025-03-03 10:02:23 -0500

    lisp/cae-tty.el

--- lisp/cae-tty.el
+++ lisp/cae-tty.el
@@ -69,34 +69,3 @@
       (if (cae-display-graphic-p)
           (apply oldfun args)
         (apply #'pdftotext-mode args)))))
-
-
-;; This is code from when I tried to run Emacs simultaneously from terminal
-;; frames and GUI frames but decided to remove it because I did not use Emacs
-;; that way much.
-
-;; Would also need to set `vertico-multiform-categories'.
-
-;; Automatically enable & disable the posframe when using multiple client frames.
-;;(add-hook! 'pre-command-hook
-;;  (defun cae-tty-setup-posframe-h ()
-;;    "Setup frame for TTY."
-;;    (if (cae-display-graphic-p)
-;;        (progn (when (and (boundp 'vertico-posframe-mode)
-;;                          (not vertico-posframe-mode))
-;;                 (vertico-posframe-mode +1))
-;;               (when (and (boundp 'helm-display-function)
-;;                          (not (eq helm-display-function #'helm-posframe-display)))
-;;                 (posframe-delete-all)
-;;                 (helm-posframe-enable)))
-;;      (when (bound-and-true-p vertico-posframe-mode)
-;;        (vertico-posframe-mode -1))
-;;      (when (and (boundp 'helm-display-function)
-;;                 (eq helm-display-function #'helm-posframe-display))
-;;        (helm-posframe-disable)))))
-;;
-;;(add-hook! 'doom-switch-frame-hook
-;;  (defun cae-tty-setup-frame-h ()
-;;    (unless (cae-display-graphic-p)
-;;      (when (bound-and-true-p corfu-mode)
-;;        (corfu-terminal-mode +1)))))
