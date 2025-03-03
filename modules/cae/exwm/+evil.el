e9e5133ba01e5472b0a147ceee5073bf01f60895
StrawberryTea <look@strawberrytea.xyz>
2025-03-03 10:55:44 -0500

    modules/cae/exwm/+evil.el

--- modules/cae/exwm/+evil.el
+++ modules/cae/exwm/+evil.el
@@ -6,8 +6,9 @@
   (add-to-list 'evil-snipe-disabled-modes 'exwm-mode))
 
 (use-package! exwm-evil
-  :defer nil :config
+  :init
   (add-hook 'exwm-manage-finish-hook #'enable-exwm-evil-mode)
+  :defer t :config
   (cl-pushnew 'escape exwm-input-prefix-keys)
 
   ;; We will disable `C-c' in insert state.
