32a31838cbddc5aadc0e500925b2e4ef13a2497c
StrawberryTea <look@strawberrytea.xyz>
2025-03-03 17:57:09 -0500

    lisp/cae-projectile.el

--- lisp/cae-projectile.el
+++ lisp/cae-projectile.el
@@ -23,8 +23,7 @@
 (defun cae-projectile-maybe-add-project ()
   "If saving a .projectile file, add its directory to projectile's known projects."
   (when (and buffer-file-name
-             (string= (file-name-nondirectory buffer-file-name) ".projectile")
-             (not (file-remote-p buffer-file-name)))
+             (string= (file-name-nondirectory buffer-file-name) ".projectile"))
     (let ((project-root (file-name-directory buffer-file-name)))
       (projectile-add-known-project project-root)
       (message "Added %s to projectile known projects" project-root))))
