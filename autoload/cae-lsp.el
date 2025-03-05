c0556e04b0cb49012664e72d9fb31d0fb3df09be
StrawberryTea <look@strawberrytea.xyz>
2025-03-04 22:17:03 -0500

    autoload/cae-lsp.el

--- autoload/cae-lsp.el
+++ autoload/cae-lsp.el
@@ -8,6 +8,6 @@
       (let ((root (project-root project)))
         (when (or (equal (expand-file-name "~") (expand-file-name root))
                   (equal "/" root))
-          (error "Eglot aborted: refusing to use home directory as project root"))
+          (message "Eglot aborted: refusing to use home directory as project root"))
         ;; Proceed to call the original eglot-ensure only if checks pass
         (call-interactively #'eglot-ensure)))))
