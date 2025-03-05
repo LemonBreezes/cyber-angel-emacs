8a889a466fa7b65b94f2696e411a323a67f98af7
StrawberryTea <look@strawberrytea.xyz>
2025-03-04 22:22:12 -0500

    autoload/cae-lsp.el

--- autoload/cae-lsp.el
+++ autoload/cae-lsp.el
@@ -3,11 +3,10 @@
 ;;;###autoload
 (defun cae-eglot-ensure-no-home-or-root-directory ()
   "Prevent Eglot from starting if the project directory is the home directory."
-  (let ((project (project-current t)))
-    (when project
-      (let ((root (project-root project)))
-        (when (or (equal (expand-file-name "~") (expand-file-name root))
-                  (equal "/" root))
-          (message "Eglot aborted: refusing to use home directory as project root"))
-        ;; Proceed to call the original eglot-ensure only if checks pass
-        (call-interactively #'eglot-ensure)))))
+  (when-let ((project (project-current))
+             (root (project-root project)))
+    (when (or (equal (expand-file-name "~") (expand-file-name root))
+              (equal "/" root))
+      (message "Eglot aborted: refusing to use home directory as project root"))
+    ;; Proceed to call the original eglot-ensure only if checks pass
+    (call-interactively #'eglot-ensure)))
