166651a0472cf996ed71dce0bd32130ba22b2d12
StrawberryTea <look@strawberrytea.xyz>
2025-03-12 14:10:39 -0400

    modules/cae/misc-applications/autoload/ai.el

--- modules/cae/misc-applications/autoload/ai.el
+++ modules/cae/misc-applications/autoload/ai.el
@@ -5,4 +5,5 @@
   (interactive)
   (let ((vterm-buffer-name (format "*claude:%s*" (doom-project-root))))
     (vterm-other-window "claude")
-    (vterm-send-string "claude")))
+    (vterm-send-string "claude")
+    (vterm-send-return)))
