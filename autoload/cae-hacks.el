a994c8804c73a242c779e0c0ae394fd7800e6a13
StrawberryTea <look@strawberrytea.xyz>
2025-03-12 13:49:45 -0400

    autoload/cae-hacks.el

--- autoload/cae-hacks.el
+++ autoload/cae-hacks.el
@@ -14,16 +14,18 @@
           (let ((found nil))
             (letrec ((search-in-wconf
                       (lambda (wc)
-                        (if (and (consp wc) (not found))
-                            (cond
-                             ((and (consp (car wc)) (eq (caar wc) 'buffer)
-                                   (string= (buffer-name buffer) (cadadr wc)))
-                              (setq found t))
-                             (t
-                              (dolist (w wc)
-                                (when (and (consp w) (not found))
-                                  (funcall search-in-wconf w)))))))))
-              (funcall search-in-wconf window-conf)
+                        (when (and (consp wc) (not found))
+                          (cond
+                           ((and (consp (car wc)) (eq (caar wc) 'buffer)
+                                 (string= (buffer-name buffer) (cadadr wc)))
+                            (setq found t))
+                           (t
+                            (dolist (w wc)
+                              (when (and (consp w) (not found))
+                                (funcall search-in-wconf w)))))))))
+              ;; Add a type check before calling search-in-wconf
+              (when (consp window-conf)
+                (funcall search-in-wconf window-conf))
               found))))))))
 
 
