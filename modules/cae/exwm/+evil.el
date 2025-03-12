35e8a6c5b8ee3dd324c1486501a88ec7d0b8c60b
StrawberryTea <look@strawberrytea.xyz>
2025-03-12 13:25:26 -0400

    modules/cae/exwm/+evil.el

--- modules/cae/exwm/+evil.el
+++ modules/cae/exwm/+evil.el
@@ -97,4 +97,6 @@
           ((and (stringp exwm-class-name)
                 (string-match-p "retroarch" exwm-class-name))
            (evil-emacs-state)
-           (setq-local exwm-input-line-mode-passthrough nil)))))
+           (setq-local exwm-input-line-mode-passthrough nil
+                       exwm-input-prefix-keys
+                       (delq 'escape exwm-input-prefix-keys))))))
