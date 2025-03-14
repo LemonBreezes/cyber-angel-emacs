a79b20b49ebf08c10268bfb0875d9eeb57ad8b72
StrawberryTea <look@strawberrytea.xyz>
2025-03-14 12:32:08 -0400

    init.el

--- init.el
+++ init.el
@@ -113,7 +113,6 @@
        (python +lsp +tree-sitter +pyright +cython)
        (haskell +lsp +tree-sitter)
        (graphql +lsp +tree-sitter)
-       (clojure +lsp +tree-sitter)
 
        :os
        (:if IS-MAC macos)
