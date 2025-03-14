79b25bb54be83a6df2e674f3230a1aaaa581ec6e
StrawberryTea <look@strawberrytea.xyz>
2025-03-14 13:14:48 -0400

    core.el

--- core.el
+++ core.el
@@ -1,31 +1,5 @@
 ;;; core.el -*- lexical-binding: t; -*-
 
-;; Sometimes packages are not compiled. This is a workaround.
-(use-package! compile-angel
-  :defer nil :init
-  (compile-angel-on-load-mode +1)
-  (setq native-comp-async-query-on-exit t)
-  (setq confirm-kill-processes t)
-  (setq package-native-compile t)
-
-  ;; Ensure that the value of `savehist-file` is updated before proceeding
-  (with-eval-after-load "savehist"
-    (let ((filename (concat "/" (file-name-nondirectory savehist-file))))
-      (unless (member filename compile-angel-excluded-files)
-        (push filename compile-angel-excluded-files))))
-
-  ;; Ensure that the value of `recentf-save-file` is updated before proceeding
-  (with-eval-after-load "recentf"
-    (let ((filename (concat "/" (file-name-nondirectory recentf-save-file))))
-      (unless (member filename compile-angel-excluded-files)
-        (push filename compile-angel-excluded-files))))
-
-  ;; Ensure that the value of `custom-file` is updated before proceeding
-  (with-eval-after-load "cus-edit"
-    (when (stringp custom-file)
-      (let ((filename (concat "/" (file-name-nondirectory custom-file))))
-        (unless (member filename compile-angel-excluded-files)
-          (push filename compile-angel-excluded-files))))))
 
 ;; Load secrets
 (when (file-exists-p (concat cae-multi-secrets-dir "secrets.el"))
