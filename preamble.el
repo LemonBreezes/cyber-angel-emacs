0e2ded897f3f58559216ccb7da19445987e031ff
StrawberryTea <look@strawberrytea.xyz>
2025-03-14 13:07:43 -0400

    preamble.el

--- preamble.el
+++ preamble.el
@@ -52,8 +52,7 @@ of ELEMENT."
     (when (file-exists-p emacs-early-init)
       (with-temp-file emacs-early-init
         (insert-file-contents emacs-early-init)
-        (let ((found (re-search-forward "\\((load \"~/.config/doom/early-init.el\")\\)\\|\\((if (let (load-suffixes
-'(\".elc\" \".el\"))\\)" nil t)))
+        (let ((found (re-search-forward "\\((load \"~/.config/doom/early-init.el\")\\)\\|\\((if (let (load-suffixes '(\".elc\" \".el\"))\\)" nil t)))
           (when (and found (match-beginning 2))
             (beginning-of-line)
             (insert "(load \"~/.config/doom/early-init.el\")\n")))))))
