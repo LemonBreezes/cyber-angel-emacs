47bbdc6959e36c65733b53f626acfdceaf8edcc0
StrawberryTea <look@strawberrytea.xyz>
2025-03-17 09:43:16 -0400

    modules/cae/exwm/+evil.el

--- modules/cae/exwm/+evil.el
+++ modules/cae/exwm/+evil.el
@@ -89,14 +89,4 @@
                      (exwm-input--fake-key 'C-k)
                      (exwm-evil-insert)))
 
-(add-hook! 'exwm-manage-finish-hook :depth 1
-  (defun cae-exwm-load-special-bindings-h ()
-    (cond ((and (stringp exwm-class-name)
-                (string-match-p "discord" exwm-class-name))
-           (cae-exwm-discord-mode +1))
-          ((and (stringp exwm-class-name)
-                (string-match-p "retroarch" exwm-class-name))
-           (evil-emacs-state)
-           (setq-local exwm-input-line-mode-passthrough nil
-                       exwm-input-prefix-keys
-                       (delq 'escape exwm-input-prefix-keys))))))
+(add-hook! 'exwm-manage-finish-hook :depth 1 #'cae-exwm-load-special-bindings-h)
