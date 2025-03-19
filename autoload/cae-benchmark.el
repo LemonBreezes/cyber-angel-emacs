b2ac28555c7dd7fe3947163be5adefd6044453ec
StrawberryTea <look@strawberrytea.xyz>
2025-03-19 11:02:41 -0400

    autoload/cae-benchmark.el

--- autoload/cae-benchmark.el
+++ autoload/cae-benchmark.el
@@ -72,6 +72,7 @@ of loading files defined by *-init-*-enabled-p variables."
                      (doom-initialize)
 
                      ;; Run all startup hooks to complete initialization
+                     (message "HELLO %s" (featurep 'doom-lib))
                      (doom-run-all-startup-hooks-h))
                  (error
                   (require 'pp)
