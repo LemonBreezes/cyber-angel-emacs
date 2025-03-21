fd0c7b23670b7f1a593b954be5a126169ab56015
StrawberryTea <look@strawberrytea.xyz>
2025-03-21 19:57:10 -0400

    modules/cae/ai/packages.el

--- modules/cae/ai/packages.el
+++ modules/cae/ai/packages.el
@@ -5,7 +5,8 @@
 
 (package! gptel)
 (package! aidermacs :recipe (:host github :repo "MatthewZMD/aidermacs" :files ("*.el")))
-(package! magit-gptcommit)
+(package! magit-gptcommit :recipe (:host github :repo "LemonBreezes/magit-gptcommit"
+                                   :branch "fix-uncaught-plz-event-error"))
 (package! chatgpt-shell)
 (package! dall-e-shell)
 (when (modulep! +copilot)
