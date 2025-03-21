bc41b529fc9816b14e4ea46aa85ef9da66b45051
StrawberryTea <look@strawberrytea.xyz>
2025-03-21 19:59:43 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -11,6 +11,7 @@
 (unpin! helpful) ; Keep it up-to-date for bug fixes.
 (unpin! :lang :tools :emacs :email :input :term :ui :editor)
 (unpin! :completion corfu)
+(unpin! :checkers)
 (unpin! smartparens)
 (unpin! projectile)
 ;; Not using the built-in package verisons can cause errors.
