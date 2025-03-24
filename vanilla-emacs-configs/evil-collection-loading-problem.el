798e7f2e2dc921cd0dace0398c4b4f44e1a6bd72
StrawberryTea <look@strawberrytea.xyz>
2025-03-24 17:10:43 -0400

    vanilla-emacs-configs/evil-collection-loading-problem.el

--- vanilla-emacs-configs/evil-collection-loading-problem.el
+++ vanilla-emacs-configs/evil-collection-loading-problem.el
@@ -1,6 +1,17 @@
 ;;; vanilla-emacs-configs/evil-collection-loading-problem.el -*- lexical-binding: t; -*-
 
 ;; Bootstrap straight
+(defvar features-not-loaded nil)
+
+;; Checking a few extra ones.
+(cl-loop for feature in '(tabulated-list tab-bar simple replace kmacro info help elisp-mode eldoc compile comint cus-edit occur) do
+         (if (featurep feature)
+             (message "Package '%s' is already loaded." feature)
+           (message "Package '%s' is not loaded." feature)
+           (setq features-not-loaded
+                 (cons feature features-not-loaded))))
+(message "Check complete.")
+
 (defvar bootstrap-version)
 
 (let ((bootstrap-file
@@ -19,10 +30,22 @@
 
 (require 'evil)
 
-(message "%s %s"
-         (let ((features-old features))
-           (require 'evil-collection)
-           (cl-set-difference features features-old))
-         (let ((features-old features))
-           (evil-collection-init)
-           (cl-set-difference features features-old)))
+(cl-loop for feature in '(info kmacro) do
+         (when (featurep feature)
+           (message "Package '%s' was loaded by Evil." feature)
+           (setq features-not-loaded
+                 (delete feature features-not-loaded))))
+
+(require 'evil-collection)
+(evil-collection-init)
+
+;; This one lists what features are loaded at the time of calling `evil-collection-init'.
+(cl-loop for feature in features do
+         (when (string-prefix-p "evil-collection-" (symbol-name feature))
+           (message "Loaded feature: %s" feature)
+           (when (featurep (intern (string-remove-prefix "evil-collection-" (symbol-name feature))))
+             (message "  - Underlying package: %s" (intern (string-remove-prefix "evil-collection-" (symbol-name feature)))))))
+
+(cl-loop for feature in features-not-loaded do
+         (when (featurep feature)
+           (message "Packages %s is now loaded" feature)))
