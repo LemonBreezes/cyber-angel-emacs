7d5aa7ea6a17ca79c43f2130d69cc27e386f150c
StrawberryTea <look@strawberrytea.xyz>
2025-03-24 14:33:59 -0400

    vanilla-emacs-configs/evil-collection-loading-problem.el

--- vanilla-emacs-configs/evil-collection-loading-problem.el
+++ vanilla-emacs-configs/evil-collection-loading-problem.el
@@ -17,6 +17,33 @@
 (straight-use-package 'evil)
 (straight-use-package 'evil-collection)
 
+(defun evil-collection-check-preloaded-packages ()
+  "Check if packages used by evil-collection are already loaded and output a message.
+This function checks a list of built-in packages that evil-collection modules depend on."
+  (interactive)
+  (let ((packages '(tabulated-list
+                    tab-bar
+                    simple
+                    replace
+                    package
+                    kmacro
+                    info
+                    help-mode
+                    elisp-mode
+                    eldoc
+                    compile
+                    comint)))
+    (dolist (package packages)
+      (if (featurep package)
+          (message "Package '%s' is already loaded." package)
+        (message "Package '%s' is not loaded." package)))
+    (message "Check complete.")))
+
+;; At this point:
+;; compile is not loaded
+;; comint is not loaded
+;; kmacro is not loaded
+
 (require 'evil)
 (message "%s %s"
          (let ((features-old features))
@@ -25,3 +52,21 @@
          (let ((features-old features))
            (evil-collection-init)
            (cl-set-difference features features-old)))
+
+;;(evil-collection annalist)
+;;(evil-collection-unimpaired
+;; evil-collection-tabulated-list
+;; evil-collection-tab-bar
+;; evil-collection-simple
+;; evil-collection-replace
+;; evil-collection-process-menu
+;; evil-collection-package-menu
+;; evil-collection-kmacro
+;; evil-collection-info
+;; evil-collection-indent
+;; evil-collection-help
+;; evil-collection-elisp-mode
+;; evil-collection-eldoc
+;; evil-collection-compile
+;; evil-collection-comint
+;; evil-collection-buff-menu)
