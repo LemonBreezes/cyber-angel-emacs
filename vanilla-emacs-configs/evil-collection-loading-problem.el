7bce146f715903c80e596f042386a98ac63b86f3
StrawberryTea <look@strawberrytea.xyz>
2025-03-24 13:43:26 -0400

    docs: add evil-collection loading example config

---
+++ vanilla-emacs-configs/evil-collection-loading-problem.el
@@ -0,0 +1,27 @@
+;;; vanilla-emacs-configs/evil-collection-loading-problem.el -*- lexical-binding: t; -*-
+
+;; Bootstrap straight
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+                                    'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'evil)
+(straight-use-package 'evil-collection)
+
+(require 'evil)
+(message "%s %s"
+         (let ((features-old features))
+           (require 'evil-collection)
+           (cl-set-difference features features-old))
+         (let ((features-old features))
+           (evil-collection-init)
+           (cl-set-difference features features-old)))
