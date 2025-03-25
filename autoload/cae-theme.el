106981b9d20293ac1918758eec938f4a21cdf85a
StrawberryTea <look@strawberrytea.xyz>
2025-03-25 11:31:40 -0400

    autoload/cae-theme.el

--- autoload/cae-theme.el
+++ autoload/cae-theme.el
@@ -54,6 +54,8 @@
   (dolist (buffer (doom-buffers-in-mode 'org-mode (buffer-list)))
     (autoload-do-load #'+org-get-todo-keywords-for)
     (with-current-buffer buffer
+      (when (autoloadp (symbol-function #'+org-get-todo-keywords-for))
+          (autoload-do-load (symbol-function #'+org-get-todo-keywords-for)))
       (+org--toggle-inline-images-in-subtree (point-min) (point-max) 'refresh)
       (unless (eq org-preview-latex-default-process 'dvisvgm)
         (org-clear-latex-preview (point-min) (point-max))
