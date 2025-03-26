92ea1ba29c4b90c31cf466e393591d465fab8e42
StrawberryTea <look@strawberrytea.xyz>
2025-03-25 21:53:54 -0400

    modules/cae/ai/config.el

--- modules/cae/ai/config.el
+++ modules/cae/ai/config.el
@@ -64,7 +64,7 @@
   :defer t :init
   (autoload 'aidermacs-transient-menu "aidermacs" nil t)
   :config
-  (setq aidermacs-default-model "openrouter/google/gemini-2.5-pro-exp-03-25:free")
+  (setq aidermacs-default-model "gemini-2.5-pro-exp-03-25")
   (setq aidermacs-auto-commits nil)
   (setq aidermacs-use-architect-mode t)
   (setq aidermacs-auto-accept-architect t)
