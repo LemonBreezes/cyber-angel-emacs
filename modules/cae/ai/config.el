90b052e0f4c0465dc3247a2b8436ca5c247e6a96
StrawberryTea <look@strawberrytea.xyz>
2025-03-25 21:51:00 -0400

    modules/cae/ai/config.el

--- modules/cae/ai/config.el
+++ modules/cae/ai/config.el
@@ -64,7 +64,7 @@
   :defer t :init
   (autoload 'aidermacs-transient-menu "aidermacs" nil t)
   :config
-  (setq aidermacs-default-model "anthropic/claude-3-7-sonnet-20250219")
+  (setq aidermacs-default-model "google/gemini-2.5-pro-exp-03-25")
   (setq aidermacs-auto-commits nil)
   (setq aidermacs-use-architect-mode t)
   (setq aidermacs-auto-accept-architect t)
