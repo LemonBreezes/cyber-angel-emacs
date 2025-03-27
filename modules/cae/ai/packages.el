22de79e72592473e3270ca8ece38f6b319cd8ff3
StrawberryTea <look@strawberrytea.xyz>
2025-03-27 18:29:05 -0400

    modules/cae/ai/packages.el

--- modules/cae/ai/packages.el
+++ modules/cae/ai/packages.el
@@ -3,7 +3,7 @@
 
 (package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
 
-(package! gptel :recipe (:host github :repo "karthink/gptel" :files ("*.el")))
+(package! gptel :recipe (:host github :repo "karthink/gptel" :files ("*.el" "*.org")))
 (package! aidermacs :recipe (:host github :repo "MatthewZMD/aidermacs" :files ("*.el")))
 (package! magit-gptcommit :recipe (:host github :repo "douo/magit-gptcommit"))
 (package! chatgpt-shell)
