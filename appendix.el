8a269aaa1dc3356e7b770f9fb52c81d5a4162349
StrawberryTea <look@strawberrytea.xyz>
2025-03-30 21:44:06 -0400

    appendix.el

--- appendix.el
+++ appendix.el
@@ -37,7 +37,7 @@
           '(copilot))
         (when (modulep! :cae lsp -copilot)
           '(minuet))
-        '(whisper greader org-ai chatgpt-shell gptel magit-gptcommit
+        '(greader org-ai chatgpt-shell gptel magit-gptcommit
           aider aidermacs elysium minuet dall-e-shell forge-llm)))
    ,@(when (modulep! :tools direnv)
        '(envrc))
