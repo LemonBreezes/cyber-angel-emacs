d33de521908c079f675d7a49ba5616cd7f11e3fe
StrawberryTea <look@strawberrytea.xyz>
2025-04-01 22:04:05 -0400

    modules/cae/org/autoload/org.el

--- modules/cae/org/autoload/org.el
+++ modules/cae/org/autoload/org.el
@@ -17,9 +17,3 @@ from running."
   "Runs all hooks in `+org-exit-src-code-hook`."
   (let ((inhibit-read-only t))
     (run-hooks '+org-exit-src-code-hook)))
-
-;;;###autoload
-(defun cae-org-export-remove-zero-width-space (text _backend _info)
-  "Remove zero width spaces from TEXT."
-  (unless (org-export-derived-backend-p 'org)
-    (replace-regexp-in-string "\u200B" "" text)))
