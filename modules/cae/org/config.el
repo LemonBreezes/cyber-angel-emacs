6fbc3ac79baf12fa26541cc4f6975a8cda1b3e88
StrawberryTea <look@strawberrytea.xyz>
2025-04-01 21:56:56 -0400

    modules/cae/org/config.el

--- modules/cae/org/config.el
+++ modules/cae/org/config.el
@@ -45,10 +45,14 @@
 
 
 (defun cae-locally-defer-font-lock ()
-  "Set jit-lock defer and stealth, when buffer is over a certain size."
-  (when (> (buffer-size) 50000)
-    (setq-local jit-lock-defer-time 0.05
-                jit-lock-stealth-time 1)))
+  "Set jit-lock defer and stealth parameters when buffer is large.
+Especially useful for large Org files with complex structure."
+  (when (> (buffer-size) 100000) ;; Increased threshold to 100KB
+    (setq-local jit-lock-defer-time 0.1
+                jit-lock-stealth-time 2
+                jit-lock-stealth-load 200  ;; Process fewer chars during idle time
+                jit-lock-chunk-size 10000  ;; Process in larger chunks
+                jit-lock-stealth-nice 0.5))) ;; Lower CPU usage during stealth fontification
 
 (add-hook 'org-mode-hook #'cae-locally-defer-font-lock)
 
