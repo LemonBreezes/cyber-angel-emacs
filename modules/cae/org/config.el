58febbbca5f817f1a619ef6d2b939a1a5285ec47
StrawberryTea <look@strawberrytea.xyz>
2025-04-01 22:03:48 -0400

    modules/cae/org/config.el

--- modules/cae/org/config.el
+++ modules/cae/org/config.el
@@ -7,7 +7,7 @@
   (map! :map org-mode-map
         "C-M-y" #'cae-org-rich-yank))
 
-;; TODO Check if this is still necessary.
+;; TODO Check if this is still necessary
 (defvar +org-exit-src-code-hook nil
   "Hook run just before exiting a org source block buffer.")
 (cae-advice-add #'org-edit-src-exit :before #'cae-org-run-exit-src-code-hooks)
