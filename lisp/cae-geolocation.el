f3b7bff543d1c6570a109497e73ba3485cf69006
StrawberryTea <look@strawberrytea.xyz>
2025-04-07 01:30:34 -0400

    lisp/cae-geolocation.el

--- lisp/cae-geolocation.el
+++ lisp/cae-geolocation.el
@@ -38,13 +38,10 @@ Returns t if a significant change occurred compared to the current state, nil ot
   "Update variables in the noaa weather package if loaded.
 LAT, LNG are coordinates. NAME is the location name string."
   (when (and lat lng name (stringp name) (> (length name) 0))
-    (message "Geolocation: Updating noaa package with location: %s (%s, %s)" name lat lng)
-    ;; Update noaa variables only after noaa package is loaded
-    (with-eval-after-load 'noaa
-      (when (boundp 'noaa-location) ; Check if vars exist before setting
-        (setq noaa-location name
-              noaa-latitude lat
-              noaa-longitude lng)))))
+    (setq noaa-location name
+          noaa-latitude lat
+          noaa-longitude lng)
+    (setq calendar-location-name)))
 
 ;; Schedule geolocation updates
 (defun cae-geolocation-schedule-updates ()
