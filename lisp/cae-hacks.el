b208dd4f99db7d741cca62fdeb9bff2dabd0a386
StrawberryTea <look@strawberrytea.xyz>
2025-04-10 22:32:52 -0400

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -119,22 +119,12 @@
             #'cae-delete-file-projectile-remove-from-cache)
 
 ;; HACK disable undo history for performance testing reasons.
-;;(defun cae-disable-undo-history ()
-;;  (setq buffer-undo-list t))
-;;(defun cae-disable-buffer-undo-h ()
-;;  "Disable undo for buffers derived from fundamental-mode but not from
-;;prog-mode, text-mode, or conf-mode."
-;;  (when (and (not (derived-mode-p 'prog-mode 'text-mode 'conf-mode))
-;;             (not (buffer-file-name)))
-;;    (cae-disable-undo-history)))
-;;(add-hook 'after-change-major-mode-hook #'cae-disable-buffer-undo-h)
-
-(setq-default buffer-undo-list t)
+(defun cae-disable-undo-history ()
+  (setq buffer-undo-list t))
 (defun cae-disable-buffer-undo-h ()
   "Disable undo for buffers derived from fundamental-mode but not from
 prog-mode, text-mode, or conf-mode."
-  (when (and (eq buffer-undo-list t)
-             (or (buffer-file-name)
-                 (derived-mode-p 'prog-mode 'text-mode 'conf-mode)))
-    (setq buffer-undo-list nil)))
+  (when (and (not (derived-mode-p 'prog-mode 'text-mode 'conf-mode))
+             (not (buffer-file-name)))
+    (cae-disable-undo-history)))
 (add-hook 'after-change-major-mode-hook #'cae-disable-buffer-undo-h)
