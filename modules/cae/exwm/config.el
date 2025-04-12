a00c0448f76d0e852f135c067de803ba9af6f61a
StrawberryTea <look@strawberrytea.xyz>
2025-04-12 16:19:00 -0400

    modules/cae/exwm/config.el

--- modules/cae/exwm/config.el
+++ modules/cae/exwm/config.el
@@ -203,33 +203,18 @@
       (apply oldfun args)))
   (cae-advice-add #'exwm-input--translate :around #'cae-exwm-input--translate-a)
 
-  ;; Fix a type validation bug in xcb library triggered by the advice below.
-  ;; The '~lsb' slot in 'xcb:icccm:-ChangeProperty' inherits an initform
-  ;; producing 't' or 'nil', but its type is overridden to 'xcb:-ignore',
-  ;; causing 'cl-typep' to fail during 'make-instance'. We patch the slot
-  ;; type back to 'xcb:-boolean'.
-  (defun cae-fix-xcb-ignore-type-validation-bug ()
-    "Patch xcb class definition to prevent type validation error."
-    (require 'xcb-icccm) ; Ensure the class is defined
-    (let ((class (eieio-find-class 'xcb:icccm:-ChangeProperty)))
-      (when class
-        (let ((slot (cl-find '~lsb (oref class :slots) :key #'oref :name)))
-          (when slot
-            (oset slot :type 'xcb:-boolean))))))
-  (cae-fix-xcb-ignore-type-validation-bug)
-
   ;; See https://github.com/emacs-exwm/exwm/issues/18. Addresses a focus issue
   ;; with EXWM but can cause increased power consumption.
   (cae-defadvice! cae-exwm-disable-net-wm-state-hidden-a (id)
     :after #'exwm-layout--hide
-    (when-let ((buffer (exwm--id->buffer id)))
+    (when-let ((buffer (exwm--id->buffer id))
+               (_ (buffer-live-p buffer)))
       (with-current-buffer buffer
-        (when (buffer-live-p buffer) ; Check if buffer still exists
-          (setq exwm--ewmh-state
-                (delq xcb:Atom:_NET_WM_STATE_HIDDEN
-                      exwm--ewmh-state))
+        (setq exwm--ewmh-state
+              (delq xcb:Atom:_NET_WM_STATE_HIDDEN
+                    exwm--ewmh-state))
           (exwm-layout--set-ewmh-state id)
-          (xcb:flush exwm--connection)))))
+          (xcb:flush exwm--connection))))
 
   (use-package! exwm-mff
     :defer t :init (add-hook 'exwm-init-hook #'exwm-mff-mode))
