3b475cb876aebafd4568d6ce4244a04e20b2eda3
StrawberryTea <look@strawberrytea.xyz>
2025-04-16 18:53:12 -0400

    modules/cae/misc-applications/config.el

--- modules/cae/misc-applications/config.el
+++ modules/cae/misc-applications/config.el
@@ -526,6 +526,9 @@
 
 ;;; Eyecandy
 
+(map! :map cae-misc-applications-eyecandy-map
+      "b" #'cae-create-black-screen)
+
 (use-package! fireplace
   :defer t :init
   (defvar cae-fireplace-workspace-name "*fireplace*")
