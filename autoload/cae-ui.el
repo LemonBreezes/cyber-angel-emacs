c7c1a89f191d03a2af44cb9f78d3fd73aba99798
StrawberryTea <look@strawberrytea.xyz>
2025-04-22 11:36:34 -0400

    autoload/cae-ui.el

--- autoload/cae-ui.el
+++ autoload/cae-ui.el
@@ -4,5 +4,6 @@
 ;;;###autoload
 (defun cae-apply-ansi-color-to-buffer-h ()
   (interactive)
-  (let ((inhibit-read-only t))
-    (ansi-color-apply-on-region (point-min) (point-max))))
+  (unless (> (buffer-size) (* 1024 32))
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region (point-min) (point-max)))))
