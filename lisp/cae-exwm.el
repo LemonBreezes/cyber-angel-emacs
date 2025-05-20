16eba5d71bb09dfcee6b4da5d5ee1b97b04215cc
StrawberryTea <look@strawberrytea.xyz>
2025-05-20 19:00:52 -0400

    lisp/cae-exwm.el

--- lisp/cae-exwm.el
+++ lisp/cae-exwm.el
@@ -18,7 +18,7 @@
   (map! "s-v" (cae-exwm-app-runner browse-url-generic-program cae-generic-browser-name)
         "s-V" #'cae-exwm-run-virtualbox
         "s-f" (cmd! () (start-process "flameshot" nil "flameshot" "gui"))
-        "s-T" (cae-exwm-app-runner "teams-for-linux" "Teams")
+        ;;"s-T" (cae-exwm-app-runner "teams-for-linux" "Teams")
         "s-t" (cae-exwm-app-runner "tiled" "Tiled")
         "s-d" (cae-exwm-app-runner "discord" "Discord")
         "s-p" (cae-exwm-app-runner "pavucontrol" "Pavucontrol")
@@ -41,14 +41,14 @@
      `(defhydra hydra-exwm-apps (:hint nil :color blue)
         "
 EXWM Apps Launcher
-_v_: [Browser]    _f_: Flameshot      _T_: Teams          _V_: VirtualBox
+_v_: [Browser]    _f_: Flameshot      _V_: VirtualBox
 _t_: Tiled        _d_: Discord        _p_: Pavucontrol
 _s_: Signal       _e_: Emacs          _E_: Vanilla Emacs    _D_: Vanilla Doom Emacs
 _RET_: Kitty      _S-RET_: Eshell     _r_: Toggle Redshift  _l_: Lock Screen    _x_: Black Screen
 "
         ("v" ,(cae-exwm-app-runner browse-url-generic-program cae-generic-browser-name))
         ("f" ,(cmd! () (start-process "flameshot" nil "flameshot" "gui")))
-        ("T" ,(cae-exwm-app-runner "teams-for-linux" "Teams"))
+        ;;("T" ,(cae-exwm-app-runner "teams-for-linux" "Teams"))
         ("t" ,(cae-exwm-app-runner "tiled" "Tiled"))
         ("d" ,(cae-exwm-app-runner "discord" "Discord"))
         ("p" ,(cae-exwm-app-runner "pavucontrol" "Pavucontrol"))
