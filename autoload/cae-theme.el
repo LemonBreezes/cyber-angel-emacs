df06b5ed6f495cf3bf163ea1fdf28163815bac3c
StrawberryTea <look@strawberrytea.xyz>
2025-07-10 18:45:27 -0500

    autoload/cae-theme.el

--- autoload/cae-theme.el
+++ autoload/cae-theme.el
@@ -23,34 +23,6 @@
              (< (cl-second now) (cl-second sunrise))))))
 
 ;;;###autoload
-(defun cae-theme-export-using-pywal (&rest _)
-  (interactive)
-  (when (and (executable-find "python")
-             (executable-find "wal")
-             (require 'theme-magic nil t)
-             (require 'ewal nil t))
-    (let (;; If we're in a pyenv directory, we might accidentally run the virtual
-          ;; version of Python instead of the user's root version. To fix this, we
-          ;; temporarily change to the user's dir.
-          (default-directory "~/"))
-      (set-process-sentinel
-       (apply #'start-process
-              "cae-theme-magic"
-              nil
-              "python"
-              theme-magic--pywal-python-script
-              (theme-magic--auto-extract-16-colors))
-       (lambda (_proc event)
-         (when (string= event "finished\n")
-           ;;(message "Theme exported using pywal.")
-           (ewal-load-colors)
-           ;;(when (executable-find "polybar-msg")
-           ;;  (start-process "restart polybar" nil "polybar-msg" "cmd" "restart"))
-           (when (executable-find "dunst")
-             ;; This assumes you are running `dunst' as a systemd service.
-             (start-process "kill dunst" nil "killall" "dunst"))))))))
-
-;;;###autoload
 (defun cae-theme-refresh-latex-images-previews-h ()
   (when (autoloadp (symbol-function #'+org-get-todo-keywords-for))
     (autoload-do-load (symbol-function #'+org-get-todo-keywords-for)))
