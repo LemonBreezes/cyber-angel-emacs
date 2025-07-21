c18def3ddb3ab1fe6c1b4520e5fc774a4ff12a39
StrawberryTea <look@strawberrytea.xyz>
2025-07-21 02:33:45 -0500

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -111,7 +111,7 @@
       "<escape>" #'keyboard-quit
       (:when (modulep! :cae ai)
         "<f5>" #'gptel-menu
-        "<f7>" #'cae-claude-code
+        "<f7>" #'claude-code-command-map
         "<f6>" #'aidermacs-transient-menu
         "<f12>" #'redraw-display)
       "C-<f1>" #'try
