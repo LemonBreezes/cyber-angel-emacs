36b98741aacf5327647dbdef53254d31721cccee
StrawberryTea <look@strawberrytea.xyz>
2025-07-30 13:51:38 -0500

    modules/cae/exwm/autoload/exwm-firefox.el

--- modules/cae/exwm/autoload/exwm-firefox.el
+++ modules/cae/exwm/autoload/exwm-firefox.el
@@ -12,7 +12,6 @@ switches pages."
   "Select and open a link with your keyboard."
   (interactive)
   (exwm-input--fake-key ?\M-j)
-  (exwm-evil-insert)
   (add-hook 'exwm-update-title-hook #'cae-exwm-firefox-core-link-hints-h))
 
 ;;;###autoload
