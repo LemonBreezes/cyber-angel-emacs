05a32e7ffdf6692d277e238a279485a8936e6def
StrawberryTea <look@strawberrytea.xyz>
2025-08-03 14:47:25 -0500

    modules/cae/ai/config.el

--- modules/cae/ai/config.el
+++ modules/cae/ai/config.el
@@ -99,12 +99,7 @@
 (use-package! magit-gptcommit
   :after magit :init
   :config
-  ;; Enable magit-gptcommit-mode to watch staged changes and generate commit message automatically in magit status buffer
-  ;; This mode is optional, you can also use `magit-gptcommit-generate' to generate commit message manually
-  ;; `magit-gptcommit-generate' should only execute on magit status buffer currently
   (magit-gptcommit-mode 1)
-  ;; Add gptcommit transient commands to `magit-commit'
-  ;; Eval (transient-remove-suffix 'magit-commit '(1 -1)) to remove gptcommit transient commands
   (magit-gptcommit-status-buffer-setup))
 (after! git-commit
   (map! :map git-commit-mode-map
