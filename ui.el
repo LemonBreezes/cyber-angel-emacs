69c6431c39ba5acfb640c571f34ad2de93f87204
StrawberryTea <look@strawberrytea.xyz>
2025-08-03 15:03:47 -0500

    ui.el

--- ui.el
+++ ui.el
@@ -204,7 +204,7 @@
        :select t :quit t :side bottom :ttl 0)
       ("\\`\\*Pp Eval Output\\*" :size +popup-shrink-to-fit
        :quit t :ttl t)
-      ("\\`\\*claude:*" :ignore t)
+      ("\\`\\*claude:*" :ignore t :select t :ttl nil :side right :size 0.5)
       ("\\`\\*org-roam\\*" :size 60 :side left :select nil
        :quit nil)
       ("\\`\\*info.*" :size cae-popup-resize-help-buffer
