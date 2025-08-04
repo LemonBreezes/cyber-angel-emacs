e5d52415e6681270a417728309a22046370ec76c
StrawberryTea <look@strawberrytea.xyz>
2025-08-03 22:32:12 -0500

    config.el

--- config.el
+++ config.el
@@ -74,52 +74,6 @@
 
 ;;; Email
 
-;; Always enable the essential email configuration, including maildirs and
-;; addresses. This is crucial for testing and debugging any issues with the
-;; email setup.
-(setq user-full-name "StrawberryTea"
-      user-mail-address "look@strawberrytea.xyz"
-      mail-host-address "strawberrytea.xyz"
-      mail-source-directory "~/.mail/")
-(make-directory "~/.mail/Fastmail/" t)
-
-(after! mu4e
-  (setq mu4e-eldoc-support t)
-  (setq mu4e-contexts
-        `(,(make-mu4e-context
-            :name "Fastmail"
-            :match-func
-            (lambda (msg)
-              (when msg
-                (string-prefix-p "/Fastmail"
-                                 (mu4e-message-field msg :maildir))))
-            :vars '((user-mail-address . "look@strawberrytea.xyz")
-                    (user-full-name . "StrawberryTea")
-                    (smtpmail-smtp-server . "smtp.fastmail.com")
-                    (smtpmail-default-smtp-server . "smtp.fastmail.com")
-                    (smtpmail-stream-type . tls)
-                    (smtpmail-smtp-service . 465)
-                    (mu4e-trash-folder . "/Fastmail/Trash")
-                    (mu4e-refile-folder . "/Fastmail/Archive")
-                    (mu4e-drafts-folder . "/Fastmail/Drafts")
-                    (mu4e-sent-folder . "/Fastmail/Sent"))))))
-
-(after! message
-  (setq message-default-mail-headers "Bcc: look@strawberrytea.xyz\n"))
-
-(after! smtpmail
-  (setq smtpmail-smtp-server "smtp.fastmail.com"
-        smtpmail-default-smtp-server "smtp.fastmail.com"
-        smtpmail-smtp-service 465
-        smtpmail-stream-type 'tls
-        smtpmail-queue-mail nil
-        smtpmail-queue-dir "~/.mail/queued-mail/"
-        smtpmail-servers-requiring-authorization ".*"
-        smtpmail-smtp-user user-mail-address))
-
-(setq +notmuch-sync-backend 'mbsync
-      +notmuch-mail-folder "~/.mail/fastmail")
-
 (when cae-init-email-enabled-p
   (load! "email" doom-user-dir))
 
