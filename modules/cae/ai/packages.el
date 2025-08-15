0db90facc88e3f81db5427975076abb9d5152907
StrawberryTea <look@strawberrytea.xyz>
2025-08-14 22:08:52 -0500

    modules/cae/ai/packages.el

--- modules/cae/ai/packages.el
+++ modules/cae/ai/packages.el
@@ -5,8 +5,6 @@
 
 (package! gptel :recipe (:nonrecursive t))
 (package! aidermacs :recipe (:host github :repo "MatthewZMD/aidermacs" :files ("*.el")))
-(package! chatgpt-shell)
-(package! dall-e-shell)
 (when (modulep! +copilot)
   (package! copilot :recipe (:host github :repo "copilot-emacs/copilot.el")))
 (when (modulep! -copilot)
