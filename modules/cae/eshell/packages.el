1190ef4a0a85d30786adaa07fbbca850eaa9ff2c
StrawberryTea <look@strawberrytea.xyz>
2025-08-19 01:28:27 -0500

    modules/cae/eshell/packages.el

--- modules/cae/eshell/packages.el
+++ modules/cae/eshell/packages.el
@@ -3,7 +3,7 @@
 
 (package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
 
-(package! eat)
+(package! eat :files ("*" "integration" "term" "terminfo"))
 (package! eshell-bookmark)
 (package! eshell-prompt-extras)
 (package! eshell-atuin)
