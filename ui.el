8006dc6eb126fc96a38640577aacfe6bd128a122
StrawberryTea <look@strawberrytea.xyz>
2025-08-25 21:13:58 -0500

    ui.el

--- ui.el
+++ ui.el
@@ -25,7 +25,7 @@
 (setq x-stretch-cursor t              ;Show me if I am on a TAB or a space
       kill-buffer-delete-auto-save-files t
       window-combination-resize t     ;Take new window space from all other
-                                        ;windows (not just current)
+                                      ;windows (not just current)
       scroll-preserve-screen-position t
       suggest-key-bindings nil)
 
