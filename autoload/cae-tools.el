f1dd2367ebdb49cb61af6d4bf5b48a3b5e932e3d
StrawberryTea <look@strawberrytea.xyz>
2025-08-31 21:06:06 -0500

    autoload/cae-tools.el

--- autoload/cae-tools.el
+++ autoload/cae-tools.el
@@ -25,11 +25,30 @@
 
 ;;;###autoload
 (defun cae-handwritten-pdf-latex ()
-  "Create handwritten PDF of current buffer using LaTeX with readable handwriting font."
+  "Create handwritten PDF of current buffer using LaTeX with readable handwriting font.
+Supports whitespace markup:
+  [[vspace:1cm]] - adds 1cm vertical space
+  [[smallskip]] - adds small vertical space
+  [[medskip]] - adds medium vertical space
+  [[bigskip]] - adds large vertical space"
   (interactive)
   (let* ((text (buffer-string))
          (temp-tex (make-temp-file "/tmp/emacs-handwritten-" nil ".tex"))
-         (temp-pdf (concat (file-name-sans-extension temp-tex) ".pdf")))
+         (temp-pdf (concat (file-name-sans-extension temp-tex) ".pdf"))
+         ;; Process whitespace markup
+         (processed-text (replace-regexp-in-string 
+                          "\\[\\[vspace:\\([^]]+\\)\\]\\]" 
+                          "\\\\vspace{\\1}" 
+                          (replace-regexp-in-string 
+                           "\\[\\[smallskip\\]\\]" 
+                           "\\\\smallskip"
+                           (replace-regexp-in-string 
+                            "\\[\\[medskip\\]\\]" 
+                            "\\\\medskip"
+                            (replace-regexp-in-string 
+                             "\\[\\[bigskip\\]\\]" 
+                             "\\\\bigskip"
+                             text))))))
     (with-temp-file temp-tex
       (insert "\\documentclass{article}\n")
       (insert "\\usepackage[T1]{fontenc}\n")
@@ -40,7 +59,7 @@
       (insert "\\renewcommand{\\familydefault}{\\rmdefault}\n")
       (insert "\\linespread{1.15}\n")      ; Slightly increased line spacing for readability
       (insert "\\begin{document}\n")
-      (insert (replace-regexp-in-string "[{}]" "\\\\\\&" text))
+      (insert (replace-regexp-in-string "[{}]" "\\\\\\&" processed-text))
       (insert "\\end{document}\n"))
     (when (executable-find "pdflatex")
       (shell-command (format "cd /tmp && pdflatex -interaction=nonstopmode %s" (file-name-nondirectory temp-tex)))
@@ -50,3 +69,16 @@
       (delete-file (concat (file-name-sans-extension temp-tex) ".aux")))
     (unless (executable-find "pdflatex")
       (error "pdflatex command not found"))))
+
+;;;###autoload
+(defun cae-insert-latex-whitespace (type)
+  "Insert LaTeX whitespace markup at point.
+TYPE can be 'vspace, 'smallskip, 'medskip, or 'bigskip."
+  (interactive (list (completing-read "Whitespace type: " 
+                                      '("vspace" "smallskip" "medskip" "bigskip"))))
+  (cond
+   ((string= type "vspace")
+    (let ((amount (read-string "Amount (e.g., 1cm, 0.5in): " "1cm")))
+      (insert (format "[[vspace:%s]]" amount))))
+   (t
+    (insert (format "[[%s]]" type)))))
