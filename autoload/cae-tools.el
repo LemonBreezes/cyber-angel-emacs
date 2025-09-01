b7be11a9a2dbfae39b3be1d8a47818dc0b71c5a7
StrawberryTea <look@strawberrytea.xyz>
2025-08-31 20:21:07 -0500

    autoload/cae-tools.el

--- autoload/cae-tools.el
+++ autoload/cae-tools.el
@@ -23,39 +23,3 @@
                       (message "internet working"))
                     (kill-buffer))))))
 
-;; Custom lpr-print-region function to print to PDF and open in Emacs
-(defun cae-lpr-print-region-to-pdf (start end &optional switches display)
-  "Print region from START to END to PDF and open in Emacs.
-SWITCHES and DISPLAY are ignored for PDF output."
-  (interactive "r")
-  (let* ((temp-file (make-temp-file "emacs-print-" nil ".ps"))
-         (pdf-file (concat (file-name-sans-extension temp-file) ".pdf"))
-         (gs-command (format "gs -q -dNOPAUSE -dBATCH -sOutputFile=%s -sDEVICE=pdfwrite %s -c quit" pdf-file temp-file)))
-    (message "=== LPR-PRINT-REGION-TO-PDF ===")
-    (message "Creating temporary PS file: %s" temp-file)
-    (message "PDF will be: %s" pdf-file)
-    (message "Ghostscript command: %s" gs-command)
-
-    ;; First create the PostScript file using ps-print
-    (message "Calling ps-print-region...")
-    (ps-print-region start end temp-file)
-    (message "PS file created, size: %d bytes" (file-attribute-size (file-attributes temp-file)))
-
-    ;; Check if PS file was actually created and has content
-    (if (and (file-exists-p temp-file)
-             (> (file-attribute-size (file-attributes temp-file)) 0))
-        (progn
-          (message "PS file exists and has content, converting to PDF...")
-          ;; Convert PS to PDF using Ghostscript
-          (let ((exit-code (shell-command gs-command)))
-            (message "Ghostscript conversion completed, exit code: %d" exit-code)
-
-            ;; Check if PDF was created and open it
-            (run-at-time 0.01 nil #'find-file pdf-file)))
-      (message "ERROR: PS file was not created or is empty"))
-
-    ;; Clean up temporary PS file
-    (when (file-exists-p temp-file)
-      (delete-file temp-file)
-      (message "Cleaned up temporary PS file"))
-    (message "=== LPR-PRINT-REGION-TO-PDF COMPLETED ===")))
