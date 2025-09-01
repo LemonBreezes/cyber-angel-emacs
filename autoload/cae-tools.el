40717efe132ccc0df8838bc468f31b4bc13f8326
StrawberryTea <look@strawberrytea.xyz>
2025-08-31 20:10:33 -0500

    autoload/cae-tools.el

--- autoload/cae-tools.el
+++ autoload/cae-tools.el
@@ -23,3 +23,25 @@
                       (message "internet working"))
                     (kill-buffer))))))
 
+;; Custom lpr-print-region function to print to PDF and open in Emacs
+(defun cae-lpr-print-region-to-pdf (start end &optional switches display)
+  "Print region from START to END to PDF and open in Emacs.
+SWITCHES and DISPLAY are ignored for PDF output."
+  (interactive "r")
+  (let* ((temp-file (make-temp-file "emacs-print-" nil ".ps"))
+         (pdf-file (concat (file-name-sans-extension temp-file) ".pdf"))
+         (gs-command (format "gs -q -dNOPAUSE -dBATCH -sOutputFile=%s -sDEVICE=pdfwrite %s -c quit" pdf-file temp-file)))
+
+    ;; First create the PostScript file
+    (ps-print-region start end temp-file)
+
+    ;; Convert PS to PDF using Ghostscript
+    (shell-command gs-command)
+
+    ;; Check if PDF was created and open it
+    (run-at-time 0.01 nil #'find-file pdf-file)
+
+    ;; Clean up temporary PS file
+    (when (file-exists-p temp-file)
+      (delete-file temp-file)
+      (message "Cleaned up temporary PS file"))))
