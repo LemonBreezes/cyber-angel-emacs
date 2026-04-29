cad75603a179422fcce137c8ffe74c1510c42604
StrawberryTea <look@strawberrytea.xyz>
2026-04-29 09:24:16 -0400

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -100,13 +100,6 @@
 (advice-add #'delete-file-projectile-remove-from-cache :override
             #'cae-delete-file-projectile-remove-from-cache)
 
-;; These are new functions Doom's Magit doesn't have yet.
-(defun transient--set-layout (prefix layout)Add commentMore actions
-       (put prefix 'transient--layout layout))
-(defun transient--get-layout (prefix)
-  (or (get prefix 'transient--layout)
-      (error "Not a transient prefix command: %s" prefix)))
-
 ;; Hack around `envrc-mode' trying to run in scratch buffers.
 (cae-defadvice! cae-hacks-ignore-buffer-without-default-directory-a (&rest _)
   :before-while #'envrc-global-mode-enable-in-buffer
