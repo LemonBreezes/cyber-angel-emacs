eaadda3c2af63537ceacadebb79d880045f3a4cd
StrawberryTea <look@strawberrytea.xyz>
2026-05-03 09:52:07 -0400

    autoload/cae-geolocation.el

--- autoload/cae-geolocation.el
+++ autoload/cae-geolocation.el
@@ -13,7 +13,8 @@
   1 - Basic confirmation (default for interactive)"
   (interactive (list 1))
 
-  (when (getenv "WSL_DISTRO_NAME")
+  (when (or (getenv "WSL_DISTRO_NAME")
+            (not (require 'dbus nil t)))
     (cl-return-from cae-geolocation-setup))
 
   ;; Set default verbosity if not provided
