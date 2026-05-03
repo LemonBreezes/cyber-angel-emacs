3faa76b03cbe1354bb96450a21925acd104cd92f
StrawberryTea <look@strawberrytea.xyz>
2026-05-03 14:45:27 -0400

    chore: Remove lean4 module

--- modules/cae/lean4/packages.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; lang/lean4/packages.el
-
-(package! cae-lib :recipe (:host github :repo "LemonBreezes/cae-lib"))
-
-(package! lean4-mode
-  :recipe (:host github
-	   :repo "leanprover/lean4-mode"
-	   :files ("*.el" "data")))
