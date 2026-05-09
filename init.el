9f1490215a98312ba2dfe162c563b8e814881ff1
StrawberryTea <look@strawberrytea.xyz>
2026-05-09 16:35:59 -0400

    init.el

--- init.el
+++ init.el
@@ -34,18 +34,18 @@
     (defvar cae-init-tty-enabled-p (and c t))
     (defvar cae-init-bindings-enabled-p (and c t))
     (defvar cae-init-multi-enabled-p (and c t)) ; For using one Emacs config on
-                                                ; many computers simultaneously.
+                                        ; many computers simultaneously.
     (defvar cae-init-smartparens-enabled-p (and c t)) ; Me testing Smartparens
-                                                      ; and Lispy alternatives.
+                                        ; and Lispy alternatives.
     (defvar cae-init-projectile-enabled-p (and c t)) ; Me testing Projectile
-                                                     ; alternatives and
-                                                     ; configuring Projectile.
+                                        ; alternatives and
+                                        ; configuring Projectile.
     (defvar cae-init-evil-enabled-p (and c t))
     (defvar cae-init-exwm-enabled-p (and c t))
     (defvar cae-init-geolocation-enabled-p (and c t))) ; For latitude/longitude
-                                                       ; based on IP.
+                                        ; based on IP.
   (defvar cae-init-keyboard-remaps-enabled-p (and q nil)) ; For alternative
-                                                          ; keyboard layouts.
+                                        ; keyboard layouts.
   (let ((p (and q t)))
     (defvar cae-init-ui-enabled-p (and p t))
     (defvar cae-init-tools-enabled-p (and p t))
