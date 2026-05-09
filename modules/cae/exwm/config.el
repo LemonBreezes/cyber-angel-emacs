5f87187e1649efb4f9d5b57eca5268bd11866b2c
StrawberryTea <look@strawberrytea.xyz>
2026-05-09 14:42:10 -0400

    modules/cae/exwm/config.el

--- modules/cae/exwm/config.el
+++ modules/cae/exwm/config.el
@@ -151,9 +151,9 @@
     (map! :map exwm-mode-map
           :localleader
           (:prefix ("d" . "debug")
-           :desc "Clear debug buffer" "l" #'xcb-debug:clear
-           :desc "Insert mark into the debug buffer" "m" #'xcb-debug:mark
-           :desc "Enable debug logging" "t" #'exwm-debug)
+                   :desc "Clear debug buffer" "l" #'xcb-debug:clear
+                   :desc "Insert mark into the debug buffer" "m" #'xcb-debug:mark
+                   :desc "Enable debug logging" "t" #'exwm-debug)
           :desc "Toggle fullscreen" "f" #'exwm-layout-toggle-fullscreen
           :desc "Hide floating window" "h" #'exwm-floating-hide
           :desc "Send next key" "q" #'exwm-input-send-next-key
