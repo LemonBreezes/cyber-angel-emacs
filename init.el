ebdf57db667dd13205e9d2316c6ba8bba4451e4e
StrawberryTea <look@strawberrytea.xyz>
2026-05-09 16:39:41 -0400

    init.el

--- init.el
+++ init.el
@@ -31,22 +31,15 @@
     (defvar cae-init-hacks-enabled-p (and p t)))
   (let ((c (and q t)))
     (defvar cae-init-core-enabled-p (and c t))
-    (defvar cae-init-tty-enabled-p (and c t)) ; Make Emacs more usable in 
-                                        ; different terminal environments.
+    (defvar cae-init-tty-enabled-p (and c t))
     (defvar cae-init-bindings-enabled-p (and c t))
-    (defvar cae-init-multi-enabled-p (and c t)) ; For using one Emacs config on
-                                        ; many computers simultaneously.
-    (defvar cae-init-smartparens-enabled-p (and c t)) ; Me testing Smartparens
-                                        ; and Lispy alternatives.
-    (defvar cae-init-projectile-enabled-p (and c t)) ; Me testing Projectile
-                                        ; alternatives and
-                                        ; configuring Projectile.
+    (defvar cae-init-multi-enabled-p (and c t))
+    (defvar cae-init-smartparens-enabled-p (and c t))
+    (defvar cae-init-projectile-enabled-p (and c t))
     (defvar cae-init-evil-enabled-p (and c t))
     (defvar cae-init-exwm-enabled-p (and c t))
-    (defvar cae-init-geolocation-enabled-p (and c t))) ; For latitude/longitude
-                                        ; based on IP.
-  (defvar cae-init-keyboard-remaps-enabled-p (and q nil)) ; For alternative
-                                        ; keyboard layouts.
+    (defvar cae-init-geolocation-enabled-p (and c t)))
+  (defvar cae-init-keyboard-remaps-enabled-p (and q nil))
   (let ((p (and q t)))
     (defvar cae-init-ui-enabled-p (and p t))
     (defvar cae-init-tools-enabled-p (and p t))
