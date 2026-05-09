729a0f653daa1a1972ea33f3e3bf15d74c62d37e
StrawberryTea <look@strawberrytea.xyz>
2026-05-09 15:35:56 -0400

    lisp/cae-hacks.el

--- lisp/cae-hacks.el
+++ lisp/cae-hacks.el
@@ -108,3 +108,10 @@
 ;; On Emacs31. Likely related to the `fset` on `ediff-set-diff-options`.
 (defvar ediff-ignore-case nil)
 (defvar ediff-diff3-options nil)
+;; More Emacs31 errors
+(defvar it nil)                             ;consult--buffer-query function
+(defvar message-cache (make-string 100 ?\0))
+(defvar request-sequence 0)
+(defvar event-lock 0)
+(defvar event-queue nil)
+(defvar str "")
