43f0580d74f08728afb836c4f4339dc5d08704ed
StrawberryTea <look@strawberrytea.xyz>
2026-05-20 16:07:38 -0400

    modules/cae/ghostel/packages.el

--- modules/cae/ghostel/packages.el
+++ modules/cae/ghostel/packages.el
@@ -2,7 +2,3 @@
 ;;; cae/ghostel/packages.el
 
 (package! ghostel)
-(when (modulep! :editor evil +everywhere)
-  (package! evil-collection :recipe
-    ;; Added 05/20/26. Check back later to possibly PR.
-    (:host github :repo "LemonBreezes/evil-collection" :branch "add-ghostel-module")))
