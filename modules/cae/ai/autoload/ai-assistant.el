b570246b2e8c7876a33e66a8bba13a34154b194e
StrawberryTea <look@strawberrytea.xyz>
2026-05-20 11:56:58 -0400

    modules/cae/ai/autoload/ai-assistant.el

--- modules/cae/ai/autoload/ai-assistant.el
+++ modules/cae/ai/autoload/ai-assistant.el
@@ -6,8 +6,7 @@
         ;; When EXWM isn't running, prefer ghostel over vterm as long as it's
         ;; usable (i.e. not in a Linux TTY, where its text is invisible) and the
         ;; module is enabled.
-        ((and (or (not (eq (cae-terminal-type) 0))
-                  (cae-display-graphic-p))
+        ((and (not (eq (cae-terminal-type) 0))
               (modulep! :cae ghostel))
          'ghostel)
         (t 'vterm))
