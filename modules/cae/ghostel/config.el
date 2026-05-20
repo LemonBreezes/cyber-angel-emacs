eaefb2391261fe3d9fa14825da45a28bb3f88100
StrawberryTea <look@strawberrytea.xyz>
2026-05-20 11:34:14 -0400

    modules/cae/ghostel/config.el

--- modules/cae/ghostel/config.el
+++ modules/cae/ghostel/config.el
@@ -1,10 +1,11 @@
 ;;; cae/ghostel/config.el -*- lexical-binding: t; -*-
 
 (use-package! ghostel
-  :when (bound-and-true-p module-file-suffix)  ; requires dynamic-modules support
+  :when (bound-and-true-p module-file-suffix) ; requires dynamic-modules support
   :commands (ghostel ghostel-mode ghostel-project)
   :hook (ghostel-mode . mode-line-invisible-mode) ; modeline serves no purpose in ghostel
   :init
+  (when (not (eq (cae-terminal-type) 0)))
   (map! :leader
         :desc "Toggle ghostel popup"  "ot" #'cae-ghostel-toggle
         :desc "Open ghostel here"     "oT" #'cae-ghostel-here)
