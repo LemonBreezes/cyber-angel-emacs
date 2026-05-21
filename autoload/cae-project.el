4fdb8c9a8425d73e2172159e5c6ec02204fdb8f9
StrawberryTea <look@strawberrytea.xyz>
2026-05-21 16:59:01 -0400

    autoload/cae-project.el

--- autoload/cae-project.el
+++ autoload/cae-project.el
@@ -11,19 +11,3 @@
 
 (defvar cae-project--src-watch-timer nil
   "Debounce timer for `cae-project--src-changed'.")
-
-;;;###autoload
-(defun cae-project--src-changed (event)
-  "Rediscover projects in ~/src/ when a subdirectory is added.
-EVENT is a `file-notify' event."
-  (pcase-let ((`(,_desc ,action ,file) event))
-    (when (and (memq action '(created renamed))
-               (file-directory-p file))
-      (when (timerp cae-project--src-watch-timer)
-        (cancel-timer cae-project--src-watch-timer))
-      (setq cae-project--src-watch-timer
-            (run-with-idle-timer
-             1.0 nil
-             (lambda ()
-               (projectile-discover-projects-in-directory
-                (expand-file-name "~/src/") 1)))))))
