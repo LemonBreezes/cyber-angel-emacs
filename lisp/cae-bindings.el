185358241adf675c098dfd6e0b863df64a763795
StrawberryTea <look@strawberrytea.xyz>
2026-05-22 17:34:29 -0400

    lisp/cae-bindings.el

--- lisp/cae-bindings.el
+++ lisp/cae-bindings.el
@@ -92,7 +92,7 @@
   (after! embark
     ;; `elp' instrument package commands from `embark-package-map' are not mapped
     ;; in `+vertico/embark-doom-package-map'.
-    (map! :map +vertico/embark-doom-package-map
+    (map! :map +vertico-embark-doom-package-map
           "t" #'try)
     (map! :map embark-region-map
           "k" #'cae-kill-region)))
