2e34b0d85a38b57078d1991d298ed3ce406e9017
StrawberryTea <look@strawberrytea.xyz>
2026-05-30 18:38:21 -0400

    modules/cae/ghostel/autoload.el

--- modules/cae/ghostel/autoload.el
+++ modules/cae/ghostel/autoload.el
@@ -47,8 +47,7 @@ Returns the ghostel buffer."
                ;; controls placement.
                (let ((ghostel-buffer-name buffer-name))
                  (save-window-excursion
-                   (ghostel--create buffer-name)
-                   (ghostel--start-process))))
+                   (ghostel t))))
              (setq-local cae-ghostel--id buffer-name))
            (pop-to-buffer buffer)))
        (get-buffer buffer-name)))))
