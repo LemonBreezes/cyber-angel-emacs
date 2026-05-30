47e50e9b79b34286e73e534a05a782af804297da
StrawberryTea <look@strawberrytea.xyz>
2026-05-30 16:28:10 -0400

    packages.el

--- packages.el
+++ packages.el
@@ -99,7 +99,6 @@
 (package! crazy-theme :recipe (:host github :repo "eval-exec/crazy-theme.el"))
 (package! girly-notebook-theme :recipe
   (:host github :repo "melissaboiko/girly-notebook-theme"))
-(package! temple-os :recipe (:host github :repo "Senka07/temple-os-emacs-theme"))
 
 (package! theme-magic)
 (package! ewal)
