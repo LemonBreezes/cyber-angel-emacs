78ba346ffb6b92e7aa4c54843caa5b7948c371e9
StrawberryTea <look@strawberrytea.xyz>
2026-05-30 16:20:05 -0400

    ui.el

--- ui.el
+++ ui.el
@@ -117,23 +117,23 @@
   (when (minibufferp)
     (setq which-key--saved-window-configuration nil)))
 ;; BUG Allow C-h to open Consult when calling which-key without a prefix.
-;;(when (modulep! :completion vertico)
-;;  (after! which-key
-;;    (setq which-key-use-C-h-commands t))
-;;  (defvar cae-which-key-current-keymap nil)
-;;  (cae-defadvice! cae-which-key-update-current-keymap-a
-;;    (_keymap-name keymap &rest args)
-;;    :before #'which-key--show-keymap
-;;    (setq cae-which-key-current-keymap keymap))
-;;  (cae-defadvice! cae-which-key-consult-C-h-dispatch (oldfun)
-;;    :around #'which-key-C-h-dispatch
-;;    (cond ((not (which-key--popup-showing-p))
-;;           (setq this-command 'embark-prefix-help-command)
-;;           (call-interactively #'embark-prefix-help-command))
-;;          ((string-empty-p (which-key--current-key-string))
-;;           (setq this-command 'embark-prefix-help-command)
-;;           (embark-bindings-in-keymap cae-which-key-current-keymap))
-;;          (t (call-interactively #'embark-prefix-help-command)))))
+(when (modulep! :completion vertico)
+  (after! which-key
+    (setq which-key-use-C-h-commands t))
+  (defvar cae-which-key-current-keymap nil)
+  (cae-defadvice! cae-which-key-update-current-keymap-a
+    (_keymap-name keymap &rest args)
+    :before #'which-key--show-keymap
+    (setq cae-which-key-current-keymap keymap))
+  (cae-defadvice! cae-which-key-consult-C-h-dispatch (oldfun)
+    :around #'which-key-C-h-dispatch
+    (cond ((not (which-key--popup-showing-p))
+           (setq this-command 'embark-prefix-help-command)
+           (call-interactively #'embark-prefix-help-command))
+          ((string-empty-p (which-key--current-key-string))
+           (setq this-command 'embark-prefix-help-command)
+           (embark-bindings-in-keymap cae-which-key-current-keymap))
+          (t (call-interactively #'embark-prefix-help-command)))))
 
 ;; Do not scale fonts in `writeroom-mode'.
 (setq +zen-text-scale 0.8)
