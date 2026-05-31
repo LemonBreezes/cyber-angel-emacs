194e9a56ff23b6d391a3a66cb5de38401958f87a
StrawberryTea <look@strawberrytea.xyz>
2026-05-30 21:15:51 -0400

    core.el

--- core.el
+++ core.el
@@ -137,5 +137,6 @@ frameworks.")
   (setq package-quickstart t))
 
 ;; Load secrets
-(when (file-exists-p (concat cae-multi-secrets-dir "secrets.el"))
+(when (and cae-secrets-enabled-p
+           (file-exists-p (concat cae-multi-secrets-dir "secrets.el")))
   (load! (concat cae-multi-secrets-dir "secrets.el") "/"))
