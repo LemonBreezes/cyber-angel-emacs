8e89418634db384a74dd4c374d257cdfa4049405
StrawberryTea <look@strawberrytea.xyz>
2026-05-31 13:45:00 -0400

    lisp/cae-restore-point.el

--- lisp/cae-restore-point.el
+++ lisp/cae-restore-point.el
@@ -311,9 +311,9 @@
   (add-hook 'doom-first-input-hook #'restore-point-mode)
   :config
   (defun cae-restore-point-handle-mark-and-conditional-disable (oldfun)
-    unless (derived-mode-p 'ghostel-mode)
-    (funcall oldfun)
-    (deactivate-mark))
+    (unless (derived-mode-p 'ghostel-mode)
+      (funcall oldfun)
+      (deactivate-mark)))
   (cae-advice-add #'rp/restore-point-position :around
                   #'deactivate-mark)
   ;; Restore point in the minibuffer.
