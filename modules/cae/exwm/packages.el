ea702dc332cff95452271b7f938520b6b9f47b38
StrawberryTea <look@strawberrytea.xyz>
2026-06-01 11:48:13 -0400

    modules/cae/exwm/packages.el

--- modules/cae/exwm/packages.el
+++ modules/cae/exwm/packages.el
@@ -8,8 +8,3 @@
 (package! exwm :recipe
   (:host github :repo "emacs-exwm/exwm"))
 (package! exwm-mff)
-(when (modulep! :editor evil +everywhere)
-  (package! exwm-evil
-    :recipe (:host github :repo "LemonBreezes/exwm-evil"))
-  (package! exwm-firefox-evil)
-  (package! app-launcher :recipe (:host github :repo "SebastienWae/app-launcher")))
